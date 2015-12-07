1c1e49ef14243f73f4685f4b7bd862fbe1214342
urihershgc <uri@guardicore.com>
2015-12-07 17:05:01 +0200

    another certificates script fix

--- monkey_island/linux/create_certificate.sh
+++ monkey_island/linux/create_certificate.sh
@@ -2,5 +2,5 @@
 
 cd /var/monkey_island
 openssl genrsa -out cc/server.key 1024
-openssl req -new -config openssl.cfg -key cc/server.key -out cc/server.csr
+openssl req -new -key cc/server.key -out cc/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
 openssl x509 -req -days 366 -in cc/server.csr -signkey cc/server.key -out cc/server.crt
