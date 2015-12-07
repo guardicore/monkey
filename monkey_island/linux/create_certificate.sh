eee37b5084333f755855a09ac7f7d51a09757201
urihershgc <uri@guardicore.com>
2015-12-07 17:25:05 +0200

    GC-4552: pre remove file added

--- monkey_island/linux/create_certificate.sh
+++ monkey_island/linux/create_certificate.sh
@@ -2,5 +2,5 @@
 
 cd /var/monkey_island
 openssl genrsa -out cc/server.key 1024
-openssl req -new -key cc/server.key -out cc/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+openssl req -new -key cc/server.key -out cc/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
 openssl x509 -req -days 366 -in cc/server.csr -signkey cc/server.key -out cc/server.crt
