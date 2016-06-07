f9f062ec0d54ad77d6045b1b942d7633f2300e9c
acepace <acepace@gmail.com>
2016-06-07 17:19:25 +0300

    Added default parameters to certificate creation in windows

--- monkey_island/windows/create_certificate.bat
+++ monkey_island/windows/create_certificate.bat
@@ -1,4 +1,4 @@
 C:\OpenSSL-Win64\bin\openssl.exe genrsa -out cc\server.key 1024
-C:\OpenSSL-Win64\bin\openssl.exe req -new -config C:\OpenSSL-Win64\bin\openssl.cfg -key cc\server.key -out cc\server.csr
+C:\OpenSSL-Win64\bin\openssl.exe req -new -config C:\OpenSSL-Win64\bin\openssl.cfg -key cc\server.key -out cc\server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
 C:\OpenSSL-Win64\bin\openssl.exe x509 -req -days 366 -in cc\server.csr -signkey cc\server.key -out cc\server.crt
 pause
