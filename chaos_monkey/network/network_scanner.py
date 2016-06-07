61ff2ed740e87c0551d2a060efd01deac5702bd7
acepace <acepace@gmail.com>
2016-06-07 17:18:51 +0300

    made fixed range actually load from configuration...

--- chaos_monkey/network/network_scanner.py
+++ chaos_monkey/network/network_scanner.py
@@ -27,7 +27,8 @@ class NetworkScanner(object):
         LOG.info("Found local IP addresses of the machine: %r", self._ip_addresses)
         # for fixed range, only scan once.
         if WormConfiguration.range_class is FixedRange:
-            self._ranges = [WormConfiguration.range_class('0.0.0.0')]
+            self._ranges = [WormConfiguration.range_class(ip_address)
+                            for ip_address in WormConfiguration.range_fixed]
         else:
             self._ranges = [WormConfiguration.range_class(ip_address)
                             for ip_address in self._ip_addresses]
