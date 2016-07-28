73d9914923cdb8430886a6f95273b843f956a26c
itsikkes <itsikkes@users.noreply.github.com>
2016-07-28 17:42:20 +0300

    Improved .deb creation and installation process for Ubuntu
    Added support for Systemd Units for MongoDB and Island; Some more
    installation fixes

---
+++ monkey_island/linux/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  # 64-bit stuff here
+  ARCH=64
+else
+  # 32-bit stuff here
+  ARCH=32
+fi
+
+MONKEY_FILE=monkey-linux-$ARCH
+cp -f /var/monkey_island/cc/binaries/$MONKEY_FILE /tmp
+/tmp/$MONKEY_FILE m0nk3y $@
