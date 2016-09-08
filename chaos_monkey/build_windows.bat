24792aec616c73d152e41b644806d7c66cd61db7
daniel goldberg <danielg@guardicore.com>
2016-09-08 08:56:11 +0300

    Remove commented build step.

--- chaos_monkey/build_windows.bat
+++ chaos_monkey/build_windows.bat
@@ -1,2 +1,1 @@
-REM c:\Python27\python -m PyInstaller.main --name monkey -F -y --clean -i monkey.ico main.py
 c:\python27\Scripts\pyinstaller -F --log-level=DEBUG --clean --upx-dir=.\bin monkey.spec
