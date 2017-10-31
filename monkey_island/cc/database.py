16a470ee2279eb4d7093add3acd2d9d9baa4410e
Itay Mizeretz <itay.mizeretz@guardicore.com>
2017-10-31 13:55:29 +0200

    Add wait for mongodb

--- monkey_island/cc/database.py
+++ monkey_island/cc/database.py
@@ -1,5 +1,16 @@
 from flask_pymongo import PyMongo
+from flask_pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 __author__ = 'Barak'
 
 mongo = PyMongo()
+
+
+def is_db_server_up(mongo_url):
+    client = MongoClient(mongo_url, serverSelectionTimeoutMS=100)
+    try:
+        client.server_info()
+        return True
+    except ServerSelectionTimeoutError:
+        return False
