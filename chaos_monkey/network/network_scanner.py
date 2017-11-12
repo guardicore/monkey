e83bb23194f03cc4517dc51a508acf5df3dcde7c
Daniel Goldberg <danielg@guardicore.com>
2017-11-12 19:30:38 +0200

    Move to iterate in chunks

--- chaos_monkey/network/network_scanner.py
+++ chaos_monkey/network/network_scanner.py
@@ -1,15 +1,32 @@
-import time
+import itertools
 import logging
-from . import HostScanner
+import time
+
 from config import WormConfiguration
 from info import local_ips, get_ips_from_interfaces
-from range import *
+from range import FixedRange
 
 __author__ = 'itamar'
 
 LOG = logging.getLogger(__name__)
 
 SCAN_DELAY = 0
+ITERATION_BLOCK_SIZE = 5
+
+
+def _grouper(iterable, size):
+    """
+    Goes over an iterable using chunks
+    :param iterable: Possible iterable, if required, will cast
+    :param size:  Chunk size, last chunk may be smaller
+    :return:
+    """
+    it = iter(iterable)
+    while True:
+        group = tuple(itertools.islice(it, size))
+        if not group:
+            break
+        yield group
 
 
 class NetworkScanner(object):
@@ -42,39 +59,39 @@ class NetworkScanner(object):
         LOG.info("Base local networks to scan are: %r", self._ranges)
 
     def get_victim_machines(self, scan_type, max_find=5, stop_callback=None):
-        assert issubclass(scan_type, HostScanner)
 
         scanner = scan_type()
         victims_count = 0
 
         for net_range in self._ranges:
             LOG.debug("Scanning for potential victims in the network %r", net_range)
-            for victim in net_range:
+            for victim_chunk in _grouper(net_range, ITERATION_BLOCK_SIZE):
                 if stop_callback and stop_callback():
                     LOG.debug("Got stop signal")
                     break
 
                 # skip self IP address
-                if victim.ip_addr in self._ip_addresses:
-                    continue
-
+                victim_chunk = [x for x in victim_chunk if x.ip_addr not in self._ip_addresses]
                 # skip IPs marked as blocked
-                if victim.ip_addr in WormConfiguration.blocked_ips:
+                bad_victims = [x for x in victim_chunk if x.ip_addr in WormConfiguration.blocked_ips]
+                for victim in bad_victims:
                     LOG.info("Skipping %s due to blacklist" % victim)
-                    continue
 
-                LOG.debug("Scanning %r...", victim)
+                victim_chunk = [x for x in victim_chunk if x.ip_addr not in WormConfiguration.blocked_ips]
 
-                # if scanner detect machine is up, add it to victims list
-                if scanner.is_host_alive(victim):
-                    LOG.debug("Found potential victim: %r", victim)
-                    victims_count += 1
-                    yield victim
+                LOG.debug("Scanning %r...", victim_chunk)
+                # refactor A
+                for victim in victim_chunk:
+                    # if scanner detect machine is up, add it to victims list
+                    if scanner.is_host_alive(victim):
+                        LOG.debug("Found potential victim: %r", victim)
+                        victims_count += 1
+                        yield victim
 
-                    if victims_count >= max_find:
-                        LOG.debug("Found max needed victims (%d), stopping scan", max_find)
+                        if victims_count >= max_find:
+                            LOG.debug("Found max needed victims (%d), stopping scan", max_find)
 
-                        break
+                            break
 
                 if SCAN_DELAY:
                     time.sleep(SCAN_DELAY)
