f8d3dc1c3da4c17512cf29c1e0214e748eb694d1
Daniel Goldberg <danielg@guardicore.com>
2017-11-12 19:39:33 +0200

    Parallel scans
    Bugfixes

--- chaos_monkey/network/network_scanner.py
+++ chaos_monkey/network/network_scanner.py
@@ -1,6 +1,8 @@
 import itertools
 import logging
 import time
+from functools import partial
+from multiprocessing.dummy import Pool as ThreadPool
 
 from config import WormConfiguration
 from info import local_ips, get_ips_from_interfaces
@@ -58,40 +60,64 @@ class NetworkScanner(object):
             self._ranges += [FixedRange([ip_address for ip_address in get_ips_from_interfaces()])]
         LOG.info("Base local networks to scan are: %r", self._ranges)
 
-    def get_victim_machines(self, scan_type, max_find=5, stop_callback=None):
+    def get_victim_machines(self, scan_type, max_find=5, scan_size=ITERATION_BLOCK_SIZE, stop_callback=None):
+        """
+        Generator of victim machines based on the network scanning configuration.
+        Every time scans
+        :param scan_type: scanner_class type
+        :param max_find:  Maximum number of victims to find
+        :param scan_size: Number of hosts to scan in parralel
+        :param stop_callback: Callback per iteration to check if we should stop activity
+        :return: Generates VictimHost instances
+        """
 
         scanner = scan_type()
         victims_count = 0
+        pool = ThreadPool(ITERATION_BLOCK_SIZE)
 
         for net_range in self._ranges:
             LOG.debug("Scanning for potential victims in the network %r", net_range)
-            for victim_chunk in _grouper(net_range, ITERATION_BLOCK_SIZE):
+            for scan_chunk in _grouper(net_range, scan_size):
                 if stop_callback and stop_callback():
                     LOG.debug("Got stop signal")
                     break
 
                 # skip self IP address
-                victim_chunk = [x for x in victim_chunk if x.ip_addr not in self._ip_addresses]
+                scan_chunk = [x for x in scan_chunk if x.ip_addr not in self._ip_addresses]
                 # skip IPs marked as blocked
-                bad_victims = [x for x in victim_chunk if x.ip_addr in WormConfiguration.blocked_ips]
+                bad_victims = [x for x in scan_chunk if x.ip_addr in WormConfiguration.blocked_ips]
                 for victim in bad_victims:
                     LOG.info("Skipping %s due to blacklist" % victim)
 
-                victim_chunk = [x for x in victim_chunk if x.ip_addr not in WormConfiguration.blocked_ips]
+                scan_chunk = [x for x in scan_chunk if x.ip_addr not in WormConfiguration.blocked_ips]
 
-                LOG.debug("Scanning %r...", victim_chunk)
-                # refactor A
-                for victim in victim_chunk:
-                    # if scanner detect machine is up, add it to victims list
-                    if scanner.is_host_alive(victim):
-                        LOG.debug("Found potential victim: %r", victim)
-                        victims_count += 1
-                        yield victim
+                LOG.debug("Scanning %r...", scan_chunk)
 
-                        if victims_count >= max_find:
-                            LOG.debug("Found max needed victims (%d), stopping scan", max_find)
+                results = pool.map(partial(self.scan_machine, scanner=scanner),
+                                   scan_chunk)
+                victims_chunk = [x for x in results if x]
 
-                            break
+                for victim in victims_chunk:
+                    victims_count += 1
+                    yield victim
+                    if victims_count >= max_find:
+                        LOG.debug("Found max needed victims (%d), stopping scan", max_find)
+                        return
+
 
                 if SCAN_DELAY:
                     time.sleep(SCAN_DELAY)
+
+    @staticmethod
+    def scan_machine(victim, scanner):
+        """
+        Scans specifc machine using given scanner
+        :param victim: VictimHost machine
+        :param scanner: HostScanner instance
+        :return: Victim or None if victim isn't alive
+        """
+        if scanner.is_host_alive(victim):
+            LOG.debug("Found potential victim: %r", victim)
+            return victim
+        else:
+            return None
