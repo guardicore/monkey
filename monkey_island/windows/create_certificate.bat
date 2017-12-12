eadf8d0eba70aed0122903e02348f7ff5d31cbfa
Itay Mizeretz <itay.mizeretz@guardicore.com>
2017-12-12 10:33:13 +0200

    Add support for both supplying monkey island dir or using current directory as monkey island dir Fix openssl.cnf not found

--- monkey_island/windows/create_certificate.bat
+++ monkey_island/windows/create_certificate.bat
@@ -1,3 +1,19 @@
-bin\openssl\openssl.exe genrsa -out cc\server.key 1024
-bin\openssl\openssl.exe req -new -config bin\openssl\openssl.cfg -key cc\server.key -out cc\server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
-bin\openssl\openssl.exe x509 -req -days 366 -in cc\server.csr -signkey cc\server.key -out cc\server.crt
+@echo off
+
+SET OPENSSL_CONF=bin\openssl\openssl.cfg
+
+IF [%1] == [] (
+	set dir=%cd%\
+) ELSE (
+	set dir=%1
+	REM - Remove double quotes -
+	set dir=%dir:"=%
+)
+
+echo Monkey Island folder: %dir%
+
+@echo on
+
+"%dir%bin\openssl\openssl.exe" genrsa -out "%dir%cc\server.key" 1024
+"%dir%bin\openssl\openssl.exe" req -new -config "%dir%bin\openssl\openssl.cfg" -key "%dir%cc\server.key" -out "%dir%cc\server.csr" -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
+"%dir%bin\openssl\openssl.exe" x509 -req -days 366 -in "%dir%cc\server.csr" -signkey "%dir%cc\server.key" -out "%dir%cc\server.crt"
