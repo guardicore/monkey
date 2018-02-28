833df8d6d4dcdac3921c7b7a785290124843b0db
Oran Nadler <orann@guardicore.com>
2018-02-28 04:53:02 -0800

    fix wmi table names

--- infection_monkey/system_info/windows_info_collector.py
+++ infection_monkey/system_info/windows_info_collector.py
@@ -16,13 +16,14 @@ __author__ = 'uri'
 
 WMI_CLASSES = set(["Win32_OperatingSystem",
                    "Win32_ComputerSystem",
-                   "Win32_GroupUser",
                    "Win32_LoggedOnUser",
+                   "Win32_UserAccount",
                    "Win32_UserProfile",
-                   "win32_UserAccount",
-                   "Win32_Product",
-                   "Win32_Process",
-                   "Win32_Service"
+                   "Win32_Group",
+                   "Win32_GroupUser",
+                   #"Win32_Product",
+                   #"Win32_Process",
+                   #"Win32_Service"
                    ])
 
 def fix_obj_for_mongo(o):
