307a7c396c70f72cd2a35a2bdfdb8f2deaa8d11a
Vakaris <vaoxygen@gmail.com>
2018-08-25 17:56:43 +0300

    Notes fixed and tested

--- infection_monkey/config.py
+++ infection_monkey/config.py
@@ -191,7 +191,8 @@ class Configuration(object):
 
     # TCP Scanner
     HTTP_PORTS = [80, 8080, 443,
-                  8008, 7001  # HTTP alternate
+                  8008, # HTTP alternate
+                  7001  # Oracle Weblogic default server port
                   ]
     tcp_target_ports = [22,
                         2222,
