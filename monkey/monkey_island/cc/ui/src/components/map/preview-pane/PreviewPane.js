b2b3d974dcbfcc34de61e90817b25adf32a2223e
Itay Mizeretz <itay.mizeretz@guardicore.com>
2018-10-10 16:01:00 +0300

    Preview Pane with all data. Still needs design improvement

--- monkey/monkey_island/cc/ui/src/components/map/preview-pane/PreviewPane.js
+++ monkey/monkey_island/cc/ui/src/components/map/preview-pane/PreviewPane.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Icon} from 'react-fa';
 import Toggle from 'react-toggle';
-import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+import {OverlayTrigger, Panel, PanelGroup, Tooltip} from 'react-bootstrap';
 import download from 'downloadjs'
 import AuthComponent from '../../AuthComponent';
 
@@ -130,6 +130,151 @@ class PreviewPaneComponent extends AuthComponent {
     );
   }
 
+  k8sContainerRow(container) {
+    return [
+      <tr key={container.name + "-container-name"}>
+        <th>Name</th>
+        <td>{container.name}</td>
+      </tr>,
+      <tr key={container.name + "-container-ports"}>
+        <th>Ports</th>
+        <td>{container.ports.length > 0 ? container.ports.reduce((total, new_str) => total + ', ' + new_str) : 'None'}</td>
+      </tr>
+
+    ];
+  }
+
+  k8sContainerPanel(k8s_pod) {
+    return (
+      <PanelGroup accordion>
+        <Panel eventKey="container-panel">
+          <Panel.Heading>
+            <Panel.Title toggle><b>Pod's Containers</b></Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <table className="table table-condensed"><tbody>
+            {k8s_pod.containers.map(x => this.k8sContainerRow(x))}
+            </tbody></table>
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    );
+  }
+
+  k8sLabelPanel(k8s_pod) {
+    return (
+      <PanelGroup accordion>
+        <Panel eventKey="label-panel">
+          <Panel.Heading>
+            <Panel.Title toggle><b>Pod's Labels</b></Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <table className="table table-condensed"><tbody>
+            {k8s_pod.labels.map(x => <tr key={x.key}><th>{x.key}</th><td>{x.value}</td></tr>)}
+            </tbody></table>
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    );
+  }
+
+  k8sPodRowsImp(k8s_pod) {
+    return [
+      <tr key="pod-name">
+        <th>Pod Name</th>
+        <td>{k8s_pod.name}</td>
+      </tr>,
+      <tr key="pod-namespace">
+        <th>Pod Namespace</th>
+        <td>{k8s_pod.namespace}</td>
+      </tr>,
+      <tr key="pod-host">
+        <th>Pod's Host</th>
+        <td>{k8s_pod.node_name}</td>
+      </tr>,
+      <tr key="pod-containers">
+        <th/>
+        <td>
+          {this.k8sContainerPanel(k8s_pod)}
+        </td>
+      </tr>,
+      <tr key="pod-label">
+        <th/>
+        <td>
+          {this.k8sLabelPanel(k8s_pod)}
+        </td>
+      </tr>
+    ];
+  }
+
+  k8sPodIpsPanel(pod_ips) {
+    return (
+      <PanelGroup accordion>
+        <Panel eventKey="container-panel">
+          <Panel.Heading>
+            <Panel.Title toggle><b>Pods' IPs</b></Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <table className="table table-condensed"><tbody>
+            {pod_ips.map(x => <tr><th/><td>{x}</td></tr>)}
+            </tbody></table>
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    );
+  }
+
+  k8sHostNetworkPodsPanel(k8s_host_pods) {
+    return (
+      <PanelGroup accordion>
+        <Panel eventKey="container-panel">
+          <Panel.Heading>
+            <Panel.Title toggle><b>Host Network Pods</b></Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <table className="table table-condensed"><tbody>
+            {k8s_host_pods.map(x => this.k8sPodRowsImp(x))}
+            </tbody></table>
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    );
+  }
+
+  k8sNodeRows(asset) {
+    return [
+      <tr key="k8s-node">
+        <th>Kubernetes</th>
+        <td>Node</td>
+      </tr>,
+      <tr key="node-name">
+        <th>Node Name</th>
+        <td>{asset.k8s_node.name}</td>
+      </tr>,
+      <tr key="pods-ips">
+        <th/>
+        <td>
+          {this.k8sPodIpsPanel(asset.k8s_node.pod_ips)}
+        </td>
+      </tr>,
+      <tr key="host-network-pods">
+        <th/>
+        <td>
+          {this.k8sHostNetworkPodsPanel(asset.k8s_host_pods)}
+        </td>
+      </tr>
+    ];
+  }
+
+  k8sPodRows(asset) {
+    return [
+      <tr key="k8s-pod">
+        <th>Kubernetes</th>
+        <td>Pod</td>
+      </tr>
+    ].concat(this.k8sPodRowsImp(asset.k8s_pod));
+  }
+
   exploitsTimeline(asset) {
     if (asset.exploits.length === 0) {
       return (<div/>);
@@ -163,34 +308,21 @@ class PreviewPaneComponent extends AuthComponent {
     );
   }
 
-  assetInfo(asset) {
+  assetInfo(asset, isInfected) {
+    let isK8sPod = asset.hasOwnProperty('k8s_pod');
+    let isK8sNode = asset.hasOwnProperty('k8s_node');
     return (
       <div>
         <table className="table table-condensed">
           <tbody>
           {this.osRow(asset)}
+          {isInfected ? this.statusRow(asset) : undefined}
           {this.ipsRow(asset)}
           {this.servicesRow(asset)}
           {this.accessibleRow(asset)}
-          </tbody>
-        </table>
-        {this.exploitsTimeline(asset)}
-      </div>
-    );
-  }
-
-  infectedAssetInfo(asset) {
-    return (
-      <div>
-        <table className="table table-condensed">
-          <tbody>
-          {this.osRow(asset)}
-          {this.statusRow(asset)}
-          {this.ipsRow(asset)}
-          {this.servicesRow(asset)}
-          {this.accessibleRow(asset)}
-          {this.forceKillRow(asset)}
-          {this.downloadLogRow(asset)}
+          {isK8sNode ? this.k8sNodeRows(asset) : isK8sPod ? this.k8sPodRows(asset) : undefined}
+          {isInfected ? this.forceKillRow(asset) : undefined}
+          {isInfected ? this.downloadLogRow(asset) : undefined}
           </tbody>
         </table>
         {this.exploitsTimeline(asset)}
@@ -252,8 +384,8 @@ class PreviewPaneComponent extends AuthComponent {
         info = this.scanInfo(this.props.item);
         break;
       case 'node':
-        info = this.props.item.group.includes('monkey', 'manual') ? this.infectedAssetInfo(this.props.item) :
-          this.props.item.group !== 'island' ? this.assetInfo(this.props.item) : this.islandAssetInfo();
+        info = this.props.item.group.includes('monkey', 'manual') ? this.assetInfo(this.props.item, true) :
+          this.props.item.group !== 'island' ? this.assetInfo(this.props.item, false) : this.islandAssetInfo();
         break;
       case 'island_edge':
         info = this.islandEdgeInfo();
