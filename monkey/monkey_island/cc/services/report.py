e8c604d7c5b06f6e9be6317c7740aa410ec78491
maor.rayzin <maorrayzin@guardicore.com>
2018-11-27 17:48:40 +0200

    * Changed the exporter to work in aws and not standard (was used for debugging)

--- monkey/monkey_island/cc/services/report.py
+++ monkey/monkey_island/cc/services/report.py
@@ -754,7 +754,7 @@ class ReportService:
     def get_active_exporters():
         # This function should be in another module in charge of building a list of active exporters
         exporters_list = []
-        if str(load_env_from_file()) == 'standard':
+        if str(load_env_from_file()) == AWS:
             exporters_list.append(AWSExporter)
         return exporters_list
 
