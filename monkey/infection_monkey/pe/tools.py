d05d2984722b21b2678cfe0c16ed466107593a5a
Dhayalanb <coe13b006@iiitdm.ac.in>
2019-07-18 02:47:31 +0200

    Introduced the functionality for the dropper to see if the monkey is running as root by using pgrep
    moved shell() to tools.py
    
    added check_system() to see if the exploit is for the current machine

--- monkey/infection_monkey/pe/tools.py
+++ monkey/infection_monkey/pe/tools.py
@@ -1,9 +1,13 @@
 import logging
+import platform
 import os
 from pwd import getpwuid
 __author__ = 'itamar'
 
 LOG = logging.getLogger(__name__)
+
+
+PGREP = "pgrep %(process_name)s -u 0"
 
 def check_if_sudoer(file):
     """
@@ -22,3 +26,39 @@ def check_if_sudoer(file):
 
     return False
 
+
+def check_system(properties):
+    """
+    Returns weather if the exploit can be run on the system
+    :param properties: list containing (distro, os)
+    :return:
+    """
+    uname = [x.lower() for x in platform.uname()]
+    if set(properties).issubset(set(uname)):
+        return True
+    return False
+
+
+def shell(cmd):
+    """
+    To run the command on the shell and to read the output.
+    :param cmd: The commands to be run on the shell
+    :return: returns the output
+    """
+    try:
+        result = os.popen(cmd).read()[:-1]
+        return result
+    except OSError as e:
+        LOG.error("Can't read from the shell!")
+        return False
+
+
+def check_running(pname):
+    """
+    Returns true if the monkey process is running as root
+    :param pname: name of the monkey process
+    :return:
+    """
+    if shell(PGREP %{'process_name': pname}):
+        return True
+    return False
