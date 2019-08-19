7eab8687c1d5d926d757696f5cd127991d2d867b
VakarisZ <vakarisz@yahoo.com>
2019-08-19 09:16:14 +0300

    Fixed bug created during merge, fixed typos in attack telemetries for usage.

--- monkey/infection_monkey/system_info/mimikatz_collector.py
+++ monkey/infection_monkey/system_info/mimikatz_collector.py
@@ -55,8 +55,8 @@ class MimikatzCollector(object):
         except Exception:
             LOG.exception("Error initializing mimikatz collector")
             status = ScanStatus.SCANNED
+        T1106Telem(status, UsageEnum.MIMIKATZ_WINAPI).send()
         T1129Telem(status, UsageEnum.MIMIKATZ).send()
-        T1106Telem(status, UsageEnum.MIMIKATZ_FILE_COPY).send()
 
     def get_logon_info(self):
         """
