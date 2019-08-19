d693b216a7a058c589543309c65d861c404668ba
Shay Nehmad <shay.nehmad@guardicore.com>
2019-08-19 11:57:39 +0300

    Removed unused files

--- monkey/common/utils/itertools_extensions.py
+++
@@ -1,9 +0,0 @@
-from itertools import chain, combinations
-
-
-def power_set(iterable):
-    """
-    https://docs.python.org/3/library/itertools.html#itertools-recipes
-    """
-    s = list(iterable)
-    return [list(x) for x in list(chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1)))]
