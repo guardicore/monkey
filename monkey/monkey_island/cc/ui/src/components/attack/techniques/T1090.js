1e661476a95f62d3848a81b9b63e0d40052bdda6
VakarisZ <vakarisz@yahoo.com>
2019-08-20 08:52:36 +0300

    UI bugfixes

--- monkey/monkey_island/cc/ui/src/components/attack/techniques/T1090.js
+++ monkey/monkey_island/cc/ui/src/components/attack/techniques/T1090.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../styles/Collapse.scss'
 import ReactTable from "react-table";
-import { renderMachineFromSystemData, scanStatus } from "./Helpers"
+import { renderMachineFromSystemData, ScanStatus } from "./Helpers"
 
 
 class T1090 extends React.Component {
@@ -25,7 +25,7 @@ class T1090 extends React.Component {
       <div>
         <div>{this.props.data.message}</div>
         <br/>
-        {this.props.data.status === scanStatus.USED ?
+        {this.props.data.status === ScanStatus.USED ?
           <ReactTable
               columns={T1090.getProxyColumns()}
               data={this.props.data.proxies}
