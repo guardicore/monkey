6843606a4f453e571b5c3e05d77a300f5f37df07
Shay Nehmad <shay.nehmad@guardicore.com>
2019-08-21 19:03:51 +0300

    Merge commit leftovers

--- monkey/monkey_island/cc/services/telemetry/processing/state.py
+++ monkey/monkey_island/cc/services/telemetry/processing/state.py
@@ -3,6 +3,7 @@ from monkey_island.cc.services.node import NodeService
 
 def process_state_telemetry(telemetry_json):
     monkey = NodeService.get_monkey_by_guid(telemetry_json['monkey_guid'])
+    NodeService.add_communication_info(monkey, telemetry_json['command_control_channel'])
     if telemetry_json['data']['done']:
         NodeService.set_monkey_dead(monkey, True)
     else:
