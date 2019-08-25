470806f3bc51e90e0381fcf2d7d31c9015a3618b
Shay Nehmad <shay.nehmad@guardicore.com>
2019-08-25 18:08:21 +0300

    Added segmentation violation test

--- monkey/common/network/segmentation_utils.py
+++ monkey/common/network/segmentation_utils.py
@@ -10,9 +10,12 @@ def get_ip_in_src_and_not_in_dst(ip_addresses, source_subnet, target_subnet):
     :param target_subnet:   Subnet we want an IP to be in.
     :return:    The cross segment IP if in source but not in target, else None.
     """
-    for ip_address in ip_addresses:
-        if target_subnet.is_in_range(ip_address):
-            return None
+    if get_ip_if_in_subnet(ip_addresses, target_subnet) is not None:
+        return None
+    return get_ip_if_in_subnet(ip_addresses, source_subnet)
+
+
+def get_ip_if_in_subnet(ip_addresses, source_subnet):
     for ip_address in ip_addresses:
         if source_subnet.is_in_range(ip_address):
             return ip_address
