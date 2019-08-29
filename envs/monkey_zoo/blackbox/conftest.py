98a60a37dfa4a0160f0a4add6efacbcfa5dddd98
Shay Nehmad <shay.nehmad@guardicore.com>
2019-08-29 14:57:04 +0300

    Works talking to server

---
+++ envs/monkey_zoo/blackbox/conftest.py
@@ -0,0 +1,10 @@
+def pytest_addoption(parser):
+    parser.addoption("--island", action="store", default="default name")
+
+
+def pytest_generate_tests(metafunc):
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    option_value = metafunc.config.option.name
+    if 'name' in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("island", [option_value])
