e627c8c85742764bfe1e6c481908a0cc2a6142fc
Shay Nehmad <shay.nehmad@guardicore.com>
2019-10-28 21:14:22 +0200

    Manual fix - auto formatter failed

--- monkey/monkey_island/cc/resources/telemetry_feed.py
+++ monkey/monkey_island/cc/resources/telemetry_feed.py
@@ -22,9 +22,8 @@ class TelemetryFeed(flask_restful.Resource):
         if "null" == timestamp or timestamp is None:  # special case to avoid ugly JS code...
             telemetries = mongo.db.telemetry.find({})
         else:
-            telemetries = mongo.db.telemetry.find({'timestamp': {'$gt': dateutil.parser.parse(timestamp)}}) \
- \
-        telemetries = telemetries.sort([('timestamp', flask_pymongo.ASCENDING)])
+            telemetries = mongo.db.telemetry.find({'timestamp': {'$gt': dateutil.parser.parse(timestamp)}})
+            telemetries = telemetries.sort([('timestamp', flask_pymongo.ASCENDING)])
 
         try:
             return \
