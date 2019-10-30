dba52fcbedb5e43036d8a2be37c3b4e87d7a1dae
VakarisZ <vakarisz@yahoo.com>
2019-10-30 14:38:17 +0200

    BB tests bugfix of not refreshing JWT

--- envs/monkey_zoo/blackbox/island_client/monkey_island_requests.py
+++ envs/monkey_zoo/blackbox/island_client/monkey_island_requests.py
@@ -1,4 +1,5 @@
 import requests
+import functools
 
 # SHA3-512 of '1234567890!@#$%^&*()_nothing_up_my_sleeve_1234567890!@#$%^&*()'
 import logging
@@ -8,6 +9,7 @@ NO_AUTH_CREDS = '55e97c9dcfd22b8079189ddaeea9bce8125887e3237b800c6176c9afa80d206
 LOGGER = logging.getLogger(__name__)
 
 
+# noinspection PyArgumentList
 class MonkeyIslandRequests(object):
     def __init__(self, server_address):
         self.addr = "https://{IP}/".format(IP=server_address)
@@ -21,29 +23,43 @@ class MonkeyIslandRequests(object):
                 "Unable to connect to island, aborting! Error information: {}. Server: {}".format(err, self.addr))
             assert False
 
+    class _Decorators:
+        @classmethod
+        def refresh_jwt_token(cls, request_function):
+            @functools.wraps(request_function)
+            def request_function_wrapper(self, *args,**kwargs):
+                self.token = self.try_get_jwt_from_server()
+                # noinspection PyArgumentList
+                return request_function(self, *args, **kwargs)
+            return request_function_wrapper
+
     def get_jwt_from_server(self):
         resp = requests.post(self.addr + "api/auth",
                              json={"username": NO_AUTH_CREDS, "password": NO_AUTH_CREDS},
                              verify=False)
         return resp.json()["access_token"]
 
+    @_Decorators.refresh_jwt_token
     def get(self, url, data=None):
         return requests.get(self.addr + url,
                             headers=self.get_jwt_header(),
                             params=data,
                             verify=False)
 
+    @_Decorators.refresh_jwt_token
     def post(self, url, data):
         return requests.post(self.addr + url,
                              data=data,
                              headers=self.get_jwt_header(),
                              verify=False)
 
+    @_Decorators.refresh_jwt_token
     def post_json(self, url, dict_data):
         return requests.post(self.addr + url,
                              json=dict_data,
                              headers=self.get_jwt_header(),
                              verify=False)
 
+    @_Decorators.refresh_jwt_token
     def get_jwt_header(self):
         return {"Authorization": "JWT " + self.token}
