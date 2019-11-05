3c194e1eb8dcc9e2fae1bab7fa9307b888072b53
Daniel Goldberg <danielg@guardicore.com>
2019-11-05 21:21:44 +0200

    Add TODO doc

--- monkey/infection_monkey/network/fingerprinters_collector.py
+++ monkey/infection_monkey/network/fingerprinters_collector.py
@@ -11,4 +11,7 @@ def get_fingerprint_instances() -> Sequence[HostFinger]:
     Returns the fingerprint objects according to configuration as a list
     :return: A list of HostFinger objects.
     """
+    # note this currently assumes we're in the same package as the fingerprinters
+    # if this changes, this file should be updated
+    # like when they move into a network plugins folder
     return get_instances(__package__, __file__, HostFinger)
