43d4c36507ddc5e63175722088c1d26e86aae52d
Daniel Goldberg <danielg@guardicore.com>
2019-11-21 15:21:41 +0200

    Refactor exploiters into plugins. Change configuration to support it and remove unneeded complex importing. Changed main monkey code to support exploiter plugins.

--- monkey/infection_monkey/monkey.py
+++ monkey/infection_monkey/monkey.py
@@ -30,6 +30,7 @@ from infection_monkey.network.tools import get_interface_to_target
 from infection_monkey.exploit.tools.exceptions import ExploitingVulnerableMachineError, FailedExploitationError
 from infection_monkey.telemetry.attack.t1106_telem import T1106Telem
 from common.utils.attack_utils import ScanStatus, UsageEnum
+from infection_monkey.exploit.HostExploiter import HostExploiter
 
 __author__ = 'itamar'
 
@@ -144,9 +145,9 @@ class InfectionMonkey(object):
 
             self._network.initialize()
 
-            self._exploiters = WormConfiguration.exploiter_classes
-
             self._fingerprint = HostFinger.get_instances()
+
+            self._exploiters = HostExploiter.get_classes()
 
             if not self._keep_running or not WormConfiguration.alive:
                 break
@@ -183,7 +184,8 @@ class InfectionMonkey(object):
                 if self._default_server:
                     if self._network.on_island(self._default_server):
                         machine.set_default_server(get_interface_to_target(machine.ip_addr) +
-                                                   (':' + self._default_server_port if self._default_server_port else ''))
+                                                   (
+                                                       ':' + self._default_server_port if self._default_server_port else ''))
                     else:
                         machine.set_default_server(self._default_server)
                     LOG.debug("Default server for machine: %r set to %s" % (machine, machine.default_server))
