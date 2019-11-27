bf44f2355f4c8cd8be81daec80603925b30c92ea
Daniel Goldberg <danielg@guardicore.com>
2019-11-27 13:58:46 +0200

    Remove property decorator, it's not a property.

--- monkey/infection_monkey/network/HostScanner.py
+++ monkey/infection_monkey/network/HostScanner.py
@@ -2,7 +2,6 @@ from abc import ABCMeta, abstractmethod
 
 
 class HostScanner(metaclass=ABCMeta):
-    @property
     @abstractmethod
     def is_host_alive(self, host):
         raise NotImplementedError()
