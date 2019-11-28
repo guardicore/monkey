037fac942355422a1300ee3acce7ee3c8a5093ac
Daniel Goldberg <danielg@guardicore.com>
2019-11-28 13:56:02 +0100

    Whitespace and imports

--- monkey/infection_monkey/utils/plugins/pluginTests/BadImport.py
+++ monkey/infection_monkey/utils/plugins/pluginTests/BadImport.py
@@ -1,4 +1,4 @@
-from infection_monkey.utils.plugins.pluginTests.PluginTestClass import TestPlugin
+from infection_monkey.utils.plugins.pluginTests.PluginTestClass import TestPlugin  # noqa: F401
 
 
 class SomeDummyPlugin:
