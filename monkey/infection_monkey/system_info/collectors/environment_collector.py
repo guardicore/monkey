5548cf2acfee76dcd120b9494d9c0d049b4a200a
Shay Nehmad <shay.nehmad@guardicore.com>
2020-01-21 16:53:00 +0200

    Fixed UTs - enum change wasn't 100% correct

--- monkey/infection_monkey/system_info/collectors/environment_collector.py
+++ monkey/infection_monkey/system_info/collectors/environment_collector.py
@@ -4,16 +4,16 @@ from common.data.system_info_collectors_names import ENVIRONMENT_COLLECTOR
 from infection_monkey.system_info.system_info_collector import SystemInfoCollector
 
 
-def get_monkey_environment() -> Environment:
+def get_monkey_environment() -> str:
     """
     Get the Monkey's running environment.
     :return: One of the cloud providers if on cloud; otherwise, assumes "on premise".
     """
     for instance in get_all_cloud_instances():
         if instance.is_instance():
-            return instance.get_cloud_provider_name()
+            return instance.get_cloud_provider_name().value
 
-    return Environment.ON_PREMISE
+    return Environment.ON_PREMISE.value
 
 
 class EnvironmentCollector(SystemInfoCollector):
