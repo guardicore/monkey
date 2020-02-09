5b6c6a34f6ba10b1422710c82e9ab8aafb15887d
Daniel Goldberg <danielg@guardicore.com>
2020-02-09 15:16:16 +0200

    Make shellcheck shut up by double quoting all the things

--- monkey/monkey_island/linux/create_certificate.sh
+++ monkey/monkey_island/linux/create_certificate.sh
@@ -3,7 +3,7 @@
 server_root=${1:-"./cc"}
 
 
-openssl genrsa -out $server_root/server.key 2048
-openssl req -new -key $server_root/server.key -out $server_root/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
-openssl x509 -req -days 366 -in $server_root/server.csr -signkey $server_root/server.key -out $server_root/server.crt
+openssl genrsa -out "$server_root"/server.key 2048
+openssl req -new -key "$server_root"/server.key -out "$server_root"/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
+openssl x509 -req -days 366 -in "$server_root"/server.csr -signkey "$server_root"/server.key -out $server_root/server.crt
 
