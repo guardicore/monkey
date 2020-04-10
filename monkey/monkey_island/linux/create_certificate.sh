305d15cc6a3d823ae5eb48a53b6590f8c8544df8
Shay Nehmad <shay.nehmad@guardicore.com>
2020-04-11 01:11:01 +0300

    Added random seed data to .rnd for openssl to work
    This is a known issue with openssl and once ubuntu 18 gets a new version we can delete this

--- monkey/monkey_island/linux/create_certificate.sh
+++ monkey/monkey_island/linux/create_certificate.sh
@@ -2,8 +2,9 @@
 
 server_root=${1:-"./cc"}
 
+# We override the RANDFILE determined by default openssl.cnf
+dd bs=1024 count=2 </dev/urandom >~/.rnd
 
 openssl genrsa -out "$server_root"/server.key 2048
 openssl req -new -key "$server_root"/server.key -out "$server_root"/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=Monkey Department/CN=monkey.com"
 openssl x509 -req -days 366 -in "$server_root"/server.csr -signkey "$server_root"/server.key -out $server_root/server.crt
-
