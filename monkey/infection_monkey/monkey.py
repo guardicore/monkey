bd18893089ef45f2a53b985f2e769da7e2e50408
Shay Nehmad <shay.nehmad@guardicore.com>
2020-05-10 15:09:43 +0300

    Added depth logging for easier debugging down the line

--- monkey/infection_monkey/monkey.py
+++ monkey/infection_monkey/monkey.py
@@ -83,6 +83,8 @@ class InfectionMonkey(object):
         if self._opts.depth is not None:
             WormConfiguration._depth_from_commandline = True
             WormConfiguration.depth = self._opts.depth
+            LOG.debug(f"Setting propagation depth from command line")
+        LOG.debug(f"Set propagation depth to {WormConfiguration.depth}")
 
         self._keep_running = True
         self._network = NetworkScanner()
