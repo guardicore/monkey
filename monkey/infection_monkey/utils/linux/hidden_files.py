15ac77056a88638394417a684012590361a40aad
Shreya <shreya.malviya@gmail.com>
2020-06-05 17:43:08 +0530

    PBA changes: Linux

--- monkey/infection_monkey/utils/linux/hidden_files.py
+++ monkey/infection_monkey/utils/linux/hidden_files.py
@@ -6,7 +6,8 @@ def get_linux_commands_to_hide_files():
     return [
         'touch',    # create file
         HIDDEN_FILE,
-        '; echo \"Successfully created hidden file\" >',    # write to
+        '; echo \"Successfully created hidden file: {}\" |'.format(HIDDEN_FILE),  # output
+        'tee -a',   # and write to file
         HIDDEN_FILE
     ]
 
@@ -17,7 +18,8 @@ def get_linux_commands_to_hide_folders():
         HIDDEN_FOLDER,
         '; touch',  # create file
         '{}/{}'.format(HIDDEN_FOLDER, 'some-file'),    # random file in hidden folder
-        '; echo \"Successfully created hidden folder\" >',  # write to
+        '; echo \"Successfully created hidden folder: {}\" |'.format(HIDDEN_FOLDER),  # output
+        'tee -a',   # and write to file
         '{}/{}'.format(HIDDEN_FOLDER, 'some-file')     # random file in hidden folder
     ]
 
