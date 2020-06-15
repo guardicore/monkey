1b040dc874d1ed614b7bb6eb9cda39da312b2ce1
Shreya <shreya.malviya@gmail.com>
2020-06-15 17:44:28 +0530

    Add windows PBA

--- monkey/infection_monkey/utils/windows/shell_startup_files_modification.py
+++ monkey/infection_monkey/utils/windows/shell_startup_files_modification.py
@@ -1,3 +1,12 @@
+SHELL_STARTUP_FILE = '$Profile'
+
+
 def get_windows_commands_to_modify_shell_startup_files():
-    # TODO: add for powershell
-    return 'dir'
+    return [
+        'powershell.exe',  # run with powershell
+        'Add-Content {0} '.format(SHELL_STARTUP_FILE),
+        '\"# Successfully modified {0}\" ;'.format(SHELL_STARTUP_FILE),  # add line to $profile
+        'cat {0} | Select -last 1 ;'.format(SHELL_STARTUP_FILE),  # print last line of $profile
+        '$OldProfile = cat {0} | Select -skiplast 1 ;'.format(SHELL_STARTUP_FILE),
+        'Set-Content {0} -Value $OldProfile ;'.format(SHELL_STARTUP_FILE)  # remove last line of $profile
+]
