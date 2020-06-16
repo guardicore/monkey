60207e3a794948d75f4a984b66cf69f44e11b1e4
Shreya <shreya.malviya@gmail.com>
2020-06-17 02:09:02 +0530

    Change file locations

--- monkey/infection_monkey/post_breach/shell_startup_files/shell_startup_files_modification.py
+++ monkey/infection_monkey/post_breach/shell_startup_files/shell_startup_files_modification.py
@@ -1,6 +1,6 @@
-from infection_monkey.utils.linux.shell_startup_files_modification import\
+from infection_monkey.post_breach.shell_startup_files.linux.shell_startup_files_modification import\
     get_linux_commands_to_modify_shell_startup_files
-from infection_monkey.utils.windows.shell_startup_files_modification import\
+from infection_monkey.post_breach.shell_startup_files.windows.shell_startup_files_modification import\
     get_windows_commands_to_modify_shell_startup_files
 
 
--- monkey/infection_monkey/post_breach/shell_startup_files/windows/shell_startup_files_modification.py
+++ monkey/infection_monkey/post_breach/shell_startup_files/windows/shell_startup_files_modification.py
@@ -9,4 +9,4 @@ def get_windows_commands_to_modify_shell_startup_files():
         'cat {0} | Select -last 1 ;'.format(SHELL_STARTUP_FILE),  # print last line of $profile
         '$OldProfile = cat {0} | Select -skiplast 1 ;'.format(SHELL_STARTUP_FILE),
         'Set-Content {0} -Value $OldProfile ;'.format(SHELL_STARTUP_FILE)  # remove last line of $profile
-]
+    ]
