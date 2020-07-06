87a184f236ea581cf7cc91ae4944f71b9625f505
Shay Nehmad <shay.nehmad@guardicore.com>
2020-07-06 14:25:23 +0300

    More work done on labels exporter
    Need to finish "exploited" and "risk assessment" labels now

--- monkey/monkey_island/cc/services/reporting/exporting/labels_exporter.py
+++ monkey/monkey_island/cc/services/reporting/exporting/labels_exporter.py
@@ -1,25 +1,50 @@
+import binascii
+import json
 import logging
+from pathlib import Path
 
 from monkey_island.cc.services.reporting.exporting.exporter import Exporter
 
 logger = logging.getLogger(__name__)
 
 
+def create_machine_object(machine):
+    ip_addrs = machine["ip_addresses"]
+    ip_addrs_formatted = ["ip:" + binascii.hexlify(x.encode()).decode() for x in ip_addrs]
+    return {
+        "ip_addresses": ip_addrs,
+        "ip_addresses_formatted": ip_addrs_formatted
+    }
+
+
 def create_seen_label(machine):
     return {
-        "label": "Monkey Scan: Machine visible",
-        "machine": {
-            "ip_addresses": machine['ip_addresses']
-        }
+        "label_key": "Monkey Scan",
+        "label_value": f"Seen be Monkeys {machine['accessible_from_nodes']}",
+        "machine": create_machine_object(machine)
     }
 
 
 def create_exploited_label(machine):
-    exploit = machine["???"]  # TODO run a demo with a lot of info in the report
+    exploit = "ssh"
+    # exploit = machine["???"]
+    logger.debug(f"Info: {machine}")
     return {
-        "label": f"Monkey Exploit: {exploit}",
+        "label_key": "Monkey Exploit",
+        "label_value": f"{exploit} used by Monkey",
         "machine": {
-            "ip_addresses": machine['ip_addresses']
+            create_machine_object(machine)
+        }
+    }
+
+
+def create_risk_assessment_label(machine):
+    risk_assessment = "Medium" # TODO run a demo with a lot of info in the report
+    return {
+        "label_key": "Monkey Risk assessment",
+        "label_value": f"Risk: {risk_assessment}",
+        "machine": {
+            create_machine_object(machine)
         }
     }
 
@@ -38,7 +63,9 @@ def create_machine_labels_from_report(report_json):
     """
     # Get Monkey seen
     for machine in report_json['glance']['scanned']:
-        labels.append(create_seen_label(machine))
+        # Might be the machine the Monkey started on
+        if len(machine["accessible_from_nodes"]) > 0:
+            labels.append(create_seen_label(machine))
     # Get Monkey Exploited
     for machine in report_json['glance']['exploited']:
         labels.append(create_exploited_label(machine))
@@ -47,10 +74,18 @@ def create_machine_labels_from_report(report_json):
     return labels
 
 
+def export_labels_to_file(machine_labels):
+    labels_file_path = Path("monkey_labels.json")  # todo get from internal config
+    logger.debug(f"Writing labels to {labels_file_path.absolute()}")
+    with open(labels_file_path, "w") as labels_file:
+        json.dump(machine_labels, labels_file, indent=2)
+
+
 class LabelsExporter(Exporter):
     @staticmethod
     def handle_report(report_json):
         machine_labels = create_machine_labels_from_report(report_json)
         logger.info(f"Created {len(machine_labels)} labels")
+        export_labels_to_file(machine_labels)
         # Save labels to X?
         # Export labels?
