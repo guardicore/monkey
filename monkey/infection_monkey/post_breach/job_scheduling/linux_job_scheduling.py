5bbef83b8322bed914e6e7164cc4aaa177cfff34
Shreya <shreya.malviya@gmail.com>
2020-07-13 20:13:47 +0530

    Linux: delete `TEMP_CRON` file even if command fails

--- monkey/infection_monkey/post_breach/job_scheduling/linux_job_scheduling.py
+++ monkey/infection_monkey/post_breach/job_scheduling/linux_job_scheduling.py
@@ -7,6 +7,6 @@ def get_linux_commands_to_schedule_jobs():
         f'crontab -l > {TEMP_CRON} &&',
         'echo \"# Successfully scheduled a job using crontab\" |',
         f'tee -a {TEMP_CRON} &&',
-        f'crontab {TEMP_CRON} &&',
+        f'crontab {TEMP_CRON} ;',
         f'rm {TEMP_CRON}'
     ]
