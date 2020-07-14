57f30a89791eebcce28ad9ab28f93dd126a2f533
Shreya <shreya.malviya@gmail.com>
2020-07-14 20:24:11 +0530

    Update after pulling from develop

--- monkey/monkey_island/cc/services/attack/technique_reports/T1166.py
+++ monkey/monkey_island/cc/services/attack/technique_reports/T1166.py
@@ -1,4 +1,7 @@
 from common.data.post_breach_consts import POST_BREACH_SETUID_SETGID
+from common.utils.attack_utils import ScanStatus
+from monkey_island.cc.database import mongo
+from monkey_island.cc.services.attack.technique_reports import AttackTechnique
 from monkey_island.cc.services.attack.technique_reports.pba_technique import \
     PostBreachTechnique
 
@@ -7,7 +10,7 @@ __author__ = "shreyamalviya"
 
 class T1166(PostBreachTechnique):
     tech_id = "T1166"
-    unscanned_msg = "Monkey didn't try setting the setuid or setgid bits since it found no Linux machines."
-    scanned_msg = "Monkey tried setting the setuid or setgid bits but failed."
-    used_msg = "Monkey successfully set the setuid or setgid bits."
+    unscanned_msg = "Monkey did not try creating hidden files or folders."
+    scanned_msg = "Monkey tried creating hidden files and folders on the system but failed."
+    used_msg = "Monkey created hidden files and folders on the system."
     pba_names = [POST_BREACH_SETUID_SETGID]
