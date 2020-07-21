594d3f2f97c7fdef82105c829bb2f5948dd9ebb8
Shay Nehmad <shay.nehmad@guardicore.com>
2020-07-21 10:52:45 +0300

    Single quotes

--- monkey/monkey_island/cc/ui/src/index.js
+++ monkey/monkey_island/cc/ui/src/index.js
@@ -1,5 +1,5 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
