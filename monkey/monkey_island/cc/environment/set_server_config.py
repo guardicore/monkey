05395fee6d57dfea839c608ef9dc047a9868f79e
VakarisZ <vakarisz@yahoo.com>
2020-08-14 15:43:06 +0300

    Created code that generates default server_config.json, so we don't need to track this file on git and remember not to commit credentials.

--- monkey/monkey_island/cc/environment/set_server_config.py
+++ monkey/monkey_island/cc/environment/set_server_config.py
@@ -1,7 +1,8 @@
 import argparse
 import json
 import logging
-from pathlib import Path
+
+from monkey_island.cc.environment.environment_config import EnvironmentConfig
 
 SERVER_CONFIG = "server_config"
 
@@ -12,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 
 def main():
     args = parse_args()
-    file_path = get_config_file_path(args)
+    file_path = EnvironmentConfig.get_config_file_path()
 
     # Read config
     with open(file_path) as config_file:
@@ -28,16 +29,9 @@ def main():
         config_file.write("\n")  # Have to add newline at end of file, since json.dump does not.
 
 
-def get_config_file_path(args):
-    file_path = Path(__file__).parent.joinpath(args.file_name)
-    logger.info("Config file path: {}".format(file_path))
-    return file_path
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("server_config", choices=["standard", "testing", "password"])
-    parser.add_argument("-f", "--file_name", required=False, default="server_config.json")
     args = parser.parse_args()
     return args
 
