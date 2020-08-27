8f3c1b9f75565def303b5e2d48dce612432e0476
VakarisZ <vakarisz@yahoo.com>
2020-08-27 15:33:54 +0300

    Changed import order in test_shellcode_obfuscator.py

--- monkey/common/utils/test_shellcode_obfuscator.py
+++ monkey/common/utils/test_shellcode_obfuscator.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from common.utils.shellcode_obfuscator import obfuscate, clarify
+from common.utils.shellcode_obfuscator import clarify, obfuscate
 
 SHELLCODE_FROM_CMD_PARAM = '\\x52\\x3d\\xf6\\xc9\\x4b\\x5d\\xe0\\x62\\x7e\\x3d\\xa8\\x07\\x7b\\x76\\x30'
 OBFUSCATED_PARAM_OUTPUT = '\\x30\\x76\\x7b\\x07\\xa8\\x3d\\x7e\\x62\\xe0\\x5d\\x4b\\xc9\\xf6\\x3d\\x52'
