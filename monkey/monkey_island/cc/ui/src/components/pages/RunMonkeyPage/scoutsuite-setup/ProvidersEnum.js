2e0d06b17ad2402ceb786fa86b58877c7015ffb0
VakarisZ <vakarisz@yahoo.com>
2020-10-01 15:18:52 +0300

    Added ProvidersEnum.js for front end

---
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/scoutsuite-setup/ProvidersEnum.js
@@ -0,0 +1,9 @@
+// Should match enum in monkey/common/cloud/scoutsuite_consts.py
+
+export const PROVIDERS = {
+  AWS : 'aws',
+  AZURE : 'azure',
+  GCP : 'gcp',
+  ALIBABA : 'aliyun',
+  ORACLE : 'oci'
+}
