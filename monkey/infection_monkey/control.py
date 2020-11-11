18d72f89dad8f07510e1e54f670b1f24584a7145
VakarisZ <vakarisz@yahoo.com>
2020-11-11 15:31:48 +0200

    Added timeouts to requests

--- monkey/infection_monkey/control.py
+++ monkey/infection_monkey/control.py
@@ -9,6 +9,7 @@ from requests.exceptions import ConnectionError
 
 import infection_monkey.monkeyfs as monkeyfs
 import infection_monkey.tunnel as tunnel
+from common.common_consts.timeouts import MEDIUM_REQUEST_TIMEOUT, LONG_REQUEST_TIMEOUT, SHORT_REQUEST_TIMEOUT
 from common.data.api_url_consts import T1216_PBA_FILE_DOWNLOAD_PATH
 from infection_monkey.config import GUID, WormConfiguration
 from infection_monkey.network.info import check_internet_access, local_ips
@@ -121,7 +122,8 @@ class ControlClient(object):
                            data=json.dumps(monkey),
                            headers={'content-type': 'application/json'},
                            verify=False,
-                           proxies=ControlClient.proxies)
+                           proxies=ControlClient.proxies,
+                           timeout=MEDIUM_REQUEST_TIMEOUT)
         except Exception as exc:
             LOG.warning("Error connecting to control server %s: %s",
                         WormConfiguration.current_server, exc)
@@ -138,7 +140,8 @@ class ControlClient(object):
                           data=json.dumps(telemetry),
                           headers={'content-type': 'application/json'},
                           verify=False,
-                          proxies=ControlClient.proxies)
+                          proxies=ControlClient.proxies,
+                          timeout=MEDIUM_REQUEST_TIMEOUT)
         except Exception as exc:
             LOG.warning("Error connecting to control server %s: %s",
                         WormConfiguration.current_server, exc)
@@ -153,7 +156,8 @@ class ControlClient(object):
                           data=json.dumps(telemetry),
                           headers={'content-type': 'application/json'},
                           verify=False,
-                          proxies=ControlClient.proxies)
+                          proxies=ControlClient.proxies,
+                          timeout=MEDIUM_REQUEST_TIMEOUT)
         except Exception as exc:
             LOG.warning("Error connecting to control server %s: %s",
                         WormConfiguration.current_server, exc)
@@ -165,7 +169,8 @@ class ControlClient(object):
         try:
             reply = requests.get("https://%s/api/monkey/%s" % (WormConfiguration.current_server, GUID),  # noqa: DUO123
                                  verify=False,
-                                 proxies=ControlClient.proxies)
+                                 proxies=ControlClient.proxies,
+                                 timeout=MEDIUM_REQUEST_TIMEOUT)
 
         except Exception as exc:
             LOG.warning("Error connecting to control server %s: %s",
@@ -194,7 +199,8 @@ class ControlClient(object):
                            data=json.dumps({'config_error': True}),
                            headers={'content-type': 'application/json'},
                            verify=False,
-                           proxies=ControlClient.proxies)
+                           proxies=ControlClient.proxies,
+                           timeout=MEDIUM_REQUEST_TIMEOUT)
         except Exception as exc:
             LOG.warning("Error connecting to control server %s: %s", WormConfiguration.current_server, exc)
             return {}
@@ -255,7 +261,8 @@ class ControlClient(object):
                 download = requests.get("https://%s/api/monkey/download/%s" %  # noqa: DUO123
                                         (WormConfiguration.current_server, filename),
                                         verify=False,
-                                        proxies=ControlClient.proxies)
+                                        proxies=ControlClient.proxies,
+                                        timeout=MEDIUM_REQUEST_TIMEOUT)
 
                 with monkeyfs.open(dest_file, 'wb') as file_obj:
                     for chunk in download.iter_content(chunk_size=DOWNLOAD_CHUNK):
@@ -281,7 +288,8 @@ class ControlClient(object):
             reply = requests.post("https://%s/api/monkey/download" % (WormConfiguration.current_server,),  # noqa: DUO123
                                   data=json.dumps(host_dict),
                                   headers={'content-type': 'application/json'},
-                                  verify=False, proxies=ControlClient.proxies)
+                                  verify=False, proxies=ControlClient.proxies,
+                                  timeout=LONG_REQUEST_TIMEOUT)
             if 200 == reply.status_code:
                 result_json = reply.json()
                 filename = result_json.get('filename')
@@ -323,7 +331,8 @@ class ControlClient(object):
             return requests.get(PBA_FILE_DOWNLOAD %  # noqa: DUO123
                                 (WormConfiguration.current_server, filename),
                                 verify=False,
-                                proxies=ControlClient.proxies)
+                                proxies=ControlClient.proxies,
+                                timeout=LONG_REQUEST_TIMEOUT)
         except requests.exceptions.RequestException:
             return False
 
@@ -334,7 +343,8 @@ class ControlClient(object):
                                         T1216_PBA_FILE_DOWNLOAD_PATH),
                                 verify=False,
                                 proxies=ControlClient.proxies,
-                                stream=True)
+                                stream=True,
+                                timeout=MEDIUM_REQUEST_TIMEOUT)
         except requests.exceptions.RequestException:
             return False
 
@@ -352,7 +362,7 @@ class ControlClient(object):
     def can_island_see_port(port):
         try:
             url = f"https://{WormConfiguration.current_server}/api/monkey_control/check_remote_port/{port}"
-            response = requests.get(url, verify=False)
+            response = requests.get(url, verify=False, timeout=SHORT_REQUEST_TIMEOUT)
             response = json.loads(response.content.decode())
             return response['status'] == "port_visible"
         except requests.exceptions.RequestException:
@@ -362,4 +372,5 @@ class ControlClient(object):
     def report_start_on_island():
         requests.post(f"https://{WormConfiguration.current_server}/api/monkey_control/started_on_island",
                       data=json.dumps({'started_on_island': True}),
-                      verify=False)
+                      verify=False,
+                      timeout=MEDIUM_REQUEST_TIMEOUT)
