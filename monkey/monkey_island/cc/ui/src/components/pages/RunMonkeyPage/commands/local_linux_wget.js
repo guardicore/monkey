22d9f703746c5ee1a3c5223272d0c5692ac14a0b
Shreya <shreya.malviya@gmail.com>
2020-12-22 22:03:49 +0530

    Add option to run as a certain user via manual command on the Run Monkey page

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_wget.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_wget.js
@@ -1,10 +1,13 @@
 import {OS_TYPES} from '../utils/OsTypes';
 
 
-export default function generateLocalLinuxWget(ip, osType) {
+export default function generateLocalLinuxWget(ip, osType, username) {
     let bitText = osType === OS_TYPES.LINUX_32 ? '32' : '64';
-    return `wget --no-check-certificate https://${ip}:5000/api/monkey/download/
-    monkey-linux-${bitText};
-    chmod +x monkey-linux-${bitText};
-    ./monkey-linux-${bitText} m0nk3y -s ${ip}:5000`;
+    let command = `wget --no-check-certificate https://${ip}:5000/api/monkey/download/`
+      + `monkey-linux-${bitText}; `
+      + `chmod +x monkey-linux-${bitText}; `
+      + `./monkey-linux-${bitText} m0nk3y -s ${ip}:5000`;
+    if (username != '')
+      command = `su - ${username} -c "${command}"`;
+    return command;
   }
