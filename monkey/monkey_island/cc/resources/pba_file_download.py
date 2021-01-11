4f43edbefb068998daf330acdb4f8363b49dea4e
VakarisZ <vakarisz@yahoo.com>
2021-01-11 08:54:01 +0200

    Removed unnecessary logging in pba_file_download.py

--- monkey/monkey_island/cc/resources/pba_file_download.py
+++ monkey/monkey_island/cc/resources/pba_file_download.py
@@ -1,13 +1,9 @@
-import logging
-
 import flask_restful
 from flask import send_from_directory
 
 from monkey_island.cc.services.post_breach_files import ABS_UPLOAD_PATH
 
 __author__ = 'VakarisZ'
-
-LOG = logging.getLogger(__name__)
 
 
 class PBAFileDownload(flask_restful.Resource):
