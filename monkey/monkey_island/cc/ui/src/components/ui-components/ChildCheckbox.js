32cdc034f32fc53c46eed596af426b681cf8b031
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-01-26 15:51:42 -0500

    ui: fix whitespace in ChildCheckbox.js

--- monkey/monkey_island/cc/ui/src/components/ui-components/ChildCheckbox.js
+++ monkey/monkey_island/cc/ui/src/components/ui-components/ChildCheckbox.js
@@ -6,25 +6,23 @@ import {faCheckSquare} from '@fortawesome/free-solid-svg-icons';
 import {faSquare} from '@fortawesome/free-regular-svg-icons';
 
 function ChildCheckbox(props) {
-    const {
-        onPaneClick,
-        onClick,
-        value,
-        disabled,
-        label,
-        checkboxState
-    } = props;
+  const {
+    onPaneClick,
+    onClick,
+    value,
+    disabled,
+    label,
+    checkboxState
+  } = props;
 
-    return (
-        <Form.Group onClick={() => onPaneClick(value)}>
-            <Button value={value} variant={'link'} disabled={disabled} onClick={() => onClick(value)}>
-                <FontAwesomeIcon icon={checkboxState ? faCheckSquare : faSquare}/>
-            </Button>
-            <span className={'option-text'}>
-                {label}
-            </span>
-        </Form.Group>
-    );
+  return (
+    <Form.Group onClick={() => onPaneClick(value)}>
+      <Button value={value} variant={'link'} disabled={disabled} onClick={() => onClick(value)}>
+        <FontAwesomeIcon icon={checkboxState ? faCheckSquare : faSquare}/>
+      </Button>
+      <span className={'option-text'}>{label}</span>
+    </Form.Group>
+  );
 }
 
 export default ChildCheckbox;
