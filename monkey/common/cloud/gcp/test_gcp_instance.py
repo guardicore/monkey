eed5ea1337f6ec74fc416cac19c94bec05b53020
Shreya <shreya.malviya@gmail.com>
2021-02-04 21:02:53 +0530

    Add tests for GcpInstance

---
+++ monkey/common/cloud/gcp/test_gcp_instance.py
@@ -0,0 +1,41 @@
+import pytest
+import requests
+import requests_mock
+
+from common.cloud.environment_names import Environment
+from common.cloud.gcp.gcp_instance import GCP_METADATA_SERVICE_URL, GcpInstance
+
+
+def get_test_gcp_instance(url, **kwargs):
+    with requests_mock.Mocker() as m:
+        m.get(url, **kwargs)
+        test_gcp_instance_object = GcpInstance()
+        return test_gcp_instance_object
+
+
+# good request
+@pytest.fixture
+def good_request_mock_instance():
+    return get_test_gcp_instance(GCP_METADATA_SERVICE_URL)
+
+
+def test_is_instance_good_request(good_request_mock_instance):
+    assert good_request_mock_instance.is_instance()
+
+
+def test_get_cloud_provider_name_good_request(good_request_mock_instance):
+    assert good_request_mock_instance.get_cloud_provider_name() == Environment.GCP
+
+
+# bad request
+@pytest.fixture
+def bad_request_mock_instance():
+    return get_test_gcp_instance(GCP_METADATA_SERVICE_URL, exc=requests.RequestException)
+
+
+def test_is_instance_bad_request(bad_request_mock_instance):
+    assert bad_request_mock_instance.is_instance() is False
+
+
+def test_get_cloud_provider_name_bad_request(bad_request_mock_instance):
+    assert bad_request_mock_instance.get_cloud_provider_name() == Environment.GCP
