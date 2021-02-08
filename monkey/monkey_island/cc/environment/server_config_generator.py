4b5415ac0b946b5b6a9e5beaf398057ab1901381
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-02-08 15:00:46 -0500

    cc: fix server_config_generator behavior

--- monkey/monkey_island/cc/environment/server_config_generator.py
+++ monkey/monkey_island/cc/environment/server_config_generator.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
+from monkey_island.cc.consts import DEFAULT_STANDARD_SERVER_CONFIG
+
 
 def create_default_config_file(path):
-    default_config_path = f"{path}.default"
-    default_config = Path(default_config_path).read_text()
+    default_config = Path(DEFAULT_STANDARD_SERVER_CONFIG).read_text()
     Path(path).write_text(default_config)
