3d1d60279af8ec13b020ab0a9ea3fd4b95bab497
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-02-10 08:33:19 -0500

    cc: rename DEFAULT_STANDARD_SERVER_CONFIG -> DEFAULT_STANDARD_SERVER_CONFIG_PATH

--- monkey/monkey_island/cc/environment/server_config_generator.py
+++ monkey/monkey_island/cc/environment/server_config_generator.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-from monkey_island.cc.consts import DEFAULT_STANDARD_SERVER_CONFIG
+from monkey_island.cc.consts import DEFAULT_STANDARD_SERVER_CONFIG_PATH
 
 
 def create_default_config_file(path):
-    default_config = Path(DEFAULT_STANDARD_SERVER_CONFIG).read_text()
+    default_config = Path(DEFAULT_STANDARD_SERVER_CONFIG_PATH).read_text()
     Path(path).write_text(default_config)
