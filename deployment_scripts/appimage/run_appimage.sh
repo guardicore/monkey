2d971d95fc3b81ceecd1ca2864c41b8893b756b5
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-02-11 07:27:56 -0500

    build: select server and logger config at runtime in appimage

--- deployment_scripts/appimage/run_appimage.sh
+++ deployment_scripts/appimage/run_appimage.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+DOT_MONKEY=$HOME/.monkey_island/
+
+configure_default_logging() {
+    if [ ! -f $DOT_MONKEY/island_logger_config.json ]; then
+        cp $APPDIR/usr/src/island_logger_config.json $DOT_MONKEY
+    fi
+}
+
+
 # Detecting command that calls python 3.7
 python_cmd=""
 if [[ $(python --version 2>&1) == *"Python 3.7"* ]]; then
@@ -12,10 +21,13 @@ if [[ $(python3.7 --version 2>&1) == *"Python 3.7"* ]]; then
   python_cmd="python3.7"
 fi
 
-DB_DIR=${1:-"./monkey_island/bin/mongodb/db"}
+mkdir -p $DOT_MONKEY
 
+DB_DIR=$DOT_MONKEY/db
 mkdir -p $DB_DIR
+
+configure_default_logging
 
 cd $APPDIR/usr/src
 ./monkey_island/bin/mongodb/bin/mongod --dbpath $DB_DIR &
-${python_cmd} ./monkey_island.py
+${python_cmd} ./monkey_island.py --server-config $DOT_MONKEY/server_config.json --logger-config $DOT_MONKEY/island_logger_config.json
