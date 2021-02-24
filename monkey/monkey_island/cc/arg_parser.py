026cb389d5990bca3a7ff67e585e2030717b9061
VakarisZ <vakarisz@yahoo.com>
2021-02-24 09:52:34 +0200

    Extracted island argument parsing into a separate file

---
+++ monkey/monkey_island/cc/arg_parser.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+from monkey_island.cc.consts import DEFAULT_SERVER_CONFIG_PATH, DEFAULT_LOGGER_CONFIG_PATH
+
+
+@dataclass
+class IslandArgs:
+    setup_only: bool
+    server_config: str
+    logger_config: str
+
+
+def parse_cli_args() -> IslandArgs:
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Infection Monkey Island CnC Server.  See https://infectionmonkey.com",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-s",
+        "--setup-only",
+        action="store_true",
+        help="Pass this flag to cause the Island to setup and exit without actually starting. "
+        "This is useful for preparing Island to boot faster later-on, so for "
+        "compiling/packaging Islands.",
+    )
+    parser.add_argument(
+        "--server-config",
+        action="store",
+        help="The path to the server configuration file.",
+        default=DEFAULT_SERVER_CONFIG_PATH,
+    )
+    parser.add_argument(
+        "--logger-config",
+        action="store",
+        help="The path to the logging configuration file.",
+        default=DEFAULT_LOGGER_CONFIG_PATH,
+    )
+    args = parser.parse_args()
+
+    return IslandArgs(args.setup_only, args.server_config, args.logger_config)
