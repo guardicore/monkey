2ef81d56889ac081bfe7fcadfb2aa2f9359505a1
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-02-26 11:06:33 -0500

    ui: change language from "use" -> submit for consistency

--- monkey/monkey_island/cc/ui/src/components/configuration-components/UnsafeOptionsConfirmationModal.js
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/UnsafeOptionsConfirmationModal.js
@@ -10,7 +10,7 @@ function UnsafeOptionsConfirmationModal(props) {
         </h2>
         <p className='text-center' style={{'fontSize': '1.2em', 'marginBottom': '2em'}}>
           Some of the selected options could cause systems to become unstable or malfunction.
-          Are you sure you want to use the selected settings?
+          Are you sure you want to submit the selected settings?
         </p>
         <div className='text-center'>
           <Button type='button'
@@ -25,7 +25,7 @@ function UnsafeOptionsConfirmationModal(props) {
                   size='lg'
                   style={{margin: '5px'}}
                   onClick={() => {props.onContinueClick()}}>
-            I know what I'm doing.
+            Submit
           </Button>
         </div>
       </Modal.Body>
