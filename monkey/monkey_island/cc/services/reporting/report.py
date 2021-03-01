ffd0fc033153e76252b52d818e330b8a87f5a20e
VakarisZ <vakarisz@yahoo.com>
2021-03-01 10:05:18 +0200

    Changed the logic of zerologon password restoration issue overview to be more consistent with the function of issue map.

--- monkey/monkey_island/cc/services/reporting/report.py
+++ monkey/monkey_island/cc/services/reporting/report.py
@@ -65,7 +65,7 @@ class ReportService:
         VSFTPD = 13
         DRUPAL = 14
         ZEROLOGON = 15
-        ZEROLOGON_PASSWORD_RESTORED = 16
+        ZEROLOGON_PASSWORD_RESTORE_FAILED = 16
 
     class WARNINGS_DICT(Enum):
         CROSS_SEGMENT = 0
@@ -715,8 +715,8 @@ class ReportService:
                 elif issue['type'] == 'drupal':
                     issues_byte_array[ReportService.ISSUES_DICT.DRUPAL.value] = True
                 elif issue['type'] == 'zerologon':
-                    if issue['password_restored']:
-                        issues_byte_array[ReportService.ISSUES_DICT.ZEROLOGON_PASSWORD_RESTORED.value] = True
+                    if not issue['password_restored']:
+                        issues_byte_array[ReportService.ISSUES_DICT.ZEROLOGON_PASSWORD_RESTORE_FAILED.value] = True
                     issues_byte_array[ReportService.ISSUES_DICT.ZEROLOGON.value] = True
                 elif issue['type'].endswith('_password') and issue['password'] in config_passwords and \
                         issue['username'] in config_users or issue['type'] == 'ssh':
