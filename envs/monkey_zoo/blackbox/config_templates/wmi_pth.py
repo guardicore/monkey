6a6dd6711392c28c084afcd1474997869e593aaa
VakarisZ <vakarisz@yahoo.com>
2021-03-08 17:16:32 +0200

    Refactored config templates to inherit from ConfigTemplate class instead of BaseTemplate

--- envs/monkey_zoo/blackbox/config_templates/wmi_pth.py
+++ envs/monkey_zoo/blackbox/config_templates/wmi_pth.py
@@ -1,9 +1,10 @@
 from copy import copy
 
 from envs.monkey_zoo.blackbox.config_templates.base_template import BaseTemplate
+from envs.monkey_zoo.blackbox.config_templates.config_template import ConfigTemplate
 
 
-class WmiPth(BaseTemplate):
+class WmiPth(ConfigTemplate):
     config_values = copy(BaseTemplate.config_values)
 
     config_values.update({
