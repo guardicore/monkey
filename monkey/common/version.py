66ce163067be5359eeefa88c65b238f639077592
VakarisZ <vakarisz@yahoo.com>
2021-03-24 15:20:34 +0200

    Bumped version number to 1.10

--- monkey/common/version.py
+++ monkey/common/version.py
@@ -3,7 +3,7 @@ import argparse
 from pathlib import Path
 
 MAJOR = "1"
-MINOR = "9"
+MINOR = "10"
 PATCH = "0"
 build_file_path = Path(__file__).parent.joinpath("BUILD")
 with open(build_file_path, "r") as build_file:
