3a346b511207a92b16c9df2f223ca81b9364eace
Shreya <shreya.malviya@gmail.com>
2021-05-06 13:30:45 +0530

    Remove unused `NoInheritance` in `infection_monkey/utils/plugins/pluginTests/ComboFile.py`

--- monkey/infection_monkey/utils/plugins/pluginTests/ComboFile.py
+++ monkey/infection_monkey/utils/plugins/pluginTests/ComboFile.py
@@ -1,10 +1,6 @@
 from infection_monkey.utils.plugins.pluginTests.PluginTestClass import PluginTester
 
 
-class NoInheritance:
-    pass
-
-
 class BadInit(PluginTester):
     def __init__(self):
         raise Exception("TestException")
