be0f7ac88134db1020d487f6a7fe381b0f2d2ee6
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-05-11 08:33:56 -0400

    island: Decouple PBAFileDownload from environment_singleton

--- monkey/monkey_island/cc/resources/pba_file_download.py
+++ monkey/monkey_island/cc/resources/pba_file_download.py
@@ -1,7 +1,7 @@
 import flask_restful
 from flask import send_from_directory
 
-import monkey_island.cc.environment.environment_singleton as env_singleton
+from monkey_island.cc.services.post_breach_files import PostBreachFilesService
 
 __author__ = "VakarisZ"
 
@@ -12,5 +12,6 @@ class PBAFileDownload(flask_restful.Resource):
     """
 
     # Used by monkey. can't secure.
-    def get(self, path):
-        return send_from_directory(env_singleton.env.get_config().data_dir_abs_path, path)
+    def get(self, filename):
+        custom_pba_dir = PostBreachFilesService.get_custom_pba_directory()
+        return send_from_directory(custom_pba_dir, filename)
