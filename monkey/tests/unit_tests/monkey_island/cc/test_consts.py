dc129c017b32bec3ebe56d374492f77d9f38e58a
shreyamalviya <shreya.malviya@gmail.com>
2021-05-20 16:43:36 +0530

    Fix unit test in test_consts.py

--- monkey/tests/unit_tests/monkey_island/cc/test_consts.py
+++ monkey/tests/unit_tests/monkey_island/cc/test_consts.py
@@ -5,8 +5,8 @@ import monkey_island.cc.server_utils.consts as consts
 
 def test_default_server_config_file_path():
     if platform.system() == "Windows":
-        server_file_path = consts.MONKEY_ISLAND_ABS_PATH + r"\cc\server_config.json"
+        server_file_path = f"{consts.DEFAULT_DATA_DIR}\\{consts.SERVER_CONFIG_FILENAME}"
     else:
-        server_file_path = consts.MONKEY_ISLAND_ABS_PATH + "/cc/server_config.json"
+        server_file_path = f"{consts.DEFAULT_DATA_DIR}/{consts.SERVER_CONFIG_FILENAME}"
 
     assert consts.DEFAULT_SERVER_CONFIG_PATH == server_file_path
