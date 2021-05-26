02ead23d044ff22ce1cd95c31010ac7359fffc2d
Shreya <shreya.malviya@gmail.com>
2021-05-26 19:27:08 +0530

    Remove unused unit test fixtures

--- monkey/tests/unit_tests/monkey_island/conftest.py
+++ monkey/tests/unit_tests/monkey_island/conftest.py
@@ -6,13 +6,3 @@ import pytest
 @pytest.fixture(scope="module")
 def server_configs_dir(data_for_tests_dir):
     return os.path.join(data_for_tests_dir, "server_configs")
-
-
-@pytest.fixture(scope="module")
-def server_config_init_only(server_configs_dir):
-    return os.path.join(server_configs_dir, "server_config_init_only.json")
-
-
-@pytest.fixture(scope="module")
-def server_config_empty(server_configs_dir):
-    return os.path.join(server_configs_dir, "server_config_empty.json")
