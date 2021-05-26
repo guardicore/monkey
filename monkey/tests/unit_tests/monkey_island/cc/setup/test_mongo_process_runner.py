f5f8f572f6efd49b292a9fee540372117f9f9a46
Shreya <shreya.malviya@gmail.com>
2021-05-26 19:05:39 +0530

    Remove unneeded function arguments in test_mongo_process_runner.py

--- monkey/tests/unit_tests/monkey_island/cc/setup/test_mongo_process_runner.py
+++ monkey/tests/unit_tests/monkey_island/cc/setup/test_mongo_process_runner.py
@@ -8,23 +8,23 @@ TEST_DIR_NAME = "test_dir"
 
 
 @pytest.fixture(autouse=True)
-def fake_db_dir(monkeypatch, tmpdir):
+def fake_db_dir(monkeypatch):
     monkeypatch.setattr("monkey_island.cc.setup.mongo_process_runner.DB_DIR_NAME", TEST_DIR_NAME)
 
 
 @pytest.fixture
-def expected_path(monkeypatch, tmpdir):
+def expected_path(tmpdir):
     expected_path = os.path.join(tmpdir, TEST_DIR_NAME)
     return expected_path
 
 
-def test_create_db_dir(monkeypatch, tmpdir, expected_path):
+def test_create_db_dir(tmpdir, expected_path):
     db_path = MongoDbRunner(tmpdir, tmpdir)._create_db_dir()
     assert os.path.isdir(expected_path)
     assert db_path == expected_path
 
 
-def test_create_db_dir__already_created(monkeypatch, tmpdir, expected_path):
+def test_create_db_dir__already_created(tmpdir, expected_path):
     os.mkdir(expected_path)
 
     db_path = MongoDbRunner(tmpdir, tmpdir)._create_db_dir()
