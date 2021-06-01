c94216b026758e8fc9c726dde498f70e9bb0d247
VakarisZ <vakarisz@yahoo.com>
2021-06-01 10:13:40 +0300

    Added an upload status icon (checkmark if successful, red x if error)

---
+++ monkey/monkey_island/cc/ui/src/components/ui-components/UploadStatusIcon.tsx
@@ -0,0 +1,23 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+
+export const UploadStatuses = {
+  clean: 'clean',
+  success: 'success',
+  error: 'error'
+}
+
+const UploadStatusIcon = (props: { status: string }) => {
+  switch (props.status) {
+    case UploadStatuses.success:
+      return (<FontAwesomeIcon icon={faCheck} className={'upload-status-icon-success'}/>);
+    case UploadStatuses.error:
+      return (<FontAwesomeIcon icon={faTimes} className={'upload-status-icon-error'}/>);
+    default:
+      return null;
+  }
+}
+
+export default UploadStatusIcon;
