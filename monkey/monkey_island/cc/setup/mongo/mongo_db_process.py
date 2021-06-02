3b958f5a6169555fc2d43c75e0b384c25a21e8c3
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-02 15:18:07 -0400

    island: Inline method _start_mongodb_process() in MongoDbProcess

--- monkey/monkey_island/cc/setup/mongo/mongo_db_process.py
+++ monkey/monkey_island/cc/setup/mongo/mongo_db_process.py
@@ -23,7 +23,17 @@ class MongoDbProcess:
         self._process = None
 
     def start(self):
-        self._start_mongodb_process()
+        logger.info("Starting MongoDb process.")
+
+        mongo_run_cmd = MongoDbProcess._build_mongo_launch_cmd(MONGO_EXECUTABLE_PATH, self._db_dir)
+        logger.info(f"Mongodb will be launched with command: {' '.join(mongo_run_cmd)}.")
+
+        mongo_log_path = os.path.join(self._logging_dir, MONGO_LOG_FILENAME)
+        logger.info(f"Mongodb log will be available at {mongo_log_path}.")
+
+        with open(mongo_log_path, "w") as log:
+            self._process = subprocess.Popen(mongo_run_cmd, stderr=subprocess.STDOUT, stdout=log)
+        logger.info("MongoDb launched successfully!")
 
     def stop(self):
         if self._process:
@@ -37,19 +47,6 @@ class MongoDbProcess:
                     f"MongoDB did not terminate gracefully and will be forcefully killed: {te}"
                 )
                 self._process.kill()
-
-    def _start_mongodb_process(self):
-        logger.info("Starting MongoDb process.")
-
-        mongo_run_cmd = MongoDbProcess._build_mongo_launch_cmd(MONGO_EXECUTABLE_PATH, self._db_dir)
-        logger.info(f"Mongodb will be launched with command: {' '.join(mongo_run_cmd)}.")
-
-        mongo_log_path = os.path.join(self._logging_dir, MONGO_LOG_FILENAME)
-        logger.info(f"Mongodb log will be available at {mongo_log_path}.")
-
-        with open(mongo_log_path, "w") as log:
-            self._process = subprocess.Popen(mongo_run_cmd, stderr=subprocess.STDOUT, stdout=log)
-        logger.info("MongoDb launched successfully!")
 
     @staticmethod
     def _build_mongo_launch_cmd(exec_path: str, db_dir: str) -> List[str]:
