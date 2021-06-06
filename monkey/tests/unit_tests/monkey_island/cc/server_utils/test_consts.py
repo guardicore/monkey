b0ac07553acfde9210e083512f6655cbf069e639
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-06 19:01:16 -0400

    island: Assert isdir() in test_monkey_island_abs_path()

--- monkey/tests/unit_tests/monkey_island/cc/server_utils/test_consts.py
+++ monkey/tests/unit_tests/monkey_island/cc/server_utils/test_consts.py
@@ -1,5 +1,8 @@
+import os
+
 from monkey_island.cc.server_utils import consts
 
 
 def test_monkey_island_abs_path():
     assert consts.MONKEY_ISLAND_ABS_PATH.endswith("monkey_island")
+    assert os.path.isdir(consts.MONKEY_ISLAND_ABS_PATH)
