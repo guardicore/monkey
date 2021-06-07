bf0fe10ea9a66551eedfb5ca9ec9f728b50eea8f
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-07 13:18:04 -0400

    island: Add unit tests for expand_path()

---
+++ monkey/tests/unit_tests/monkey_island/cc/server_utils/test_file_utils.py
@@ -0,0 +1,17 @@
+import os
+
+from monkey_island.cc.server_utils import file_utils
+
+
+def test_expand_user(patched_home_env):
+    input_path = os.path.join("~", "test")
+    expected_path = os.path.join(patched_home_env, "test")
+
+    assert file_utils.expand_path(input_path) == expected_path
+
+
+def test_expand_vars(patched_home_env):
+    input_path = os.path.join("$HOME", "test")
+    expected_path = os.path.join(patched_home_env, "test")
+
+    assert file_utils.expand_path(input_path) == expected_path
