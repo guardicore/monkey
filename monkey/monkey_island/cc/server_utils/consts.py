4e1b4fbf6b85f078109fcdd5d81844b405632300
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-07 13:21:16 -0400

    island: Replace calls to os.{expandpath,expandusers} with expand_path()

--- monkey/monkey_island/cc/server_utils/consts.py
+++ monkey/monkey_island/cc/server_utils/consts.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from monkey_island.cc.environment.utils import is_windows_os
+from monkey_island.cc.server_utils import file_utils
 
 __author__ = "itay.mizeretz"
 
@@ -25,7 +26,7 @@ SERVER_CONFIG_FILENAME = "server_config.json"
 
 MONKEY_ISLAND_ABS_PATH = _get_monkey_island_abs_path()
 
-DEFAULT_DATA_DIR = os.path.expandvars(get_default_data_dir())
+DEFAULT_DATA_DIR = file_utils.expand_path(get_default_data_dir())
 
 DEFAULT_MONKEY_TTL_EXPIRY_DURATION_IN_SECONDS = 60 * 5
 
@@ -36,7 +37,7 @@ MONGO_EXECUTABLE_PATH = (
     _MONGO_EXECUTABLE_PATH_WIN if is_windows_os() else _MONGO_EXECUTABLE_PATH_LINUX
 )
 
-DEFAULT_SERVER_CONFIG_PATH = os.path.expandvars(
+DEFAULT_SERVER_CONFIG_PATH = file_utils.expand_path(
     os.path.join(DEFAULT_DATA_DIR, SERVER_CONFIG_FILENAME)
 )
 
