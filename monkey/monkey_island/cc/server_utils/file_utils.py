937dbac4d04c5d4af13a176ebe9888a3054c8530
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-10 06:50:00 -0400

    island: Remove SSL permissions checks
    These checks prevent the docker container from working properly, as the
    default SSL cert must have at least 444 permissions.

--- monkey/monkey_island/cc/server_utils/file_utils.py
+++ monkey/monkey_island/cc/server_utils/file_utils.py
@@ -1,57 +1,5 @@
 import os
 
-from monkey_island.cc.environment.utils import is_windows_os
-
 
 def expand_path(path: str) -> str:
     return os.path.expandvars(os.path.expanduser(path))
-
-
-def has_expected_permissions(path: str, expected_permissions: int) -> bool:
-    if is_windows_os():
-        return _has_expected_windows_permissions(path, expected_permissions)
-
-    return _has_expected_linux_permissions(path, expected_permissions)
-
-
-def _has_expected_linux_permissions(path: str, expected_permissions: int) -> bool:
-    file_mode = os.stat(path).st_mode
-    file_permissions = file_mode & 0o777
-
-    return file_permissions == expected_permissions
-
-
-def _has_expected_windows_permissions(path: str, expected_permissions: int) -> bool:
-    import win32api  # noqa: E402
-    import win32security  # noqa: E402
-
-    FULL_CONTROL = 2032127
-    ACE_TYPE_ALLOW = 0
-    ACE_TYPE_DENY = 1
-
-    admins_sid, _, _ = win32security.LookupAccountName("", "Administrators")
-    user_sid, _, _ = win32security.LookupAccountName("", win32api.GetUserName())
-
-    security_descriptor = win32security.GetNamedSecurityInfo(
-        path, win32security.SE_FILE_OBJECT, win32security.DACL_SECURITY_INFORMATION
-    )
-
-    acl = security_descriptor.GetSecurityDescriptorDacl()
-
-    for i in range(acl.GetAceCount()):
-        ace = acl.GetAce(i)
-        ace_type, _ = ace[0]  # 0 for allow, 1 for deny
-        permissions = ace[1]
-        sid = ace[-1]
-
-        if sid == user_sid:
-            if not (permissions == expected_permissions and ace_type == ACE_TYPE_ALLOW):
-                return False
-        elif sid == admins_sid:
-            continue
-        # TODO: consider removing; so many system accounts/groups exist, it's likely to fail
-        else:
-            if not (permissions == FULL_CONTROL and ace_type == ACE_TYPE_DENY):
-                return False
-
-    return True
--- monkey/tests/unit_tests/monkey_island/cc/server_utils/test_file_utils.py
+++ monkey/tests/unit_tests/monkey_island/cc/server_utils/test_file_utils.py
@@ -1,7 +1,4 @@
 import os
-import subprocess
-
-import pytest
 
 from monkey_island.cc.server_utils import file_utils
 
@@ -18,35 +15,3 @@ def test_expand_vars(patched_home_env):
     expected_path = os.path.join(patched_home_env, "test")
 
     assert file_utils.expand_path(input_path) == expected_path
-
-
-@pytest.mark.skipif(os.name != "posix", reason="Tests Posix (not Windows) permissions.")
-def test_has_expected_permissions_true_linux(tmpdir, create_empty_tmp_file):
-    file_name = create_empty_tmp_file("test")
-    os.chmod(file_name, 0o754)
-
-    assert file_utils.has_expected_permissions(file_name, 0o754)
-
-
-@pytest.mark.skipif(os.name != "posix", reason="Tests Posix (not Windows) permissions.")
-def test_has_expected_permissions_false_linux(tmpdir, create_empty_tmp_file):
-    file_name = create_empty_tmp_file("test")
-    os.chmod(file_name, 0o755)
-
-    assert not file_utils.has_expected_permissions(file_name, 0o700)
-
-
-@pytest.mark.skipif(os.name == "posix", reason="Tests Windows (not Posix) permissions.")
-def test_has_expected_permissions_true_windows(tmpdir, create_empty_tmp_file):
-    file_name = create_empty_tmp_file("test")
-    subprocess.run(f"echo y| cacls {file_name} /p %USERNAME%:F", shell=True)  # noqa: DUO116
-
-    assert file_utils.has_expected_permissions(file_name, 2032127)
-
-
-@pytest.mark.skipif(os.name == "posix", reason="Tests Windows (not Posix) permissions.")
-def test_has_expected_permissions_false_windows(tmpdir, create_empty_tmp_file):
-    file_name = create_empty_tmp_file("test")
-    subprocess.run(f"echo y| cacls {file_name} /p %USERNAME%:R", shell=True)  # noqa: DUO116
-
-    assert not file_utils.has_expected_permissions(file_name, 2032127)
