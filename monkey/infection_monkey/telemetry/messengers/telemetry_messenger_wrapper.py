6773f695ba88bbd514542c84059374fb561c9740
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-06-24 15:57:10 -0400

    agent: Use ITelem in send_telemetry() typehint

--- monkey/infection_monkey/telemetry/messengers/telemetry_messenger_wrapper.py
+++ monkey/infection_monkey/telemetry/messengers/telemetry_messenger_wrapper.py
@@ -1,7 +1,7 @@
-from infection_monkey.telemetry.base_telem import BaseTelem
+from infection_monkey.telemetry.i_telem import ITelem
 from infection_monkey.telemetry.messengers.i_telemetry_messenger import ITelemetryMessenger
 
 
 class TelemetryMessengerWrapper(ITelemetryMessenger):
-    def send_telemetry(self, telemetry: BaseTelem):
+    def send_telemetry(self, telemetry: ITelem):
         telemetry.send()
