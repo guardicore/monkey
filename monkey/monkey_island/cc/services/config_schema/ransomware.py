13a94804b44150877ab2b6d195478c4427acaaaf
Shreya <shreya.malviya@gmail.com>
2021-06-29 16:02:20 +0530

    cc: Add checkbox for ransomware encryption

--- monkey/monkey_island/cc/services/config_schema/ransomware.py
+++ monkey/monkey_island/cc/services/config_schema/ransomware.py
@@ -2,23 +2,36 @@ RANSOMWARE = {
     "title": "Ransomware",
     "type": "object",
     "properties": {
-        "directories": {
-            "title": "Directories to encrypt",
+        "encryption": {
+            "title": "Encryption",
             "type": "object",
             "properties": {
-                "linux_dir": {
-                    "title": "Linux encryptable directory",
-                    "type": "string",
-                    "default": "",
-                    "description": "Files in the specified directory will be encrypted "
-                    "using bitflip to simulate ransomware.",
+                "should_encrypt": {
+                    "title": "Encrypt files",
+                    "type": "boolean",
+                    "default": True,
+                    "description": "Selected files will be encrypted using bitflip to simulate "
+                    "ransomware. Enter target directories below.",
                 },
-                "windows_dir": {
-                    "title": "Windows encryptable directory",
-                    "type": "string",
-                    "default": "",
-                    "description": "Files in the specified directory will be encrypted "
-                    "using bitflip to simulate ransomware.",
+                "directories": {
+                    "title": "Directories to encrypt",
+                    "type": "object",
+                    "properties": {
+                        "linux_dir": {
+                            "title": "Linux encryptable directory",
+                            "type": "string",
+                            "default": "",
+                            "description": "Files in the specified directory will be encrypted "
+                            "using bitflip to simulate ransomware.",
+                        },
+                        "windows_dir": {
+                            "title": "Windows encryptable directory",
+                            "type": "string",
+                            "default": "",
+                            "description": "Files in the specified directory will be encrypted "
+                            "using bitflip to simulate ransomware.",
+                        },
+                    },
                 },
             },
         },
