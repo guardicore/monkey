5a7778516461890fde470c66e81688917179f6ff
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-07-06 11:56:25 -0400

    Tests: Remove Path -> str implicit conversion warning

--- monkey/tests/unit_tests/conftest.py
+++ monkey/tests/unit_tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.fixture
 def patched_home_env(monkeypatch, tmp_path):
-    monkeypatch.setenv("HOME", tmp_path)
+    monkeypatch.setenv("HOME", str(tmp_path))
 
     return tmp_path
