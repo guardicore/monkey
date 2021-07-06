a512fd947acca1edd20584f4b357edb190d97426
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-07-06 11:31:12 -0400

    Tests: Return Path object from patched_home_env() fixture

--- monkey/tests/unit_tests/conftest.py
+++ monkey/tests/unit_tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture
-def patched_home_env(monkeypatch, tmpdir):
-    monkeypatch.setenv("HOME", str(tmpdir))
+def patched_home_env(monkeypatch, tmp_path):
+    monkeypatch.setenv("HOME", tmp_path)
 
-    return tmpdir
+    return tmp_path
