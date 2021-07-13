1f1b9bf2fcca6fadc565c05b6ad978daaa847670
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-07-13 11:21:56 -0400

    Island: Deduplicate <p> in renderFileEncryptionStats()

--- monkey/monkey_island/cc/ui/src/components/report-components/common/RenderFileEncryptionStats.tsx
+++ monkey/monkey_island/cc/ui/src/components/report-components/common/RenderFileEncryptionStats.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 
 function renderFileEncryptionStats(successful: number, total: number) {
-  if(successful > 0){
-    return (<p className={"text-success"}>{successful} out of {total}</p>);
+  let textClassName = ""
+  if(successful > 0) {
+    textClassName = "text-success"
   } else {
-    return (<p className={"text-danger"}>{successful} out of {total}</p>);
+    textClassName = "text-danger"
   }
+  
+  return (<p className={textClassName}>{successful} out of {total}</p>);
 }
 
 export default renderFileEncryptionStats;
