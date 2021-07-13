9310463f44886a72c542452c6f10214177892ed0
Ilija Lazoroski <ilija.la@live.com>
2021-07-13 12:27:00 +0200

    UT: Refactor island mode test for set model

--- monkey/tests/unit_tests/monkey_island/cc/resources/test_island_mode.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_island_mode.py
@@ -5,6 +5,11 @@ import pytest
 from monkey_island.cc.models.island_mode_model import IslandMode
 
 
+@pytest.fixture(scope="function")
+def uses_database():
+    IslandMode.objects().delete()
+
+
 def test_island_mode_post(flask_client):
     resp = flask_client.post(
         "/api/island-mode", data=json.dumps({"mode": "ransomware"}), follow_redirects=True
@@ -19,16 +24,14 @@ def test_island_mode_post__invalid_mode(flask_client):
     assert resp.status_code == 404
 
 
-@pytest.mark.usefixtures("uses_database")
-def test_island_mode_post__set_model(flask_client):
+def test_island_mode_post__set_model(flask_client, uses_database):
     flask_client.post(
         "/api/island-mode", data=json.dumps({"mode": "ransomware"}), follow_redirects=True
     )
     assert IslandMode.objects[0].mode == "ransomware"
 
 
-@pytest.mark.usefixtures("uses_database")
-def test_island_mode_post__set_invalid_model(flask_client):
+def test_island_mode_post__set_invalid_model(flask_client, uses_database):
     flask_client.post(
         "/api/island-mode", data=json.dumps({"mode": "bogus mode"}), follow_redirects=True
     )
