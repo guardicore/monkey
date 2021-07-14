b03c6d9f92b2420710ef7c050ad14dd6fa0cd658
VakarisZ <vakarisz@yahoo.com>
2021-07-14 12:44:16 +0300

    Island: Add LandingPage.tsx and DisabledSidebarLayoutComponent.tsx

---
+++ monkey/monkey_island/cc/ui/src/components/layouts/DisabledSidebarLayoutComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import SideNavComponent from '../SideNavComponent.tsx'
+import {Col, Row} from 'react-bootstrap';
+
+export const DisabledSidebarLayoutComponent = ({component: Component, ...rest}) => (
+  <Route {...rest} render={() => (
+    <Row>
+      <Col sm={3} md={3} lg={3} xl={2} className='sidebar'>
+        <SideNavComponent disabled={true} completedSteps={rest['completedSteps']}/>
+      </Col>
+      <Component {...rest} />
+    </Row>
+    )}/>
+)
