7b79c297b9a3616b1b8fd92f537dff42e4056b71
Shreya <shreya.malviya@gmail.com>
2021-07-20 16:31:28 +0530

    island: Replace config "filter" for "manipulator" as file names/constants/functions

--- monkey/monkey_island/cc/services/config_manipulator.py
+++ monkey/monkey_island/cc/services/config_manipulator.py
@@ -3,7 +3,7 @@ from typing import Dict
 import dpath.util
 
 import monkey_island.cc.services.config as config_service
-from monkey_island.cc.services.config_filters import FILTER_PER_MODE
+from monkey_island.cc.services.config_manipulators import MANIPULATOR_PER_MODE
 from monkey_island.cc.services.mode.mode_enum import IslandModeEnum
 
 
@@ -20,12 +20,12 @@ def update_config_per_mode(mode: str, config: Dict, should_encrypt: bool) -> boo
 
 
 def _set_default_config_values_per_mode(mode: str, config: Dict) -> Dict:
-    config_filter = FILTER_PER_MODE[mode]
-    config = _apply_config_filter(config, config_filter)
+    config_manipulator = MANIPULATOR_PER_MODE[mode]
+    config = _apply_config_manipulator(config, config_manipulator)
     return config
 
 
-def _apply_config_filter(config: Dict, config_filter: Dict):
-    for path, value in config_filter.items():
+def _apply_config_manipulator(config: Dict, config_manipulator: Dict):
+    for path, value in config_manipulator.items():
         dpath.util.set(config, path, value, ".")
     return config
