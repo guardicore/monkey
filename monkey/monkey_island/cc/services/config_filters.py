fab77381744a183584deded75f27795ee9a7cdb3
Shreya <shreya.malviya@gmail.com>
2021-07-20 13:17:30 +0530

    island: Add config filter for advanced mode

--- monkey/monkey_island/cc/services/config_filters.py
+++ monkey/monkey_island/cc/services/config_filters.py
@@ -1,3 +1,6 @@
 from monkey_island.cc.services.mode.mode_enum import IslandModeEnum
 
-FILTER_PER_MODE = {IslandModeEnum.RANSOMWARE.value: {"monkey.post_breach.post_breach_actions": []}}
+FILTER_PER_MODE = {
+    IslandModeEnum.ADVANCED.value: {},
+    IslandModeEnum.RANSOMWARE.value: {"monkey.post_breach.post_breach_actions": []},
+}
