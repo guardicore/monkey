0f84cc19c1fb7973676c85c449f925153b2dc72a
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-07-23 11:14:55 -0400

    Island: Remove "encrypted_files_table" from ransomware report endpoint

--- monkey/monkey_island/cc/resources/ransomware_report.py
+++ monkey/monkey_island/cc/resources/ransomware_report.py
@@ -8,10 +8,8 @@ from monkey_island.cc.services.ransomware import ransomware_report
 class RansomwareReport(flask_restful.Resource):
     @jwt_required
     def get(self):
-        encrypted_files_table = ransomware_report.get_encrypted_files_table()
         return jsonify(
             {
-                "encrypted_files_table": encrypted_files_table,
                 "propagation_stats": ransomware_report.get_propagation_stats(),
             }
         )
