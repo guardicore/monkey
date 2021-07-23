52207c15b80ef2bc88a1ebe5a0f4cc4770254a11
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-07-23 06:56:46 -0400

    UI: Add lateral movement section to ransomware report

--- monkey/monkey_island/cc/ui/src/components/report-components/RansomwareReport.js
+++ monkey/monkey_island/cc/ui/src/components/report-components/RansomwareReport.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import ReportHeader, {ReportTypes} from './common/ReportHeader';
 import ReportLoader from './common/ReportLoader';
-import pluralize from 'pluralize'
 import FileEncryptionTable from './ransomware/FileEncryptionTable';
+import LateralMovement from './ransomware/LateralMovement';
 
 import '../../styles/pages/report/RansomwareReport.scss';
 
@@ -16,53 +16,10 @@ class RansomwareReport extends React.Component {
   generateReportContent() {
     return (
       <div>
-        {this.getExploitationStats()}
+        <LateralMovement propagationStats={this.props.report.propagation_stats} />
         <FileEncryptionTable tableData={this.props.report.encrypted_files_table} />
       </div>
     )
-  }
-
-  getExploitationStats() {
-    return (
-      <div>
-        <h3 className={'report-section-header'}>
-          Propagation
-        </h3>
-        {this.getScannedVsExploitedStats()}
-        {this.getExploitationStatsPerExploit()}
-      </div>
-    )
-  }
-
-  getScannedVsExploitedStats() {
-    let num_scanned = this.props.report.propagation_stats.num_scanned_nodes;
-    let num_exploited = this.props.report.propagation_stats.num_exploited_nodes;
-
-    return(
-      <p>
-        The Monkey discovered <span className='badge badge-warning'>{num_scanned}</span> machines
-        and successfully breached <span className='badge badge-danger'>{num_exploited}</span> of them.
-      </p>
-    )
-  }
-
-  getExploitationStatsPerExploit() {
-    let exploit_counts = this.props.report.propagation_stats.num_exploited_per_exploit;
-
-    let exploitation_details = [];
-
-    for (let exploit in exploit_counts) {
-      let count = exploit_counts[exploit];
-      exploitation_details.push(
-        <div>
-          <span className='badge badge-danger'>{count}</span>&nbsp;
-          {pluralize('machine', count)} {pluralize('was', count)} exploited by the&nbsp;
-          <span className='badge badge-danger'>{exploit}</span>.
-        </div>
-      );
-    }
-
-    return exploitation_details;
   }
 
   render() {
