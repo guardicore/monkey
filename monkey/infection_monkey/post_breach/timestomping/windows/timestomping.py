536b061cc7592380490b9070f58097f52591324e
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-08-23 11:10:57 -0400

    Agent: Remove unused TEMP_FILE constant from windows timestomping PBA

--- monkey/infection_monkey/post_breach/timestomping/windows/timestomping.py
+++ monkey/infection_monkey/post_breach/timestomping/windows/timestomping.py
@@ -1,6 +1,3 @@
-TEMP_FILE = "monkey-timestomping-file.txt"
-
-
 def get_windows_timestomping_commands():
     return "powershell.exe infection_monkey/post_breach/timestomping/windows/timestomping.ps1"
 
