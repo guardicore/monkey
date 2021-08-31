86fd7351adbf458dbe9b103e411044e9a9973260
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-08-31 09:57:32 -0400

    Island: Remove unused loggers

--- monkey/monkey_island/cc/resources/pba_file_upload.py
+++ monkey/monkey_island/cc/resources/pba_file_upload.py
@@ -1,5 +1,4 @@
 import copy
-import logging
 
 import flask_restful
 from flask import Response, request, send_from_directory
@@ -11,7 +10,6 @@ from monkey_island.cc.resources.auth.auth import jwt_required
 from monkey_island.cc.services.config import ConfigService
 from monkey_island.cc.services.post_breach_files import PostBreachFilesService
 
-logger = logging.getLogger(__name__)
 # Front end uses these strings to identify which files to work with (linux or windows)
 LINUX_PBA_TYPE = "PBAlinux"
 WINDOWS_PBA_TYPE = "PBAwindows"
