2fd38061b26aa2321add9f6a87497dc4b97dca6c
Ilija Lazoroski <ilija.la@live.com>
2021-09-10 14:01:58 +0200

    UT: Add unit tests for pba_upload

--- monkey/monkey_island/cc/resources/pba_file_upload.py
+++ monkey/monkey_island/cc/resources/pba_file_upload.py
@@ -27,6 +27,9 @@ class FileUpload(flask_restful.Resource):
         :param file_type: Type indicates which file to send, linux or windows
         :return: Returns file contents
         """
+        if self.check_file_type(file_type):
+            return Response(status=422, mimetype="text/plain")
+
         # Verify that file_name is indeed a file from config
         if file_type == LINUX_PBA_TYPE:
             filename = ConfigService.get_config_value(copy.deepcopy(PBA_LINUX_FILENAME_PATH))
@@ -41,6 +44,9 @@ class FileUpload(flask_restful.Resource):
         :param file_type: Type indicates which file was received, linux or windows
         :return: Returns flask response object with uploaded file's filename
         """
+        if self.check_file_type(file_type):
+            return Response(status=422, mimetype="text/plain")
+
         filename = FileUpload.upload_pba_file(
             request.files["filepond"], (file_type == LINUX_PBA_TYPE)
         )
@@ -74,6 +80,9 @@ class FileUpload(flask_restful.Resource):
         :param file_type: Type indicates which file was deleted, linux of windows
         :return: Empty response
         """
+        if self.check_file_type(file_type):
+            return Response(status=422, mimetype="text/plain")
+
         filename_path = (
             PBA_LINUX_FILENAME_PATH if file_type == "PBAlinux" else PBA_WINDOWS_FILENAME_PATH
         )
@@ -83,3 +92,12 @@ class FileUpload(flask_restful.Resource):
             ConfigService.set_config_value(filename_path, "")
 
         return {}
+
+    @staticmethod
+    def check_file_type(file_type):
+        """
+        Check if the file type is not supported
+        :param file_type: Type indicates which file was received, linux or windows
+        :return: Boolean
+        """
+        return file_type not in {LINUX_PBA_TYPE, WINDOWS_PBA_TYPE}
