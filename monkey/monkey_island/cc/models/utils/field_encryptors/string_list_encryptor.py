627a31c902a2a34e07591acd94b3de7f18c32df1
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-09-21 13:56:19 -0400

    Island: Remove string_encryptor.py

--- monkey/monkey_island/cc/models/utils/field_encryptors/string_list_encryptor.py
+++ monkey/monkey_island/cc/models/utils/field_encryptors/string_list_encryptor.py
@@ -1,14 +1,14 @@
 from typing import List
 
 from monkey_island.cc.models.utils.field_encryptors.i_field_encryptor import IFieldEncryptor
-from monkey_island.cc.server_utils.encryption import string_encryptor
+from monkey_island.cc.server_utils.encryptor import get_encryptor
 
 
 class StringListEncryptor(IFieldEncryptor):
     @staticmethod
     def encrypt(value: List[str]):
-        return [string_encryptor.encrypt(string) for string in value]
+        return [get_encryptor().enc(string) for string in value]
 
     @staticmethod
     def decrypt(value: List[str]):
-        return [string_encryptor.decrypt(string) for string in value]
+        return [get_encryptor().dec(string) for string in value]
