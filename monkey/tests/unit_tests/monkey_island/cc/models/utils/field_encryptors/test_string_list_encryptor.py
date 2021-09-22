67b23c42bfac2baf967ef38dceb8989b9972d547
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-09-22 07:44:54 -0400

    Tests: Simplify test names in test_string_list_encryptor.py

--- monkey/tests/unit_tests/monkey_island/cc/models/utils/field_encryptors/test_string_list_encryptor.py
+++ monkey/tests/unit_tests/monkey_island/cc/models/utils/field_encryptors/test_string_list_encryptor.py
@@ -12,13 +12,14 @@ def uses_encryptor(data_for_tests_dir):
     initialize_encryptor(data_for_tests_dir)
 
 
-def test_string_list_encryptor(uses_encryptor):
+def test_encryption_and_decryption(uses_encryptor):
     encrypted_list = StringListEncryptor.encrypt(MOCK_STRING_LIST)
     assert not encrypted_list == MOCK_STRING_LIST
     decrypted_list = StringListEncryptor.decrypt(encrypted_list)
     assert decrypted_list == MOCK_STRING_LIST
 
 
-def test_string_list_encryptor__empty_list(uses_encryptor):
+def test_empty_list(uses_encryptor):
+    # Tests that no errors are raised
     encrypted_list = StringListEncryptor.encrypt(EMPTY_LIST)
     StringListEncryptor.decrypt(encrypted_list)
