1996387cc587980720a4cd8b9b83d998265b2eb5
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-09-23 13:39:48 -0400

    Remove unnecessary # noqa: E402 from __init__.py files

--- monkey/monkey_island/cc/models/__init__.py
+++ monkey/monkey_island/cc/models/__init__.py
@@ -1,10 +1,10 @@
-from .command_control_channel import CommandControlChannel  # noqa: E402
+from .command_control_channel import CommandControlChannel
 
 # Order of importing matters here, for registering the embedded and referenced documents before
 # using them.
-from .config import Config  # noqa: E402
-from .creds import Creds  # noqa: E402
-from .monkey import Monkey  # noqa: E402
-from .monkey_ttl import MonkeyTtl  # noqa: E402
-from .pba_results import PbaResults  # noqa: E402
-from .report import Report  # noqa: E402
+from .config import Config
+from .creds import Creds
+from .monkey import Monkey
+from .monkey_ttl import MonkeyTtl
+from .pba_results import PbaResults
+from .report import Report
