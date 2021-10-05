e7fcf933b7912f039c3bd271516d5d3d7889f6ae
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-10-05 12:12:38 -0400

    Island: Remove try/except from MimikatzResultsEncryptor.encrypt()
    Catching this exception was a workaround for an issue that was resolved
    in PR #1508.

--- monkey/monkey_island/cc/server_utils/encryption/dict_encryption/field_encryptors/mimikatz_results_encryptor.py
+++ monkey/monkey_island/cc/server_utils/encryption/dict_encryption/field_encryptors/mimikatz_results_encryptor.py
@@ -14,16 +14,9 @@ class MimikatzResultsEncryptor(IFieldEncryptor):
     def encrypt(results: dict) -> dict:
         for _, credentials in results.items():
             for secret_type in MimikatzResultsEncryptor.secret_types:
-                try:
-                    credentials[secret_type] = get_datastore_encryptor().encrypt(
-                        credentials[secret_type]
-                    )
-                except ValueError as e:
-                    logger.error(
-                        f"Failed encrypting sensitive field for "
-                        f"user {credentials['username']}! Error: {e}"
-                    )
-                    credentials[secret_type] = get_datastore_encryptor().encrypt("")
+                credentials[secret_type] = get_datastore_encryptor().encrypt(
+                    credentials[secret_type]
+                )
         return results
 
     @staticmethod
