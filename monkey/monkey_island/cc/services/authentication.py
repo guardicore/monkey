a5d3c218b4d1b17fba49e9904d302f34fa74f75d
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-10-06 14:10:38 -0400

    Island: Move registration logic into AuthenticationService
    Resoures should be kept relatively thin and serve as the layer between
    the API and the backend.

--- monkey/monkey_island/cc/services/authentication.py
+++ monkey/monkey_island/cc/services/authentication.py
@@ -1,7 +1,12 @@
+import bcrypt
+
+import monkey_island.cc.environment.environment_singleton as env_singleton
+from monkey_island.cc.environment.user_creds import UserCreds
 from monkey_island.cc.server_utils.encryption import (
     reset_datastore_encryptor,
     unlock_datastore_encryptor,
 )
+from monkey_island.cc.setup.mongo.database_initializer import reset_database
 
 
 class AuthenticationService:
@@ -13,6 +18,13 @@ class AuthenticationService:
     @classmethod
     def initialize(cls, key_file_directory):
         cls.KEY_FILE_DIRECTORY = key_file_directory
+
+    @classmethod
+    def register_new_user(cls, username: str, password: str):
+        credentials = UserCreds(username, _hash_password(password))
+        env_singleton.env.try_add_user(credentials)
+        AuthenticationService.reset_datastore_encryptor(username, password)
+        reset_database()
 
     @staticmethod
     def unlock_datastore_encryptor(username: str, password: str):
@@ -27,3 +39,10 @@ class AuthenticationService:
     @staticmethod
     def _get_secret_from_credentials(username: str, password: str) -> str:
         return f"{username}:{password}"
+
+
+def _hash_password(plaintext_password):
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(plaintext_password.encode("utf-8"), salt)
+
+    return password_hash.decode()
