7bdbdb1bfbbeaaa2118d748384b4754a208da7d0
Shreya Malviya <shreya.malviya@gmail.com>
2021-10-12 15:42:40 +0530

    island: Go through internal config when generating reverse schema for unscanned attack techniques' reasons

--- monkey/monkey_island/cc/services/config_schema/config_schema_per_attack_technique.py
+++ monkey/monkey_island/cc/services/config_schema/config_schema_per_attack_technique.py
@@ -25,6 +25,21 @@ def get_config_schema_per_attack_technique(schema: Dict) -> Dict[str, Dict[str, 
                     definition_type, config_field, attack_technique, reverse_schema
                 )
 
+    properties = schema["properties"]
+    for prop in properties:
+        property_type = properties[prop]["title"]
+        for tab_name in properties[prop]["properties"]:
+            tab = properties[prop]["properties"][tab_name]
+            for config_option_name in tab["properties"]:
+                config_option = tab["properties"][config_option_name]
+                for attack_technique in config_option.get("related_attack_techniques", []):
+                    # No config values could be a reason that related attack techniques are left
+                    # unscanned. See https://github.com/guardicore/monkey/issues/1518 for more.
+                    config_field = f"{config_option['title']} ({tab['title']})"
+                    _add_config_field_to_reverse_schema(
+                        property_type, config_field, attack_technique, reverse_schema
+                    )
+
     return reverse_schema
 
 
