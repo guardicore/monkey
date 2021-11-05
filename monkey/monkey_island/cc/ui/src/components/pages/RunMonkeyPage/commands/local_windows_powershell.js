896cf7a21dd002eccc92f31934efaaf6235fabfd
VakarisZ <vakarisz@yahoo.com>
2021-11-05 09:57:20 +0200

    UI: remove -noexit flag from manual run command, because there's no point in opening an empty powershell window

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
@@ -4,7 +4,7 @@ import {OS_TYPES} from '../utils/OsTypes';
 export function getAgentDownloadCommand(ip, osType) {
   let bitText = osType === OS_TYPES.WINDOWS_32 ? '32' : '64';
   return `$execCmd = @"\r\n`
-    + `-noexit [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {\`$true};`
+    + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {\`$true};`
     + `(New-Object System.Net.WebClient).DownloadFile('https://${ip}:5000/api/monkey/download/monkey-windows-${bitText}.exe',`
     + `"""$env:TEMP\\monkey.exe""");Start-Process -FilePath '$env:TEMP\\monkey.exe' -ArgumentList 'm0nk3y -s ${ip}:5000';`
     + `\r\n"@; \r\n`
