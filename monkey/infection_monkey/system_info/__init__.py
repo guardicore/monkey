a4f9aa1dfa6f63c5b8ae628d7cf73675e142a15c
VakarisZ <vakarisz@yahoo.com>
2021-11-15 12:48:40 +0200

    Agent, Common: remove azure credential collector

--- monkey/infection_monkey/system_info/__init__.py
+++ monkey/infection_monkey/system_info/__init__.py
@@ -4,9 +4,7 @@ from enum import IntEnum
 
 import psutil
 
-from common.common_consts.system_info_collectors_names import AZURE_CRED_COLLECTOR
 from infection_monkey.network.info import get_host_subnets
-from infection_monkey.system_info.azure_cred_collector import AzureCollector
 from infection_monkey.system_info.system_info_collectors_handler import SystemInfoCollectorsHandler
 
 logger = logging.getLogger(__name__)
@@ -63,7 +61,6 @@ class InfoCollector(object):
     def get_info(self):
         # Collect all hardcoded
         self.get_network_info()
-        self.get_azure_info()
 
         # Collect all plugins
         SystemInfoCollectorsHandler().execute_all_configured()
@@ -77,35 +74,3 @@ class InfoCollector(object):
         """
         logger.debug("Reading subnets")
         self.info["network_info"] = {"networks": get_host_subnets()}
-
-    def get_azure_info(self):
-        """
-        Adds credentials possibly stolen from an Azure VM instance (if we're on one)
-        Updates the credentials structure, creating it if necessary (compat with mimikatz)
-        :return: None. Updates class information
-        """
-        # noinspection PyBroadException
-        try:
-            from infection_monkey.config import WormConfiguration
-
-            if AZURE_CRED_COLLECTOR not in WormConfiguration.system_info_collector_classes:
-                return
-            logger.debug("Harvesting creds if on an Azure machine")
-            azure_collector = AzureCollector()
-            azure_creds = azure_collector.extract_stored_credentials()
-            for cred in azure_creds:
-                username = cred[0]
-                password = cred[1]
-                if username not in self.info["credentials"]:
-                    self.info["credentials"][username] = {}
-                # we might be losing passwords in case of multiple reset attempts on same username
-                # or in case another collector already filled in a password for this user
-                self.info["credentials"][username]["password"] = password
-                self.info["credentials"][username]["username"] = username
-            if len(azure_creds) != 0:
-                self.info["Azure"] = {}
-                self.info["Azure"]["usernames"] = [cred[0] for cred in azure_creds]
-        except Exception:
-            # If we failed to collect azure info, no reason to fail all the collection. Log and
-            # continue.
-            logger.error("Failed collecting Azure info.", exc_info=True)
