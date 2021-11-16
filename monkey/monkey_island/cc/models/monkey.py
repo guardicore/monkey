90a063c47cf2cdcbe75a0e6322d1874e0bca55c4
VakarisZ <vakarisz@yahoo.com>
2021-11-16 11:57:46 +0200

    Island: remove environment property from monkey model because it's unused

--- monkey/monkey_island/cc/models/monkey.py
+++ monkey/monkey_island/cc/models/monkey.py
@@ -55,14 +55,8 @@ class Monkey(Document):
     tunnel = ReferenceField("self")
     command_control_channel = EmbeddedDocumentField(CommandControlChannel)
 
-    # Environment related fields
-    environment = StringField(
-        default=environment_names.Environment.UNKNOWN.value,
-        choices=environment_names.ALL_ENVIRONMENTS_NAMES,
-    )
-    aws_instance_id = StringField(
-        required=False
-    )  # This field only exists when the monkey is running on an AWS
+    # This field only exists when the monkey is running on an AWS
+    aws_instance_id = StringField(required=False)
 
     # instance. See https://github.com/guardicore/monkey/issues/426.
 
