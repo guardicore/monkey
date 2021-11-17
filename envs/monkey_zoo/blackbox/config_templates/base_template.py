86f2a0dc784b62107650472f9da1dc2c0179b5ea
VakarisZ <vakarisz@yahoo.com>
2021-11-17 12:59:34 +0200

    BB: remove deleted system info collectors from config templates

--- envs/monkey_zoo/blackbox/config_templates/base_template.py
+++ envs/monkey_zoo/blackbox/config_templates/base_template.py
@@ -9,10 +9,7 @@ class BaseTemplate(ConfigTemplate):
         "basic_network.scope.local_network_scan": False,
         "basic_network.scope.depth": 1,
         "internal.classes.finger_classes": ["PingScanner", "HTTPFinger"],
-        "internal.monkey.system_info.system_info_collector_classes": [
-            "EnvironmentCollector",
-            "HostnameCollector",
-        ],
+        "internal.monkey.system_info.system_info_collector_classes": [],
         "monkey.post_breach.post_breach_actions": [],
         "internal.general.keep_tunnel_open_time": 0,
     }
