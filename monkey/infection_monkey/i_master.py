082f034d58eff19161364b6b997def7793afe66a
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-11-22 13:02:09 -0500

    Agent: Change the method docstrings for IMaster

--- monkey/infection_monkey/i_master.py
+++ monkey/infection_monkey/i_master.py
@@ -5,23 +5,18 @@ class IMaster(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def start(self) -> None:
         """
-        With the help of the puppet, starts and instructs the Agent to
-        perform various actions like scanning or exploiting a specific host.
+        Run the control logic that will instruct the Puppet to perform various actions like scanning
+        or exploiting a specific host.
         """
-        pass
 
     @abc.abstractmethod
     def terminate(self) -> None:
         """
-        Effectively marks the Agent as dead, telling all actions being
-        performed by the Agent to stop.
+        Stop the master and interrupt any actions that are currently being executed.
         """
-        pass
 
     @abc.abstractmethod
     def cleanup(self) -> None:
         """
-        With the help of the puppet, instructs the Agent to cleanup whatever
-        is required since the Agent was killed.
+        Revert any changes that the master has directly or indirectly caused to the system.
         """
-        pass
