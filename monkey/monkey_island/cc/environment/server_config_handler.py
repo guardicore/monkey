7e21635733ddbca5dd91b082692c15a537ed7720
VakarisZ <vakarisz@yahoo.com>
2021-11-25 12:11:09 +0200

    Island: separate the workflow of config extraction from data directory setup

--- monkey/monkey_island/cc/environment/server_config_handler.py
+++ monkey/monkey_island/cc/environment/server_config_handler.py
@@ -1,22 +1,6 @@
 import json
-import os
-from pathlib import Path
 
-from monkey_island.cc.server_utils.consts import DEFAULT_SERVER_CONFIG_PATH, SERVER_CONFIG_FILENAME
 from monkey_island.cc.setup.island_config_options import IslandConfigOptions
-
-
-def create_default_server_config_file(data_dir: str) -> str:
-    config_file_path = os.path.join(data_dir, SERVER_CONFIG_FILENAME)
-    if not os.path.isfile(config_file_path):
-        write_default_server_config_to_file(config_file_path)
-
-    return config_file_path
-
-
-def write_default_server_config_to_file(path: str) -> None:
-    default_config = Path(DEFAULT_SERVER_CONFIG_PATH).read_text()
-    Path(path).write_text(default_config)
 
 
 def load_server_config_from_file(server_config_path) -> IslandConfigOptions:
--- monkey/monkey_island/cc/server_setup.py
+++ monkey/monkey_island/cc/server_setup.py
@@ -5,12 +5,12 @@ import sys
 from pathlib import Path
 from sys import exit
 from threading import Thread
-from typing import Tuple
 
 import gevent.hub
 from gevent.pywsgi import WSGIServer
 
 from monkey_island.cc.server_utils.consts import ISLAND_PORT
+from monkey_island.cc.setup.config_setup import extract_server_config
 
 # Add the monkey_island directory to the path, to make sure imports that don't start with
 # "monkey_island." work.
@@ -18,7 +18,6 @@ MONKEY_ISLAND_DIR_BASE_PATH = str(Path(__file__).parent.parent)
 if str(MONKEY_ISLAND_DIR_BASE_PATH) not in sys.path:
     sys.path.insert(0, MONKEY_ISLAND_DIR_BASE_PATH)
 
-import monkey_island.cc.setup.config_setup as config_setup  # noqa: E402
 from common.version import get_version  # noqa: E402
 from monkey_island.cc.app import init_app  # noqa: E402
 from monkey_island.cc.arg_parser import IslandCmdArgs  # noqa: E402
@@ -34,7 +33,7 @@ from monkey_island.cc.services.initialize import initialize_services  # noqa: E4
 from monkey_island.cc.services.reporting.exporter_init import populate_exporter_list  # noqa: E402
 from monkey_island.cc.services.utils.network_utils import local_ip_addresses  # noqa: E402
 from monkey_island.cc.setup import island_config_options_validator  # noqa: E402
-from monkey_island.cc.setup.data_dir import IncompatibleDataDirectory  # noqa: E402
+from monkey_island.cc.setup.data_dir import IncompatibleDataDirectory, setup_data_dir  # noqa: E402
 from monkey_island.cc.setup.gevent_hub_error_handler import GeventHubErrorHandler  # noqa: E402
 from monkey_island.cc.setup.island_config_options import IslandConfigOptions  # noqa: E402
 from monkey_island.cc.setup.mongo import mongo_setup  # noqa: E402
@@ -45,12 +44,13 @@ logger = logging.getLogger(__name__)
 
 def run_monkey_island():
     island_args = parse_cli_args()
-    config_options, server_config_path = _setup_data_dir(island_args)
+    config_options = _extract_config(island_args)
+    _setup_data_dir(config_options.data_dir)
 
     _exit_on_invalid_config_options(config_options)
 
     _configure_logging(config_options)
-    _initialize_globals(config_options, server_config_path)
+    _initialize_globals(config_options.data_dir)
 
     mongo_db_process = None
     if config_options.start_mongodb:
@@ -58,19 +58,24 @@ def run_monkey_island():
 
     _connect_to_mongodb(mongo_db_process)
 
-    _configure_gevent_exception_handling(Path(config_options.data_dir))
+    _configure_gevent_exception_handling(config_options.data_dir)
     _start_island_server(island_args.setup_only, config_options)
 
 
-def _setup_data_dir(island_args: IslandCmdArgs) -> Tuple[IslandConfigOptions, str]:
+def _extract_config(island_args: IslandCmdArgs) -> IslandConfigOptions:
     try:
-        return config_setup.setup_server_config(island_args)
+        return extract_server_config(island_args)
     except OSError as ex:
         print(f"Error opening server config file: {ex}")
         exit(1)
     except json.JSONDecodeError as ex:
         print(f"Error loading server config: {ex}")
         exit(1)
+
+
+def _setup_data_dir(data_dir_path: Path):
+    try:
+        setup_data_dir(data_dir_path)
     except IncompatibleDataDirectory as ex:
         print(f"Incompatible data directory: {ex}")
         exit(1)
@@ -89,8 +94,8 @@ def _configure_logging(config_options):
     setup_logging(config_options.data_dir, config_options.log_level)
 
 
-def _initialize_globals(config_options: IslandConfigOptions, server_config_path: str):
-    initialize_services(config_options.data_dir)
+def _initialize_globals(data_dir: Path):
+    initialize_services(data_dir)
 
 
 def _start_mongodb(data_dir: Path) -> MongoDbProcess:
