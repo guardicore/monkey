f46bb60da590269131db9f8f9a3937511fe91971
Mike Salvatore <mike.s.salvatore@gmail.com>
2021-12-15 09:00:41 -0500

    Agent: Add block parameter to IMaster.terminate()
    This allows the caller to decide whether or not they're willing to wait
    for the master to finish shutting down.

--- monkey/infection_monkey/i_master.py
+++ monkey/infection_monkey/i_master.py
@@ -10,9 +10,10 @@ class IMaster(metaclass=abc.ABCMeta):
         """
 
     @abc.abstractmethod
-    def terminate(self) -> None:
+    def terminate(self, block: bool = False) -> None:
         """
         Stop the master and interrupt any actions that are currently being executed.
+        :param bool block: Whether or not to block and wait for the master to terminate.
         """
 
     @abc.abstractmethod
