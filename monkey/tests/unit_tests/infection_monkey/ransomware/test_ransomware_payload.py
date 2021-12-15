20890e51ec1509e64c3c416b5900b81e47574f21
Ilija Lazoroski <ilija.la@live.com>
2021-12-15 12:44:48 +0100

    Agent: Remove ransomware cleanup function

--- monkey/tests/unit_tests/infection_monkey/ransomware/test_ransomware_payload.py
+++ monkey/tests/unit_tests/infection_monkey/ransomware/test_ransomware_payload.py
@@ -1,4 +1,4 @@
-from pathlib import Path, PurePosixPath
+from pathlib import PurePosixPath
 from unittest.mock import MagicMock
 
 import pytest
@@ -184,28 +184,3 @@ def test_leave_readme_exceptions_handled(build_ransomware_payload, ransomware_pa
 
     # Test will fail if exception is raised and not handled
     ransomware_payload.run_payload()
-    ransomware_payload.cleanup()
-
-
-def test_cleanup_incomplete_readme(build_ransomware_payload, ransomware_payload_config):
-    def leave_readme(_: Path, dest: Path):
-        if leave_readme.i == 0:
-            dest.touch()
-
-        leave_readme.i += 1
-
-        raise Exception("Test exception when leaving README")
-
-    leave_readme.i = 0
-
-    ransomware_payload_config.readme_enabled = True
-    ransomware_payload = build_ransomware_payload(
-        config=ransomware_payload_config, leave_readme=leave_readme
-    )
-
-    ransomware_payload.run_payload()
-    assert (ransomware_payload_config.target_directory / README_FILE_NAME).exists()
-
-    ransomware_payload.cleanup()
-    assert not (ransomware_payload_config.target_directory / README_FILE_NAME).exists()
-    assert leave_readme.i == 2
