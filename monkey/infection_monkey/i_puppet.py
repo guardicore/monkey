b7982552498b3f7fbcc25edf3ae94471f0fe41c1
Ilija Lazoroski <ilija.la@live.com>
2021-12-16 16:26:40 +0100

    Agent: Add plugin_name attribute to puppet's load_plugin

--- monkey/infection_monkey/i_puppet.py
+++ monkey/infection_monkey/i_puppet.py
@@ -27,9 +27,10 @@ PostBreachData = namedtuple("PostBreachData", ["command", "result"])
 
 class IPuppet(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def load_plugin(self, plugin: object, plugin_type: PluginType) -> None:
+    def load_plugin(self, plugin_name: str, plugin: object, plugin_type: PluginType) -> None:
         """
-        Loads a plugin into the puppet.
+        Loads a plugin into the puppet
+        :param str plugin_name: The plugin class name
         :param object plugin: The plugin object to load
         :param PluginType plugin_type: The type of plugin being loaded
         """
