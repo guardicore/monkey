76d6071f7c175b6526a34e56018a1b175aab1888
vakarisz <vakarisz@yahoo.com>
2022-01-19 15:09:03 +0200

    Island: add cve link to log4shell report

--- monkey/monkey_island/cc/ui/src/components/report-components/security/issues/Log4ShellIssue.js
+++ monkey/monkey_island/cc/ui/src/components/report-components/security/issues/Log4ShellIssue.js
@@ -14,7 +14,8 @@ export function log4shellIssueReport(issue) {
           className="badge badge-info" style={{margin: '2px'}}>{issue.ip_address}:{issue.port}</span>) is vulnerable to <span
           className="badge badge-danger">the Log4Shell remote code execution</span> attack.
           <br/>
-          The attack was made possible due to an old version of Apache Log4j component.
+          The attack was made possible due to an old version of Apache Log4j component. <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=2021-44228"
+          target="_blank">CVE-2021-44228</a>
         </CollapsibleWellComponent>
       </>
     );
