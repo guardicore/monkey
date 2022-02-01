a35f141cbed1cc01107b44f9aa8feda07b20845d
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-02-01 15:32:37 -0500

    Island: Remove scoutsuite findings and rules

--- monkey/monkey_island/cc/services/zero_trust/scoutsuite/consts/rule_names/cloudformation_rules.py
+++
@@ -1,8 +0,0 @@
-from monkey_island.cc.services.zero_trust.scoutsuite.consts.rule_names.rule_name_enum import (
-    RuleNameEnum,
-)
-
-
-class CloudformationRules(RuleNameEnum):
-    # Service Security
-    CLOUDFORMATION_STACK_WITH_ROLE = "cloudformation-stack-with-role"
