cc3be599d7bccba7f5e93fc769835af27e7d8015
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-02-08 12:43:10 -0500

    Agent: Refactor ElasticSearchFingerprinter
    * Rename ElasticFinger -> ElasticSearchFingerprinter
    * Don't scan port if port is closed or not configured
    * Refactor code to conform to the IFingerprinter interface
    * Add unit tests

--- monkey/infection_monkey/network/elasticsearch_fingerprinter.py
+++ monkey/infection_monkey/network/elasticsearch_fingerprinter.py
@@ -1,48 +1,64 @@
-import json
 import logging
 from contextlib import closing
+from typing import Any, Dict
 
 import requests
-from requests.exceptions import ConnectionError, Timeout
 
-import infection_monkey.config
 from common.common_consts.network_consts import ES_SERVICE
-from infection_monkey.network.HostFinger import HostFinger
+from infection_monkey.i_puppet import (
+    FingerprintData,
+    IFingerprinter,
+    PingScanData,
+    PortScanData,
+    PortStatus,
+)
 
 ES_PORT = 9200
 ES_HTTP_TIMEOUT = 5
 logger = logging.getLogger(__name__)
 
 
-class ElasticFinger(HostFinger):
+class ElasticSearchFingerprinter(IFingerprinter):
     """
     Fingerprints elastic search clusters, only on port 9200
     """
 
-    _SCANNED_SERVICE = "Elastic search"
+    def get_host_fingerprint(
+        self,
+        host: str,
+        _ping_scan_data: PingScanData,
+        port_scan_data: Dict[int, PortScanData],
+        _options: Dict,
+    ) -> FingerprintData:
+        services = {}
 
-    def __init__(self):
-        self._config = infection_monkey.config.WormConfiguration
+        if (ES_PORT not in port_scan_data) or (port_scan_data[ES_PORT].status != PortStatus.OPEN):
+            return FingerprintData(None, None, services)
 
-    def get_host_fingerprint(self, host):
-        """
-        Returns elasticsearch metadata
-        :param host:
-        :return: Success/failure, data is saved in the host struct
-        """
         try:
-            url = "http://%s:%s/" % (host.ip_addr, ES_PORT)
-            with closing(requests.get(url, timeout=ES_HTTP_TIMEOUT)) as req:
-                data = json.loads(req.text)
-                self.init_service(host.services, ES_SERVICE, ES_PORT)
-                host.services[ES_SERVICE]["cluster_name"] = data["cluster_name"]
-                host.services[ES_SERVICE]["name"] = data["name"]
-                host.services[ES_SERVICE]["version"] = data["version"]["number"]
-                return True
-        except Timeout:
-            logger.debug("Got timeout while trying to read header information")
-        except ConnectionError:  # Someone doesn't like us
-            logger.debug("Unknown connection error")
-        except KeyError:
-            logger.debug("Failed parsing the ElasticSearch JSOn response")
-        return False
+            elasticsearch_info = _query_elasticsearch(host)
+            services[ES_SERVICE] = _get_service_from_query_info(elasticsearch_info)
+        except Exception as ex:
+            logger.debug(f"Did not detect an ElasticSearch cluster: {ex}")
+
+        return FingerprintData(None, None, services)
+
+
+def _query_elasticsearch(host: str) -> Dict[str, Any]:
+    url = "http://%s:%s/" % (host, ES_PORT)
+    logger.debug(f"Sending request to {url}")
+    with closing(requests.get(url, timeout=ES_HTTP_TIMEOUT)) as response:
+        return response.json()
+
+
+def _get_service_from_query_info(elasticsearch_info: Dict[str, Any]) -> Dict[str, Any]:
+    try:
+        return {
+            "display_name": "ElasticSearch",
+            "port": ES_PORT,
+            "cluster_name": elasticsearch_info["cluster_name"],
+            "name": elasticsearch_info["name"],
+            "version": elasticsearch_info["version"]["number"],
+        }
+    except KeyError as ke:
+        raise Exception(f"Unable to find the key {ke} in the server's response") from ke
