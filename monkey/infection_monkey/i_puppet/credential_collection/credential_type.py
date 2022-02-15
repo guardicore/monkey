a9bb2dee70e5b4eee62f61a1bda98f8d50010435
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-02-15 14:26:15 -0500

    Agent: Renumber the CredentialType Enum

--- monkey/infection_monkey/i_puppet/credential_collection/credential_type.py
+++ monkey/infection_monkey/i_puppet/credential_collection/credential_type.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class CredentialType(Enum):
-    USERNAME = 2
-    PASSWORD = 3
-    NT_HASH = 4
-    LM_HASH = 5
+    USERNAME = 1
+    PASSWORD = 2
+    NT_HASH = 3
+    LM_HASH = 4
