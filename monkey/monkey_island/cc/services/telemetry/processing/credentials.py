aa303ae6d6a150aa56bab626b434c9039dc20f87
vakarisz <vakarisz@yahoo.com>
2022-02-17 17:27:09 +0200

    Island: remove credentials parsing boundary

--- monkey/monkey_island/cc/services/telemetry/processing/credentials.py
+++
@@ -1,7 +0,0 @@
-from monkey_island.cc.services.telemetry.processing.credentials.credentials_parser import (
-    parse_credentials,
-)
-
-
-def process_credentials_telemetry(telemetry: dict):
-    parse_credentials(telemetry)
