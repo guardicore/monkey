600753b53cec34539666fe45370034dc78c19554
vakarisz <vakarisz@yahoo.com>
2022-02-21 17:22:36 +0200

    Island: add username processor

--- monkey/monkey_island/cc/services/telemetry/processing/credentials/identities/username_processor.py
+++ monkey/monkey_island/cc/services/telemetry/processing/credentials/identities/username_processor.py
@@ -1,2 +1,5 @@
-def process_username():
-    pass
+from monkey_island.cc.services.config import ConfigService
+
+
+def process_username(username: dict):
+    ConfigService.creds_add_username(username["username"])
