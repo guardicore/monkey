40820a5ba501d9b9affc7680ceaa882ec212fa4e
vakarisz <vakarisz@yahoo.com>
2022-02-25 17:28:07 +0200

    Island: refactor report generation to take credentials from model
    Reporting used to fetch credentials from telemetries, but they are no longer stored. Instead, credentials are being fetched from stolen_credentials collection

--- monkey/monkey_island/cc/services/attack/technique_reports/T1003.py
+++ monkey/monkey_island/cc/services/attack/technique_reports/T1003.py
@@ -1,7 +1,7 @@
 from common.utils.attack_utils import ScanStatus
-from monkey_island.cc.database import mongo
+from monkey_island.cc.models import StolenCredentials
 from monkey_island.cc.services.attack.technique_reports import AttackTechnique
-from monkey_island.cc.services.reporting.report import ReportService
+from monkey_island.cc.services.reporting.stolen_credentials import get_stolen_creds
 
 
 class T1003(AttackTechnique):
@@ -14,29 +14,10 @@ class T1003(AttackTechnique):
     scanned_msg = ""
     used_msg = "Monkey successfully obtained some credentials from systems on the network."
 
-    query = {
-        "$or": [
-            {
-                "telem_category": "system_info",
-                "$and": [
-                    {"data.credentials": {"$exists": True}},
-                    {"data.credentials": {"$gt": {}}},
-                ],
-            },  # $gt: {} checks if field is not an empty object
-            {
-                "telem_category": "exploit",
-                "$and": [
-                    {"data.info.credentials": {"$exists": True}},
-                    {"data.info.credentials": {"$gt": {}}},
-                ],
-            },
-        ]
-    }
-
     @staticmethod
     def get_report_data():
         def get_technique_status_and_data():
-            if mongo.db.telemetry.count_documents(T1003.query):
+            if list(StolenCredentials.objects()):
                 status = ScanStatus.USED.value
             else:
                 status = ScanStatus.UNSCANNED.value
@@ -47,6 +28,5 @@ class T1003(AttackTechnique):
 
         data.update(T1003.get_message_and_status(status))
         data.update(T1003.get_mitigation_by_status(status))
-        data["stolen_creds"] = ReportService.get_stolen_creds()
-        data["stolen_creds"].extend(ReportService.get_ssh_keys())
+        data["stolen_creds"] = get_stolen_creds()
         return data
---
+++ monkey/monkey_island/cc/services/reporting/stolen_credentials.py
@@ -0,0 +1,59 @@
+import logging
+from typing import Mapping, Sequence
+
+from common.common_consts.credential_component_type import CredentialComponentType
+from monkey_island.cc.models import StolenCredentials
+
+logger = logging.getLogger(__name__)
+
+
+def get_stolen_creds() -> Sequence[Mapping]:
+    stolen_creds = _fetch_from_db()
+    stolen_creds = _format_creds_for_reporting(stolen_creds)
+
+    logger.info("Stolen creds generated for reporting")
+    return stolen_creds
+
+
+def extract_ssh_keys(credentials: Sequence[Mapping]) -> Sequence[Mapping]:
+    ssh_keys = []
+    for credential in credentials:
+        if credential["_type"] == CredentialComponentType.SSH_KEYPAIR.name:
+            ssh_keys.append(credential)
+    return ssh_keys
+
+
+def _fetch_from_db() -> Sequence[StolenCredentials]:
+    return list(StolenCredentials.objects())
+
+
+def _format_creds_for_reporting(credentials: Sequence[StolenCredentials]):
+    formatted_creds = []
+    cred_type_dict = {
+        CredentialComponentType.PASSWORD.name: "Clear Password",
+        CredentialComponentType.LM_HASH.name: "LM hash",
+        CredentialComponentType.NT_HASH.name: "NTLM hash",
+        CredentialComponentType.SSH_KEYPAIR.name: "Clear SSH private key",
+    }
+
+    for cred in credentials:
+        for secret_type in cred.secrets:
+            if secret_type not in cred_type_dict:
+                continue
+            username = _get_username(cred)
+            cred_row = {
+                "username": username,
+                "_type": secret_type,
+                "type": cred_type_dict[secret_type],
+                "origin": cred.monkey.hostname,
+            }
+            if cred_row not in formatted_creds:
+                formatted_creds.append(cred_row)
+    return formatted_creds
+
+
+def _get_username(credentials: StolenCredentials) -> str:
+    if credentials.identities:
+        return credentials.identities[0]["username"]
+    else:
+        return ""
