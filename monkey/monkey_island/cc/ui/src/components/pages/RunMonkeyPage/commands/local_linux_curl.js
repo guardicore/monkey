b20abad0b6bdc574cbe40c005fa46a5cf8df5ec0
vakarisz <vakarisz@yahoo.com>
2022-03-03 17:42:10 +0200

    Island: change manual run commands to target /os download endpoints
    Now monkey agents are downloaded not by name, but by os, so url's had to change

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_curl.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_curl.js
@@ -1,5 +1,5 @@
 export default function generateLocalLinuxCurl(ip, username) {
-  let command = `curl https://${ip}:5000/api/monkey/download/monkey-linux-64 -k `
+  let command = `curl https://${ip}:5000/api/monkey/download/linux -k `
     + `-o monkey-linux-64; `
     + `chmod +x monkey-linux-64; `
     + `./monkey-linux-64 m0nk3y -s ${ip}:5000;`;
