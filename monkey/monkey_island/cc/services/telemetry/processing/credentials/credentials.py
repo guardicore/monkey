32a9fe7bf9e10c6177b57e7b6c4ce89fc7aa9ad6
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-03-30 13:55:35 -0400

    Island: Fix import sorting in credentials.py

--- monkey/monkey_island/cc/services/telemetry/processing/credentials/credentials.py
+++ monkey/monkey_island/cc/services/telemetry/processing/credentials/credentials.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Sequence, Mapping, Any
+from typing import Any, Mapping, Sequence
 
 
 @dataclass(frozen=True)
