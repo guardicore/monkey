cbc6d160f6589c97e00cc7d89dc0b1c72af68530
vakarisz <vakarisz@yahoo.com>
2022-03-31 12:55:28 +0300

    Agent: Add a timeout to linux account discovery

--- monkey/infection_monkey/post_breach/account_discovery/linux_account_discovery.py
+++ monkey/infection_monkey/post_breach/account_discovery/linux_account_discovery.py
@@ -1,7 +1,10 @@
+from common.common_consts.timeouts import SHORT_REQUEST_TIMEOUT
+
+
 def get_linux_commands_to_discover_accounts():
     return [
         "echo 'Discovered the following user accounts:'; ",
-        "cut -d: -f1,3 /etc/passwd | ",
+        f"timeout {SHORT_REQUEST_TIMEOUT} cut -d: -f1,3 /etc/passwd | ",
         "egrep ':[0-9]{4}$' | ",
         "cut -d: -f1",
     ]
