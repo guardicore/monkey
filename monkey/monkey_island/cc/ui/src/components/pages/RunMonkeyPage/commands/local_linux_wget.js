59aec706b2629e59e9b8d87f41330def5fe9f507
Ilija Lazoroski <ilija.la@live.com>
2022-04-08 15:07:23 +0200

    UI: Add output to the wget manual run command

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_wget.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_wget.js
@@ -1,6 +1,6 @@
 export default function generateLocalLinuxWget(ip, username) {
   let command = `wget --no-check-certificate https://${ip}:5000/api/monkey/download/`
-    + `linux; `
+    + `linux -O ./monkey-linux-64; `
     + `chmod +x monkey-linux-64; `
     + `./monkey-linux-64 m0nk3y -s ${ip}:5000`;
 
