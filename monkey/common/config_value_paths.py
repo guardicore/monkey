c4edec80a6581bb10e476697b949e2f4e37c8fcc
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-04-27 19:08:37 -0400

    Common: Fix typo touples -> tuples
    Co-authored-by: ilija-lazoroski <ilija.la@live.com>

--- monkey/common/config_value_paths.py
+++ monkey/common/config_value_paths.py
@@ -11,6 +11,6 @@ LOCAL_NETWORK_SCAN_PATH = ["basic_network", "scope", "local_network_scan"]
 LM_HASH_LIST_PATH = ["internal", "exploits", "exploit_lm_hash_list"]
 NTLM_HASH_LIST_PATH = ["internal", "exploits", "exploit_ntlm_hash_list"]
 
-# TODO: These are tuples so that they are immutable. Make the rest of these paths touples as well.
+# TODO: These are tuples so that they are immutable. Make the rest of these paths tuples as well.
 PBA_LINUX_FILENAME_PATH = ("monkey", "post_breach", "PBA_linux_filename")
 PBA_WINDOWS_FILENAME_PATH = ("monkey", "post_breach", "PBA_windows_filename")
