1bfffb0bfdc89ce052f21ee796d138c0865b7907
Ilija Lazoroski <ilija.la@live.com>
2022-05-30 09:46:25 +0200

    Project: Fix travis build

--- monkey/monkey_island/cc/repository/ILogRepository.py
+++ monkey/monkey_island/cc/repository/ILogRepository.py
@@ -4,10 +4,10 @@ from typing import Optional, Sequence
 
 class ILogRepository(ABC):
     # Define log object
-    def get_logs(self, agent_id: Optional[str] = None) -> Sequence[Log]:
+    def get_logs(self, agent_id: Optional[str] = None) -> Sequence[Log]:  # noqa: F821
         pass
 
-    def save_log(self, log: Log):
+    def save_log(self, log: Log):  # noqa: F821
         pass
 
     def delete_log(self, agent_id: str):
--- monkey/monkey_island/cc/repository/IMachineRepository.py
+++ monkey/monkey_island/cc/repository/IMachineRepository.py
@@ -4,7 +4,7 @@ from typing import Optional, Sequence
 
 class IMachineRepository(ABC):
     # TODO define Machine object(ORM model)
-    def save_machine(self, machine: Machine):
+    def save_machine(self, machine: Machine):  # noqa: F821
         pass
 
     # TODO define Machine object(ORM model)
@@ -14,7 +14,7 @@ class IMachineRepository(ABC):
         self,
         id: Optional[str] = None,
         ips: Optional[Sequence[str]] = None,
-        state: Optional[MachineState] = None,
+        state: Optional[MachineState] = None,  # noqa: F821
         is_island: Optional[bool] = None,
-    ) -> Sequence[Machine]:
+    ) -> Sequence[Machine]:  # noqa: F821
         pass
