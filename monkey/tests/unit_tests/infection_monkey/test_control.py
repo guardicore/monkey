7fe6c170cdbdb23f86bfac22e5d894d74d6b830c
Ilija Lazoroski <ilija.la@live.com>
2022-06-13 17:51:41 +0200

    UT: Fix control client location

--- monkey/tests/unit_tests/infection_monkey/test_control.py
+++ monkey/tests/unit_tests/infection_monkey/test_control.py
@@ -8,7 +8,7 @@ from monkey.infection_monkey.control import ControlClient
     [(True, "http://8.8.8.8:45455"), (False, "8.8.8.8:45455")],
 )
 def test_control_set_proxies(monkeypatch, is_windows_os, expected_proxy_string):
-    monkeypatch.setattr("infection_monkey.cc.control.is_windows_os", lambda: is_windows_os)
+    monkeypatch.setattr("monkey.infection_monkey.control.is_windows_os", lambda: is_windows_os)
     control_client = ControlClient("8.8.8.8:5000")
 
     control_client.set_proxies(("8.8.8.8", "45455"))
