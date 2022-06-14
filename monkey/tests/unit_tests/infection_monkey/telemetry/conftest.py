444b34d548816badb1b67759d7a8571784e245bb
vakarisz <vakarisz@yahoo.com>
2022-06-14 12:14:27 +0300

    UT: Change send telemetry spy syntax in conftest.py

--- monkey/tests/unit_tests/infection_monkey/telemetry/conftest.py
+++ monkey/tests/unit_tests/infection_monkey/telemetry/conftest.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from infection_monkey.control import ControlClient
@@ -13,5 +15,5 @@ def spy_send_telemetry(monkeypatch):
     _spy_send_telemetry.data = None
     control_client = ControlClient("localhost:5000")
     ControlClient.control_client_object = control_client
-    monkeypatch.setattr(ControlClient.control_client_object, "send_telemetry", _spy_send_telemetry)
+    ControlClient.control_client_object.send_telemetry = MagicMock(side_effect=_spy_send_telemetry)
     return _spy_send_telemetry
