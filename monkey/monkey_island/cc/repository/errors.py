b99ad70774026151132075d5fc95721ee92d5590
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-17 19:56:09 -0400

    Island: Add StorageError

--- monkey/monkey_island/cc/repository/errors.py
+++ monkey/monkey_island/cc/repository/errors.py
@@ -2,4 +2,13 @@ class RetrievalError(RuntimeError):
     """
     Raised when a repository encounters an error while attempting to retrieve data.
     """
+
+    pass
+
+
+class StorageError(RuntimeError):
+    """
+    Raised when a repository encounters an error while attempting to store data.
+    """
+
     pass
