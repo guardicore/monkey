ae0c440603bbc116bbec16692e5a9ab8723b2d95
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-17 11:08:45 -0400

    Island: Add documentation for IConfigRepository

--- monkey/monkey_island/cc/repository/i_config_repository.py
+++ monkey/monkey_island/cc/repository/i_config_repository.py
@@ -4,8 +4,21 @@ from common.configuration import AgentConfiguration
 
 
 class IConfigRepository(ABC):
+    """
+    A repository used to store and retrieve the agent configuration.
+    """
     def get_config(self) -> AgentConfiguration:
+        """
+        Retrieve the agent configuration from the repository
+
+        :return: The agent configuration
+        """
         pass
 
-    def set_config(self, config: AgentConfiguration):
+    def set_config(self, agent_config: AgentConfiguration):
+        """
+        Store the agent configuration in the repository
+
+        :param agent_config: The agent configuration to store in the repository
+        """
         pass
