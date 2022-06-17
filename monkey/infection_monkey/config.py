d3a03d265317026006b3b5c25130a204d71935a8
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-17 13:11:47 -0400

    Agent: Remove disused constants

--- monkey/infection_monkey/config.py
+++ monkey/infection_monkey/config.py
@@ -1,11 +1,3 @@
 import uuid
 
 GUID = str(uuid.getnode())
-
-SENSITIVE_FIELDS = [
-    "exploit_password_list",
-    "exploit_user_list",
-    "exploit_ssh_keys",
-]
-LOCAL_CONFIG_VARS = ["name", "id", "max_depth"]
-HIDDEN_FIELD_REPLACEMENT_CONTENT = "hidden"
