c008db4cf2857779eb0f062748da1e313d3c98d6
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-21 09:40:00 -0400

    Island: Raise RetrievalError in FileAgentConfigurationRepository

--- monkey/tests/unit_tests/monkey_island/cc/repository/test_file_agent_configuration_repository.py
+++ monkey/tests/unit_tests/monkey_island/cc/repository/test_file_agent_configuration_repository.py
@@ -1,8 +1,9 @@
+import pytest
 from tests.common.example_agent_configuration import AGENT_CONFIGURATION
-from tests.monkey_island import SingleFileRepository
+from tests.monkey_island import OpenErrorFileRepository, SingleFileRepository
 
 from common.configuration import DEFAULT_AGENT_CONFIGURATION, AgentConfigurationSchema
-from monkey_island.cc.repository import FileAgentConfigurationRepository
+from monkey_island.cc.repository import FileAgentConfigurationRepository, RetrievalError
 
 
 def test_store_agent_config():
@@ -24,3 +25,10 @@ def test_get_default_agent_config():
     retrieved_agent_configuration = repository.get_configuration()
 
     assert retrieved_agent_configuration == default_agent_configuration
+
+
+def test_get_agent_config_retrieval_error():
+    repository = FileAgentConfigurationRepository(OpenErrorFileRepository())
+
+    with pytest.raises(RetrievalError):
+        repository.get_configuration()
