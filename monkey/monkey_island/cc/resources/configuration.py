5dd27eeea535e6250f241a8fb3d22816124630d4
Shreya Malviya <shreya.malviya@gmail.com>
2022-06-20 19:50:38 -0700

    Island: Add definition for GET in new Configuration resource

--- monkey/monkey_island/cc/resources/configuration.py
+++ monkey/monkey_island/cc/resources/configuration.py
@@ -1,13 +1,26 @@
+from enum import Enum
+from flask import jsonify, request
+
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 from monkey_island.cc.resources.request_authentication import jwt_required
+from monkey_island.cc.repository import FileAgentConfigurationRepository
+
+class ConfigurationTypeEnum(Enum):
+    ISLAND = "island"
+    AGENT = "agent"
 
 
 class Configuration(AbstractResource):
-    urls = ["/api/configuration"]
+    urls = ["/api/configuration/<string:configuration_type>"]
 
     @jwt_required
-    def get(self):
-        pass
+    def get(self, configuration_type: str):
+        # we probably still need this because of credential fields, HTTP ports, etc in the config?
+        if configuration_type == ConfigurationTypeEnum.ISLAND:
+            pass
+        elif configuration_type == ConfigurationTypeEnum.AGENT:
+            configuration = FileAgentConfigurationRepository.get_configuration()
+            return jsonify(configuration=configuration)
 
     @jwt_required
     def post(self):
