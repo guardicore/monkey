8d2078048cdfb381ed5c22dc56dd40003c19bdc2
vakarisz <vakarisz@yahoo.com>
2022-06-22 17:39:25 +0300

    Agent: Change the positive_int definition to contain 0

--- monkey/common/utils/argparse_types.py
+++ monkey/common/utils/argparse_types.py
@@ -3,7 +3,7 @@ import argparse
 
 def positive_int(_input: str):
     int_value = int(_input)
-    if int_value <= 0:
+    if int_value < 0:
         raise argparse.ArgumentTypeError(f"{_input} is not a positive integer")
 
     return int_value
