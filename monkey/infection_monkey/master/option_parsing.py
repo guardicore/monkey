86ed174d743e0a1f27912b1800584919280861b2
vakaris_zilius <vakarisz@yahoo.com>
2022-06-23 15:06:48 +0000

    Agent: Usa agent config object instead of dict in option_parsing.py

--- monkey/infection_monkey/master/option_parsing.py
+++ monkey/infection_monkey/master/option_parsing.py
@@ -1,13 +1,12 @@
-from typing import Dict
-
+from common.configuration import CustomPBAConfiguration
 from infection_monkey.utils.environment import is_windows_os
 
 
-def custom_pba_is_enabled(pba_options: Dict) -> bool:
+def custom_pba_is_enabled(pba_options: CustomPBAConfiguration) -> bool:
     if not is_windows_os():
-        if pba_options["linux_command"]:
+        if pba_options.linux_command:
             return True
     else:
-        if pba_options["windows_command"]:
+        if pba_options.windows_command:
             return True
     return False
