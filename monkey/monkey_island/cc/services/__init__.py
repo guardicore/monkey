33053254196299fe8716e3c9333df69c6b23d69d
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-26 09:42:08 -0400

    Island: Remove unnecessary aws_service import

--- monkey/monkey_island/cc/services/__init__.py
+++ monkey/monkey_island/cc/services/__init__.py
@@ -2,7 +2,3 @@ from .authentication.authentication_service import AuthenticationService
 from .authentication.json_file_user_datastore import JsonFileUserDatastore
 
 from .aws import AWSService
-
-# TODO: This is a temporary import to keep some tests passing. Remove it before merging #1928 to
-#       develop.
-from .aws import aws_service
