41b1c56fd06fa2a5f270803daddbb7909b3df5ad
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-06-30 11:35:29 -0400

    UT: Add reset_to_default() to InMemoryAgentConfigurationRepository

--- monkey/tests/monkey_island/in_memory_agent_configuration_repository.py
+++ monkey/tests/monkey_island/in_memory_agent_configuration_repository.py
@@ -6,10 +6,14 @@ from monkey_island.cc.repository import IAgentConfigurationRepository
 
 class InMemoryAgentConfigurationRepository(IAgentConfigurationRepository):
     def __init__(self):
-        self._configuration = AgentConfiguration.from_mapping(AGENT_CONFIGURATION)
+        self._default_configuration = AgentConfiguration.from_mapping(AGENT_CONFIGURATION)
+        self._configuration = self._default_configuration
 
     def get_configuration(self):
         return self._configuration
 
     def store_configuration(self, agent_configuration):
         self._configuration = agent_configuration
+
+    def reset_to_default(self):
+        self._configuration = self._default_configuration
