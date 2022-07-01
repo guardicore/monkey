7ec558a5f448cddf4ab8dc349ee0479fc570b035
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-01 12:17:10 -0400

    Island: Move IslandModeService out of unnecessary subpackage

--- monkey/monkey_island/cc/services/__init__.py
+++ monkey/monkey_island/cc/services/__init__.py
@@ -2,4 +2,4 @@ from .authentication.authentication_service import AuthenticationService
 from .authentication.json_file_user_datastore import JsonFileUserDatastore
 
 from .aws import AWSService
-from .mode.island_mode_service import IslandModeService
+from .island_mode_service import IslandModeService
