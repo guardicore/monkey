d2906ef23bf384207c9ecc83411d4643a73658d7
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-01 10:00:56 -0400

    Island: Add docstrings to {get,save}_simulation()

--- monkey/monkey_island/cc/repository/i_simulation_repository.py
+++ monkey/monkey_island/cc/repository/i_simulation_repository.py
@@ -7,10 +7,22 @@ from monkey_island.cc.services.mode.mode_enum import IslandModeEnum
 class ISimulationRepository(ABC):
     @abstractmethod
     def save_simulation(self, simulation: Simulation):
+        """
+        Save the simulation state
+
+        :param simulation: The simulation state
+        :raises StorageError: If the simulation states could not be saved
+        """
         pass
 
     @abstractmethod
     def get_simulation(self) -> Simulation:
+        """
+        Get the simulation state
+
+        :raises RetrievalError: If the simulation state could not be retrieved
+        """
+
         pass
 
     @abstractmethod
