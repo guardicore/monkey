a24bf921c1302e9fccc2b9cafb318930099705d7
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-01 09:19:51 -0400

    Island: Add {get,set}_mode() to ISimulationRepository

--- monkey/monkey_island/cc/repository/i_simulation_repository.py
+++ monkey/monkey_island/cc/repository/i_simulation_repository.py
@@ -1,6 +1,7 @@
 from abc import ABC
 
 from monkey_island.cc.models import Simulation
+from monkey_island.cc.services.mode.mode_enum import IslandModeEnum
 
 
 class ISimulationRepository(ABC):
@@ -8,4 +9,23 @@ class ISimulationRepository(ABC):
         pass
 
     def get_simulation(self):
+        pass
+
+    def get_mode(self) -> IslandModeEnum:
+        """
+        Get's the island's current mode
+
+        :return The island's current mode
+        :raises RetrievalError: If the mode could not be retrieved
+        """
+        pass
+
+    def set_mode(self, mode: IslandModeEnum):
+        """
+        Set the island's mode
+
+        :param mode: The island's new mode
+        :raises StorageError: If the mode could not be saved
+        """
+
         pass
