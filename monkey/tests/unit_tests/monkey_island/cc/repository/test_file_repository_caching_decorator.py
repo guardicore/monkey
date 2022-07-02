9affe10f67c4b15c48703d2dfd83e2f05763a80e
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-02 10:33:06 -0400

    Island: Add FileRepositoryCachingDecorator

---
+++ monkey/tests/unit_tests/monkey_island/cc/repository/test_file_repository_caching_decorator.py
@@ -0,0 +1,55 @@
+import io
+
+import pytest
+from tests.monkey_island import SingleFileRepository
+
+from monkey_island.cc import repository
+from monkey_island.cc.repository import FileRepositoryCachingDecorator
+
+
+@pytest.fixture
+def file_repository():
+    return FileRepositoryCachingDecorator(SingleFileRepository())
+
+
+def test_open_cache_file(file_repository):
+    file_name = "test.txt"
+    file_contents = b"Hello World!"
+
+    file_repository.save_file(file_name, io.BytesIO(file_contents))
+    assert file_repository.open_file(file_name).read() == file_contents
+    assert file_repository.open_file(file_name).read() == file_contents
+
+
+def test_overwrite_file(file_repository):
+    file_name = "test.txt"
+    file_contents_1 = b"Hello World!"
+    file_contents_2 = b"Goodbye World!"
+
+    file_repository.save_file(file_name, io.BytesIO(file_contents_1))
+    assert file_repository.open_file(file_name).read() == file_contents_1
+
+    file_repository.save_file(file_name, io.BytesIO(file_contents_2))
+    assert file_repository.open_file(file_name).read() == file_contents_2
+
+
+def test_delete_file(file_repository):
+    file_name = "test.txt"
+    file_contents = b"Hello World!"
+
+    file_repository.save_file(file_name, io.BytesIO(file_contents))
+    file_repository.delete_file(file_name)
+
+    with pytest.raises(repository.FileNotFoundError):
+        file_repository.open_file(file_name)
+
+
+def test_delete_all_files(file_repository):
+    file_name = "test.txt"
+    file_contents = b"Hello World!"
+
+    file_repository.save_file(file_name, io.BytesIO(file_contents))
+    file_repository.delete_all_files()
+
+    with pytest.raises(repository.FileNotFoundError):
+        file_repository.open_file(file_name)
