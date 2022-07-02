bc0c4a1c8e697b1e27dee331fd72a4302f87b633
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-02 09:49:12 -0400

    Island: Add FileRepositoryLoggingDecorator

---
+++ monkey/monkey_island/cc/repository/file_repository_logging_decorator.py
@@ -0,0 +1,31 @@
+import logging
+from typing import BinaryIO
+
+from . import IFileRepository
+
+logger = logging.getLogger(__name__)
+
+
+class FileRepositoryLoggingDecorator(IFileRepository):
+    def __init__(self, file_repository: IFileRepository):
+        self._file_repository = file_repository
+
+    def save_file(self, unsafe_file_name: str, file_contents: BinaryIO):
+        logger.info(f"Saving file {unsafe_file_name}")
+
+        return self._file_repository.save_file(unsafe_file_name, file_contents)
+
+    def open_file(self, unsafe_file_name: str) -> BinaryIO:
+        logger.info(f"Opening file {unsafe_file_name}")
+
+        return self._file_repository.open_file(unsafe_file_name)
+
+    def delete_file(self, unsafe_file_name: str):
+        logger.info(f"Deleting file {unsafe_file_name}")
+
+        return self._file_repository.delete_file(unsafe_file_name)
+
+    def delete_all_files(self):
+        logger.info("Deleting all files in the repository")
+
+        return self._file_repository.delete_all_files()
