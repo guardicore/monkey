4c2c55e5fa53ec99244be39b0e4b9d6cdc64cbdf
Ilija Lazoroski <ilija.la@live.com>
2022-07-06 15:16:05 +0200

    UT: Compare actual propagation credentials vs expected

--- monkey/tests/monkey_island/__init__.py
+++ monkey/tests/monkey_island/__init__.py
@@ -4,3 +4,7 @@ from .open_error_file_repository import OpenErrorFileRepository
 from .in_memory_agent_configuration_repository import InMemoryAgentConfigurationRepository
 from .in_memory_simulation_configuration import InMemorySimulationRepository
 from .stub_propagation_credentials_repository import StubPropagationCredentialsRepository
+from .stub_propagation_credentials_repository import (
+    PROPAGATION_CREDENTIALS_1,
+    PROPAGATION_CREDENTIALS_2,
+)
