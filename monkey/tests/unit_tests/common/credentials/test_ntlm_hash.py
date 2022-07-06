82ce091063c50fe3df2d98239f9f0b2b52e45fa0
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-06 19:32:18 -0400

    Common: Export InvalidCredentialComponent from common.credentials

--- monkey/tests/unit_tests/common/credentials/test_ntlm_hash.py
+++ monkey/tests/unit_tests/common/credentials/test_ntlm_hash.py
@@ -1,6 +1,6 @@
 import pytest
 
-from common.credentials import LMHash, NTHash
+from common.credentials import InvalidCredentialComponent, LMHash, NTHash
 
 VALID_HASH = "E520AC67419A9A224A3B108F3FA6CB6D"
 INVALID_HASHES = (
@@ -22,5 +22,5 @@ def test_construct_valid_ntlm_hash(ntlm_hash_class):
 @pytest.mark.parametrize("ntlm_hash_class", (LMHash, NTHash))
 def test_construct_invalid_ntlm_hash(ntlm_hash_class):
     for invalid_hash in INVALID_HASHES:
-        with pytest.raises(Exception):
+        with pytest.raises(InvalidCredentialComponent):
             ntlm_hash_class(invalid_hash)
