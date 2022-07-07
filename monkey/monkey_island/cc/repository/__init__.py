938aec9d49d917101d8c21090bce541793e68ad7
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-07 13:11:21 -0400

    Island: Reorder repository/__init.py
    Group the interfaces and implementations together to make it easier to
    understand what functionality has been implemented so far.

--- monkey/monkey_island/cc/repository/__init__.py
+++ monkey/monkey_island/cc/repository/__init__.py
@@ -1,14 +1,19 @@
 from .errors import RemovalError, RetrievalError, StorageError
+
+
 from .i_file_repository import FileNotFoundError, IFileRepository
+from .i_agent_binary_repository import IAgentBinaryRepository
+from .i_agent_configuration_repository import IAgentConfigurationRepository
+from .i_simulation_repository import ISimulationRepository
+from .i_credentials_repository import ICredentialsRepository
+
+
 from .local_storage_file_repository import LocalStorageFileRepository
 from .file_repository_caching_decorator import FileRepositoryCachingDecorator
 from .file_repository_locking_decorator import FileRepositoryLockingDecorator
 from .file_repository_logging_decorator import FileRepositoryLoggingDecorator
-from .i_agent_binary_repository import IAgentBinaryRepository
+
 from .agent_binary_repository import AgentBinaryRepository
-from .i_agent_configuration_repository import IAgentConfigurationRepository
 from .file_agent_configuration_repository import FileAgentConfigurationRepository
-from .i_simulation_repository import ISimulationRepository
 from .file_simulation_repository import FileSimulationRepository
-from .i_credentials_repository import ICredentialsRepository
 from .mongo_credentials_repository import MongoCredentialsRepository
