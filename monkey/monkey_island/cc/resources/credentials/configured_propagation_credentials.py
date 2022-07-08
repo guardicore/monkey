b26246689e46a4642dfa55c8945dafc9730e65a9
vakarisz <vakarisz@yahoo.com>
2022-07-08 17:05:16 +0300

    Island: Add ConfiguredPropagationCredentials resource

---
+++ monkey/monkey_island/cc/resources/credentials/configured_propagation_credentials.py
@@ -0,0 +1,27 @@
+import logging
+
+from flask import jsonify, request
+
+from common.credentials import Credentials
+from monkey_island.cc.repository import ICredentialsRepository
+from monkey_island.cc.resources.AbstractResource import AbstractResource
+
+logger = logging.getLogger(__name__)
+
+
+class ConfiguredPropagationCredentials(AbstractResource):
+    # API Spec: Resource name should be plural
+    urls = ["/api/propagation-credentials/configured"]
+
+    def __init__(self, credentials_repository: ICredentialsRepository):
+        self._credentials_repository = credentials_repository
+
+    def get(self):
+        return jsonify(self._credentials_repository.get_configured_credentials())
+
+    def post(self):
+        credentials = Credentials.from_mapping(request.json)
+        self._credentials_repository.save_configured_credentials(credentials)
+
+    def delete(self):
+        self._credentials_repository.remove_configured_credentials()
