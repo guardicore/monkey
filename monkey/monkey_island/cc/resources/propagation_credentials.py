f1e00dd3ee3aead26b9560276533e03eb9d9adeb
vakarisz <vakarisz@yahoo.com>
2022-07-12 13:13:49 +0300

    Island: Simplify propagation credentials get return

--- monkey/monkey_island/cc/resources/propagation_credentials.py
+++ monkey/monkey_island/cc/resources/propagation_credentials.py
@@ -26,7 +26,7 @@ class PropagationCredentials(AbstractResource):
         else:
             return {}, HTTPStatus.NOT_FOUND
 
-        return make_response(Credentials.to_json_array(propagation_credentials), HTTPStatus.OK)
+        return propagation_credentials, HTTPStatus.OK
 
     def post(self, collection=None):
         credentials = [Credentials.from_json(c) for c in request.json]
