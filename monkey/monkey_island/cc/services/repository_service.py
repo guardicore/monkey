a4b8c944802f87f7a66cb0081314783729a4f8e0
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-13 08:11:21 -0400

    Island: Add empty RepositoryService

---
+++ monkey/monkey_island/cc/services/repository_service.py
@@ -0,0 +1,12 @@
+class RepositoryService:
+    def reset_agent_configuration(self):
+        pass
+
+    def unlock(self):
+        pass
+
+    def reset_key(self):
+        pass
+
+    def clear_simulation_data(self):
+        pass
