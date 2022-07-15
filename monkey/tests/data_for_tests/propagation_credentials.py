febec2ecef98c2821796f88bf5c401ca5ad9cc58
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-15 09:30:05 -0400

    UT: Use all caps for constants in propagation_credentials.py

--- monkey/tests/data_for_tests/propagation_credentials.py
+++ monkey/tests/data_for_tests/propagation_credentials.py
@@ -1,20 +1,20 @@
 from common.credentials import Credentials, LMHash, NTHash, Password, SSHKeypair, Username
 
-username = "m0nk3y_user"
-special_username = "m0nk3y.user"
-nt_hash = "C1C58F96CDF212B50837BC11A00BE47C"
-lm_hash = "299BD128C1101FD6299BD128C1101FD6"
-password_1 = "trytostealthis"
-password_2 = "password"
+USERNAME = "m0nk3y_user"
+SPECIAL_USERNAME = "m0nk3y.user"
+NT_HASH = "C1C58F96CDF212B50837BC11A00BE47C"
+LM_HASH = "299BD128C1101FD6299BD128C1101FD6"
+PASSWORD_1 = "trytostealthis"
+PASSWORD_2 = "password!"
 PUBLIC_KEY = "MY_PUBLIC_KEY"
 PRIVATE_KEY = "MY_PRIVATE_KEY"
 
-PASSWORD_CREDENTIALS_1 = Credentials(identity=Username(username), secret=Password(password_1))
-PASSWORD_CREDENTIALS_2 = Credentials(identity=Username(username), secret=Password(password_2))
-LM_HASH_CREDENTIALS = Credentials(identity=Username(special_username), secret=LMHash(lm_hash))
-NT_HASH_CREDENTIALS = Credentials(identity=Username(username), secret=NTHash(nt_hash))
+PASSWORD_CREDENTIALS_1 = Credentials(identity=Username(USERNAME), secret=Password(PASSWORD_1))
+PASSWORD_CREDENTIALS_2 = Credentials(identity=Username(USERNAME), secret=Password(PASSWORD_2))
+LM_HASH_CREDENTIALS = Credentials(identity=Username(SPECIAL_USERNAME), secret=LMHash(LM_HASH))
+NT_HASH_CREDENTIALS = Credentials(identity=Username(USERNAME), secret=NTHash(NT_HASH))
 SSH_KEY_CREDENTIALS = Credentials(
-    identity=Username(username), secret=SSHKeypair(PRIVATE_KEY, PUBLIC_KEY)
+    identity=Username(USERNAME), secret=SSHKeypair(PRIVATE_KEY, PUBLIC_KEY)
 )
 
 PROPAGATION_CREDENTIALS = [
