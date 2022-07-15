27c0b838c4aebce02f684e00a50fe0fe524ce062
Ilija Lazoroski <ilija.la@live.com>
2022-07-15 17:40:43 +0200

    Island: Fix one missed telemetry processor

--- monkey/monkey_island/cc/services/telemetry/processing/credentials/credentials_parser.py
+++ monkey/monkey_island/cc/services/telemetry/processing/credentials/credentials_parser.py
@@ -15,10 +15,10 @@ class CredentialsParser:
     def __init__(self, credentials_repository: ICredentialsRepository):
         self._credentials_repository = credentials_repository
 
-    def __call__(self, telemetry_dict):
-        self._parse_credentials(telemetry_dict)
+    def __call__(self, telemetry_dict, _agent_configuration):
+        self._parse_credentials(telemetry_dict, _agent_configuration)
 
-    def _parse_credentials(self, telemetry_dict: Mapping):
+    def _parse_credentials(self, telemetry_dict: Mapping, _agent_configuration):
         credentials = [
             Credentials.from_mapping(credential) for credential in telemetry_dict["data"]
         ]
