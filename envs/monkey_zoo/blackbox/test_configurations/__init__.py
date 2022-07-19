367b6de9015af9d9cea6a7d69b7fcb2c27462b7d
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-19 13:48:06 -0400

    BB: Add powershell_credentials_reuse_test_configuration

--- envs/monkey_zoo/blackbox/test_configurations/__init__.py
+++ envs/monkey_zoo/blackbox/test_configurations/__init__.py
@@ -3,3 +3,4 @@ from .zerologon import zerologon_test_configuration
 from .depth_1_a import depth_1_a_test_configuration
 from .depth_2_a import depth_2_a_test_configuration
 from .depth_3_a import depth_3_a_test_configuration
+from .powershell_credentials_reuse import powershell_credentials_reuse_test_configuration
---
+++ envs/monkey_zoo/blackbox/test_configurations/powershell_credentials_reuse.py
@@ -0,0 +1,40 @@
+from common.configuration import AgentConfiguration, PluginConfiguration
+
+from .noop import noop_test_configuration
+from .utils import (
+    add_exploiters,
+    add_subnets,
+    add_tcp_ports,
+    replace_agent_configuration,
+    set_maximum_depth,
+)
+
+
+def _add_exploiters(agent_configuration: AgentConfiguration) -> AgentConfiguration:
+    brute_force = [
+        PluginConfiguration(name="PowerShellExploiter", options={}),
+    ]
+
+    return add_exploiters(agent_configuration, brute_force=brute_force, vulnerability=[])
+
+
+def _add_subnets(agent_configuration: AgentConfiguration) -> AgentConfiguration:
+    subnets = [
+        "10.2.3.46",
+    ]
+    return add_subnets(agent_configuration, subnets)
+
+
+def _add_tcp_ports(agent_configuration: AgentConfiguration) -> AgentConfiguration:
+    ports = [5985, 5986]
+    return add_tcp_ports(agent_configuration, ports)
+
+
+agent_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 1)
+agent_configuration = _add_exploiters(agent_configuration)
+agent_configuration = _add_subnets(agent_configuration)
+agent_configuration = _add_tcp_ports(agent_configuration)
+
+powershell_credentials_reuse_test_configuration = replace_agent_configuration(
+    noop_test_configuration, agent_configuration
+)
