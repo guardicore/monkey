6000fdcaf1c86b666d139017f4b54d419e6e65e9
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-19 19:01:47 +0530

    Island: Check type of 'key' in EncryptionKey32Bytes

--- monkey/monkey_island/cc/server_utils/encryption/encryption_key_types.py
+++ monkey/monkey_island/cc/server_utils/encryption/encryption_key_types.py
@@ -1,6 +1,8 @@
 class EncryptionKey32Bytes(bytes):
     def __init__(self, key: bytes):
+        if not isinstance(key, bytes):
+            raise TypeError("Key is not of type `bytes`")
         if len(key) == 32:
             self.key = key
         else:
-            raise ValueError("Key size should be 32 bytes.")
+            raise ValueError("Key size should be 32 bytes")
