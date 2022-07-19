d354622f9ad138962fe214d50deb57e9ebae0bfe
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-07-19 13:25:35 -0400

    BB: Add depth_3_a_test_configuration

--- envs/monkey_zoo/blackbox/test_configurations/__init__.py
+++ envs/monkey_zoo/blackbox/test_configurations/__init__.py
@@ -2,3 +2,4 @@ from .test_configuration import TestConfiguration
 from .zerologon import zerologon_test_configuration
 from .depth_1_a import depth_1_a_test_configuration
 from .depth_2_a import depth_2_a_test_configuration
+from .depth_3_a import depth_3_a_test_configuration
