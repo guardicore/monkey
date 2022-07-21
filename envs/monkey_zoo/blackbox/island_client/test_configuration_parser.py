5768bc7620e02a07c2efd2115fd502ca329b3911
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-21 19:25:45 +0530

    BB: Rename IslandConfigParser -> TestConfigurationParser
    Even though the names start with "test", they shouldn't cause any issues
    since we only run "test_blackbox.py".

--- envs/monkey_zoo/blackbox/island_client/test_configuration_parser.py
+++ envs/monkey_zoo/blackbox/island_client/test_configuration_parser.py
@@ -3,7 +3,7 @@ from typing import Iterable
 from envs.monkey_zoo.blackbox.test_configurations.test_configuration import TestConfiguration
 
 
-class IslandConfigParser:
+class TestConfigurationParser:
     @staticmethod
     def get_target_ips(test_configuration: TestConfiguration) -> Iterable[str]:
         return test_configuration.agent_configuration.propagation.network_scan.targets.subnets
