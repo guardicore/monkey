930a3e8a8566a6a50dc09571a5c2ad923646b1d8
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-21 12:09:49 +0530

    BB: Remove unused apply_template_to_config() from IslandConfigParser

--- envs/monkey_zoo/blackbox/island_client/island_config_parser.py
+++ envs/monkey_zoo/blackbox/island_client/island_config_parser.py
@@ -1,22 +1,14 @@
 import json
 
 import dpath.util
-from typing_extensions import Type
 
 from common.configuration.agent_configuration import AgentConfiguration
-from envs.monkey_zoo.blackbox.config_templates.config_template import ConfigTemplate
 
 
 class IslandConfigParser:
     @staticmethod
     def get_serialized_config(agent_configuration: AgentConfiguration) -> str:
         return agent_configuration.to_json()
-
-    @staticmethod
-    def apply_template_to_config(config_template: Type[ConfigTemplate], config: dict) -> dict:
-        for path, value in config_template.config_values.items():
-            dpath.util.set(config, path, value, ".")
-        return config
 
     @staticmethod
     def get_ips_of_targets(raw_config):
