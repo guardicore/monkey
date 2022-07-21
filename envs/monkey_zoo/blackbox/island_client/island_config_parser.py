42efe1fc56a8b63ce6c864f0e9fc4ed350448d18
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-21 19:05:07 +0530

    BB: Remove unused function from IslandConfigParser

--- envs/monkey_zoo/blackbox/island_client/island_config_parser.py
+++ envs/monkey_zoo/blackbox/island_client/island_config_parser.py
@@ -5,9 +5,5 @@ from common.configuration.agent_configuration import AgentConfiguration
 
 class IslandConfigParser:
     @staticmethod
-    def get_serialized_config(agent_configuration: AgentConfiguration) -> str:
-        return agent_configuration.to_json()
-
-    @staticmethod
     def get_target_ips_from_configuration(agent_configuration: AgentConfiguration) -> Iterable:
         return agent_configuration.propagation.network_scan.targets.subnets
