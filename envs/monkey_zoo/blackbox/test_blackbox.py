379ada7a2435b40201e275e9b9cde9e65992f023
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-21 19:25:45 +0530

    BB: Rename IslandConfigParser -> TestConfigurationParser
    Even though the names start with "test", they shouldn't cause any issues
    since we only run "test_blackbox.py".

--- envs/monkey_zoo/blackbox/test_blackbox.py
+++ envs/monkey_zoo/blackbox/test_blackbox.py
@@ -7,8 +7,8 @@ import pytest
 from envs.monkey_zoo.blackbox.analyzers.communication_analyzer import CommunicationAnalyzer
 from envs.monkey_zoo.blackbox.analyzers.zerologon_analyzer import ZerologonAnalyzer
 from envs.monkey_zoo.blackbox.gcp_test_machine_list import GCP_TEST_MACHINE_LIST
-from envs.monkey_zoo.blackbox.island_client.island_config_parser import IslandConfigParser
 from envs.monkey_zoo.blackbox.island_client.monkey_island_client import MonkeyIslandClient
+from envs.monkey_zoo.blackbox.island_client.test_configuration_parser import TestConfigurationParser
 from envs.monkey_zoo.blackbox.log_handlers.test_logs_handler import TestLogsHandler
 from envs.monkey_zoo.blackbox.test_configurations import (
     depth_1_a_test_configuration,
@@ -91,7 +91,7 @@ class TestMonkeyBlackbox:
     ):
         analyzer = CommunicationAnalyzer(
             island_client,
-            IslandConfigParser.get_target_ips(test_configuration),
+            TestConfigurationParser.get_target_ips(test_configuration),
         )
         log_handler = TestLogsHandler(
             test_name, island_client, TestMonkeyBlackbox.get_log_dir_path()
@@ -146,7 +146,7 @@ class TestMonkeyBlackbox:
         zero_logon_analyzer = ZerologonAnalyzer(island_client, expected_creds)
         communication_analyzer = CommunicationAnalyzer(
             island_client,
-            IslandConfigParser.get_target_ips(zerologon_test_configuration),
+            TestConfigurationParser.get_target_ips(zerologon_test_configuration),
         )
         log_handler = TestLogsHandler(
             test_name, island_client, TestMonkeyBlackbox.get_log_dir_path()
