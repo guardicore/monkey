b6703becbc83a71eca0926e05d9fc187677f3b9e
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-22 12:54:20 +0530

    BB: Get rid of TestConfigurationParser and move its functions outside

--- envs/monkey_zoo/blackbox/island_client/test_configuration_parser.py
+++ envs/monkey_zoo/blackbox/island_client/test_configuration_parser.py
@@ -3,7 +3,5 @@ from typing import Iterable
 from envs.monkey_zoo.blackbox.test_configurations.test_configuration import TestConfiguration
 
 
-class TestConfigurationParser:
-    @staticmethod
-    def get_target_ips(test_configuration: TestConfiguration) -> Iterable[str]:
-        return test_configuration.agent_configuration.propagation.network_scan.targets.subnets
+def get_target_ips(test_configuration: TestConfiguration) -> Iterable[str]:
+    return test_configuration.agent_configuration.propagation.network_scan.targets.subnets
