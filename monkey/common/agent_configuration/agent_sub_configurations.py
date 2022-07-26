fad0c5fe4d1727aaf459a69c9098fd16e5e981f2
Shreya Malviya <shreya.malviya@gmail.com>
2022-07-26 16:42:06 +0530

    Common: Add docstring to PluginConfiguration dataclass

--- monkey/common/agent_configuration/agent_sub_configurations.py
+++ monkey/common/agent_configuration/agent_sub_configurations.py
@@ -27,6 +27,25 @@ class CustomPBAConfiguration:
 
 @dataclass(frozen=True)
 class PluginConfiguration:
+    """
+    Dataclass for the configuration of plugins
+
+    :param name: Name of the plugin.
+                 Example: "ransomware"
+    :param options: Any other information/configuration fields relevant to the plugin.
+                    Example: {
+                        "encryption": {
+                            "enabled": True,
+                            "directories": {
+                                "linux_target_dir": "~/this_dir",
+                                "windows_target_dir": "C:\that_dir"
+                            },
+                        },
+                        "other_behaviors": {
+                            "readme": True
+                        },
+                    }
+    """
     name: str
     options: Dict
 
