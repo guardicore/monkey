977860efb2c532ac7073a64843ccc5d561d7d817
Shreya Malviya <shreya.malviya@gmail.com>
2022-08-01 13:33:24 +0530

    Island: Modify IslandLog resource to fetch and return log file contents

--- monkey/monkey_island/cc/resources/island_logs.py
+++ monkey/monkey_island/cc/resources/island_logs.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 from monkey_island.cc.resources.request_authentication import jwt_required
-from monkey_island.cc.services.island_logs import IslandLogService
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +16,11 @@ class IslandLog(AbstractResource):
     @jwt_required
     def get(self):
         try:
-            return IslandLogService.get_log_file()
+            return self._get_log_file_contents()
         except Exception:
             logger.error("Monkey Island logs failed to download", exc_info=True)
+
+    def _get_log_file_contents(self):
+        with open(self._island_log_file_path, "rt") as f:
+            log_file = f.read()
+        return {"log_file": log_file}
