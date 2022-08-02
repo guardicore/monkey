b061613a0b83f527feae24f722337564cf2f372d
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-08-02 12:08:49 -0400

    Island: Add IslandLog to resources/__init__.py

--- monkey/monkey_island/cc/resources/__init__.py
+++ monkey/monkey_island/cc/resources/__init__.py
@@ -1,5 +1,6 @@
 from .remote_run import RemoteRun
 from .agent_binaries import AgentBinaries
 from .clear_simulation_data import ClearSimulationData
+from .island_log import IslandLog
 from .reset_agent_configuration import ResetAgentConfiguration
 from .propagation_credentials import PropagationCredentials
