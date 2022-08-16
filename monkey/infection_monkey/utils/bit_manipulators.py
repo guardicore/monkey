639fb264454180de31119f0401993406e282a409
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-08-16 19:27:56 +0000

    Agent: Improve the speed of bit flipping code
    - Remove a function call
    - Use a generator
    - Use a more efficient flip calculation (subtraction instead of xor)
    
    Issue #2123

--- monkey/infection_monkey/utils/bit_manipulators.py
+++ monkey/infection_monkey/utils/bit_manipulators.py
@@ -1,30 +1,21 @@
+from typing import Iterable
+
+
+def generate_flipped_bits(data: bytes) -> Iterable[int]:
+    """
+    Yield bytes with the bits flipped
+
+    :param data: The data whose bits to flip
+    """
+    for byte in data:
+        yield 255 - byte
+
+
 def flip_bits(data: bytes) -> bytes:
-    flipped_bits = bytearray(len(data))
+    """
+    Flip all bits in the given bytes
 
-    for i, byte in enumerate(data):
-        # TODO: The function call to flip_bits_in_single_byte() adds significant
-        #       overhead. While python is supposed to "inline" function calls
-        #       like this, I've yet to see any changes in runtime that indicate
-        #       this optimization is actually happening.
-        #
-        #       The value of breaking this into separate functions is the unit
-        #       test that tests all possible bytes (0-255). This gives us
-        #       confidence that our bit-flip operation is correct.
-        #
-        #       Remove the flip_bits_in_single_byte() function and rework the
-        #       unit tests so that we still have a high-degree of confidence
-        #       that this code is correct.
-        #
-        #       EDIT: I believe PyPy will attempt to inline functions
-        #       automatically. I don't know that CPython makes any such
-        #       optimizations.
-        flipped_bits[i] = flip_bits_in_single_byte(byte)
-
-    return bytes(flipped_bits)
-
-
-def flip_bits_in_single_byte(byte) -> int:
-    # TODO: The operation `255 - byte` appears to be 12% faster than 255 ^ byte.
-    #       Switch the operator and thoroughly test the ransomware payload to
-    #       ensure this doesn't introduce any defects.
-    return 255 ^ byte
+    :param data: The bytes whose bits to flip
+    :return: Bytes with the bits flipped
+    """
+    return bytes(generate_flipped_bits(data))
