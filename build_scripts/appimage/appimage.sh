a96efcdc7d484071ad3bc113cf77a8a78f705f79
Ilija Lazoroski <ilija.la@live.com>
2022-08-17 14:29:23 +0200

    Build: Downgrade pipenv to use 2022.7.4
    It seems that every time a new version of pipenv is released, it breaks
    our build. See also 01e886f866.

--- build_scripts/appimage/appimage.sh
+++ build_scripts/appimage/appimage.sh
@@ -77,7 +77,7 @@ install_monkey_island_python_dependencies() {
   log_message "Installing island requirements"
 
   log_message "Installing pipenv"
-  "$APPDIR"/AppRun -m pip install pipenv || handle_error
+  "$APPDIR"/AppRun -m pip install pipenv==2022.7.4 || handle_error
 
   log_message "Installing dependencies"
   pushd "$BUILD_DIR/monkey_island" || handle_error
