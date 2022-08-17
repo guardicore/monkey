4f776f0102d0934efdbb88c248b0511fa24fa59f
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-08-17 20:10:23 +0000

    UI: Add field for ransomed file extension

--- monkey/monkey_island/cc/ui/src/components/configuration-components/ValidationErrorMessages.js
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/ValidationErrorMessages.js
@@ -1,4 +1,4 @@
-import {IP, IP_RANGE, VALID_RANSOMWARE_TARGET_PATH_LINUX, VALID_RANSOMWARE_TARGET_PATH_WINDOWS} from './ValidationFormats';
+import { IP, IP_RANGE, VALID_FILE_EXTENSION, VALID_RANSOMWARE_TARGET_PATH_LINUX, VALID_RANSOMWARE_TARGET_PATH_WINDOWS } from './ValidationFormats';
 
 let invalidDirMessage = 'Invalid directory. Path should be absolute or begin with an environment variable.';
 
@@ -10,6 +10,8 @@ export default function transformErrors(errors) {
       error.message = 'Invalid IP range, refer to description for valid examples.'
     } else if (error.name === 'format' && error.params.format === IP) {
       error.message = 'Invalid IP.'
+    } else if (error.name === 'format' && error.params.format === VALID_FILE_EXTENSION) {
+      error.message = 'Invalid file extension.'
     } else if (error.name === 'format' && error.params.format === VALID_RANSOMWARE_TARGET_PATH_LINUX) {
       error.message = invalidDirMessage
     } else if (error.name === 'format' && error.params.format === VALID_RANSOMWARE_TARGET_PATH_WINDOWS) {
