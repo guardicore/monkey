11901b18358a91c52aef2e09cbb2f7d9f43cf4be
Shreya Malviya <shreya.malviya@gmail.com>
2022-08-17 14:44:43 +0530

    UT: Simplify variable logic in MimikatzCredentialCollector's event publishing test

--- monkey/tests/unit_tests/infection_monkey/credential_collectors/test_mimikatz_collector.py
+++ monkey/tests/unit_tests/infection_monkey/credential_collectors/test_mimikatz_collector.py
@@ -121,9 +121,10 @@ def test_pypykatz_result_parsing_no_secrets(monkeypatch):
 
 def test_mimikatz_credentials_stolen_event_published(monkeypatch):
     mock_event_queue = MagicMock(spec=IEventQueue)
-    mock_subscriber = Mock()
+    mock_subscriber = mock_pypykatz_handler = Mock()
     monkeypatch.setattr(
-        "infection_monkey.credential_collectors.mimikatz_collector.pypykatz_handler", Mock()
+        "infection_monkey.credential_collectors.mimikatz_collector.pypykatz_handler",
+        mock_pypykatz_handler,
     )
 
     for event_tag in MIMIKATZ_EVENT_TAGS:
