f418571d9318fb7527404297cb930e8053e6bd3e
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-08-22 10:01:24 -0400

    Common: Add HardwareID type

--- monkey/common/__init__.py
+++ monkey/common/__init__.py
@@ -3,3 +3,4 @@ Used for a common things between agent and island
 """
 from .di_container import DIContainer, UnresolvableDependencyError
 from .operating_system import OperatingSystem
+from . import types
