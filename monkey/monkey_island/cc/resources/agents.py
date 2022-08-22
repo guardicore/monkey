ab399e4f34e8c02bceb6a9566c2f099c98a2e3a6
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-08-22 14:49:31 -0400

    Island: Add debug logging to Agents resource

--- monkey/monkey_island/cc/resources/agents.py
+++ monkey/monkey_island/cc/resources/agents.py
@@ -1,10 +1,13 @@
 import json
+import logging
 from http import HTTPStatus
 
 from flask import make_response, request
 
 from common import AgentRegistrationData
 from monkey_island.cc.resources.AbstractResource import AbstractResource
+
+logger = logging.getLogger(__name__)
 
 
 class Agents(AbstractResource):
@@ -13,7 +16,10 @@ class Agents(AbstractResource):
     def post(self):
         try:
             # Just parse for now
-            AgentRegistrationData(**request.json)
+            agent_registration_data = AgentRegistrationData(**request.json)
+
+            logger.debug(f"Agent registered: {agent_registration_data}")
+
             return make_response({}, HTTPStatus.NO_CONTENT)
         except (TypeError, ValueError, json.JSONDecodeError) as err:
             return make_response(
