6252c9a16beaf57bf6df4a71a5271cfdd14e17ba
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-08-23 17:06:08 +0000

    Agent: Pass ITelemetryMessenger to Ransomware

--- monkey/infection_monkey/payload/ransomware/ransomware_payload.py
+++ monkey/infection_monkey/payload/ransomware/ransomware_payload.py
@@ -2,11 +2,15 @@ import threading
 from typing import Dict
 
 from infection_monkey.payload.i_payload import IPayload
+from infection_monkey.telemetry.messengers.i_telemetry_messenger import ITelemetryMessenger
 
 from . import ransomware_builder
 
 
 class RansomwarePayload(IPayload):
+    def __init__(self, telemetry_messenger: ITelemetryMessenger):
+        self._telemetry_messenger = telemetry_messenger
+
     def run(self, options: Dict, interrupt: threading.Event):
-        ransomware = ransomware_builder.build_ransomware(options)
+        ransomware = ransomware_builder.build_ransomware(options, self._telemetry_messenger)
         ransomware.run(interrupt)
