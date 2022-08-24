43a53112fc2d940cc91d4484fe9688daa9ce35eb
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-08-24 08:55:35 -0400

    Island: Use monkey_island.cc.models.Machine in IMachineRepository

--- monkey/monkey_island/cc/repository/i_machine_repository.py
+++ monkey/monkey_island/cc/repository/i_machine_repository.py
@@ -1,20 +1,19 @@
 from abc import ABC
 from typing import Optional, Sequence
 
+from monkey_island.cc.models import Machine
+
 
 class IMachineRepository(ABC):
-    # TODO define Machine object(ORM model)
-    def save_machine(self, machine: Machine):  # noqa: F821
+    def save_machine(self, machine: Machine):
         pass
 
-    # TODO define Machine object(ORM model)
     # TODO define or re-use machine state.
-    # TODO investigate where should the state be stored in edge or both edge and machine
     def get_machines(
         self,
         id: Optional[str] = None,
         ips: Optional[Sequence[str]] = None,
         state: Optional[MachineState] = None,  # noqa: F821
-        is_island: Optional[bool] = None,  # noqa: F841
-    ) -> Sequence[Machine]:  # noqa: F821
+        is_island: Optional[bool] = None,
+    ) -> Sequence[Machine]:
         pass
