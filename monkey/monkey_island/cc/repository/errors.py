6b083ca61b8e3596ff065cefadfbfa0df51f9f4f
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-08-29 15:15:49 -0400

    Island: Remove superfluous "pass" from repository.errors

--- monkey/monkey_island/cc/repository/errors.py
+++ monkey/monkey_island/cc/repository/errors.py
@@ -3,23 +3,17 @@ class RemovalError(RuntimeError):
     Raised when a repository encounters an error while attempting to remove data.
     """
 
-    pass
-
 
 class RetrievalError(RuntimeError):
     """
     Raised when a repository encounters an error while attempting to retrieve data.
     """
 
-    pass
-
 
 class StorageError(RuntimeError):
     """
     Raised when a repository encounters an error while attempting to store data.
     """
-
-    pass
 
 
 class UnknownRecordError(RuntimeError):
