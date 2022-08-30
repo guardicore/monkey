9a69b83af27738fea57705fa62510416f6ee4939
Shreya Malviya <shreya.malviya@gmail.com>
2022-08-30 23:57:48 +0530

    UT: Simplify assertion logic in test_agent_configuration_endpoint

--- monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration.py
@@ -5,7 +5,6 @@ from tests.common import StubDIContainer
 from tests.common.example_agent_configuration import AGENT_CONFIGURATION
 from tests.monkey_island import InMemoryAgentConfigurationRepository
 from tests.unit_tests.monkey_island.conftest import get_url_for_resource
-from tests.utils import convert_all_lists_to_tuples_in_mapping
 
 from common.agent_configuration import AgentConfiguration
 from monkey_island.cc.repository import IAgentConfigurationRepository
@@ -35,9 +34,7 @@ def test_agent_configuration_endpoint(flask_client):
 
     assert resp.status_code == 200
 
-    assert convert_all_lists_to_tuples_in_mapping(
-        json.loads(resp.data)
-    ) == convert_all_lists_to_tuples_in_mapping(AGENT_CONFIGURATION.copy())
+    assert AgentConfiguration(**json.loads(resp.data)) == AgentConfiguration(**AGENT_CONFIGURATION)
 
 
 def test_agent_configuration_invalid_config(flask_client):
