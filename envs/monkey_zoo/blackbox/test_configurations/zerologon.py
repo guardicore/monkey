434337dce54de955119b8c338e5904fe7b29d3cb
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-01 17:01:05 +0530

    BB: Fix configuration modification logic in zerologon.py

--- envs/monkey_zoo/blackbox/test_configurations/zerologon.py
+++ envs/monkey_zoo/blackbox/test_configurations/zerologon.py
@@ -1,13 +1,7 @@
 from common.agent_configuration import AgentConfiguration, PluginConfiguration
 
 from .noop import noop_test_configuration
-from .utils import (
-    add_exploiters,
-    add_subnets,
-    add_tcp_ports,
-    replace_agent_configuration,
-    set_maximum_depth,
-)
+from .utils import add_exploiters, add_subnets, add_tcp_ports, set_maximum_depth
 
 
 def _add_exploiters(agent_configuration: AgentConfiguration) -> AgentConfiguration:
@@ -27,11 +21,10 @@ def _add_subnets(agent_configuration: AgentConfiguration) -> AgentConfiguration:
     return add_subnets(agent_configuration, subnets)
 
 
-test_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 1)
-test_configuration = _add_exploiters(test_configuration)
-test_configuration = _add_tcp_ports(test_configuration)
-test_configuration = _add_subnets(test_configuration)
+test_agent_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 1)
+test_agent_configuration = _add_exploiters(test_agent_configuration)
+test_agent_configuration = _add_tcp_ports(test_agent_configuration)
+test_agent_configuration = _add_subnets(test_agent_configuration)
 
-zerologon_test_configuration = replace_agent_configuration(
-    noop_test_configuration, test_configuration
-)
+zerologon_test_configuration = noop_test_configuration.copy()
+zerologon_test_configuration.agent_configuration = test_agent_configuration
