b0db917aab826f9d9d36d624eba0ddd3a436a539
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-01 16:55:03 +0530

    BB: Fix configuration modification logic in powershell_credentials_reuse.py

--- envs/monkey_zoo/blackbox/test_configurations/powershell_credentials_reuse.py
+++ envs/monkey_zoo/blackbox/test_configurations/powershell_credentials_reuse.py
@@ -1,13 +1,7 @@
 from common.agent_configuration import AgentConfiguration, PluginConfiguration
 
 from .noop import noop_test_configuration
-from .utils import (
-    add_exploiters,
-    add_subnets,
-    add_tcp_ports,
-    replace_agent_configuration,
-    set_maximum_depth,
-)
+from .utils import add_exploiters, add_subnets, add_tcp_ports, set_maximum_depth
 
 
 def _add_exploiters(agent_configuration: AgentConfiguration) -> AgentConfiguration:
@@ -30,11 +24,10 @@ def _add_tcp_ports(agent_configuration: AgentConfiguration) -> AgentConfiguratio
     return add_tcp_ports(agent_configuration, ports)
 
 
-test_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 1)
-test_configuration = _add_exploiters(test_configuration)
-test_configuration = _add_subnets(test_configuration)
-test_configuration = _add_tcp_ports(test_configuration)
+test_agent_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 1)
+test_agent_configuration = _add_exploiters(test_agent_configuration)
+test_agent_configuration = _add_subnets(test_agent_configuration)
+test_agent_configuration = _add_tcp_ports(test_agent_configuration)
 
-powershell_credentials_reuse_test_configuration = replace_agent_configuration(
-    noop_test_configuration, test_configuration
-)
+powershell_credentials_reuse_test_configuration = noop_test_configuration.copy()
+powershell_credentials_reuse_test_configuration.agent_configuration = test_agent_configuration
