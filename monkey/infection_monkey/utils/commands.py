21cfe30dce6f4e75f4ab33628775b0d91c28b7d2
Ilija Lazoroski <ilija.la@live.com>
2022-09-02 14:35:13 +0200

    Agent: Modify agent build command line to accept list of servers

--- monkey/infection_monkey/utils/commands.py
+++ monkey/infection_monkey/utils/commands.py
@@ -1,19 +1,20 @@
+from typing import List, Optional
+
 from infection_monkey.config import GUID
 from infection_monkey.exploit.tools.helpers import AGENT_BINARY_PATH_LINUX, AGENT_BINARY_PATH_WIN64
 from infection_monkey.model import CMD_CARRY_OUT, CMD_EXE, MONKEY_ARG
-from infection_monkey.model.host import VictimHost
 
 # Dropper target paths
 DROPPER_TARGET_PATH_LINUX = AGENT_BINARY_PATH_LINUX
 DROPPER_TARGET_PATH_WIN64 = AGENT_BINARY_PATH_WIN64
 
 
-def build_monkey_commandline(target_host: VictimHost, depth: int, location: str = None) -> str:
+def build_monkey_commandline(servers: List[str], depth: int, location: Optional[str] = None) -> str:
 
     return " " + " ".join(
         build_monkey_commandline_explicitly(
             GUID,
-            target_host.default_server,
+            servers,
             depth,
             location,
         )
@@ -21,19 +22,19 @@ def build_monkey_commandline(target_host: VictimHost, depth: int, location: str 
 
 
 def build_monkey_commandline_explicitly(
-    parent: str = None,
-    server: str = None,
-    depth: int = None,
-    location: str = None,
-) -> list:
+    parent: Optional[str] = None,
+    servers: Optional[List[str]] = None,
+    depth: Optional[int] = None,
+    location: Optional[str] = None,
+) -> List[str]:
     cmdline = []
 
     if parent is not None:
         cmdline.append("-p")
         cmdline.append(str(parent))
-    if server is not None:
+    if servers:
         cmdline.append("-s")
-        cmdline.append(str(server))
+        cmdline.append(",".join(servers))
     if depth is not None:
         cmdline.append("-d")
         cmdline.append(str(depth))
@@ -44,13 +45,13 @@ def build_monkey_commandline_explicitly(
     return cmdline
 
 
-def get_monkey_commandline_windows(destination_path: str, monkey_cmd_args: list) -> list:
+def get_monkey_commandline_windows(destination_path: str, monkey_cmd_args: List[str]) -> List[str]:
     monkey_cmdline = [CMD_EXE, CMD_CARRY_OUT, destination_path, MONKEY_ARG]
 
     return monkey_cmdline + monkey_cmd_args
 
 
-def get_monkey_commandline_linux(destination_path: str, monkey_cmd_args: list) -> list:
+def get_monkey_commandline_linux(destination_path: str, monkey_cmd_args: List[str]) -> List[str]:
     monkey_cmdline = [destination_path, MONKEY_ARG]
 
     return monkey_cmdline + monkey_cmd_args
