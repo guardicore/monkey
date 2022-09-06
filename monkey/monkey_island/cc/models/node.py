b33fd7f4369e8982ece9bb55e1d8bead95697a9f
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-06 08:33:17 -0400

    Island: Add ConnectionTarget TypeAlias

--- monkey/monkey_island/cc/models/node.py
+++ monkey/monkey_island/cc/models/node.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Tuple
+from typing import Collection, Sequence, Tuple, TypeAlias
 
 from pydantic import Field, validator
 
@@ -7,12 +7,12 @@ from common.transforms import make_immutable_nested_sequence
 
 from . import CommunicationType, MachineID
 
-ConnectionsSequence = Sequence[Tuple[MachineID, Sequence[CommunicationType]]]
+ConnectionTarget: TypeAlias = Tuple[MachineID, Sequence[CommunicationType]]
 
 
 class Node(MutableInfectionMonkeyBaseModel):
     machine_id: MachineID = Field(..., allow_mutation=False)
-    connections: ConnectionsSequence
+    connections: Collection[ConnectionTarget]
 
     _make_immutable_nested_sequence = validator("connections", pre=True, allow_reuse=True)(
         make_immutable_nested_sequence
