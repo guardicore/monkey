1036189fcc26b17b874a7ace08b4d472ec163bc0
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-06 15:01:10 -0400

    Island: Rename _pypubsub_publisher_wrappe{d,r}

--- monkey/monkey_island/cc/event_queue/pypubsub_island_event_queue.py
+++ monkey/monkey_island/cc/event_queue/pypubsub_island_event_queue.py
@@ -12,17 +12,17 @@ logger = logging.getLogger(__name__)
 
 class PyPubSubIslandEventQueue(IIslandEventQueue):
     def __init__(self, pypubsub_publisher: Publisher):
-        self._pypubsub_publisher_wrapped = PyPubSubPublisherWrapper(pypubsub_publisher)
+        self._pypubsub_publisher_wrapper = PyPubSubPublisherWrapper(pypubsub_publisher)
 
     def subscribe(self, topic: IslandEventTopic, subscriber: IslandEventSubscriber):
         topic_name = topic.name  # needs to be a string for pypubsub
-        self._pypubsub_publisher_wrapped.subscribe(topic_name, subscriber)
+        self._pypubsub_publisher_wrapper.subscribe(topic_name, subscriber)
 
     def publish(self, topic: IslandEventTopic, event: Any = None):
         topic_name = topic.name  # needs to be a string for pypubsub
         logger.debug(f"Publishing {topic_name} event")
 
         if event is None:
-            self._pypubsub_publisher_wrapped.publish(topic_name)
+            self._pypubsub_publisher_wrapper.publish(topic_name)
         else:
-            self._pypubsub_publisher_wrapped.publish(topic_name, event=event)
+            self._pypubsub_publisher_wrapper.publish(topic_name, event=event)
