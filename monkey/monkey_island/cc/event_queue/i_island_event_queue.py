f2e7a3d66f12c285efa52bd0599110b835c62fc3
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-06 18:58:33 +0530

    Island: Add Island event queue stuff to monkey_island/

---
+++ monkey/monkey_island/cc/event_queue/i_island_event_queue.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Any
+
+from . import IslandEventSubscriber
+
+IslandEventTopic = Enum(
+    "IslandEventTopic", ["AGENT_CONNECTED", "CLEAR_SIMULATION_DATA", "RESET_AGENT_CONFIGURATION"]
+)
+
+
+class IIslandEventQueue(ABC):
+    """
+    Manages subscription and publishing of events in the Island
+    """
+
+    @abstractmethod
+    def subscribe(self, topic: IslandEventTopic, subscriber: IslandEventSubscriber):
+        """
+        Subscribes a subscriber to the specified event topic
+
+        :param topic: Event topic to which the subscriber should subscribe
+        :param subscriber: A subscriber that will receive events
+        """
+
+        pass
+
+    @abstractmethod
+    def publish(self, topic: IslandEventTopic, event: Any = None):
+        """
+        Publishes an event topic with the given data
+
+        :param topic: Event topic to publish
+        :param event: Event to pass to subscribers with the event publish
+        """
+
+        pass
---
+++ monkey/monkey_island/cc/event_queue/types.py
@@ -0,0 +1,3 @@
+from typing import Callable
+
+IslandEventSubscriber = Callable[..., None]
