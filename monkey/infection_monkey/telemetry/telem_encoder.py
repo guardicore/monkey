bcf2f6361a8cca8457bc0e2570c30bbdfff7147b
vakaris_zilius <vakarisz@yahoo.com>
2022-09-06 12:54:24 +0000

    Agent: Use SecretStr and SecretBytes instead of SecretField
    SecretField is a base type for secrets, but it doesn't seem to be exported. When the agent is ran, this import fails

--- monkey/infection_monkey/telemetry/telem_encoder.py
+++ monkey/infection_monkey/telemetry/telem_encoder.py
@@ -1,6 +1,6 @@
 import json
 
-from pydantic import BaseModel, SecretField
+from pydantic import BaseModel, SecretBytes, SecretStr
 
 from common import OperatingSystem
 
@@ -11,6 +11,6 @@ class TelemetryJSONEncoder(json.JSONEncoder):
             return obj.name
         if issubclass(type(obj), BaseModel):
             return obj.dict(simplify=True)
-        if issubclass(type(obj), SecretField):
+        if issubclass(type(obj), (SecretStr, SecretBytes)):
             return obj.get_secret_value()
         return json.JSONEncoder.default(self, obj)
