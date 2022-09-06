12d1aa2ff8da569700d35f98f8ba02ab459e0cf3
vakaris_zilius <vakarisz@yahoo.com>
2022-09-06 11:33:38 +0000

    Island, Agent: Change json encryptors to handle pydantic models

--- monkey/infection_monkey/telemetry/telem_encoder.py
+++ monkey/infection_monkey/telemetry/telem_encoder.py
@@ -1,5 +1,7 @@
 import json
 
+from pydantic import BaseModel, SecretField
+
 from common import OperatingSystem
 
 
@@ -7,4 +9,8 @@ class TelemetryJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, OperatingSystem):
             return obj.name
+        if issubclass(type(obj), BaseModel):
+            return obj.dict(simplify=True)
+        if issubclass(type(obj), SecretField):
+            return obj.get_secret_value()
         return json.JSONEncoder.default(self, obj)
