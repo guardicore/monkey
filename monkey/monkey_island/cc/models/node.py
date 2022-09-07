35ecdb11ab01dee4e74d7f9f25950ec84db93c15
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-07 09:39:39 -0400

    Island: Add docstrings to Node model

--- monkey/monkey_island/cc/models/node.py
+++ monkey/monkey_island/cc/models/node.py
@@ -10,5 +10,16 @@ NodeConnections: TypeAlias = Mapping[MachineID, Tuple[CommunicationType, ...]]
 
 
 class Node(MutableInfectionMonkeyBaseModel):
+    """
+    A network node and its outbound connections/communications
+
+    A node is identified by a MachineID and tracks all outbound communication to other machines on
+    the network. This is particularly useful for creating graphs of Infection Monkey's activity
+    throughout the network.
+    """
+
     machine_id: MachineID = Field(..., allow_mutation=False)
+    """The MachineID of the node (source)"""
+
     connections: NodeConnections
+    """All outbound connections from this node to other machines"""
