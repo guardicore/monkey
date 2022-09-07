360600be39e17c4bb004accb5c77d10e2ed545ce
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-09-07 14:25:57 +0000

    Agent: No need to check relay existence

--- monkey/infection_monkey/monkey.py
+++ monkey/infection_monkey/monkey.py
@@ -192,9 +192,7 @@ class InfectionMonkey:
             client_disconnect_timeout=config.keep_tunnel_open_time,
         )
 
-        if self._relay and maximum_depth_reached(
-            config.propagation.maximum_depth, self._current_depth
-        ):
+        if maximum_depth_reached(config.propagation.maximum_depth, self._current_depth):
             self._relay.start()
 
         StateTelem(is_done=False, version=get_version()).send()
