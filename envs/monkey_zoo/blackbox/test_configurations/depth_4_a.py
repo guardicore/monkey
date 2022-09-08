72c76319d1aa95c950f970cc5d9d38f253df5597
vakarisz <vakarisz@yahoo.com>
2022-09-08 16:58:14 +0300

    BB: Move tunneling to depth 4a test suite

--- envs/monkey_zoo/blackbox/test_configurations/depth_4_a.py
+++ envs/monkey_zoo/blackbox/test_configurations/depth_4_a.py
@@ -1,7 +1,7 @@
 import dataclasses
 
 from common.agent_configuration import AgentConfiguration, PluginConfiguration
-from common.credentials import Credentials, NTHash, Password, Username
+from common.credentials import Credentials, Password, Username
 
 from .noop import noop_test_configuration
 from .utils import (
@@ -15,13 +15,12 @@ from .utils import (
 )
 
 # Tests:
-#     Powershell (10.2.3.45, 10.2.3.46, 10.2.3.47, 10.2.3.48)
-#     WMI pass the hash (10.2.2.15)
+#     Tunneling (SSH brute force) (10.2.2.9, 10.2.1.10, 10.2.0.12, 10.2.0.13)
 
 
 def _add_exploiters(agent_configuration: AgentConfiguration) -> AgentConfiguration:
     brute_force = [
-        PluginConfiguration(name="PowerShellExploiter", options={}),
+        PluginConfiguration(name="SSHExploiter", options={}),
         PluginConfiguration(name="WmiExploiter", options={"smb_download_timeout": 30}),
     ]
 
@@ -30,21 +29,20 @@ def _add_exploiters(agent_configuration: AgentConfiguration) -> AgentConfigurati
 
 def _add_subnets(agent_configuration: AgentConfiguration) -> AgentConfiguration:
     subnets = [
-        "10.2.3.45",
-        "10.2.3.46",
-        "10.2.3.47",
-        "10.2.3.48",
-        "10.2.2.15",
+        "10.2.2.9",
+        "10.2.1.10",
+        "10.2.0.12",
+        "10.2.2.13",
     ]
     return add_subnets(agent_configuration, subnets)
 
 
 def _add_tcp_ports(agent_configuration: AgentConfiguration) -> AgentConfiguration:
-    ports = [135, 5985, 5986]
+    ports = [22, 135, 5985, 5986]
     return add_tcp_ports(agent_configuration, ports)
 
 
-test_agent_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 3)
+test_agent_configuration = set_maximum_depth(noop_test_configuration.agent_configuration, 4)
 test_agent_configuration = set_keep_tunnel_open_time(test_agent_configuration, 20)
 test_agent_configuration = _add_exploiters(test_agent_configuration)
 test_agent_configuration = _add_subnets(test_agent_configuration)
@@ -52,17 +50,16 @@ test_agent_configuration = _add_tcp_ports(test_agent_configuration)
 
 CREDENTIALS = (
     Credentials(Username("m0nk3y"), None),
-    Credentials(Username("m0nk3y-user"), None),
-    Credentials(None, Password("Passw0rd!")),
-    Credentials(None, NTHash("d0f0132b308a0c4e5d1029cc06f48692")),
-    Credentials(None, NTHash("5da0889ea2081aa79f6852294cba4a5e")),
-    Credentials(None, NTHash("50c9987a6bf1ac59398df9f911122c9b")),
+    Credentials(None, Password("3Q=(Ge(+&w]*")),
+    Credentials(None, Password("`))jU7L(w}")),
+    Credentials(None, Password("prM2qsroTI")),
+    Credentials(None, Password("t67TC5ZDmz")),
 )
 
-depth_3_a_test_configuration = dataclasses.replace(noop_test_configuration)
+depth_4_a_test_configuration = dataclasses.replace(noop_test_configuration)
 replace_agent_configuration(
-    test_configuration=depth_3_a_test_configuration, agent_configuration=test_agent_configuration
+    test_configuration=depth_4_a_test_configuration, agent_configuration=test_agent_configuration
 )
 replace_propagation_credentials(
-    test_configuration=depth_3_a_test_configuration, propagation_credentials=CREDENTIALS
+    test_configuration=depth_4_a_test_configuration, propagation_credentials=CREDENTIALS
 )
