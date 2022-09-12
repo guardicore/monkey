f9b1c5ebd2572c55c6f5236b86f23c0ca6027548
Ilija Lazoroski <ilija.la@live.com>
2022-09-12 13:18:06 +0200

    Island: Export reset_agent_configuration from __init__

--- monkey/monkey_island/cc/services/__init__.py
+++ monkey/monkey_island/cc/services/__init__.py
@@ -3,3 +3,4 @@ from .authentication_service import AuthenticationService
 from .aws import AWSService
 from .island_mode_service import IslandModeService
 from .repository_service import RepositoryService
+from .reset_agent_configuration import reset_agent_configuration
