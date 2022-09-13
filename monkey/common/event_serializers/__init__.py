5905d3bf5b0cbfeeaa05237f28168480e17338ab
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-13 15:02:07 -0400

    Common: Add register_common_agent_event_serializers()

--- monkey/common/event_serializers/__init__.py
+++ monkey/common/event_serializers/__init__.py
@@ -1,3 +1,4 @@
 from .i_event_serializer import IEventSerializer, JSONSerializable
 from .event_serializer_registry import EventSerializerRegistry
 from .pydantic_event_serializer import PydanticEventSerializer
+from .register import register_common_agent_event_serializers
