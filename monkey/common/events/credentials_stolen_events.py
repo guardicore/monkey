a68a765722568fdef452dfeea7e2a82c4ace714f
Ilija Lazoroski <ilija.la@live.com>
2022-09-13 17:31:34 +0200

    Common: Make CredentialsStolenEvent to be a class

--- monkey/common/events/credentials_stolen_events.py
+++ monkey/common/events/credentials_stolen_events.py
@@ -1,14 +1,12 @@
-from dataclasses import field
 from typing import Sequence
 
-import pydantic
+from pydantic import Field
 
 from common.credentials import Credentials
 
 from . import AbstractAgentEvent
 
 
-@pydantic.dataclasses.dataclass(frozen=True)
 class CredentialsStolenEvent(AbstractAgentEvent):
     """
     An event that occurs when an agent collects credentials from the victim
@@ -17,4 +15,4 @@ class CredentialsStolenEvent(AbstractAgentEvent):
         :param stolen_credentials: The credentials that were stolen by an agent
     """
 
-    stolen_credentials: Sequence[Credentials] = field(default_factory=list)
+    stolen_credentials: Sequence[Credentials] = Field(default_factory=list)
