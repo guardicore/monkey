599c753cf7a80534c09af87670150f03f1c857da
Ilija Lazoroski <ilija.la@live.com>
2022-09-13 09:31:38 +0200

    Island: Add docstrings to reset_agent_configuration event handler

--- monkey/monkey_island/cc/island_event_handlers/reset_agent_configuration.py
+++ monkey/monkey_island/cc/island_event_handlers/reset_agent_configuration.py
@@ -2,6 +2,11 @@ from monkey_island.cc.repository import IAgentConfigurationRepository, IFileRepo
 
 
 class reset_agent_configuration:
+    """
+    Callable class that handles removal of PBA files and reset of agent configuration
+    on the Island.
+    """
+
     def __init__(
         self,
         agent_configuration_repository: IAgentConfigurationRepository,
@@ -15,6 +20,9 @@ class reset_agent_configuration:
         self._agent_configuration_repository.reset_to_default()
 
     def _remove_pba_files(self):
+        """
+        Removes PBA files from the file repository
+        """
         agent_configuration = self._agent_configuration_repository.get_configuration()
         custom_pbas = agent_configuration.custom_pbas
 
