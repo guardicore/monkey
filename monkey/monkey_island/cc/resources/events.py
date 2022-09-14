bb0910371cbf1ee01e07bf700aa190fb7007c2b8
Ilija Lazoroski <ilija.la@live.com>
2022-09-14 11:52:16 +0200

    Island: Refactor api/events to publish list of events
    PR #2285

--- monkey/monkey_island/cc/resources/events.py
+++ monkey/monkey_island/cc/resources/events.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 
 from flask import request
 
+from common.event_queue import IAgentEventQueue
+from common.event_serializers import EVENT_TYPE_FIELD, EventSerializerRegistry
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 
 logger = logging.getLogger(__name__)
@@ -11,10 +13,26 @@ logger = logging.getLogger(__name__)
 class Events(AbstractResource):
     urls = ["/api/events"]
 
+    def __init__(
+        self,
+        agent_event_queue: IAgentEventQueue,
+        event_serializer_registry: EventSerializerRegistry,
+    ):
+        self._agent_event_queue = agent_event_queue
+        self._event_serializer_registry = event_serializer_registry
+
     # Agents needs this
     def post(self):
-        event = request.json
+        events = request.json
 
-        logger.info(f"Event: {event}")
+        for event in events:
+            try:
+                serializer = self._event_serializer_registry[event[EVENT_TYPE_FIELD]]
+                deserialized_event = serializer.deserialize(event)
+            except (TypeError, ValueError) as err:
+                logger.debug(f"Error occured deserializing an event {event}: {err}")
+                return {"error": str(err)}, HTTPStatus.BAD_REQUEST
+
+            self._agent_event_queue.publish(deserialized_event)
 
         return {}, HTTPStatus.NO_CONTENT
