b3e9bbe05f26aa6898c4ea3a3e5859d6e6535b18
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-14 13:54:38 -0400

    Island: Subscribe save_stolen_credentials_to_repository()

--- monkey/monkey_island/cc/setup/agent_event_handlers.py
+++ monkey/monkey_island/cc/setup/agent_event_handlers.py
@@ -1,7 +1,11 @@
 from common import DIContainer
 from common.event_queue import IAgentEventQueue
-from monkey_island.cc.agent_event_subscribers import save_event_to_event_repository
-from monkey_island.cc.repository import IEventRepository
+from common.events import CredentialsStolenEvent
+from monkey_island.cc.agent_event_subscribers import (
+    save_event_to_event_repository,
+    save_stolen_credentials_to_repository,
+)
+from monkey_island.cc.repository import ICredentialsRepository, IEventRepository
 
 
 def setup_agent_event_handlers(container: DIContainer):
@@ -14,3 +18,8 @@ def _subscribe_and_store_to_event_repository(container: DIContainer):
     event_repository = container.resolve(IEventRepository)
     save_event_subscriber = save_event_to_event_repository(event_repository)
     agent_event_queue.subscribe_all_events(save_event_subscriber)
+
+    save_stolen_credentials_subscriber = save_stolen_credentials_to_repository(
+        container.resolve(ICredentialsRepository)
+    )
+    agent_event_queue.subscribe_type(CredentialsStolenEvent, save_stolen_credentials_subscriber)
