940b479ca2dcbf2823299d5976049707c48d110f
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-16 12:30:13 +0530

    Common: Rename event_serializer_registry.py -> agent_event_serializer_registry.py

--- monkey/common/event_serializers/__init__.py
+++ monkey/common/event_serializers/__init__.py
@@ -1,5 +1,5 @@
 from .consts import EVENT_TYPE_FIELD
 from .i_agent_event_serializer import IAgentEventSerializer, JSONSerializable
-from .event_serializer_registry import AgentEventSerializerRegistry
+from .agent_event_serializer_registry import AgentEventSerializerRegistry
 from .pydantic_event_serializer import PydanticEventSerializer
 from .register import register_common_agent_event_serializers
