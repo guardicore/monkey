1b4f1dba4e626752b0b5cf17ca4ae55abab79ec8
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-16 08:16:46 -0400

    Agent: Change the order of COMMON_PORTS
    COMMON_PORTS should be ordered with the the most likely to succeed ports
    first.

--- monkey/infection_monkey/network/ports.py
+++ monkey/infection_monkey/network/ports.py
@@ -1,15 +1,15 @@
 from typing import List
 
 COMMON_PORTS: List[int] = [
+    8080,  # http-proxy
+    3306,  # mysql
+    3389,  # Windows Terminal Server (RDP)
+    8888,  # sun-answerbook
     1025,  # NFS, IIS
     1433,  # Microsoft SQL Server
     1434,  # Microsoft SQL Monitor
     1720,  # h323q931
     1723,  # Microsoft PPTP VPN
-    3306,  # mysql
-    3389,  # Windows Terminal Server (RDP)
     5900,  # vnc
     6001,  # X11:1
-    8080,  # http-proxy
-    8888,  # sun-answerbook
 ]
