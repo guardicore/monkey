99366052421be4074cfcc3c540f399e642c22fde
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-19 19:21:03 -0400

    Agent: Add IIslandAPIClient.send_log()

--- monkey/infection_monkey/island_api_client/i_island_api_client.py
+++ monkey/infection_monkey/island_api_client/i_island_api_client.py
@@ -17,3 +17,16 @@ class IIslandAPIClient(ABC):
         :raises IslandAPIError: If an unexpected error occurs while attempting to connect to the
                                 island
         """
+
+    @abstractmethod
+    def send_log(self, log_contents: str):
+        """
+        Send the contents of the agent's log to the island
+
+        :param log_contents: The contents of the agent's log
+        :raises IslandAPIConnectionError: If a connection cannot be made to the island
+        :raises IslandAPITimeoutError: If a timeout occurs while attempting to connect to the island
+                                       or send the log contents
+        :raises IslandAPIError: If an unexpected error occurred while attempting to send the
+                                contents of the agent's log to the island
+        """
