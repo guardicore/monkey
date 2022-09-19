94dc2288cd3cc0f867c556dad1290796aed240aa
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-19 17:06:27 +0530

    Project: Change IslandApiClient -> IslandAPIClient in Vulture allowlist

--- vulture_allowlist.py
+++ vulture_allowlist.py
@@ -9,7 +9,7 @@ from common.agent_configuration.agent_sub_configurations import (
 )
 from common.credentials import Credentials, LMHash, NTHash
 from infection_monkey.exploit.log4shell_utils.ldap_server import LDAPServerFactory
-from infection_monkey.transport import IslandApiClient
+from infection_monkey.transport import IslandAPIClient
 from infection_monkey.transport.island_api_client_errors import IslandAPIRequestFailedError
 from monkey_island.cc.event_queue import IslandEventTopic, PyPubSubIslandEventQueue
 from monkey_island.cc.models import Report
@@ -332,5 +332,5 @@ IslandEventTopic.CLEAR_SIMULATION_DATA
 IslandEventTopic.RESET_AGENT_CONFIGURATION
 
 # TODO: Remove after #2292 is closed
-IslandApiClient
+IslandAPIClient
 IslandAPIRequestFailedError
