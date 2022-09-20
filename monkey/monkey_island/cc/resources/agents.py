d514ac283c8ae6eb74489ea27ea158442c229823
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-20 15:01:00 -0400

    Island: Publish AgentRegistrationData from Agents resource

--- monkey/monkey_island/cc/resources/agents.py
+++ monkey/monkey_island/cc/resources/agents.py
@@ -5,6 +5,7 @@ from http import HTTPStatus
 from flask import make_response, request
 
 from common import AgentRegistrationData
+from monkey_island.cc.event_queue import IIslandEventQueue, IslandEventTopic
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 
 logger = logging.getLogger(__name__)
@@ -13,12 +14,18 @@ logger = logging.getLogger(__name__)
 class Agents(AbstractResource):
     urls = ["/api/agents"]
 
+    def __init__(self, island_event_queue: IIslandEventQueue):
+        self._island_event_queue = island_event_queue
+
     def post(self):
         try:
             # Just parse for now
             agent_registration_data = AgentRegistrationData(**request.json)
 
             logger.debug(f"Agent registered: {agent_registration_data}")
+            self._island_event_queue(
+                IslandEventTopic.AGENT_CONNECTED, agent_registration_data=agent_registration_data
+            )
 
             return make_response({}, HTTPStatus.NO_CONTENT)
         except (TypeError, ValueError, json.JSONDecodeError) as err:
