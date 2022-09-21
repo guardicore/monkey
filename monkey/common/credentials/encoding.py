4f04cf1c18ee5e51a36a736bb002e6ee599baa75
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-09-21 17:07:11 +0000

    Common: Fix mypy error in encoding.py

--- monkey/common/credentials/encoding.py
+++ monkey/common/credentials/encoding.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 from pydantic import SecretBytes, SecretStr
 
 
-def get_plaintext(secret: Union[SecretStr, SecretBytes, None, str]) -> Optional[str]:
+def get_plaintext(secret: Union[SecretStr, SecretBytes, None, str]) -> Optional[Union[str, bytes]]:
     if isinstance(secret, (SecretStr, SecretBytes)):
         return secret.get_secret_value()
     else:
