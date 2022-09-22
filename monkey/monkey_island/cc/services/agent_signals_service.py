a04a6a3cea0c5f6b06f5626eae3fc4a33c74662f
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-22 13:35:16 -0400

    Island: Implement AgentSignalsService.get_signals()

--- monkey/monkey_island/cc/services/agent_signals_service.py
+++ monkey/monkey_island/cc/services/agent_signals_service.py
@@ -1,13 +1,17 @@
 from datetime import datetime
+from typing import Optional
 
 from common.types import AgentID
-from monkey_island.cc.repository import ISimulationRepository
 from monkey_island.cc.models import AgentSignals
+from monkey_island.cc.repository import IAgentRepository, ISimulationRepository
 
 
 class AgentSignalsService:
-    def __init__(self, simulation_repository: ISimulationRepository):
+    def __init__(
+        self, simulation_repository: ISimulationRepository, agent_repository: IAgentRepository
+    ):
         self._simulation_repository = simulation_repository
+        self._agent_repository = agent_repository
 
     def get_signals(self, agent_id: AgentID) -> AgentSignals:
         """
@@ -16,7 +20,24 @@ class AgentSignalsService:
         :param agent_id: The ID of the agent whose signals need to be retrieved
         :return: Signals sent to the relevant agent
         """
-        return AgentSignals(timestamp=datetime.now())
+        terminate_timestamp = self._get_terminate_signal_timestamp(agent_id)
+        return AgentSignals(terminate=terminate_timestamp)
+
+    def _get_terminate_signal_timestamp(self, agent_id: AgentID) -> Optional[datetime]:
+        simulation = self._simulation_repository.get_simulation()
+        terminate_all_signal_time = simulation.terminate_signal_time
+        if terminate_all_signal_time is None:
+            return None
+
+        agent = self._agent_repository.get_agent_by_id(agent_id)
+        if agent.start_time <= terminate_all_signal_time:
+            return terminate_all_signal_time
+
+        progenitor = self._agent_repository.get_progenitor(agent)
+        if progenitor.start_time <= terminate_all_signal_time:
+            return terminate_all_signal_time
+
+        return None
 
     def on_terminate_agents_signal(self, timestamp: datetime):
         """
