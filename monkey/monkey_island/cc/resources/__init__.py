645e03e46f896f1333c5124cd97c2a5c99dfc434
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-23 17:59:31 +0530

    Island: Import TerminateAllAgents in cc/resources/__init__.py

--- monkey/monkey_island/cc/resources/__init__.py
+++ monkey/monkey_island/cc/resources/__init__.py
@@ -11,3 +11,4 @@ from .pba_file_download import PBAFileDownload
 from .agent_events import AgentEvents
 from .agents import Agents
 from .agent_signals import AgentSignals
+from .terminate_all_agents import TerminateAllAgents
