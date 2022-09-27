01f1d62272e4e6996cde9c3cdea4ee99594b9ab4
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-27 16:19:23 +0530

    UT: Simplify logic in test data in test_http_island_api_client.py

--- monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_island_api_client.py
+++ monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_island_api_client.py
@@ -30,7 +30,7 @@ AGENT_REGISTRATION = AgentRegistrationData(
     machine_hardware_id=1,
     start_time=0,
     parent_id=None,
-    cc_server=str(SERVER),
+    cc_server=SERVER,
     network_interfaces=[],
 )
 
