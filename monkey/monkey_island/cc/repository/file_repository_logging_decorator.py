d5b62651a08cc78fd2539df27e676d28f2afcb98
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-28 16:23:47 +0530

    Island: Implement delete_files_by_pattern() in FileRepositoryLoggingDecorator

--- monkey/monkey_island/cc/repository/file_repository_logging_decorator.py
+++ monkey/monkey_island/cc/repository/file_repository_logging_decorator.py
@@ -26,6 +26,10 @@ class FileRepositoryLoggingDecorator(IFileRepository):
         logger.debug(f"Deleting file {unsafe_file_name}")
         return self._file_repository.delete_file(unsafe_file_name)
 
+    def delete_files_by_pattern(self, file_name_pattern: str):
+        logger.debug(f'Deleting files whose names match the pattern "{file_name_pattern}"')
+        return self._file_repository.delete_files_by_pattern(file_name_pattern)
+
     def delete_all_files(self):
         logger.debug("Deleting all files in the repository")
         return self._file_repository.delete_all_files()
