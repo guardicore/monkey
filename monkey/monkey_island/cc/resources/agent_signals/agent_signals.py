10954e0a6eb47a59c17b9e9ef580126c4adb7db6
Ilija Lazoroski <ilija.la@live.com>
2022-09-28 16:45:28 +0200

    Island: Use UUID for AgentSignals endpoint parameter

--- monkey/monkey_island/cc/resources/agent_signals/agent_signals.py
+++ monkey/monkey_island/cc/resources/agent_signals/agent_signals.py
@@ -1,6 +1,7 @@
 import logging
 from http import HTTPStatus
 
+from common.types import AgentID
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 from monkey_island.cc.services import AgentSignalsService
 
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class AgentSignals(AbstractResource):
-    urls = ["/api/agent-signals/<string:agent_id>"]
+    urls = ["/api/agent-signals/<uuid:agent_id>"]
 
     def __init__(
         self,
@@ -16,6 +17,6 @@ class AgentSignals(AbstractResource):
     ):
         self._agent_signals_service = agent_signals_service
 
-    def get(self, agent_id: str):
+    def get(self, agent_id: AgentID):
         agent_signals = self._agent_signals_service.get_signals(agent_id)
         return agent_signals.dict(simplify=True), HTTPStatus.OK
