e374341ce1804f1a3873674cc13fa675c4513879
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-28 20:01:13 +0530

    UT: Fix tests in test_file_agent_log_repository.py

--- monkey/tests/unit_tests/monkey_island/cc/repository/test_file_agent_log_repository.py
+++ monkey/tests/unit_tests/monkey_island/cc/repository/test_file_agent_log_repository.py
@@ -3,7 +3,7 @@ from uuid import UUID
 import pytest
 from tests.monkey_island import OpenErrorFileRepository, SingleFileRepository
 
-from monkey_island.cc.repository import FileAgentLogRepository, RetrievalError, UnknownRecordError
+from monkey_island.cc.repository import FileAgentLogRepository, RetrievalError
 
 LOG_CONTENTS = "lots of useful information"
 AGENT_ID = UUID("6bfd8b64-43d8-4449-8c70-d898aca74ad8")
@@ -22,7 +22,7 @@ def test_store_agent_log(repository):
 
 
 def test_get_agent_log__unknown_record_error(repository):
-    with pytest.raises(UnknownRecordError):
+    with pytest.raises(RetrievalError):
         repository.get_agent_log(AGENT_ID)
 
 
@@ -35,5 +35,5 @@ def test_get_agent_log__retrieval_error():
 def test_reset_agent_logs(repository):
     repository.upsert_agent_log(AGENT_ID, LOG_CONTENTS)
     repository.reset()
-    with pytest.raises(UnknownRecordError):
+    with pytest.raises(RetrievalError):
         repository.get_agent_log(AGENT_ID)
