00d72390ff277e7e0d80a3b3deca55757d444b43
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-29 12:39:57 -0400

    Common: Accept a lock in LockingIslandEventQueueDecorator's constructor

--- monkey/monkey_island/cc/event_queue/locking_island_event_queue_decorator.py
+++ monkey/monkey_island/cc/event_queue/locking_island_event_queue_decorator.py
@@ -8,8 +8,8 @@ class LockingIslandEventQueueDecorator(IIslandEventQueue):
     Makes an IIslandEventQueue thread-safe by locking publish()
     """
 
-    def __init__(self, island_event_queue: IIslandEventQueue):
-        self._lock = Lock()
+    def __init__(self, island_event_queue: IIslandEventQueue, lock: Lock):
+        self._lock = lock
         self._island_event_queue = island_event_queue
 
     def subscribe(self, topic: IslandEventTopic, subscriber: IslandEventSubscriber):
