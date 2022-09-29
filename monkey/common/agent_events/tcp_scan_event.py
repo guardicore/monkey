ba7e44038cc80ee0f0cbb013f70cd5401283be32
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-29 13:29:43 -0400

    Common: Remove "service" from TCPScanEvent
    It's not the responsibility of the agent to format this information for
    display.

--- monkey/common/agent_events/tcp_scan_event.py
+++ monkey/common/agent_events/tcp_scan_event.py
@@ -13,11 +13,9 @@ class TCPScanEvent(AbstractAgentEvent):
         :param port: Port on which the scan was performed
         :param port_status: Status of the port (closed/open)
         :param banner: Information from the tcp response
-        :param service: Name of the service which runs on the port
     """
 
     target: IPv4Address
     port: NetworkPort
     port_status: PortStatus
     banner: str
-    service: str
