07fa283ce1712e4182e500d713fc36d8feb5ca50
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-09-30 12:28:22 -0400

    Island: Use container.resolve(ScanEventHandler)

--- monkey/monkey_island/cc/setup/agent_event_handlers.py
+++ monkey/monkey_island/cc/setup/agent_event_handlers.py
@@ -6,13 +6,7 @@ from monkey_island.cc.agent_event_handlers import (
     save_event_to_event_repository,
     save_stolen_credentials_to_repository,
 )
-from monkey_island.cc.repository import (
-    IAgentEventRepository,
-    IAgentRepository,
-    ICredentialsRepository,
-    IMachineRepository,
-    INodeRepository,
-)
+from monkey_island.cc.repository import IAgentEventRepository, ICredentialsRepository
 
 
 def setup_agent_event_handlers(container: DIContainer):
@@ -23,6 +17,7 @@ def setup_agent_event_handlers(container: DIContainer):
 def _subscribe_and_store_to_event_repository(container: DIContainer):
     agent_event_queue = container.resolve(IAgentEventQueue)
 
+    # TODO: Can't we just `container.resolve(save_event_to_event_repository)`?
     save_event_subscriber = save_event_to_event_repository(container.resolve(IAgentEventRepository))
     agent_event_queue.subscribe_all_events(save_event_subscriber)
 
@@ -34,11 +29,7 @@ def _subscribe_and_store_to_event_repository(container: DIContainer):
 
 def _subscribe_scan_events(container: DIContainer):
     agent_event_queue = container.resolve(IAgentEventQueue)
-    agent_repository = container.resolve(IAgentRepository)
-    machine_repository = container.resolve(IMachineRepository)
-    node_repository = container.resolve(INodeRepository)
-
-    scan_event_handler = ScanEventHandler(agent_repository, machine_repository, node_repository)
+    scan_event_handler = container.resolve(ScanEventHandler)
 
     agent_event_queue.subscribe_type(PingScanEvent, scan_event_handler.handle_ping_scan_event)
     agent_event_queue.subscribe_type(TCPScanEvent, scan_event_handler.handle_tcp_scan_event)
