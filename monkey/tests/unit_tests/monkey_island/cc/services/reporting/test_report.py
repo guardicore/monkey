e8ed30660e354f2a47041c09aca2b324b725bdec
vakarisz <vakarisz@yahoo.com>
2022-09-30 15:54:47 +0300

    UT: Fix report test

--- monkey/tests/unit_tests/monkey_island/cc/services/reporting/test_report.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/reporting/test_report.py
@@ -48,15 +48,15 @@ MACHINES = [MACHINE_1, MACHINE_2, MACHINE_3, ISLAND_MACHINE]
 EXPECTED_SCANNED_MACHINES = [
     {
         "hostname": MACHINE_1.hostname,
-        "ip_addresses": MACHINE_1.network_interfaces,
-        "accessible_from_nodes": [ISLAND_MACHINE],
+        "ip_addresses": list(map(str, MACHINE_1.network_interfaces)),
+        "accessible_from_nodes": [ISLAND_MACHINE.dict(simplify=True)],
         "services": [],
         "domain_name": "",
     },
     {
         "hostname": MACHINE_2.hostname,
-        "ip_addresses": MACHINE_2.network_interfaces,
-        "accessible_from_nodes": [MACHINE_1],
+        "ip_addresses": list(map(str, MACHINE_2.network_interfaces)),
+        "accessible_from_nodes": [MACHINE_1.dict(simplify=True)],
         "services": [],
         "domain_name": "",
     },
