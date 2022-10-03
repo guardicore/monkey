3d22e49ccc178166ba955144ee8d8abbd9846cb2
Ilija Lazoroski <ilija.la@live.com>
2022-10-03 13:51:39 +0200

    Common: Add PropagationEvent to agent_events

--- monkey/common/agent_events/__init__.py
+++ monkey/common/agent_events/__init__.py
@@ -2,3 +2,4 @@ from .abstract_agent_event import AbstractAgentEvent
 from .credentials_stolen_events import CredentialsStolenEvent
 from .ping_scan_event import PingScanEvent
 from .tcp_scan_event import TCPScanEvent
+from .propagation_event import PropagationEvent
