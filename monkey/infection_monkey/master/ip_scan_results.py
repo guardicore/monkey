80a095b6574b325fe091070f8d5c9413331543ba
vakarisz <vakarisz@yahoo.com>
2022-10-03 14:45:29 +0300

    Agent: Use NetworkPort instead of Port

--- monkey/infection_monkey/master/ip_scan_results.py
+++ monkey/infection_monkey/master/ip_scan_results.py
@@ -1,15 +1,14 @@
 from dataclasses import dataclass
 from typing import Dict
 
-from common.types import PingScanData
+from common.types import NetworkPort, PingScanData
 from infection_monkey.i_puppet import FingerprintData, PortScanData
 
-Port = int
 FingerprinterName = str
 
 
 @dataclass
 class IPScanResults:
     ping_scan_data: PingScanData
-    port_scan_data: Dict[Port, PortScanData]
+    port_scan_data: Dict[NetworkPort, PortScanData]
     fingerprint_data: Dict[FingerprinterName, FingerprintData]
