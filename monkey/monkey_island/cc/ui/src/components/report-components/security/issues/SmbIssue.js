c4b7bfbcc19a023dbdc3fa717a87184e62d94fb0
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-10-10 17:24:08 +0000

    UI: Remove credentials from SMB issue report

--- monkey/monkey_island/cc/ui/src/components/report-components/security/issues/SmbIssue.js
+++ monkey/monkey_island/cc/ui/src/components/report-components/security/issues/SmbIssue.js
@@ -4,15 +4,13 @@ import CollapsibleWellComponent from '../CollapsibleWell';
 export function smbPasswordReport(issue) {
   return (
     <>
-      Change <span className="badge badge-success">{issue.username}</span>'s password to a complex one-use password
-      that is not shared with other computers on the network.
+      Change user passwords to a complex one-use password that is not shared with other computers on the network.
       <CollapsibleWellComponent>
         The machine <span className="badge badge-primary">{issue.machine}</span> (<span
-        className="badge badge-info" style={{margin: '2px'}}>{issue.ip_address}</span>) is vulnerable to a <span
-        className="badge badge-danger">SMB</span> attack.
-        <br/>
-        The Monkey authenticated over the SMB protocol with user <span
-        className="badge badge-success">{issue.username}</span> and its password.
+          className="badge badge-info" style={{ margin: '2px' }}>{issue.ip_address}</span>) is vulnerable to a <span
+            className="badge badge-danger">SMB</span> attack.
+        <br />
+        The Monkey authenticated over the SMB protocol with username and password.
       </CollapsibleWellComponent>
     </>
   );
@@ -21,15 +19,13 @@ export function smbPasswordReport(issue) {
 export function smbPthReport(issue) {
   return (
     <>
-      Change <span className="badge badge-success">{issue.username}</span>'s password to a complex one-use password
-      that is not shared with other computers on the network.
+      Change user passwords to a complex one-use password that is not shared with other computers on the network.
       <CollapsibleWellComponent>
         The machine <span className="badge badge-primary">{issue.machine}</span> (<span
-        className="badge badge-info" style={{margin: '2px'}}>{issue.ip_address}</span>) is vulnerable to a <span
-        className="badge badge-danger">SMB</span> attack.
-        <br/>
-        The Monkey used a pass-the-hash attack over SMB protocol with user <span
-        className="badge badge-success">{issue.username}</span>.
+          className="badge badge-info" style={{ margin: '2px' }}>{issue.ip_address}</span>) is vulnerable to a <span
+            className="badge badge-danger">SMB</span> attack.
+        <br />
+        The Monkey used a pass-the-hash attack over SMB protocol.
       </CollapsibleWellComponent>
     </>
   );
