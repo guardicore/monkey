9d4d11400960cd52e2e3a2ddf468f699bbb901f1
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-10-11 15:55:53 -0400

    Island: Simplify resolution of agent event subscribers

--- monkey/monkey_island/cc/setup/agent_event_handlers.py
+++ monkey/monkey_island/cc/setup/agent_event_handlers.py
@@ -14,7 +14,6 @@ from monkey_island.cc.agent_event_handlers import (
     update_agent_shutdown_status,
     update_nodes_on_exploitation,
 )
-from monkey_island.cc.repository import IAgentEventRepository, ICredentialsRepository
 
 
 def setup_agent_event_handlers(container: DIContainer):
@@ -28,14 +27,11 @@ def setup_agent_event_handlers(container: DIContainer):
 def _subscribe_and_store_to_event_repository(
     container: DIContainer, agent_event_queue: IAgentEventQueue
 ):
-    # TODO: Can't we just `container.resolve(save_event_to_event_repository)`?
-    save_event_subscriber = save_event_to_event_repository(container.resolve(IAgentEventRepository))
-    agent_event_queue.subscribe_all_events(save_event_subscriber)
+    agent_event_queue.subscribe_all_events(container.resolve(save_event_to_event_repository))
 
-    save_stolen_credentials_subscriber = save_stolen_credentials_to_repository(
-        container.resolve(ICredentialsRepository)
+    agent_event_queue.subscribe_type(
+        CredentialsStolenEvent, container.resolve(save_stolen_credentials_to_repository)
     )
-    agent_event_queue.subscribe_type(CredentialsStolenEvent, save_stolen_credentials_subscriber)
     agent_event_queue.subscribe_type(
         AgentShutdownEvent, container.resolve(update_agent_shutdown_status)
     )
