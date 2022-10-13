4b709c05e8c2b23d6b50625e4885faf7efd458cb
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-10-13 13:52:11 -0400

    Island: Add register_common_agent_events()

--- monkey/common/agent_events/__init__.py
+++ monkey/common/agent_events/__init__.py
@@ -7,3 +7,4 @@ from .exploitation_event import ExploitationEvent
 from .propagation_event import PropagationEvent
 from .password_restoration_event import PasswordRestorationEvent
 from .agent_shutdown_event import AgentShutdownEvent
+from .register import register_common_agent_events
