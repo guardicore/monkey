c8dac2ccf7356c03490e2458f10f867bb50a8edc
Shreya Malviya <shreya.malviya@gmail.com>
2022-10-13 14:27:28 +0530

    UI: Extract _getAllAgents() in ServerUtils.js

--- monkey/monkey_island/cc/ui/src/components/utils/ServerUtils.js
+++ monkey/monkey_island/cc/ui/src/components/utils/ServerUtils.js
@@ -2,26 +2,28 @@ import IslandHttpClient from "../IslandHttpClient";
 
 function doesAnyAgentExist() {
     let any_agent_exists = false;
-    IslandHttpClient.get('/api/agents')
-        .then(res => res.json())
-        .then(res => {
-        if (res.length > 0) {
-            any_agent_exists = true;
-        }});
+    all_agents = _getAllAgents();
+    if (all_agents.length > 0) {
+        any_agent_exists = true;
+    }
     return any_agent_exists;
 }
 
-
 function didAllAgentsShutdown() {
     let all_agents_shutdown = true;
-    IslandHttpClient.get('/api/agents')
-        .then(res => res.json())
-        .then(res => {
-        for (idx in res) {
-            agent = res[idx];
-            if (agent.stop_time === null) {
-                all_agents_shutdown = false;
-            }
-        }});
+    all_agents = _getAllAgents();
+    for (idx in all_agents) {
+        agent = all_agents[idx];
+        if (agent.stop_time === null) {
+            all_agents_shutdown = false;
+        }
+    }
     return all_agents_shutdown;
+}
+
+function _getAllAgents() {
+    return IslandHttpClient.get('/api/agents')
+    .then(res => {
+        return res.json()
+    });
 }
