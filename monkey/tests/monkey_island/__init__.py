6df8801eef919f45de7ea8d05660815089fcdfa6
Ilija Lazoroski <ilija.la@live.com>
2022-10-13 18:42:27 +0200

    UT: Add InMemoryNodeRepository

--- monkey/tests/monkey_island/__init__.py
+++ monkey/tests/monkey_island/__init__.py
@@ -8,3 +8,4 @@ from .in_memory_agent_log_repository import InMemoryAgentLogRepository
 from .in_memory_file_repository import InMemoryFileRepository
 from .in_memory_agent_repository import InMemoryAgentRepository
 from .in_memory_machine_repository import InMemoryMachineRepository
+from .in_memory_node_repository import InMemoryNodeRepository
