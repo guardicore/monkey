75e341e52c9b6afb9f1bdf5c8522d396f1ad5952
Shreya Malviya <shreya.malviya@gmail.com>
2022-10-13 14:32:26 +0530

    UI: Remove ExecutionStatus.js

--- monkey/monkey_island/cc/ui/src/components/report-components/common/ExecutionStatus.js
+++
@@ -1,6 +0,0 @@
-export function extractExecutionStatusFromServerResponse(completed_steps_from_server) {
-  return {
-    allMonkeysAreDead: (!completed_steps_from_server['run_monkey']) || (completed_steps_from_server['infection_done']),
-    runStarted: completed_steps_from_server['run_monkey']
-  };
-}
