6697d691f6e7912e8fdc24e3a27bd41576037273
Shreya Malviya <shreya.malviya@gmail.com>
2022-10-14 18:37:25 +0530

    UI: Get rid of unneeded code in CompletedSteps.tsx

--- monkey/monkey_island/cc/ui/src/components/side-menu/CompletedSteps.tsx
+++ monkey/monkey_island/cc/ui/src/components/side-menu/CompletedSteps.tsx
@@ -2,26 +2,12 @@ export class CompletedSteps {
   runMonkey: boolean
   infectionDone: boolean
   reportDone: boolean
-  isLoggedIn: boolean
-  needsRegistration: boolean
 
   public constructor(runMonkey?: boolean,
-                     infectinDone?: boolean,
+                     infectionDone?: boolean,
                      reportDone?: boolean) {
     this.runMonkey = runMonkey || false;
-    this.infectionDone = infectinDone || false;
+    this.infectionDone = infectionDone || false;
     this.reportDone = reportDone || false;
   }
-
-  static buildFromResponse(response: CompletedStepsRequest) {
-    return new CompletedSteps(response.run_monkey,
-                              response.infection_done,
-                              response.report_done);
-  }
-}
-
-type CompletedStepsRequest = {
-  run_monkey: boolean,
-  infection_done: boolean,
-  report_done: boolean
 }
