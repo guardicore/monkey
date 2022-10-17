a1ebcfe970d57b3510d2c800f707132982245dec
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-10-17 18:33:48 +0000

    UI: Remove nodeStateList

--- monkey/monkey_island/cc/ui/src/components/pages/MapPage.js
+++ monkey/monkey_island/cc/ui/src/components/pages/MapPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Col, Modal, Row} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faStopCircle} from '@fortawesome/free-solid-svg-icons/faStopCircle';
-import {faMinus} from '@fortawesome/free-solid-svg-icons/faMinus';
+import { Col, Modal, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStopCircle } from '@fortawesome/free-solid-svg-icons/faStopCircle';
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
 import PreviewPaneComponent from 'components/map/preview-pane/PreviewPane';
 import ReactiveGraph from 'components/reactive-graph/ReactiveGraph';
 import { edgeGroupToColor } from 'components/map/MapOptions';
 import AuthComponent from '../AuthComponent';
 import '../../styles/components/Map.scss';
-import {faInfoCircle} from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
 import TelemetryLog from '../map/TelemetryLog';
 
 class MapPageComponent extends AuthComponent {
@@ -43,9 +43,9 @@ class MapPageComponent extends AuthComponent {
       .then(res => {
         if (Object.prototype.hasOwnProperty.call(res, 'edges')) {
           res.edges.forEach(edge => {
-            edge.color = {'color': edgeGroupToColor(edge.group)};
+            edge.color = { 'color': edgeGroupToColor(edge.group) };
           });
-          this.setState({graph: res});
+          this.setState({ graph: res });
           this.props.onStatusChange();
         }
       });
@@ -55,21 +55,21 @@ class MapPageComponent extends AuthComponent {
     if (event.nodes.length === 1) {
       this.authFetch('/api/netmap/node?id=' + event.nodes[0])
         .then(res => res.json())
-        .then(res => this.setState({selected: res, selectedType: 'node'}));
+        .then(res => this.setState({ selected: res, selectedType: 'node' }));
     } else if (event.edges.length === 1) {
       let displayedEdge = this.state.graph.edges.find(
         function (edge) {
           return edge['id'] === event.edges[0];
         });
       if (displayedEdge['group'] === 'island') {
-        this.setState({selected: displayedEdge, selectedType: 'island_edge'});
+        this.setState({ selected: displayedEdge, selectedType: 'island_edge' });
       } else {
         this.authFetch('/api/netmap/edge?id=' + event.edges[0])
           .then(res => res.json())
-          .then(res => this.setState({selected: res.edge, selectedType: 'edge'}));
+          .then(res => this.setState({ selected: res.edge, selectedType: 'edge' }));
       }
     } else {
-      this.setState({selected: null, selectedType: null});
+      this.setState({ selected: null, selectedType: null });
     }
   }
 
@@ -77,34 +77,34 @@ class MapPageComponent extends AuthComponent {
     this.authFetch('/api/agent-signals/terminate-all-agents',
       {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         // Python uses floating point seconds, Date.now uses milliseconds, so convert
-        body: JSON.stringify({terminate_time: Date.now() / 1000.0})
+        body: JSON.stringify({ terminate_time: Date.now() / 1000.0 })
       })
       .then(res => res.json())
-      .then(() => {this.setState({killPressed: true})});
+      .then(() => { this.setState({ killPressed: true }) });
   };
 
   renderKillDialogModal = () => {
     return (
-      <Modal show={this.state.showKillDialog} onHide={() => this.setState({showKillDialog: false})}>
+      <Modal show={this.state.showKillDialog} onHide={() => this.setState({ showKillDialog: false })}>
         <Modal.Body>
           <h2>
             <div className="text-center">Are you sure you want to kill all monkeys?</div>
           </h2>
-          <p style={{'fontSize': '1.2em', 'marginBottom': '2em'}}>
+          <p style={{ 'fontSize': '1.2em', 'marginBottom': '2em' }}>
             This might take up to <b>2 minutes</b>...
           </p>
           <div className="text-center">
-            <button type="button" className="btn btn-danger btn-lg" style={{margin: '5px'}}
-                    onClick={() => {
-                      this.killAllMonkeys();
-                      this.setState({showKillDialog: false});
-                    }}>
+            <button type="button" className="btn btn-danger btn-lg" style={{ margin: '5px' }}
+              onClick={() => {
+                this.killAllMonkeys();
+                this.setState({ showKillDialog: false });
+              }}>
               Kill all monkeys
             </button>
-            <button type="button" className="btn btn-success btn-lg" style={{margin: '5px'}}
-                    onClick={() => this.setState({showKillDialog: false})}>
+            <button type="button" className="btn btn-success btn-lg" style={{ margin: '5px' }}
+              onClick={() => this.setState({ showKillDialog: false })}>
               Cancel
             </button>
           </div>
@@ -116,9 +116,9 @@ class MapPageComponent extends AuthComponent {
 
   render() {
     return (
-      <Col sm={{offset: 3, span: 9}} md={{offset: 3, span: 9}}
-           lg={{offset: 3, span: 9}} xl={{offset: 2, span: 10}}
-           className={'main'}>
+      <Col sm={{ offset: 3, span: 9 }} md={{ offset: 3, span: 9 }}
+        lg={{ offset: 3, span: 9 }} xl={{ offset: 2, span: 10 }}
+        className={'main'}>
         <Row>
           {this.renderKillDialogModal()}
           <Col xs={12} lg={8}>
@@ -127,13 +127,13 @@ class MapPageComponent extends AuthComponent {
           <Col xs={8}>
             <div className="map-legend">
               <b>Legend: </b>
-              <span>Exploit <FontAwesomeIcon icon={faMinus} size="lg" style={{color: '#cc0200'}}/></span>
-              <b style={{color: '#aeaeae'}}> | </b>
-              <span>Scan <FontAwesomeIcon icon={faMinus} size="lg" style={{color: '#ff9900'}}/></span>
-              <b style={{color: '#aeaeae'}}> | </b>
-              <span>Tunnel <FontAwesomeIcon icon={faMinus} size="lg" style={{color: '#0158aa'}}/></span>
-              <b style={{color: '#aeaeae'}}> | </b>
-              <span>Island Communication <FontAwesomeIcon icon={faMinus} size="lg" style={{color: '#a9aaa9'}}/></span>
+              <span>Exploit <FontAwesomeIcon icon={faMinus} size="lg" style={{ color: '#cc0200' }} /></span>
+              <b style={{ color: '#aeaeae' }}> | </b>
+              <span>Scan <FontAwesomeIcon icon={faMinus} size="lg" style={{ color: '#ff9900' }} /></span>
+              <b style={{ color: '#aeaeae' }}> | </b>
+              <span>Tunnel <FontAwesomeIcon icon={faMinus} size="lg" style={{ color: '#0158aa' }} /></span>
+              <b style={{ color: '#aeaeae' }}> | </b>
+              <span>Island Communication <FontAwesomeIcon icon={faMinus} size="lg" style={{ color: '#a9aaa9' }} /></span>
             </div>
             <div style={{ height: '80vh' }} className={'map-window'}>
               <ReactiveGraph graph={this.state.graph} events={this.events} />
@@ -141,23 +141,23 @@ class MapPageComponent extends AuthComponent {
             </div>
           </Col>
           <Col xs={4}>
-            <div style={{'overflow': 'auto', 'marginBottom': '1em'}}>
-              <Link to="/infection/telemetry" className="btn btn-light pull-left" style={{'width': '48%'}}>Monkey
+            <div style={{ 'overflow': 'auto', 'marginBottom': '1em' }}>
+              <Link to="/infection/telemetry" className="btn btn-light pull-left" style={{ 'width': '48%' }}>Monkey
                 Telemetry</Link>
-              <button onClick={() => this.setState({showKillDialog: true})} className="btn btn-danger pull-right"
-                      style={{'width': '48%'}}>
-                <FontAwesomeIcon icon={faStopCircle} style={{'marginRight': '0.5em'}}/>
+              <button onClick={() => this.setState({ showKillDialog: true })} className="btn btn-danger pull-right"
+                style={{ 'width': '48%' }}>
+                <FontAwesomeIcon icon={faStopCircle} style={{ 'marginRight': '0.5em' }} />
                 Kill All Monkeys
               </button>
             </div>
             {this.state.killPressed ?
               <div className="alert alert-info">
-                <FontAwesomeIcon icon={faInfoCircle} style={{'marginRight': '5px'}} />
+                <FontAwesomeIcon icon={faInfoCircle} style={{ 'marginRight': '5px' }} />
                 Kill command sent to all monkeys
               </div>
               : ''}
 
-            <PreviewPaneComponent item={this.state.selected} type={this.state.selectedType}/>
+            <PreviewPaneComponent item={this.state.selected} type={this.state.selectedType} />
           </Col>
         </Row>
       </Col>
