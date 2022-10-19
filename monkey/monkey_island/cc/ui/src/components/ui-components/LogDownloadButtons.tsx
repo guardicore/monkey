94c4127c0b8e664275c9c6a0d61918bd5f3b106b
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-10-19 18:05:48 +0000

    UI: Don't modify the log file contents

--- monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
@@ -10,24 +10,11 @@ type Props = { url: string, filename: string, variant?: string }
 
 
 export const AgentLogDownloadButton = ({ url, filename, variant = 'primary' }: Props) => {
-
-  function unescapeLog(st) {
-    return st.substr(1, st.length - 2) // remove quotation marks on beginning and end of string.
-      .replace(/\\n/g, '\n')
-      .replace(/\\r/g, '\r')
-      .replace(/\\t/g, '\t')
-      .replace(/\\b/g, '\b')
-      .replace(/\\f/g, '\f')
-      .replace(/\\"/g, '"')
-      .replace(/\\'/g, '\'')
-      .replace(/\\&/g, '&');
-  }
-
   function downloadAgentLog() {
     authComponent.authFetch(url)
       .then(res => res.json())
       .then(res => {
-        let logContent = unescapeLog(res);
+        let logContent = res;
         download(logContent, filename, 'text/plain');
       });
   }
