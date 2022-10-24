b42e8e9e6d1d915d939247f770d5921dd4668dff
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-10-24 10:55:09 -0400

    Common: Remove disused ScanStatus.UNSCANNED

--- monkey/common/utils/attack_utils.py
+++ monkey/common/utils/attack_utils.py
@@ -2,8 +2,6 @@ from enum import Enum
 
 
 class ScanStatus(Enum):
-    # Technique wasn't scanned
-    UNSCANNED = 0
     # Technique was attempted/scanned
     SCANNED = 1
     # Technique was attempted and succeeded
