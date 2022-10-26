a691f564c477cf2b27fe4b375ce43170a2c42f06
vakarisz <vakarisz@yahoo.com>
2022-10-26 17:35:33 +0300

    Island: Remove Config document, nothing is using it

--- monkey/monkey_island/cc/models/__init__.py
+++ monkey/monkey_island/cc/models/__init__.py
@@ -2,7 +2,6 @@ from .command_control_channel import CommandControlChannel
 
 # Order of importing matters here, for registering the embedded and referenced documents before
 # using them.
-from .config import Config
 from .monkey import Monkey
 from .monkey_ttl import MonkeyTtl
 from monkey_island.cc.models.report.report import Report
--- monkey/tests/unit_tests/monkey_island/cc/services/test_infection_lifecycle.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/test_infection_lifecycle.py
@@ -1,11 +1,10 @@
 import uuid
 
-from monkey_island.cc.models import Config, Monkey
+from monkey_island.cc.models import Monkey
 
 
 def create_monkey(launch_time):
     monkey = Monkey(guid=str(uuid.uuid4()))
-    monkey.config = Config()
     monkey.should_stop = False
     monkey.launch_time = launch_time
     monkey.save()
