5c4776003a1675b67b9589fcce97a722b94b66d9
Ilija Lazoroski <ilija.la@live.com>
2022-10-27 17:39:01 +0200

    Zoo: Add allow-all-tunneling2 firewall rule for debuging purpose

--- envs/monkey_zoo/terraform/firewalls.tf
+++ envs/monkey_zoo/terraform/firewalls.tf
@@ -227,3 +227,18 @@ resource "google_compute_firewall" "monkeyzoo-test-in" {
   // Here goes your public IP so you can SSH/RDP in the instances
   source_ranges = ["127.0.0.1/32"]
 }
+
+resource "google_compute_firewall" "allow-all-tunneling2" {
+  name    = "${local.resource_prefix}allow-all-tunneling2"
+  network = google_compute_network.tunneling2.name
+  description = "Allows access to the tunneling2 instances  in the MonkeyZoo. Add your public IP under Source filters if you want to SSH/RDP in the instances."
+
+  allow {
+    protocol = "all"
+  }
+
+  direction = "INGRESS"
+  priority = "900"
+  // Here goes your public IP so you can SSH/RDP in the instances
+  source_ranges = ["127.0.0.1/32"]
+}
