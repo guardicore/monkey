2a04535e77d60b0f7e5f98ea9b82cd41cffe8f56
Ilija Lazoroski <ilija.la@live.com>
2022-10-27 18:12:17 +0200

    Island: Remove unused telemetry_dal.save_telemetry

--- monkey/monkey_island/cc/models/telemetries/telemetry_dal.py
+++ monkey/monkey_island/cc/models/telemetries/telemetry_dal.py
@@ -3,16 +3,6 @@ from __future__ import annotations
 from typing import List
 
 from monkey_island.cc.database import mongo
-from monkey_island.cc.models.telemetries.telemetry import Telemetry
-
-
-def save_telemetry(telemetry_dict: dict):
-    Telemetry(
-        data=telemetry_dict["data"],
-        timestamp=telemetry_dict["timestamp"],
-        monkey_guid=telemetry_dict["monkey_guid"],
-        telem_category=telemetry_dict["telem_category"],
-    ).save()
 
 
 def get_telemetry_by_query(query: dict, output_fields=None) -> List[dict]:
