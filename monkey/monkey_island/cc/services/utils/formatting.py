44a50d02ab50fc0eb6204edb925e169d73fe8220
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-10-27 14:47:26 -0400

    Island: Remove the monkey_island.cc.services.utils subpackage
    This subpackage contained exactly one function that was used in exactly
    one place. The function was extracted and put close to where it was
    used.

--- monkey/monkey_island/cc/services/utils/formatting.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-from common.common_consts.time_formats import DEFAULT_TIME_FORMAT
-
-
-def timestamp_to_date(timestamp: int) -> str:
-    return datetime.fromtimestamp(timestamp).strftime(DEFAULT_TIME_FORMAT)
