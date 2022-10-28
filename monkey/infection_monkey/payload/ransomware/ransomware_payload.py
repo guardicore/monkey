2b3a4065017b067c4e702421e8725e19a6ce3dba
Ilija Lazoroski <ilija.la@live.com>
2022-10-28 14:30:54 +0200

    Agent: Build RansomwarePayload with IAgentEventQueue

--- monkey/infection_monkey/payload/ransomware/ransomware_payload.py
+++ monkey/infection_monkey/payload/ransomware/ransomware_payload.py
@@ -1,16 +1,16 @@
 import threading
 from typing import Dict
 
+from common.event_queue import IAgentEventQueue
 from infection_monkey.payload.i_payload import IPayload
-from infection_monkey.telemetry.messengers.i_telemetry_messenger import ITelemetryMessenger
 
 from . import ransomware_builder
 
 
 class RansomwarePayload(IPayload):
-    def __init__(self, telemetry_messenger: ITelemetryMessenger):
-        self._telemetry_messenger = telemetry_messenger
+    def __init__(self, agent_event_queue: IAgentEventQueue):
+        self._agent_event_queue = agent_event_queue
 
     def run(self, options: Dict, interrupt: threading.Event):
-        ransomware = ransomware_builder.build_ransomware(options, self._telemetry_messenger)
+        ransomware = ransomware_builder.build_ransomware(options, self._agent_event_queue)
         ransomware.run(interrupt)
