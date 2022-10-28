fe71e69f6c0aae0c5bfb74e3b3080d97ef638ef7
Ilija Lazoroski <ilija.la@live.com>
2022-10-28 14:29:34 +0200

    Agent: Publish FileEncryptionEvents from Ransomware

--- monkey/infection_monkey/payload/ransomware/ransomware.py
+++ monkey/infection_monkey/payload/ransomware/ransomware.py
@@ -3,8 +3,9 @@ import threading
 from pathlib import Path
 from typing import Callable, Iterable
 
-from infection_monkey.telemetry.file_encryption_telem import FileEncryptionTelem
-from infection_monkey.telemetry.messengers.i_telemetry_messenger import ITelemetryMessenger
+from common.agent_events import FileEncryptionEvent
+from common.event_queue import IAgentEventQueue
+from infection_monkey.utils.ids import get_agent_id
 from infection_monkey.utils.threading import interruptible_function, interruptible_iter
 
 from .consts import README_FILE_NAME, README_SRC
@@ -20,14 +21,14 @@ class Ransomware:
         encrypt_file: Callable[[Path], None],
         select_files: Callable[[Path], Iterable[Path]],
         leave_readme: Callable[[Path, Path], None],
-        telemetry_messenger: ITelemetryMessenger,
+        agent_event_queue: IAgentEventQueue,
     ):
         self._config = config
 
         self._encrypt_file = encrypt_file
         self._select_files = select_files
         self._leave_readme = leave_readme
-        self._telemetry_messenger = telemetry_messenger
+        self._agent_event_queue = agent_event_queue
 
         self._target_directory = self._config.target_directory
         self._readme_file_path = (
@@ -64,18 +65,20 @@ class Ransomware:
                 # Note that encrypting a single file is not interruptible. This is so that we avoid
                 # leaving half-encrypted files on the user's system.
                 self._encrypt_file(filepath)
-                self._send_telemetry(filepath, True, "")
-            except Exception as ex:
-                logger.warning(f"Error encrypting {filepath}: {ex}")
-                self._send_telemetry(filepath, False, str(ex))
+                self._publish_file_encryption_event(filepath, True, "")
+            except Exception as err:
+                logger.warning(f"Error encrypting {filepath}: {err}")
+                self._publish_file_encryption_event(filepath, False, str(err))
 
-    def _send_telemetry(self, filepath: Path, success: bool, error: str):
-        encryption_attempt = FileEncryptionTelem(filepath, success, error)
-        self._telemetry_messenger.send_telemetry(encryption_attempt)
+    def _publish_file_encryption_event(self, filepath: Path, success: bool, error: str):
+        file_encryption_event = FileEncryptionEvent(
+            source=get_agent_id(), file_path=filepath, success=success, error_message=error
+        )
+        self._agent_event_queue.publish(file_encryption_event)
 
     @interruptible_function(msg="Received a stop signal, skipping leave readme")
     def _leave_readme_in_target_directory(self, *, interrupt: threading.Event):
         try:
             self._leave_readme(README_SRC, self._readme_file_path)  # type: ignore
-        except Exception as ex:
-            logger.warning(f"An error occurred while attempting to leave a README.txt file: {ex}")
+        except Exception as err:
+            logger.warning(f"An error occurred while attempting to leave a README.txt file: {err}")
