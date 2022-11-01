6791322b07296ec45c01d6236a8033e78e27669f
Ilija Lazoroski <ilija.la@live.com>
2022-11-01 13:27:31 +0100

    UI: Show event specific fields in EventsTable

--- monkey/monkey_island/cc/ui/src/components/ui-components/EventsTable.js
+++ monkey/monkey_island/cc/ui/src/components/ui-components/EventsTable.js
@@ -5,18 +5,18 @@ import AuthService from '../../services/AuthService';
 import '../../styles/pages/EventPage.scss';
 import IslandHttpClient, {APIEndpoint} from '../IslandHttpClient';
 
-const timestamp_options =  [{year: 'numeric'}, {month: '2-digit'},{day: '2-digit'},{'hour': '2-digit'},{'minutes': '2-digit'},{'second': 'numeric'}];
-const renderTime = (val) => new Date(val*1000).toLocaleString('en-us', timestamp_options);
-const renderTarget = (val) => val ?  val : 'Unknown';
-const renderJson = (val) => <JSONTree data={val} level={1} theme="eighties" invertTheme={true}/>;
-const renderTags = (val) => val.join(', ');
-
 const columns = [
   {label: 'Time', name: 'timestamp'},
   {label: 'Monkey', name: 'source'},
   {label: 'Target', name: 'target'},
   {label: 'Type', name: 'type'},
-  {label: 'Tags', name: 'tags'}
+  {label: 'Tags', name: 'tags'},
+  {label: 'Fields', name: 'fields',  options: {
+      setCellProps: () => ({ style: { width: '30%' }}),
+      filter:false,
+      sort:false
+    }
+  }
 ];
 
 const table_options = {
@@ -31,6 +31,29 @@ const table_options = {
   rowsPerPageOptions: [10, 20, 50, 100],
   selectableRows: 'none'
 };
+
+const timestamp_options =  [{year: 'numeric'}, {month: '2-digit'},{day: '2-digit'},{'hour': '2-digit'},{'minutes': '2-digit'},{'second': 'numeric'}];
+const renderTime = (val) => new Date(val*1000).toLocaleString('en-us', timestamp_options);
+const renderTarget = (val) => val ?  val : 'Unknown';
+const renderTags = (val) => val.join(', ');
+
+const abstract_agent_event_fields = ['source', 'target', 'timestamp', 'type', 'tags'];
+
+function deleteKeys(myObj, array) {
+  let tempObj = JSON.parse(JSON.stringify(myObj)); /* deepcopy */
+  for (let index = 0; index < array.length; index++) {
+        delete tempObj[array[index]];
+    }
+      return tempObj;
+}
+
+function setEventSpecificFields(event) {
+  console.log(event);
+  let filtered_event = deleteKeys(event, abstract_agent_event_fields);
+  return <JSONTree data={filtered_event} level={1} theme="eighties" invertTheme={true}/>
+}
+
+const renderEventSpecificFields = (val) => setEventSpecificFields(val);
 
 class EventsTable extends React.Component {
   constructor(props) {
@@ -61,8 +84,8 @@ class EventsTable extends React.Component {
               item.source,
               renderTarget(item.target),
               item.type,
-              renderTags(item.tags)
-              //renderJson(item.data)
+              renderTags(item.tags),
+              renderEventSpecificFields(item)
             ]})}
           options={table_options}
         />
