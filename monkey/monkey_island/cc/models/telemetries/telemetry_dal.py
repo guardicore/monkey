50a067868907103485e6f4e47b06ae6df79d3153
Ilija Lazoroski <ilija.la@live.com>
2022-11-02 11:02:52 +0100

    Island: Remove Telemetry model

--- monkey/monkey_island/cc/models/telemetries/telemetry_dal.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import List
-
-from monkey_island.cc.database import mongo
-
-
-def get_telemetry_by_query(query: dict, output_fields=None) -> List[dict]:
-    return mongo.db.telemetry.find(query, output_fields)
