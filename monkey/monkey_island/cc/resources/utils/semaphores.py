c4bde76229881b1fc980811ab4bc65d260ac662c
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-03 13:56:46 +0530

    Island: Remove unused cc/resources/utils/semaphores.py

--- monkey/monkey_island/cc/resources/utils/semaphores.py
+++
@@ -1,7 +0,0 @@
-# TODO: Switch to threading.lock and test. Calling gevent.patch_all() is supposed to monkeypatch
-#       threading to be compatible with gevent/greenlets.
-from gevent.lock import BoundedSemaphore
-
-# Mutex avoids race condition between monkeys
-# being marked dead and monkey waking up as alive
-agent_killing_mutex = BoundedSemaphore()
