94918d482359f56d5b5c2ce909c0a03169eb7e74
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-03 13:45:33 +0530

    Island: Remove MonkeyTtl model

--- monkey/monkey_island/cc/models/__init__.py
+++ monkey/monkey_island/cc/models/__init__.py
@@ -1,7 +1,6 @@
 # Order of importing matters here, for registering the embedded and referenced documents before
 # using them.
 from .monkey import Monkey
-from .monkey_ttl import MonkeyTtl
 from monkey_island.cc.models.report.report import Report
 from .simulation import Simulation, IslandMode
 from .user_credentials import UserCredentials
