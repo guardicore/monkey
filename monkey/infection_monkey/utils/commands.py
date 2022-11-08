aca7d3c1a0fda3fe5955b593fd69424efcbff5fe
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-08 16:17:44 +0530

    Agent: Use get_agent_id() instead of GUID in build_monkey_commandline()

--- monkey/infection_monkey/utils/commands.py
+++ monkey/infection_monkey/utils/commands.py
@@ -1,9 +1,10 @@
 from pathlib import PurePath
 from typing import List, Optional, Union
 
-from infection_monkey.config import GUID
+from common.types import AgentID
 from infection_monkey.exploit.tools.helpers import AGENT_BINARY_PATH_LINUX, AGENT_BINARY_PATH_WIN64
 from infection_monkey.model import CMD_CARRY_OUT, CMD_EXE, MONKEY_ARG
+from infection_monkey.utils.ids import get_agent_id
 
 # Dropper target paths
 DROPPER_TARGET_PATH_LINUX = AGENT_BINARY_PATH_LINUX
@@ -16,7 +17,7 @@ def build_monkey_commandline(
 
     return " " + " ".join(
         build_monkey_commandline_explicitly(
-            GUID,
+            get_agent_id(),
             servers,
             depth,
             location,
@@ -25,7 +26,7 @@ def build_monkey_commandline(
 
 
 def build_monkey_commandline_explicitly(
-    parent: Optional[str] = None,
+    parent: Optional[AgentID] = None,
     servers: Optional[List[str]] = None,
     depth: Optional[int] = None,
     location: Union[str, PurePath, None] = None,
