d76ed4d85927eb522244dd360b42a6741eb03343
Ilija Lazoroski <ilija.la@live.com>
2022-11-08 14:46:20 +0100

    UI: Add DateUtils.parseTimeToDateString

---
+++ monkey/monkey_island/cc/ui/src/components/utils/DateUtils.tsx
@@ -0,0 +1,14 @@
+export function parseTimeToDateString(time) {
+  // If the time is a string timestamp
+  let parsedTimeDate = Date.parse(time);
+
+   // If the time is a unix timestamp
+  if(isNaN(parsedTimeDate)){
+      parsedTimeDate = time;
+  }
+
+  let timeDate = new Date();
+  timeDate.setTime(parsedTimeDate);
+
+  return timeDate.toLocaleString('en-us');
+}
