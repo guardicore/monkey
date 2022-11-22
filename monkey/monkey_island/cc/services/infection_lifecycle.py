60c345f8a7fd1e2d8dfbf5c3e8af828156ecc810
Ilija Lazoroski <ilija.la@live.com>
2022-11-22 17:35:24 +0100

    Island: Modify infection_lifecycle to not call is_report_being_generated

--- monkey/monkey_island/cc/services/infection_lifecycle.py
+++ monkey/monkey_island/cc/services/infection_lifecycle.py
@@ -2,10 +2,6 @@ import logging
 
 from monkey_island.cc.repository import IAgentRepository
 from monkey_island.cc.services.reporting.report import ReportService
-from monkey_island.cc.services.reporting.report_generation_synchronisation import (
-    is_report_being_generated,
-    safe_generate_reports,
-)
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +10,7 @@ def is_report_done(agent_repository: IAgentRepository) -> bool:
     infection_done = _is_infection_done(agent_repository)
 
     if infection_done:
-        _on_finished_infection()
+        _ = ReportService.get_report()
         report_done = ReportService.is_report_generated()
     else:  # Infection is not done
         report_done = False
@@ -26,11 +22,3 @@ def _is_infection_done(agent_repository: IAgentRepository) -> bool:
     any_agent_exists = bool(agent_repository.get_agents())
     any_agent_running = bool(agent_repository.get_running_agents())
     return any_agent_exists and not any_agent_running
-
-
-def _on_finished_infection():
-    # Checking is_report_being_generated here, because we don't want to wait to generate a
-    # report; rather,
-    # we want to skip and reply.
-    if not is_report_being_generated() and ReportService.report_is_outdated():
-        safe_generate_reports()
