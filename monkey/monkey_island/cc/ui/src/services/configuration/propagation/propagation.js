4bf9ca71b11f7497c05963e54317615787eec302
Ilija Lazoroski <ilija.la@live.com>
2022-11-22 13:19:37 +0100

    UI: Change maximum_depth to be property of general propagation schema

--- monkey/monkey_island/cc/ui/src/services/configuration/propagation/propagation.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/propagation/propagation.js
@@ -8,18 +8,24 @@ const PROPAGATION_CONFIGURATION_SCHEMA = {
   'properties': {
     'exploitation': EXPLOITATION_CONFIGURATION_SCHEMA,
     'credentials': CREDENTIALS,
-    'maximum_depth': {
-      'title': 'Maximum scan depth',
-      'type': 'integer',
-      'minimum': 0,
-      'default': 2,
-      'description': 'Amount of hops allowed for the monkey to spread from the ' +
-        'Island server. \n' +
-        ' \u26A0' +
-        ' Note that setting this value too high may result in the ' +
-        'Monkey propagating too far, ' +
-        'if "Scan Agent\'s networks" is enabled.\n' +
-        'Setting this to 0 will disable all scanning and exploitation.'
+    'general': {
+      'title': 'General',
+      'type': 'object',
+      'properties': {
+        'maximum_depth': {
+          'title': 'Maximum scan depth',
+          'type': 'integer',
+          'minimum': 0,
+          'default': 2,
+          'description': 'Amount of hops allowed for the monkey to spread from the ' +
+          'Island server. \n' +
+          ' \u26A0' +
+          ' Note that setting this value too high may result in the ' +
+          'Monkey propagating too far, ' +
+          'if "Scan Agent\'s networks" is enabled.\n' +
+          'Setting this to 0 will disable all scanning and exploitation.'
+        }
+      }
     },
     'network_scan': NETWORK_SCAN_CONFIGURATION_SCHEMA
   }
