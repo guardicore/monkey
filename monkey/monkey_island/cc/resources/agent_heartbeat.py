d27f00d2662c8ae8f2a21b67046fca6b62f85c44
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-28 19:19:04 +0530

    Island: Add AgentHeartbeat resource

---
+++ monkey/monkey_island/cc/resources/agent_heartbeat.py
@@ -0,0 +1,35 @@
+import time
+from http import HTTPStatus
+from json import JSONDecodeError
+
+from flask import request
+
+from common.types import AgentID
+from monkey_island.cc.event_queue import IIslandEventQueue, IslandEventTopic
+from monkey_island.cc.resources.AbstractResource import AbstractResource
+from monkey_island.cc.resources.request_authentication import jwt_required
+
+
+class AgentHeartbeat(AbstractResource):
+    urls = ["/api/agent/<uuid:agent_id>/heartbeat"]
+
+    def __init__(self, island_event_queue: IIslandEventQueue):
+        self._island_event_queue = island_event_queue
+
+    @jwt_required
+    def post(self, agent_id: AgentID):
+        try:
+            heartbeat_timestamp = request.json["heartbeat_timestamp"]
+            if heartbeat_timestamp is None:
+                heartbeat_timestamp = time.time()
+            elif heartbeat_timestamp <= 0:
+                raise ValueError("Terminate signal's timestamp is not a positive integer")
+
+            self._island_event_queue.publish(
+                IslandEventTopic.AGENT_HEARTBEAT, agent_id=agent_id, timestamp=heartbeat_timestamp
+            )
+
+            return {}, HTTPStatus.NO_CONTENT
+
+        except (JSONDecodeError, TypeError, ValueError, KeyError) as err:
+            return {"error": err}, HTTPStatus.BAD_REQUEST
