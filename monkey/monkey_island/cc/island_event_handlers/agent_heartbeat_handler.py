8408d25f3d253964204ec36aa9f0fae28b186286
Ilija Lazoroski <ilija.la@live.com>
2022-11-29 17:21:43 +0100

    Island: Use Dict to save last agent heartbeats

--- monkey/monkey_island/cc/island_event_handlers/agent_heartbeat_handler.py
+++ monkey/monkey_island/cc/island_event_handlers/agent_heartbeat_handler.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from typing import Optional
+from threading import Lock
+from typing import Dict, Optional
 
 from common.types import AgentID
 from monkey_island.cc.repositories import IAgentRepository
@@ -15,9 +16,9 @@ class AgentHeartbeatHandler:
         agent_repository: IAgentRepository,
     ):
         self._agent_repository = agent_repository
+        self._last_agent_heartbeats: Dict[AgentID, Optional[datetime]] = {}
+        self._lock: Lock = Lock()
 
-    def __call__(self, agent_id: AgentID, timestamp: Optional[datetime]):
-        agent = self._agent_repository.get_agent_by_id(agent_id)
-        agent.last_heartbeat_time = timestamp
-
-        self._agent_repository.upsert_agent(agent)
+    def update_agent_last_heartbeat(self, agent_id: AgentID, timestamp: Optional[datetime]):
+        with self._lock:
+            self._last_agent_heartbeats[agent_id] = timestamp
