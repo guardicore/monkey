c4ddff2fa17ea24e8d27b1b0932e104c712ebc92
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-30 19:43:55 +0530

    Agent: Fix AgentHeartbeat import

--- monkey/infection_monkey/island_api_client/http_island_api_client.py
+++ monkey/infection_monkey/island_api_client/http_island_api_client.py
@@ -6,13 +6,12 @@ from typing import List, Sequence
 
 import requests
 
-from common import AgentRegistrationData, AgentSignals, OperatingSystem
+from common import AgentHeartbeat, AgentRegistrationData, AgentSignals, OperatingSystem
 from common.agent_configuration import AgentConfiguration
 from common.agent_event_serializers import AgentEventSerializerRegistry
 from common.agent_events import AbstractAgentEvent
 from common.common_consts.timeouts import SHORT_REQUEST_TIMEOUT
 from common.credentials import Credentials
-from common.request_data import AgentHeartbeat
 from common.types import AgentID, JSONSerializable, PluginType, SocketAddress
 
 from . import AbstractIslandAPIClientFactory, IIslandAPIClient, IslandAPIRequestError
