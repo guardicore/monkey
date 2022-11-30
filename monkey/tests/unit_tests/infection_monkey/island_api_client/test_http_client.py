365ec07bd1f51c42a0450bc21388206038b23a53
vakarisz <vakarisz@yahoo.com>
2022-11-30 16:29:41 +0200

    UT: Improve readability of test_http_client.py

--- monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_client.py
+++ monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_client.py
@@ -6,7 +6,6 @@ import requests
 import requests_mock
 from urllib3.exceptions import ConnectTimeoutError
 
-from common import AgentRegistrationData
 from common.types import SocketAddress
 from infection_monkey.island_api_client import (
     IslandAPIConnectionError,
@@ -18,40 +17,27 @@ from infection_monkey.island_api_client import (
 from infection_monkey.island_api_client.http_client import RETRIES, HTTPClient
 
 SERVER = SocketAddress(ip="1.1.1.1", port=9999)
-WINDOWS = "windows"
 AGENT_ID = UUID("80988359-a1cd-42a2-9b47-5b94b37cd673")
-AGENT_REGISTRATION = AgentRegistrationData(
-    id=AGENT_ID,
-    machine_hardware_id=1,
-    start_time=0,
-    parent_id=None,
-    cc_server=SERVER,
-    network_interfaces=[],
-)
-
-TIMESTAMP = 123456789
 
 ISLAND_URI = f"https://{SERVER}/api?action=is-up"
 LOG_ENDPOINT = f"agent-logs/{AGENT_ID}"
 ISLAND_SEND_LOG_URI = f"https://{SERVER}/api/{LOG_ENDPOINT}"
-ISLAND_GET_AGENT_BINARY_URI = f"https://{SERVER}/api/agent-binaries/{WINDOWS}"
-ISLAND_SEND_EVENTS_URI = f"https://{SERVER}/api/agent-events"
-ISLAND_REGISTER_AGENT_URI = f"https://{SERVER}/api/agents"
-ISLAND_AGENT_STOP_URI = f"https://{SERVER}/api/monkey-control/needs-to-stop/{AGENT_ID}"
-ISLAND_GET_CONFIG_URI = f"https://{SERVER}/api/agent-configuration"
 PROPAGATION_CREDENTIALS_ENDPOINT = "propagation-credentials"
 ISLAND_GET_PROPAGATION_CREDENTIALS_URI = f"https://{SERVER}/api/{PROPAGATION_CREDENTIALS_ENDPOINT}"
-ISLAND_GET_AGENT_SIGNALS = f"https://{SERVER}/api/agent-signals/{AGENT_ID}"
 
 
 @pytest.fixture
-def connected_client():
-    http_client = HTTPClient()
-
+def request_mock_instance():
     with requests_mock.Mocker() as m:
-        m.get(ISLAND_URI)
-        http_client.connect(SERVER)
-        yield {"client": http_client, "mock": m}
+        yield m
+
+
+@pytest.fixture
+def connected_client(request_mock_instance):
+    http_client = HTTPClient()
+    request_mock_instance.get(ISLAND_URI)
+    http_client.connect(SERVER)
+    return http_client
 
 
 @pytest.mark.parametrize(
@@ -61,10 +47,13 @@ def connected_client():
         (TimeoutError, IslandAPITimeoutError),
     ],
 )
-def test_http_client__error_handling(initial_error, raised_error, connected_client):
+def test_http_client__error_handling(
+    initial_error, raised_error, connected_client, request_mock_instance
+):
+    request_mock_instance.get(ISLAND_GET_PROPAGATION_CREDENTIALS_URI, exc=initial_error)
+
     with pytest.raises(raised_error):
-        connected_client["mock"].get(ISLAND_GET_PROPAGATION_CREDENTIALS_URI, exc=initial_error)
-        connected_client["client"].get(PROPAGATION_CREDENTIALS_ENDPOINT)
+        connected_client.get(PROPAGATION_CREDENTIALS_ENDPOINT)
 
 
 @pytest.mark.parametrize(
@@ -74,38 +63,42 @@ def test_http_client__error_handling(initial_error, raised_error, connected_clie
         (501, IslandAPIRequestFailedError),
     ],
 )
-def test_http_client__status_handling(status_code, expected_error, connected_client):
+def test_http_client__status_handling(
+    status_code, expected_error, connected_client, request_mock_instance
+):
+    request_mock_instance.put(ISLAND_SEND_LOG_URI, status_code=status_code)
+
     with pytest.raises(expected_error):
-        connected_client["mock"].put(ISLAND_SEND_LOG_URI, status_code=status_code)
-        connected_client["client"].put(LOG_ENDPOINT, "Fake log contents")
+        connected_client.put(LOG_ENDPOINT, "Fake log contents")
 
 
-def test_http_client__incorrect_call(connected_client):
+def test_http_client__incorrect_call(connected_client, request_mock_instance):
+    request_mock_instance.get(ISLAND_SEND_LOG_URI)
+
+    # get requests have no data/json
     with pytest.raises(IslandAPIError):
-        connected_client["mock"].get(ISLAND_SEND_LOG_URI)
-        # get requests have no data/json
-        connected_client["client"].get(LOG_ENDPOINT, hokus_pokus="something")
+        connected_client.get(LOG_ENDPOINT, hokus_pokus="something")
 
 
 def test_http_client__unconnected():
     http_client = HTTPClient()
 
     with requests_mock.Mocker() as m:
+        m.get(ISLAND_SEND_LOG_URI)
+
         with pytest.raises(IslandAPIError):
-            m.get(ISLAND_SEND_LOG_URI)
             http_client.get(LOG_ENDPOINT)
 
 
-def test_http_client_retries(monkeypatch):
+def test_http_client__retries(monkeypatch):
     http_client = HTTPClient()
-
     # skip the connect method
     http_client._api_url = f"https://{SERVER}/api"
-
+    mock_send = MagicMock(side_effect=ConnectTimeoutError)
     # requests_mock can't be used for this, because it mocks higher level than we are testing
+    monkeypatch.setattr("urllib3.connectionpool.HTTPSConnectionPool._validate_conn", mock_send)
+
     with pytest.raises(IslandAPIConnectionError):
-        mock_send = MagicMock(side_effect=ConnectTimeoutError)
-        monkeypatch.setattr("urllib3.connectionpool.HTTPSConnectionPool._validate_conn", mock_send)
         http_client.get(LOG_ENDPOINT)
 
     assert mock_send.call_count == RETRIES + 1
@@ -115,5 +108,6 @@ def test_http_client__additional_args(monkeypatch, connected_client):
     get = MagicMock()
     monkeypatch.setattr("requests.Session.get", get)
 
-    connected_client["client"].get(LOG_ENDPOINT, auth="authentication")
+    connected_client.get(LOG_ENDPOINT, auth="authentication")
+
     assert get.call_args[1]["auth"] == "authentication"
