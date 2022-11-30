08518d0257484d64775a766ac6fbd3ee31782d69
Shreya Malviya <shreya.malviya@gmail.com>
2022-11-30 15:41:55 +0530

    Common: Remove TerminateAllAgents pydantic object

--- monkey/common/request_data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .agent_heartbeat import AgentHeartbeat
-from .terminate_all_agents import TerminateAllAgents
--- monkey/common/request_data/terminate_all_agents.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-from common.base_models import InfectionMonkeyBaseModel
-
-
-class TerminateAllAgents(InfectionMonkeyBaseModel):
-    timestamp: datetime
