45aa5370012af46739e7cfedaa538118b633eed5
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-01 11:54:04 -0500

    Agent: Use PeriodicCaller to implement Heart
    Issue #2518
    PR #2648

--- monkey/infection_monkey/heart.py
+++ monkey/infection_monkey/heart.py
@@ -1,11 +1,10 @@
 import logging
 import time
-from threading import Event
 
 from common.common_consts import HEARTBEAT_INTERVAL
+from common.utils.code_utils import PeriodicCaller
 from infection_monkey.island_api_client import IIslandAPIClient
 from infection_monkey.utils.ids import get_agent_id
-from infection_monkey.utils.threading import create_daemon_thread
 
 logger = logging.getLogger(__name__)
 
@@ -13,31 +12,22 @@ logger = logging.getLogger(__name__)
 class Heart:
     def __init__(self, island_api_client: IIslandAPIClient):
         self._island_api_client = island_api_client
-        self._heartbeat_thread = create_daemon_thread(
-            target=self._send_heartbeats, name="HeartbeatThread"
+        self._periodic_caller = PeriodicCaller(
+            self._send_heartbeats, HEARTBEAT_INTERVAL, "AgentHeart"
         )
         self._agent_id = get_agent_id()
-        self._stop = Event()
 
     def start(self):
-        logger.debug("Starting the Agent's heart")
-        self._heartbeat_thread.start()
+        logger.info("Starting the Agent's heart")
+        self._periodic_caller.start()
 
     def _send_heartbeats(self):
-        logger.info("Agent's heart started")
-
-        while not self._stop.is_set():
-            self._island_api_client.send_heartbeat(self._agent_id, time.time())
-            self._stop.wait(HEARTBEAT_INTERVAL)
-
-        logger.info("Agent's heart stopped")
+        self._island_api_client.send_heartbeat(self._agent_id, time.time())
 
     def stop(self):
-        logger.debug("Stopping the Agent's heart")
-        self._stop.set()
+        logger.info("Stopping the Agent's heart")
 
-        # Waiting HEARTBEAT_INTERVAL is more than enough time. In practice, this join() should never
-        # take longer than it takes for `send_heartbeat()`'s to timeout.
-        self._heartbeat_thread.join(timeout=HEARTBEAT_INTERVAL)
-        if self._heartbeat_thread.is_alive():
-            logger.warning("Timed out waiting for the agent's heart to stop")
+        # Waiting HEARTBEAT_INTERVAL is more than enough time. In practice, stopping
+        # self._periodic_caller should never take longer than it takes for
+        # `self._island_api_client.send_heartbeat()` to timeout.
+        self._periodic_caller.stop(timeout=HEARTBEAT_INTERVAL)
