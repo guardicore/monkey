68af604921fd3bdb7ff5c65523306ec8e7e86a4a
vakarisz <vakarisz@yahoo.com>
2022-12-05 12:39:57 +0200

    UT: Add unit tests for  AgentPlugin and AgentPluginManifest

--- monkey/common/agent_plugins/agent_plugin.py
+++ monkey/common/agent_plugins/agent_plugin.py
@@ -1,10 +1,13 @@
+from typing import Any, Dict
+
 from common.agent_plugins import AgentPluginManifest
 from common.base_models import InfectionMonkeyBaseModel
-from common.types import JSONSerializable
 
 
 class AgentPlugin(InfectionMonkeyBaseModel):
     plugin_manifest: AgentPluginManifest
-    config_schema: JSONSerializable
-    default_config: JSONSerializable
+    # Should be JSONSerializable but it's only supported in 3.9:
+    # https://github.com/pydantic/pydantic/pull/1844
+    config_schema: Dict[str, Any]
+    default_config: Dict[str, Any]
     source_archive: bytes
