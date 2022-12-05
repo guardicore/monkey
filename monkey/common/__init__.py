b0a5b151424c4acc6c181a2f2f001deac482da51
Ilija Lazoroski <ilija.la@live.com>
2022-12-05 12:09:53 +0100

    Common: Categorize types into separate package under common.types

--- monkey/common/__init__.py
+++ monkey/common/__init__.py
@@ -4,7 +4,6 @@ Used for a common things between agent and island
 from . import transforms
 from .di_container import DIContainer, UnresolvableDependencyError
 from .operating_system import OperatingSystem
-from . import types
 from . import base_models
 from .agent_registration_data import AgentRegistrationData
 from .agent_signals import AgentSignals
--- monkey/common/concurrency/__init__.py
+++
@@ -1,1 +0,0 @@
-from .protocols import Lock, Event
