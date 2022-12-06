4eacd6f61446eb1010f88732ef261bbc2d371c41
vakarisz <vakarisz@yahoo.com>
2022-12-06 11:26:04 +0200

    Common: Add documentation to the plugin classes

--- monkey/common/agent_plugins/agent_plugin.py
+++ monkey/common/agent_plugins/agent_plugin.py
@@ -5,6 +5,16 @@ from common.base_models import InfectionMonkeyBaseModel
 
 
 class AgentPlugin(InfectionMonkeyBaseModel):
+    """
+    Class describing agent plugin
+
+    Attributes:
+        :param plugin_manifest: Metadata describing the plugin
+        :param config_schema: JSONSchema describing the configuration options
+        :param default_config: Dictionary of default configuration options
+        :param source_archive: Contents of the plugin codebase
+    """
+
     plugin_manifest: AgentPluginManifest
     # Should be JSONSerializable but it's only supported in 3.9:
     # https://github.com/pydantic/pydantic/pull/1844
--- monkey/common/agent_plugins/agent_plugin_manifest.py
+++ monkey/common/agent_plugins/agent_plugin_manifest.py
@@ -6,6 +6,21 @@ from common.base_models import InfectionMonkeyBaseModel
 
 
 class AgentPluginManifest(InfectionMonkeyBaseModel):
+    """
+    Class describing agent plugin
+
+    Attributes:
+        :param name: Plugin name in snake case
+        :param plugin_type: Type of the plugin (exploiter, fingerprinter,
+         credential collector, etc.)
+        :param supported_operating_systems: List of operating systems the plugin supports
+        :param title: Human readable name for the plugin
+        :param description: Description of the plugin
+        :param link_to_documentation: Link to the documentation of the plugin
+        :param safe: Is the plugin safe to run. If there's a chance that running the plugin could
+         disrupt the regular activities of the servers or the network, then the plugin is not safe.
+    """
+
     name: str
     plugin_type: AgentPluginType
     supported_operating_systems: Tuple[OperatingSystem, ...] = (
