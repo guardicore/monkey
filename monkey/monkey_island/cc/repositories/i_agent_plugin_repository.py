af5bbd2eb75b84e835816c35728e4f52d39aa667
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-06 10:56:31 -0500

    Island: Fix docstring formatting in IAgentPluginRepository

--- monkey/monkey_island/cc/repositories/i_agent_plugin_repository.py
+++ monkey/monkey_island/cc/repositories/i_agent_plugin_repository.py
@@ -11,11 +11,9 @@ class IAgentPluginRepository(ABC):
         """
         Retrieve AgentPlugin based on it's name and type
 
-
         :param name: The name of the plugin
         :param plugin_type: The type of the plugin
         :raises RetrievalError: If an error occurs while attempting to retrieve the plugin
-        :raises UnknownRecordError: If a plugin with specified name and type doesn't
-         exist in the repository
+        :raises UnknownRecordError: If a plugin with specified name and type doesn't exist
         """
         pass
