de57dfa21a5eb766ce9c83dbd111fae5ef33fddb
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-12-07 19:32:57 +0000

    Island: Update plugin_utils errors

--- monkey/monkey_island/cc/repositories/plugin_utils.py
+++ monkey/monkey_island/cc/repositories/plugin_utils.py
@@ -1,28 +1,43 @@
 import json
-from tarfile import TarFile
+from tarfile import TarFile, TarInfo
 from typing import Any, BinaryIO, Dict
 
 import yaml
 
 from common.agent_plugins import AgentPlugin, AgentPluginManifest
 
-from . import RetrievalError
-
 MANIFEST_FILENAME = "plugin.yaml"
 CONFIG_SCHEMA_FILENAME = "config-schema.json"
 SOURCE_ARCHIVE_FILENAME = "data.tar"
+
+
+def tarinfo_type(tar: TarInfo) -> str:
+    if tar.isfile():
+        return "file"
+    elif tar.isdir():
+        return "dir"
+    elif tar.issym():
+        return "symlink"
+    elif tar.islnk():
+        return "hardlink"
+    elif tar.isdev():
+        return "device"
+    return "unknown"
 
 
 def load_plugin(file: BinaryIO) -> AgentPlugin:
     """
     Load a plugin from a tar file.
 
-    :raises RetrievalError: if the plugin failed to load
+    :raises ValueError: if the file is not a valid plugin
     """
     tar_file = TarFile(fileobj=file)
-    manifest = get_plugin_manifest(tar_file)
-    schema = get_plugin_schema(tar_file)
-    source = get_plugin_source(tar_file)
+    try:
+        manifest = get_plugin_manifest(tar_file)
+        schema = get_plugin_schema(tar_file)
+        source = get_plugin_source(tar_file)
+    except KeyError as e:
+        raise ValueError("Bad plugin") from e
 
     return AgentPlugin(plugin_manifest=manifest, config_schema=schema, source_archive=source)
 
@@ -32,12 +47,12 @@ def get_plugin_manifest(tar: TarFile) -> AgentPluginManifest:
     Retrieve the plugin manifest from a tar file.
 
     :raises KeyError: if the manifest was not found in the tar file
-    :raises RetrievalError: if the manifest could not be loaded
+    :raises ValueError: if the manifest is not a file
     """
     manifest_info = tar.getmember(MANIFEST_FILENAME)
     manifest_buf = tar.extractfile(manifest_info)
     if not manifest_buf:
-        raise RetrievalError("Error retrieving plugin manifest file")
+        raise ValueError(f"Plugin manifest file is of incorrect type {tarinfo_type(manifest_info)}")
 
     manifest = yaml.safe_load(manifest_buf)
     return AgentPluginManifest(**manifest)
@@ -48,12 +63,12 @@ def get_plugin_schema(tar: TarFile) -> Dict[str, Any]:
     Retrieve the plugin schema from a tar file.
 
     :raises KeyError: if the schema was not found in the tar file
-    :raises RetrievalError: if the schema could not be loaded
+    :raises ValueError: if the schema is not a file
     """
     schema_info = tar.getmember(CONFIG_SCHEMA_FILENAME)
     schema_buf = tar.extractfile(schema_info)
     if not schema_buf:
-        raise RetrievalError("Error retrieving plugin schema file")
+        raise ValueError(f"Plugin schema file has incorrect type {tarinfo_type(schema_info)}")
 
     return json.load(schema_buf)
 
@@ -63,11 +78,11 @@ def get_plugin_source(tar: TarFile) -> bytes:
     Retrieve the plugin source from a tar file.
 
     :raises KeyError: if the source was not found in the tar file
-    :raises RetrievalError: if the source could not be loaded
+    :raises ValueError: if the source is not a file
     """
     archive_info = tar.getmember(SOURCE_ARCHIVE_FILENAME)
     archive_buf = tar.extractfile(archive_info)
     if not archive_buf:
-        raise RetrievalError("Error retrieving plugin source file")
+        raise ValueError(f"Plugin source file has incorrect type {tarinfo_type(archive_info)}")
 
     return archive_buf.read()
