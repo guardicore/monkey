20bbc32d89d25a6f96b240e4baab4ca2f65bf447
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-12-07 15:34:09 +0000

    Island: Remove TarPluginLoader class

--- monkey/monkey_island/cc/repositories/plugin_utils.py
+++ monkey/monkey_island/cc/repositories/plugin_utils.py
@@ -13,42 +13,61 @@ CONFIG_SCHEMA_FILENAME = "config-schema.json"
 SOURCE_ARCHIVE_FILENAME = "data.tar"
 
 
-# TODO: We probably don't need a class
-class TarPluginLoader:
-    @staticmethod
-    def load_plugin(file: BinaryIO) -> AgentPlugin:
-        tar_file = TarFile(fileobj=file)
-        manifest = TarPluginLoader.get_plugin_manifest(tar_file)
-        schema = TarPluginLoader.get_plugin_schema(tar_file)
-        source = TarPluginLoader.get_plugin_source(tar_file)
+def load_plugin(file: BinaryIO) -> AgentPlugin:
+    """
+    Load a plugin from a tar file.
 
-        # TODO: Add default_config
-        return AgentPlugin(plugin_manifest=manifest, config_schema=schema, source_archive=source)
+    :raises RetrievalError: if the plugin failed to load
+    """
+    tar_file = TarFile(fileobj=file)
+    manifest = get_plugin_manifest(tar_file)
+    schema = get_plugin_schema(tar_file)
+    source = get_plugin_source(tar_file)
 
-    @staticmethod
-    def get_plugin_manifest(tar: TarFile) -> AgentPluginManifest:
-        manifest_info = tar.getmember(MANIFEST_FILENAME)
-        manifest_buf = tar.extractfile(manifest_info)
-        if not manifest_buf:
-            raise RetrievalError("Error retrieving plugin manifest file")
+    return AgentPlugin(plugin_manifest=manifest, config_schema=schema, source_archive=source)
 
-        manifest = yaml.safe_load(manifest_buf)
-        return AgentPluginManifest(**manifest)
 
-    @staticmethod
-    def get_plugin_schema(tar: TarFile) -> Dict[str, Any]:
-        schema_info = tar.getmember(CONFIG_SCHEMA_FILENAME)
-        schema_buf = tar.extractfile(schema_info)
-        if not schema_buf:
-            raise RetrievalError("Error retrieving plugin schema file")
+def get_plugin_manifest(tar: TarFile) -> AgentPluginManifest:
+    """
+    Retrieve the plugin manifest from a tar file.
 
-        return json.load(schema_buf)
+    :raises KeyError: if the manifest was not found in the tar file
+    :raises RetrievalError: if the manifest could not be loaded
+    """
+    manifest_info = tar.getmember(MANIFEST_FILENAME)
+    manifest_buf = tar.extractfile(manifest_info)
+    if not manifest_buf:
+        raise RetrievalError("Error retrieving plugin manifest file")
 
-    @staticmethod
-    def get_plugin_source(tar: TarFile) -> bytes:
-        archive_info = tar.getmember(SOURCE_ARCHIVE_FILENAME)
-        archive_buf = tar.extractfile(archive_info)
-        if not archive_buf:
-            raise RetrievalError("Error retrieving plugin source file")
+    manifest = yaml.safe_load(manifest_buf)
+    return AgentPluginManifest(**manifest)
 
-        return archive_buf.read()
+
+def get_plugin_schema(tar: TarFile) -> Dict[str, Any]:
+    """
+    Retrieve the plugin schema from a tar file.
+
+    :raises KeyError: if the schema was not found in the tar file
+    :raises RetrievalError: if the schema could not be loaded
+    """
+    schema_info = tar.getmember(CONFIG_SCHEMA_FILENAME)
+    schema_buf = tar.extractfile(schema_info)
+    if not schema_buf:
+        raise RetrievalError("Error retrieving plugin schema file")
+
+    return json.load(schema_buf)
+
+
+def get_plugin_source(tar: TarFile) -> bytes:
+    """
+    Retrieve the plugin source from a tar file.
+
+    :raises KeyError: if the source was not found in the tar file
+    :raises RetrievalError: if the source could not be loaded
+    """
+    archive_info = tar.getmember(SOURCE_ARCHIVE_FILENAME)
+    archive_buf = tar.extractfile(archive_info)
+    if not archive_buf:
+        raise RetrievalError("Error retrieving plugin source file")
+
+    return archive_buf.read()
--- monkey/tests/unit_tests/monkey_island/cc/repositories/test_plugin_utils.py
+++ monkey/tests/unit_tests/monkey_island/cc/repositories/test_plugin_utils.py
@@ -6,7 +6,11 @@ import pytest
 
 from common import OperatingSystem
 from common.agent_plugins import AgentPluginManifest, AgentPluginType
-from monkey_island.cc.repositories.plugin_utils import TarPluginLoader
+from monkey_island.cc.repositories.plugin_utils import (
+    get_plugin_manifest,
+    get_plugin_schema,
+    get_plugin_source,
+)
 
 
 @pytest.fixture
@@ -34,18 +38,18 @@ def test_get_plugin_manifest(plugin_tarfile):
         safe=True,
     )
 
-    actual = TarPluginLoader.get_plugin_manifest(plugin_tarfile)
+    actual = get_plugin_manifest(plugin_tarfile)
 
     assert actual == expected
 
 
 def test_get_plugin_schema(plugin_tarfile):
-    schema = TarPluginLoader.get_plugin_schema(plugin_tarfile)
+    schema = get_plugin_schema(plugin_tarfile)
 
     assert isinstance(schema, dict)
 
 
 def test_get_plugin_source(plugin_tarfile):
-    data = TarPluginLoader.get_plugin_source(plugin_tarfile)
+    data = get_plugin_source(plugin_tarfile)
 
     assert len(data) > 0
