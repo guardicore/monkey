b93f23678af242a5bb98bd3263c1dfbc36d78a91
Shreya Malviya <shreya.malviya@gmail.com>
2022-12-12 13:32:36 +0530

    UT: Register InMemoryAgentPluginRepository() in DI container in test_agent_plugins.py

--- monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_plugins.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_plugins.py
@@ -2,14 +2,17 @@ from http import HTTPStatus
 
 import pytest
 from tests.common import StubDIContainer
+from tests.monkey_island import InMemoryAgentPluginRepository
 from tests.unit_tests.monkey_island.conftest import get_url_for_resource
 
+from monkey_island.cc.repositories import IAgentPluginRepository
 from monkey_island.cc.resources import AgentPlugins
 
 
 @pytest.fixture
 def flask_client(build_flask_client):
     container = StubDIContainer()
+    container.register_instance(IAgentPluginRepository, InMemoryAgentPluginRepository())
 
     with build_flask_client(container) as flask_client:
         yield flask_client
