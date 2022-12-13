43408109c1f5af003ebaabe9679d1cc831b968c9
Shreya Malviya <shreya.malviya@gmail.com>
2022-12-13 15:01:45 +0530

    UI: Add info to map node preview pane if log file does not exist

--- monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthComponent from '../AuthComponent';
 import download from 'downloadjs';
 import { Button } from 'react-bootstrap';
@@ -9,33 +9,62 @@ type Props = { url: string, filename: string, variant?: string }
 
 
 export const AgentLogDownloadButton = ({ url, filename, variant = 'primary' }: Props) => {
+  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(<></>);
+
   function downloadAgentLog() {
     authComponent.authFetch(url)
-      .then(res => res.json())
       .then(res => {
-        download(res, filename, 'text/plain');
+        if (res.status === 404) {
+          setNoLogFileExistsComponent(
+            <p style={{'marginTop': '1em', 'marginBottom': '1em', 'color': 'red'}}>
+              The server could not find the requested log file.
+            </p>
+          );
+        }
+        return res.json()
+      })
+      .then(res => {
+        if (res !== "") {
+          download(res, filename, 'text/plain');
+        }
       });
   }
 
-  return (<Button variant={variant}
-    onClick={downloadAgentLog}>
-    Download Log
-  </Button>);
+  return (<>
+    <Button variant={variant} onClick={downloadAgentLog}>
+      Download Log
+    </Button>
+    {noLogFileExistsComponent}
+  </>);
 }
 
 type IslandLogDownloadProps = {url: string, variant?: string}
 
 export const IslandLogDownloadButton = ({url, variant = 'primary'}: IslandLogDownloadProps) => {
+  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(<></>);
   function downloadIslandLog() {
     authComponent.authFetch(url)
-      .then(res => res.json())
-      .then(res => {
+    .then(res => {
+      if (res.status === 404) {
+        setNoLogFileExistsComponent(
+          <p style={{'marginTop': '1em', 'marginBottom': '1em', 'color': 'red'}}>
+            The server could not find the requested log file.
+          </p>
+        );
+      }
+      return res.json()
+    })
+    .then(res => {
+      if (res !== "") {
         let filename = 'Island_log';
         download(res, filename, 'text/plain');
-      });
+      }
+    });
   }
-  return (<Button variant={variant}
-    onClick={downloadIslandLog}>
-    Download Log
-  </Button>);
+  return (<>
+    <Button variant={variant} onClick={downloadIslandLog}>
+      Download Log
+    </Button>
+    {noLogFileExistsComponent}
+  </>);
 }
