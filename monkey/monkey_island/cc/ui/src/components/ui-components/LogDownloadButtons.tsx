12e7a60e3c5d06737df32949b5f375b2096b161d
Shreya Malviya <shreya.malviya@gmail.com>
2022-12-14 14:46:40 +0530

    UI: Show Error404Modal if log file not found when requested from map preview pane

--- monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/LogDownloadButtons.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import AuthComponent from '../AuthComponent';
 import download from 'downloadjs';
 import { Button } from 'react-bootstrap';
+import Error404Modal from './Error404Modal';
 
 const authComponent = new AuthComponent({})
 
@@ -9,17 +10,13 @@ type Props = { url: string, filename: string, variant?: string }
 
 
 export const AgentLogDownloadButton = ({ url, filename, variant = 'primary' }: Props) => {
-  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(<></>);
+  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(false);
 
   function downloadAgentLog() {
     authComponent.authFetch(url)
       .then(res => {
         if (res.status === 404) {
-          setNoLogFileExistsComponent(
-            <p style={{'marginTop': '1em', 'marginBottom': '1em', 'color': 'red'}}>
-              The server could not find the requested log file.
-            </p>
-          );
+          setNoLogFileExistsComponent(true);
         }
         return res.json()
       })
@@ -30,27 +27,31 @@ export const AgentLogDownloadButton = ({ url, filename, variant = 'primary' }: P
       });
   }
 
+  function closeModal() {
+    setNoLogFileExistsComponent(false);
+  };
+
   return (<>
     <Button variant={variant} onClick={downloadAgentLog}>
       Download Log
     </Button>
-    {noLogFileExistsComponent}
+    <Error404Modal
+      showModal={noLogFileExistsComponent}
+      onClose={closeModal}
+    />
   </>);
 }
 
 type IslandLogDownloadProps = {url: string, variant?: string}
 
 export const IslandLogDownloadButton = ({url, variant = 'primary'}: IslandLogDownloadProps) => {
-  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(<></>);
+  const [noLogFileExistsComponent, setNoLogFileExistsComponent] = useState(false);
+
   function downloadIslandLog() {
     authComponent.authFetch(url)
     .then(res => {
       if (res.status === 404) {
-        setNoLogFileExistsComponent(
-          <p style={{'marginTop': '1em', 'marginBottom': '1em', 'color': 'red'}}>
-            The server could not find the requested log file.
-          </p>
-        );
+        setNoLogFileExistsComponent(true);
       }
       return res.json()
     })
@@ -61,10 +62,18 @@ export const IslandLogDownloadButton = ({url, variant = 'primary'}: IslandLogDow
       }
     });
   }
+
+  function closeModal() {
+    setNoLogFileExistsComponent(false);
+  };
+
   return (<>
     <Button variant={variant} onClick={downloadIslandLog}>
       Download Log
     </Button>
-    {noLogFileExistsComponent}
+    <Error404Modal
+      showModal={noLogFileExistsComponent}
+      onClose={closeModal}
+    />
   </>);
 }
