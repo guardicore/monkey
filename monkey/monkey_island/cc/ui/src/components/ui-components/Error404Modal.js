af1db283731908d8d9121ec6a35d139c68ac82fa
Shreya Malviya <shreya.malviya@gmail.com>
2022-12-14 14:45:31 +0530

    UI: Add Error404Modal

---
+++ monkey/monkey_island/cc/ui/src/components/ui-components/Error404Modal.js
@@ -0,0 +1,45 @@
+import {Modal} from 'react-bootstrap';
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faExclamationTriangle, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {Button, Row, Col} from 'react-bootstrap';
+
+
+class Error404Modal extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: this.props.showModal,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      this.setState({
+        showModal: this.props.showModal,
+      })
+    }
+  }
+
+  render = () => {
+    return (
+      <Modal show={this.state.showModal} onHide={() => this.props.onClose()}>
+        <Modal.Header closeButton>
+          <Modal.Title> Uh oh... </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div style={{'marginTop': '1em', 'marginBottom': '1em'}}>
+            <p className="alert alert-danger">
+              <FontAwesomeIcon icon={faExclamationTriangle} style={{'marginRight': '5px'}}/>
+              The server returned a 404 (NOT FOUND) response.
+            </p>
+          </div>
+        </Modal.Body>
+      </Modal>
+    )
+  };
+}
+
+export default Error404Modal;
