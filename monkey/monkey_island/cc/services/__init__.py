229ae935a58e51f24839510d2a1dc3bc4fb3206f
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-12-15 21:51:38 +0000

    Island: Add ConfigSchemaService

--- monkey/monkey_island/cc/services/__init__.py
+++ monkey/monkey_island/cc/services/__init__.py
@@ -2,3 +2,5 @@ from .agent_signals_service import AgentSignalsService
 from .authentication_service import AuthenticationService
 
 from .aws import AWSService
+
+from .config_schema_service import ConfigSchemaService
