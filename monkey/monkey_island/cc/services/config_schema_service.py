1cc2514a64769de82aac09fa3b76a1a08d177fe8
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-12-15 21:52:18 +0000

    Island: Add plugin schemas

--- monkey/monkey_island/cc/services/config_schema_service.py
+++ monkey/monkey_island/cc/services/config_schema_service.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
-from common.agent_configuration import AgentConfiguration
+from common.agent_configuration import AgentConfiguration, PluginConfiguration
+from common.agent_plugins import AgentPlugin, AgentPluginType
 from monkey_island.cc.repositories import IAgentPluginRepository
 
 
@@ -15,6 +16,36 @@ class ConfigSchemaService:
     ):
         self._agent_plugin_repository = agent_plugin_repository
 
+    def _create_plugin_schema(self, plugin: AgentPlugin) -> Dict[str, Any]:
+        schema = PluginConfiguration.schema()
+        schema["properties"]["name"]["enum"] = [plugin.plugin_manifest.name]
+        schema["properties"]["options"]["properties"] = plugin.config_schema
+
+        return schema
+
+    def _add_plugins_to_schema(self, schema: Dict[str, Any]):
+        # Note: Can throw RetrievalError, UnknownRecordError
+        plugins = self._agent_plugin_repository.get_plugin_catalog()
+        if plugins:
+            schema["plugins"] = {}
+
+        for (plugin_type, name) in plugins:
+            plugin = self._agent_plugin_repository.get_plugin(plugin_type, name)
+            plugin_schema = self._create_plugin_schema(plugin)
+
+            plugin_type_name = str(plugin_type.name).lower()
+            if plugin_type_name not in schema["plugins"]:
+                schema["plugins"][plugin_type_name] = {"anyOf": []}
+            schema["plugins"][plugin_type_name]["anyOf"].append(plugin_schema)
+
+            # Add reference, based on type
+            # TODO: It would actually probably be easier to forego the
+            # reference and just add the schema here
+            if plugin_type == AgentPluginType.EXPLOITER:
+                exploitation = schema["definitions"]["ExploitationConfiguration"]
+                brute_force = exploitation["properties"]["brute_force"]
+                brute_force["items"] = {"$ref": "#/plugins/exploiter"}
+
     def get_schema(self) -> Dict[str, Any]:
         """
         Get the schema.
@@ -23,6 +54,9 @@ class ConfigSchemaService:
         """
         try:
             agent_config_schema = AgentConfiguration.schema()
+
+            self._add_plugins_to_schema(agent_config_schema)
+
             return agent_config_schema
         except Exception as err:
             raise RuntimeError(err)
