f94b53b30aa02ba6c2acdbf5413aee995af89715
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2022-12-16 20:11:45 +0000

    Agent: Create the plugin parent dir

--- monkey/infection_monkey/monkey.py
+++ monkey/infection_monkey/monkey.py
@@ -317,6 +317,7 @@ class InfectionMonkey:
         return list(ordered_servers.keys())
 
     def _build_puppet(self) -> IPuppet:
+        create_secure_directory(TMP_DIR)
         create_secure_directory(PLUGIN_DIR)
         plugin_registry = PluginRegistry(self._island_api_client, PluginLoader(PLUGIN_DIR))
         puppet = Puppet(self._agent_event_queue, plugin_registry)
