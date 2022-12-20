a5f4ec567a2c0e6544d283d10dbdb551f597f9a0
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-20 14:45:03 -0500

    UT: Rework PluginRegistry tests

--- monkey/tests/unit_tests/infection_monkey/puppet/test_plugin_registry.py
+++ monkey/tests/unit_tests/infection_monkey/puppet/test_plugin_registry.py
@@ -1,54 +1,40 @@
 from unittest.mock import MagicMock
 
 import pytest
-from flask import Response
 
 from common.agent_plugins import AgentPluginType
 from infection_monkey.i_puppet import UnknownPluginError
+from infection_monkey.island_api_client import IslandAPIError, IslandAPIRequestError
 from infection_monkey.puppet import PluginRegistry
 
 
-class StubIslandAPIClient:
-    def __init__(self, status: int):
-        self._status = status
-
-    def get_agent_plugin(self, _, __):
-        return Response(status=self._status)
-
-
 @pytest.fixture
-def stub_plugin_source_extractor():
+def dummy_plugin_source_extractor():
     return MagicMock()
 
 
 @pytest.fixture
-def stub_plugin_loader():
+def dummy_plugin_loader():
     return MagicMock()
 
 
-def test_get_plugin_not_found(plugin_source_extractor, plugin_loader):
+def test_get_plugin__unexpected_response(dummy_plugin_source_extractor, dummy_plugin_loader):
+    mock_island_api_client = MagicMock()
+    mock_island_api_client.get_agent_plugin = MagicMock(side_effect=IslandAPIError)
     plugin_registry = PluginRegistry(
-        StubIslandAPIClient(status=404), plugin_source_extractor, plugin_loader
+        mock_island_api_client, dummy_plugin_source_extractor, dummy_plugin_loader
+    )
+
+    with pytest.raises(IslandAPIError):
+        plugin_registry.get_plugin("Ghost", AgentPluginType.PAYLOAD)
+
+
+def test_get_plugin__unknown_plugin(dummy_plugin_source_extractor, dummy_plugin_loader):
+    mock_island_api_client = MagicMock()
+    mock_island_api_client.get_agent_plugin = MagicMock(side_effect=IslandAPIRequestError)
+    plugin_registry = PluginRegistry(
+        mock_island_api_client, dummy_plugin_source_extractor, dummy_plugin_loader
     )
 
     with pytest.raises(UnknownPluginError):
-        plugin_registry.get_plugin("Ghost", AgentPluginType.PAYLOAD)
-
-
-# modify when plugin architecture is fully implemented
-def test_get_plugin_not_implemented(plugin_source_extractor, plugin_loader):
-    plugin_registry = PluginRegistry(
-        StubIslandAPIClient(status=200), plugin_source_extractor, plugin_loader
-    )
-
-    with pytest.raises(NotImplementedError):
-        plugin_registry.get_plugin("Ghost", AgentPluginType.PAYLOAD)
-
-
-def test_get_plugin_unexpected_response(plugin_source_extractor, plugin_loader):
-    plugin_registry = PluginRegistry(
-        StubIslandAPIClient(status=100), plugin_source_extractor, plugin_loader
-    )
-
-    with pytest.raises(Exception):
         plugin_registry.get_plugin("Ghost", AgentPluginType.PAYLOAD)
