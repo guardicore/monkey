0cd26b4941cbb67b86f9828be55bd678877e0e2e
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-22 11:44:19 -0500

    Island: Reference correct schema service in config schema endpoint

--- monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration_schema.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration_schema.py
@@ -7,20 +7,20 @@ from tests.unit_tests.monkey_island.conftest import get_url_for_resource
 
 from monkey_island.cc.repositories import RetrievalError
 from monkey_island.cc.resources import AgentConfigurationSchema
-from monkey_island.cc.services import ConfigSchemaService
+from monkey_island.cc.services import AgentConfigSchemaService
 
 AGENT_CONFIGURATION_SCHEMA_URL = get_url_for_resource(AgentConfigurationSchema)
 
 
 @pytest.fixture
 def mock_config_schema_service():
-    return MagicMock(spec=ConfigSchemaService)
+    return MagicMock(spec=AgentConfigSchemaService)
 
 
 @pytest.fixture
 def flask_client(build_flask_client, mock_config_schema_service):
     container = StubDIContainer()
-    container.register_instance(ConfigSchemaService, mock_config_schema_service)
+    container.register_instance(AgentConfigSchemaService, mock_config_schema_service)
 
     with build_flask_client(container) as flask_client:
         yield flask_client
