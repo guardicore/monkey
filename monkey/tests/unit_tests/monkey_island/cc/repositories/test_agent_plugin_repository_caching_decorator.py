8cd9d522c627ec45d8891c77c396a2c375afbc3a
Ilija Lazoroski <ilija.la@live.com>
2022-12-22 12:12:59 +0100

    UT: Remove name from InMemoryAgentPluginRepository.save_plugin
    We save plugins based on plugin_manifest.name
    
    PR: #2751

--- monkey/tests/unit_tests/monkey_island/cc/repositories/test_agent_plugin_repository_caching_decorator.py
+++ monkey/tests/unit_tests/monkey_island/cc/repositories/test_agent_plugin_repository_caching_decorator.py
@@ -23,21 +23,25 @@ def agent_plugin_repository(in_memory_agent_plugin_repository) -> IAgentPluginRe
 
 
 def test_get_cached_plugin(agent_plugin_repository, in_memory_agent_plugin_repository):
-    in_memory_agent_plugin_repository.save_plugin("ssh_one", FAKE_AGENT_PLUGIN_1)
-    request_1_plugin = agent_plugin_repository.get_plugin(AgentPluginType.EXPLOITER, "ssh_one")
+    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_1)
+    request_1_plugin = agent_plugin_repository.get_plugin(
+        AgentPluginType.EXPLOITER, FAKE_AGENT_PLUGIN_1.plugin_manifest.name
+    )
 
-    in_memory_agent_plugin_repository.save_plugin("ssh_one", FAKE_AGENT_PLUGIN_2)
-    request_2_plugin = agent_plugin_repository.get_plugin(AgentPluginType.EXPLOITER, "ssh_one")
+    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_2)
+    request_2_plugin = agent_plugin_repository.get_plugin(
+        AgentPluginType.EXPLOITER, FAKE_AGENT_PLUGIN_2.plugin_manifest.name
+    )
 
     assert request_2_plugin == request_1_plugin
     assert request_2_plugin != FAKE_AGENT_PLUGIN_2
 
 
 def test_get_cached_plugin_catalog(agent_plugin_repository, in_memory_agent_plugin_repository):
-    in_memory_agent_plugin_repository.save_plugin("ssh_exploiter", FAKE_AGENT_PLUGIN_1)
+    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_1)
     request_1_plugin_catalog = agent_plugin_repository.get_plugin_catalog()
 
-    in_memory_agent_plugin_repository.save_plugin("wmi_exploiter", FAKE_AGENT_PLUGIN_2)
+    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_2)
     request_2_plugin_catalog = agent_plugin_repository.get_plugin_catalog()
 
     assert request_2_plugin_catalog == request_1_plugin_catalog
