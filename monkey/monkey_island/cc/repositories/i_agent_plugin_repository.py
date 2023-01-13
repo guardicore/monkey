e263da0ab4bc12b03bf2521ffd0135768bf9d2e0
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-01-13 19:15:56 +0000

    Island: Add IAgentPluginRepository.get_plugin_for_os()

--- monkey/monkey_island/cc/repositories/i_agent_plugin_repository.py
+++ monkey/monkey_island/cc/repositories/i_agent_plugin_repository.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Sequence, Tuple
 
+from common import OperatingSystem
 from common.agent_plugins import AgentPlugin, AgentPluginType
 
 
@@ -10,8 +11,23 @@ class IAgentPluginRepository(ABC):
     @abstractmethod
     def get_plugin(self, plugin_type: AgentPluginType, name: str) -> AgentPlugin:
         """
-        Retrieve AgentPlugin based on it's name and type
+        Retrieve AgentPlugin based on its name and type
 
+        :param plugin_type: The type of the plugin
+        :param name: The name of the plugin
+        :raises RetrievalError: If an error occurs while attempting to retrieve the plugin
+        :raises UnknownRecordError: If a plugin with specified name and type doesn't exist
+        """
+        pass
+
+    @abstractmethod
+    def get_plugin_for_os(
+        self, host_operating_system: OperatingSystem, plugin_type: AgentPluginType, name: str
+    ) -> AgentPlugin:
+        """
+        Retrieve AgentPlugin based on its name and type, for a given operating system
+
+        :param host_operating_system: The operating system on which the plugin will run
         :param plugin_type: The type of the plugin
         :param name: The name of the plugin
         :raises RetrievalError: If an error occurs while attempting to retrieve the plugin
