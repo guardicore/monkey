368842928c939a5a86b74e1812076b1d2365bc97
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-01-13 19:19:24 +0000

    Island: Add get_plugin_for_os() to the logging decorator

--- monkey/monkey_island/cc/repositories/agent_plugin_repository_logging_decorator.py
+++ monkey/monkey_island/cc/repositories/agent_plugin_repository_logging_decorator.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Sequence, Tuple
 
+from common import OperatingSystem
 from common.agent_plugins import AgentPlugin, AgentPluginType
 
 from . import IAgentPluginRepository
@@ -20,6 +21,16 @@ class AgentPluginRepositoryLoggingDecorator(IAgentPluginRepository):
     def get_plugin(self, plugin_type: AgentPluginType, name: str) -> AgentPlugin:
         logger.debug(f"Retrieving plugin {name} of type {plugin_type}")
         return self._agent_plugin_repository.get_plugin(plugin_type, name)
+
+    def get_plugin_for_os(
+        self, host_operating_system: OperatingSystem, plugin_type: AgentPluginType, name: str
+    ) -> AgentPlugin:
+        logger.debug(
+            f"Retrieving plugin {name} of type {plugin_type} for os {host_operating_system}"
+        )
+        return self._agent_plugin_repository.get_plugin_for_os(
+            host_operating_system, plugin_type, name
+        )
 
     def get_plugin_catalog(self) -> Sequence[Tuple[AgentPluginType, str]]:
         logger.debug("Retrieving plugin catalog")
