2e64b530a8f09944de6fcfbe5e40838a4efeeb7c
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-01-13 19:20:19 +0000

    Island: Add get_plugin_for_os() to the caching decorator

--- monkey/monkey_island/cc/repositories/agent_plugin_repository_caching_decorator.py
+++ monkey/monkey_island/cc/repositories/agent_plugin_repository_caching_decorator.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from typing import Sequence, Tuple
 
+from common import OperatingSystem
 from common.agent_plugins import AgentPlugin, AgentPluginType
 
 from . import IAgentPluginRepository
@@ -17,6 +18,14 @@ class AgentPluginRepositoryCachingDecorator(IAgentPluginRepository):
     @lru_cache()
     def get_plugin(self, plugin_type: AgentPluginType, name: str) -> AgentPlugin:
         return self._agent_plugin_repository.get_plugin(plugin_type, name)
+
+    @lru_cache()
+    def get_plugin_for_os(
+        self, host_operating_system: OperatingSystem, plugin_type: AgentPluginType, name: str
+    ) -> AgentPlugin:
+        return self._agent_plugin_repository.get_plugin_for_os(
+            host_operating_system, plugin_type, name
+        )
 
     @lru_cache()
     def get_plugin_catalog(self) -> Sequence[Tuple[AgentPluginType, str]]:
