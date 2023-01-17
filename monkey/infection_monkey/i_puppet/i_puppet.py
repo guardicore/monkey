351226e8885ff6195e8c1b168ece41ded3fa4ace
Ilija Lazoroski <ilija.la@live.com>
2023-01-17 16:59:42 +0100

    Agent: Change IPuppet.exloit_host docstring

--- monkey/infection_monkey/i_puppet/i_puppet.py
+++ monkey/infection_monkey/i_puppet/i_puppet.py
@@ -129,6 +129,7 @@ class IPuppet(metaclass=abc.ABCMeta):
                              exploiter
         :param Event interrupt: An `Event` object that signals the exploit to stop
                                           executing and clean itself up.
+        :raises IncompatibleOperatingSystemError: If a exploit is not compatabile with the host
         :return: True if exploitation was successful, False otherwise
         :rtype: ExploiterResultData
         """
