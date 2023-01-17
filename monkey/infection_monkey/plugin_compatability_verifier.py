a6f1b32153b96d5ac4477a22f75e87dbc5b803dd
Ilija Lazoroski <ilija.la@live.com>
2023-01-17 12:38:47 +0100

    Agent: Implement PluginCompatabilityVerifier

--- monkey/infection_monkey/plugin_compatability_verifier.py
+++ monkey/infection_monkey/plugin_compatability_verifier.py
@@ -2,7 +2,7 @@ import logging
 from typing import Dict
 
 from common import HARD_CODED_EXPLOITER_MANIFESTS
-from common.agent_plugins import AgentPluginManifest
+from common.agent_plugins import AgentPluginManifest, AgentPluginType
 
 from .i_puppet import IncompatibleOperatingSystemError
 from .island_api_client import IIslandAPIClient, IslandAPIError
@@ -12,6 +12,10 @@ logger = logging.getLogger(__name__)
 
 
 class PluginCompatabilityVerifier:
+    """
+    Verify plugin compatibility to run
+    """
+
     def __init__(
         self,
         island_api_client: IIslandAPIClient,
@@ -21,6 +25,15 @@ class PluginCompatabilityVerifier:
         self._exploiter_plugin_manifests = exploiter_plugin_manifests
 
     def verify_exploiter_compatibility(self, exploiter_name: str, target_host: TargetHost):
+        """
+        Verify exploiter compatibility to run on a target host
+
+        It verifies based on a target host operating system and exploiter manifest
+        supported operating systems.
+        :param exploiter_name: Name of the exploiter
+        :param target_host: Target host
+        :raises IncompatibleOperatingSystemError: If exploiter is not compatible to run
+        """
         exploiter_agent_plugin_manifest = self._get_exploiter_plugin_manifest(exploiter_name)
         supported_exploiter_os = exploiter_agent_plugin_manifest.supported_operating_systems
 
@@ -28,16 +41,24 @@ class PluginCompatabilityVerifier:
             target_host.operating_system is None
             or target_host.operating_system in supported_exploiter_os
         ):
-            return True
+            return target_host
         else:
             raise IncompatibleOperatingSystemError(
-                f"Incompatible operating system for "
-                f"exploiter:{exploiter_name} and "
-                f"target_host:{target_host}"
+                f"Incompatible operating system for exploiter:{exploiter_name} and"
+                f" target_host:{target_host}"
             )
 
-    def _get_exploiter_plugin_manifest(self, exploiter_name: str):
+    def _get_exploiter_plugin_manifest(self, exploiter_name: str) -> AgentPluginManifest:
+        """
+        Get exploiter plugin manifest
+
+        Request island to find the plugin manifest, if it is not
+        successful it looks at the hard coded exploiter manifests.
+        :param exploiter_name: Name of exploiter
+        """
         try:
-            return self._island_api_client.get_agent_plugin_manifest(exploiter_name)
-        except Exception:
+            return self._island_api_client.get_agent_plugin_manifest(
+                AgentPluginType.EXPLOITER, exploiter_name
+            )
+        except IslandAPIError:
             return self._exploiter_plugin_manifests[exploiter_name]
