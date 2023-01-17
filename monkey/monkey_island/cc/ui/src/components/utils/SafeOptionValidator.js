d86edd69fa8781808d3fc463482c44e8202af73b
vakarisz <vakarisz@yahoo.com>
2023-01-17 16:09:47 +0200

    UI: Adjust safe options validation to validate plugins
    Issue #2698
    PR #2843

--- monkey/monkey_island/cc/ui/src/components/utils/SafeOptionValidator.js
+++ monkey/monkey_island/cc/ui/src/components/utils/SafeOptionValidator.js
@@ -1,10 +1,5 @@
-function getPluginDescriptors(schema, config) {
+function getLegacyPluginDescriptors(schema, config) {
   return ([
-    {
-      name: 'Exploiters',
-      allPlugins: schema.properties.propagation.properties.exploitation.properties.exploiters.items.properties.name.anyOf,
-      selectedPlugins: config.propagation.exploitation.exploiters
-    },
     {
       name: 'Fingerprinters',
       allPlugins: schema.properties.propagation.properties.network_scan.properties.fingerprinters.items.properties.name.anyOf,
@@ -18,11 +13,25 @@ function getPluginDescriptors(schema, config) {
   ]);
 }
 
+function getPluginDescriptors(schema, config) {
+  return ([
+    {
+      name: 'Exploiters',
+      allPlugins: schema.properties.propagation.properties.exploitation.properties.exploiters.properties,
+      selectedPlugins: Object.keys(config.propagation.exploitation.exploiters)
+    }
+  ])
+}
+
 function isUnsafeOptionSelected(schema, config) {
-  let pluginDescriptors = getPluginDescriptors(schema, config);
+  return isUnsafeLegacyPluginEnabled(schema, config) || isUnsafePluginEnabled(schema, config);
+}
+
+function isUnsafeLegacyPluginEnabled(schema, config) {
+  let pluginDescriptors = getLegacyPluginDescriptors(schema, config);
 
   for (let descriptor of pluginDescriptors) {
-    if (isUnsafePluginSelected(descriptor)) {
+    if (getUnsafeLegacyPlugins(descriptor).length > 0) {
       return true;
     }
   }
@@ -30,17 +39,36 @@ function isUnsafeOptionSelected(schema, config) {
   return false;
 }
 
-function isUnsafePluginSelected(pluginDescriptor) {
-  let pluginSafety = new Map();
-  pluginDescriptor.allPlugins.forEach(i => pluginSafety[i.enum[0]] = i.safe);
+function getUnsafeLegacyPlugins(pluginDescriptor) {
+  let unsafePlugins = [];
+  for (let selectedPlugin of pluginDescriptor.selectedPlugins) {
+    unsafePlugins = pluginDescriptor.allPlugins.filter(
+      (pluginSchema) => pluginSchema.enum[0] === selectedPlugin.name
+        && !isPluginSafe(pluginSchema))
+  }
 
-  for (let selected of pluginDescriptor.selectedPlugins) {
-    if (!pluginSafety[selected.name]) {
+  return unsafePlugins;
+}
+
+function isUnsafePluginEnabled(schema, config) {
+  let pluginDescriptors = getPluginDescriptors(schema, config);
+
+  for (let descriptor of pluginDescriptors) {
+    if (getUnsafePlugins(descriptor).length > 0) {
       return true;
     }
   }
 
   return false;
+}
+
+function getUnsafePlugins(pluginDescriptor) {
+  return pluginDescriptor.selectedPlugins.filter(
+    (pluginName) => !isPluginSafe(pluginDescriptor.allPlugins[pluginName]))
+}
+
+function isPluginSafe(pluginSchema) {
+  return pluginSchema.safe !== undefined && pluginSchema.safe
 }
 
 export default isUnsafeOptionSelected;
