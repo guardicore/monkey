469d2aec5e79d95b6390665fda36014089f35fe3
vakarisz <vakarisz@yahoo.com>
2023-01-17 18:09:18 +0200

    UI: Display plugin options in a pane

--- monkey/monkey_island/cc/ui/src/components/configuration-components/PluginSelectorTemplate.tsx
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/PluginSelectorTemplate.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import ChildCheckboxContainer from '../ui-components/ChildCheckbox';
 import {AdvancedMultiSelectHeader} from '../ui-components/AdvancedMultiSelect';
 import {MasterCheckboxState} from '../ui-components/MasterCheckbox';
+import {InfoPane, WarningType} from '../ui-components/InfoPane';
 
 
 export default function PluginSelectorTemplate(props: ObjectFieldTemplateProps) {
@@ -10,9 +11,15 @@ export default function PluginSelectorTemplate(props: ObjectFieldTemplateProps) 
   let [selectedPlugin, setSelectedPlugin] = useState(null);
 
   function getPluginDisplay(plugin, allPlugins) {
-    let selectedPlugin = allPlugins.filter((pluginInArray) => pluginInArray.name == plugin)
-    if (selectedPlugin.length === 1) {
-      return <div className="property-wrapper">{selectedPlugin[0].content}</div>
+    let selectedPlugins = allPlugins.filter((pluginInArray) => pluginInArray.name == plugin)
+    if (selectedPlugins.length === 1) {
+      let selectedPlugin = selectedPlugins[0];
+      let pluginWarningType = isPluginSafe(selectedPlugin.name) ?
+        WarningType.NONE : WarningType.SINGLE;
+      return <InfoPane title={''}
+                       body={selectedPlugin.content}
+                       link={selectedPlugin.content.props.schema.link}
+                       warningType={pluginWarningType}/>
     }
   }
 
