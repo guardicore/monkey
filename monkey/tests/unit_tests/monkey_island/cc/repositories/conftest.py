8e395d387eb70b7fcdab5164e748b1e93801974d
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-01-18 14:02:07 -0500

    UT: Refactor plugin_archive_parser tests

--- monkey/tests/unit_tests/monkey_island/cc/repositories/conftest.py
+++ monkey/tests/unit_tests/monkey_island/cc/repositories/conftest.py
@@ -9,16 +9,6 @@ def plugin_file(plugin_data_dir) -> Path:
 
 
 @pytest.fixture
-def plugin_with_one_vendor_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "plugin-with-one-vendor.tar"
-
-
-@pytest.fixture
-def plugin_with_two_vendors_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "plugin-with-two-vendors.tar"
-
-
-@pytest.fixture
 def bad_plugin_file(plugin_data_dir) -> Path:
     return plugin_data_dir / "bad-exploiter.tar"
 
@@ -31,23 +21,3 @@ def symlink_plugin_file(plugin_data_dir) -> Path:
 @pytest.fixture
 def dir_plugin_file(plugin_data_dir) -> Path:
     return plugin_data_dir / "dir-exploiter.tar"
-
-
-@pytest.fixture
-def plugin_with_three_vendors_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "plugin-with-three-vendors.tar"
-
-
-@pytest.fixture
-def plugin_with_two_vendor_dirs_one_vendor_file_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "plugin-with-two-vendor-dir-one-vendor-file.tar"
-
-
-@pytest.fixture
-def only_windows_vendor_plugin_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "only-windows-vendor-plugin-file.tar"
-
-
-@pytest.fixture
-def plugin_with_no_vendor_file(plugin_data_dir) -> Path:
-    return plugin_data_dir / "plugin-with-no-vendor.tar"
