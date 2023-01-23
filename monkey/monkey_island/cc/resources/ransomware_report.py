88feef41639fad2201c19bdad4175921ac0c4770
Ilija Lazoroski <ilija.la@live.com>
2023-01-23 18:27:23 +0100

    Island: Accept IAgentPluginRepository in RansomwareReport resource

--- monkey/monkey_island/cc/resources/ransomware_report.py
+++ monkey/monkey_island/cc/resources/ransomware_report.py
@@ -1,6 +1,10 @@
 from flask import jsonify
 
-from monkey_island.cc.repositories import IAgentEventRepository, IMachineRepository
+from monkey_island.cc.repositories import (
+    IAgentEventRepository,
+    IAgentPluginRepository,
+    IMachineRepository,
+)
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 from monkey_island.cc.resources.request_authentication import jwt_required
 from monkey_island.cc.services.ransomware import ransomware_report
@@ -10,17 +14,21 @@ class RansomwareReport(AbstractResource):
     urls = ["/api/report/ransomware"]
 
     def __init__(
-        self, event_repository: IAgentEventRepository, machine_repository: IMachineRepository
+        self,
+        event_repository: IAgentEventRepository,
+        machine_repository: IMachineRepository,
+        agent_plugin_repository: IAgentPluginRepository,
     ):
         self._event_repository = event_repository
         self._machine_repository = machine_repository
+        self._agent_plugin_repository = agent_plugin_repository
 
     @jwt_required
     def get(self):
         return jsonify(
             {
                 "propagation_stats": ransomware_report.get_propagation_stats(
-                    self._event_repository, self._machine_repository
+                    self._event_repository, self._machine_repository, self._agent_plugin_repository
                 ),
             }
         )
