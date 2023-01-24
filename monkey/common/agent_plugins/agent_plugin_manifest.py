de2de696c51071dc47380b71e38ac8bc563e2c84
Shreya Malviya <shreya.malviya@gmail.com>
2023-01-24 16:36:24 +0530

    Common: Reword AgentPluginManifest's docstring

--- monkey/common/agent_plugins/agent_plugin_manifest.py
+++ monkey/common/agent_plugins/agent_plugin_manifest.py
@@ -14,8 +14,8 @@ class AgentPluginManifest(InfectionMonkeyBaseModel):
         :param name: Plugin name in snake case
         :param plugin_type: Type of the plugin (exploiter, fingerprinter,
          credential collector, etc.)
-        :param supported_operating_systems: List of operating systems the plugin can run on
-        :param target_operating_systems: List of operating systems the plugin can target
+        :param supported_operating_systems: Operating systems that the plugin can run on
+        :param target_operating_systems: Operating systems that the plugin can target
         :param title: Human readable name for the plugin
         :param description: Description of the plugin
         :param link_to_documentation: Link to the documentation of the plugin
