680a30728b2ead24a3d4a2874d23a7fe079f1e59
Ilija Lazoroski <ilija.la@live.com>
2023-01-26 17:18:03 +0100

    Common: Add PluginVersion type

--- monkey/common/types/__init__.py
+++ monkey/common/types/__init__.py
@@ -3,3 +3,4 @@ from .serialization import JSONSerializable
 from .ids import AgentID, HardwareID, MachineID
 from .networking import NetworkService, NetworkPort, PortStatus, SocketAddress
 from .plugin_name import PluginName
+from .plugin_name import PluginVersion
--- monkey/common/types/plugin_name.py
+++ monkey/common/types/plugin_name.py
@@ -1,6 +1,7 @@
 import re
 
 from pydantic import ConstrainedStr
+from semver import Version
 
 
 class PluginName(ConstrainedStr):
@@ -12,3 +13,23 @@ class PluginName(ConstrainedStr):
 
     strip_whitespace = True
     regex = re.compile("^[a-zA-Z0-9_]+$")
+
+
+class PluginVersion(Version):
+    @classmethod
+    def __get_validators__(cls):
+        """Return a list of validator methods for pydantic models."""
+        yield cls.parse
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        """Inject/mutate the pydantic field schema in-place."""
+        field_schema.update(
+            examples=[
+                "1.0.2",
+                "3.6.2+dev",
+                "1.3.4+23daf123",
+                "2.15.3-alpha",
+                "21.3.15-beta+12345",
+            ]
+        )
