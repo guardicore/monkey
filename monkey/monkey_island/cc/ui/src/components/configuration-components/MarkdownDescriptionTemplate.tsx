c6b77b74a29da58aa24b90e7eef160a4146ff5a1
vakarisz <vakarisz@yahoo.com>
2023-02-01 10:09:36 +0200

    UI: Fix typescript error in MarkdownDescriptionTemplate.tsx

--- monkey/monkey_island/cc/ui/src/components/configuration-components/MarkdownDescriptionTemplate.tsx
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/MarkdownDescriptionTemplate.tsx
@@ -1,16 +1,15 @@
 import {DescriptionFieldProps} from '@rjsf/utils';
 import React from 'react';
-import remarkBreaks from 'remark-breaks';
 import ReactMarkdown from 'react-markdown';
 
 
 function MarkdownDescriptionTemplate(props: DescriptionFieldProps) {
   const {description, id} = props;
   return (
-    <ReactMarkdown plugins={[remarkBreaks]}
-                   linkTarget={'_blank'}
-                   className={'markdown'}
-                   children={description}/>
+    <ReactMarkdown linkTarget={'_blank'}
+                   className={'markdown'} >
+      {description.toString()}
+    </ReactMarkdown>
   );
 }
 
