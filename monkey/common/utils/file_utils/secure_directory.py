7a194ff167f86f467267f39fc76ca461897cbef1
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-01 15:14:56 +0530

    Common: Fix comments and reorder functions in utils/file_utils/secure_directory.py

--- monkey/common/utils/file_utils/secure_directory.py
+++ monkey/common/utils/file_utils/secure_directory.py
@@ -14,8 +14,6 @@ logger = logging.getLogger(__name__)
 
 
 def create_secure_directory(path: Path):
-    # TODO: Raise an exception if the directory exists and is not secure. Otherwise, the caller may
-    #       think a secure directory was created when it wasn't.
     if is_windows_os():
         _check_existing_directory_is_secure = _check_existing_directory_is_secure_windows
         _create_secure_directory = _create_secure_directory_windows
@@ -29,39 +27,6 @@ def create_secure_directory(path: Path):
         _check_existing_directory_is_secure(path)
     else:
         _create_secure_directory(path)
-
-
-def _create_secure_directory_linux(path: Path):
-    try:
-        # Don't split directory creation and permission setting
-        # because it will temporarily create an accessible directory which anyone can use.
-        path.mkdir(mode=stat.S_IRWXU)
-
-    except Exception as ex:
-        logger.error(f'Could not create a directory at "{path}": {str(ex)}')
-        raise ex
-
-
-def _check_existing_directory_is_secure_linux(path: Path):
-    path_mode = path.stat().st_mode
-
-    is_secure = (path_mode & (stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)) == stat.S_IRWXU
-
-    if not is_secure:
-        raise Exception(f'The directory "{path}" already exists and is insecure')
-
-
-def _create_secure_directory_windows(path: Path):
-    try:
-        security_attributes = win32security.SECURITY_ATTRIBUTES()
-        security_attributes.SECURITY_DESCRIPTOR = (
-            windows_permissions.get_security_descriptor_for_owner_only_perms()
-        )
-        win32file.CreateDirectory(str(path), security_attributes)
-
-    except Exception as ex:
-        logger.error(f'Could not create a directory at "{path}": {str(ex)}')
-        raise ex
 
 
 def _check_existing_directory_is_secure_windows(path: Path):
@@ -85,3 +50,38 @@ def _check_existing_directory_is_secure_windows(path: Path):
 
     if not is_secure_and_accessible:
         raise Exception(f'The directory "{path}" already exists and is insecure or unaccessible')
+
+
+def _create_secure_directory_windows(path: Path):
+    try:
+        # Don't split directory creation and permission setting
+        # because it will temporarily create an accessible directory which anyone can use.
+        security_attributes = win32security.SECURITY_ATTRIBUTES()
+        security_attributes.SECURITY_DESCRIPTOR = (
+            windows_permissions.get_security_descriptor_for_owner_only_perms()
+        )
+        win32file.CreateDirectory(str(path), security_attributes)
+
+    except Exception as ex:
+        logger.error(f'Could not create a directory at "{path}": {str(ex)}')
+        raise ex
+
+
+def _check_existing_directory_is_secure_linux(path: Path):
+    path_mode = path.stat().st_mode
+
+    is_secure = (path_mode & (stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)) == stat.S_IRWXU
+
+    if not is_secure:
+        raise Exception(f'The directory "{path}" already exists and is insecure')
+
+
+def _create_secure_directory_linux(path: Path):
+    try:
+        # Don't split directory creation and permission setting
+        # because it will temporarily create an accessible directory which anyone can use.
+        path.mkdir(mode=stat.S_IRWXU)
+
+    except Exception as ex:
+        logger.error(f'Could not create a directory at "{path}": {str(ex)}')
+        raise ex
