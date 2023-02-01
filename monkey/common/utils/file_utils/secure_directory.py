284a6664400a35615570ec657d75238e813de82f
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-01 19:21:09 +0530

    Common: Change directory permissions to secure on Windows if it exists already

--- monkey/common/utils/file_utils/secure_directory.py
+++ monkey/common/utils/file_utils/secure_directory.py
@@ -40,28 +40,16 @@ def _check_path_is_directory(path: Path):
 
 
 def _change_existing_directory_to_secure_windows(path: Path):
-    acl, user_sid = windows_permissions.get_acl_and_sid_from_path(path)
-
-    if acl.GetAceCount() != 1:
-        is_secure_and_accessible = False
-    else:
-        ace = acl.GetExplicitEntriesFromAcl()[0]
-        ace_sid = ace["Trustee"]["Identifier"]
-        ace_permissions = ace["AccessPermissions"]
-        ace_access_mode = ace["AccessMode"]
-        ace_inheritance = ace["Inheritance"]
-
-        is_secure_and_accessible = (
-            (ace_sid == user_sid)
-            & (ace_permissions == windows_permissions.ACCESS_PERMISSIONS_FULL_CONTROL)
-            & (ace_access_mode == windows_permissions.ACCESS_MODE_GRANT_ACCESS)
-            & (ace_inheritance == windows_permissions.INHERITANCE_OBJECT_AND_CONTAINER)
+    try:
+        security_descriptor = (
+            windows_permissions.get_security_descriptor_for_owner_only_permissions()
         )
-
-    if not is_secure_and_accessible:
-        raise FailedDirectoryCreationError(
-            f'The directory "{path}" already exists and is insecure or unaccessible'
+        win32security.SetFileSecurity(
+            str(path), win32security.DACL_SECURITY_INFORMATION, security_descriptor
         )
+    except Exception as err:
+        logger.exception(f'An error occured while changing permissions for directory: "{path}"')
+        raise err
 
 
 def _create_secure_directory_windows(path: Path):
@@ -74,8 +62,8 @@ def _create_secure_directory_windows(path: Path):
         )
         win32file.CreateDirectory(str(path), security_attributes)
 
-    except Exception as ex:
-        message = f'Could not create a directory at "{path}": {str(ex)}'
+    except Exception as err:
+        message = f'Could not create a directory at "{path}": {str(err)}'
         logger.error(message)
         raise FailedDirectoryCreationError(message)
 
@@ -94,7 +82,7 @@ def _create_secure_directory_linux(path: Path):
         # because it will temporarily create an accessible directory which anyone can use.
         path.mkdir(mode=stat.S_IRWXU)
 
-    except Exception as ex:
-        message = f'Could not create a directory at "{path}": {str(ex)}'
+    except Exception as err:
+        message = f'Could not create a directory at "{path}": {str(err)}'
         logger.error(message)
         raise FailedDirectoryCreationError(message)
