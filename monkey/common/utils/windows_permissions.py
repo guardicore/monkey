9056e77aaa99faa31309f15a6e4c504ec082c3ec
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-01 14:51:00 +0530

    Common: Refactor common/utils/windows_permissions.py

--- monkey/common/utils/windows_permissions.py
+++ monkey/common/utils/windows_permissions.py
@@ -1,37 +1,42 @@
 from pathlib import Path
+from typing import Any, Mapping
 
 import ntsecuritycon
 import win32api
 import win32con
 import win32security
 
-FULL_CONTROL = 2032127
-ACE_ACCESS_MODE_GRANT_ACCESS = win32security.GRANT_ACCESS
-ACE_INHERIT_OBJECT_AND_CONTAINER = 3
+ACCESS_MODE_GRANT_ACCESS = win32security.GRANT_ACCESS
+ACCESS_PERMISSIONS_FULL_CONTROL = ntsecuritycon.FILE_ALL_ACCESS
+INHERITANCE_OBJECT_AND_CONTAINER = (
+    win32security.CONTAINER_INHERIT_ACE | win32security.OBJECT_INHERIT_ACE
+)
 
 
 def get_security_descriptor_for_owner_only_perms():
     user_sid = get_user_pySID_object()
-    security_descriptor = win32security.SECURITY_DESCRIPTOR()
-    dacl = win32security.ACL()
+    entries = [get_ace_for_owner_only_permissions(user_sid)]
 
-    entries = [
-        {
-            "AccessMode": win32security.GRANT_ACCESS,
-            "AccessPermissions": ntsecuritycon.FILE_ALL_ACCESS,
-            "Inheritance": win32security.CONTAINER_INHERIT_ACE | win32security.OBJECT_INHERIT_ACE,
-            "Trustee": {
-                "TrusteeType": win32security.TRUSTEE_IS_USER,
-                "TrusteeForm": win32security.TRUSTEE_IS_SID,
-                "Identifier": user_sid,
-            },
-        }
-    ]
+    dacl = win32security.ACL()
     dacl.SetEntriesInAcl(entries)
 
+    security_descriptor = win32security.SECURITY_DESCRIPTOR()
     security_descriptor.SetSecurityDescriptorDacl(1, dacl, 0)
 
     return security_descriptor
+
+
+def get_ace_for_owner_only_permissions(user_sid) -> Mapping[str, Any]:
+    return {
+        "AccessMode": ACCESS_MODE_GRANT_ACCESS,
+        "AccessPermissions": ACCESS_PERMISSIONS_FULL_CONTROL,
+        "Inheritance": INHERITANCE_OBJECT_AND_CONTAINER,
+        "Trustee": {
+            "TrusteeType": win32security.TRUSTEE_IS_USER,
+            "TrusteeForm": win32security.TRUSTEE_IS_SID,
+            "Identifier": user_sid,
+        },
+    }
 
 
 def get_acl_and_sid_from_path(path: Path):
