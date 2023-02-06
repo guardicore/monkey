c525bb83519ddb3ff586be1ba2de1de3053d6740
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-06 13:54:18 +0530

    UI: Fix StolenCredentialsTable bug
    Issue: #2923
    PR: #2931

--- monkey/monkey_island/cc/ui/src/components/report-components/security/StolenCredentialsTable.tsx
+++ monkey/monkey_island/cc/ui/src/components/report-components/security/StolenCredentialsTable.tsx
@@ -3,6 +3,7 @@ import ReactTable from 'react-table'
 import LoadingIcon from '../../ui-components/LoadingIcon';
 import {reformatSecret} from '../credentialParsing';
 import _ from 'lodash';
+import IslandHttpClient, { APIEndpoint } from '../../IslandHttpClient';
 
 
 const columns = [
@@ -17,31 +18,32 @@ const columns = [
 
 const pageSize = 10;
 
-const StolenCredentialsTable = (props) => {
+const StolenCredentialsTable = () => {
 
   const [credentialsTableData, setCredentialsTableData] = useState(null);
 
   useEffect(() => {
-    setCredentialsTableData(getCredentialsTableData(props.stolenCredentials));
-  }, [props.stolenCredentials])
-
-  if(credentialsTableData === null){
-    return (<LoadingIcon />)
-  } else {
-
-    let defaultPageSize = credentialsTableData.length > pageSize ? pageSize : credentialsTableData.length;
-    let showPagination = credentialsTableData.length > pageSize;
-    return (
-      <div className="data-table-container">
-        <ReactTable
-          columns={columns}
-          data={credentialsTableData}
-          showPagination={showPagination}
-          defaultPageSize={defaultPageSize}
-        />
-      </div>
+    IslandHttpClient.get(APIEndpoint.stolenCredentials).then(
+      res => setCredentialsTableData(getCredentialsTableData(res.body))
     );
+  }, [])
+
+  if (credentialsTableData === null) {
+    return <LoadingIcon />
   }
+
+  let defaultPageSize = credentialsTableData.length > pageSize ? pageSize : credentialsTableData.length;
+  let showPagination = credentialsTableData.length > pageSize;
+  return (
+    <div className="data-table-container">
+      <ReactTable
+        columns={columns}
+        data={credentialsTableData}
+        showPagination={showPagination}
+        defaultPageSize={defaultPageSize}
+      />
+    </div>
+  );
 }
 
 export default StolenCredentialsTable;
@@ -57,12 +59,10 @@ function getCredentialsTableData(credentials){
       rowData['username'] = credential['identity']['username'];
     }
     rowData['title'] = reformatSecret(credential['secret'])['title'];
-    if (! _.find(tableData, rowData)){
+    if (! _.find(tableData, rowData)) {
       tableData.push(rowData);
     }
   }
 
-  // React table v6 is deprecated and seems to not compare arrays properly, thus it
-  // does not update once array is changed, so don't use []
-  return _.isEmpty(tableData) ? null : tableData;
+  return tableData;
 }
