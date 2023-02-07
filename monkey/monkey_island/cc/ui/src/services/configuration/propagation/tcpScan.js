96d8ab11411ab87dabe8c2190979c72a07bbd1d4
Ilija Lazoroski <ilija.la@live.com>
2023-02-07 16:22:35 +0100

    UI: Fix ranges in configuration schema
    PR: #2945

--- monkey/monkey_island/cc/ui/src/services/configuration/propagation/tcpScan.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/propagation/tcpScan.js
@@ -6,7 +6,9 @@ const TCP_SCAN_CONFIGURATION_SCHEMA = {
       'title': 'TCP target ports',
       'type': 'array',
       'items': {
-        'type': 'integer'
+        'type': 'integer',
+        'minimum': 0,
+        'maximum': 65535
       },
       'default': [22,2222,445,135,389,80,8080,443,8008,3306,7001,8088,5885,5986],
       'description': 'List of TCP ports the monkey will check whether they\'re open'
@@ -14,6 +16,8 @@ const TCP_SCAN_CONFIGURATION_SCHEMA = {
     'timeout': {
       'title': 'TCP scan timeout',
       'type': 'number',
+      'minimum': 0,
+      'default': 1,
       'description': 'Maximum time to wait for TCP response in seconds'
     }
   }
