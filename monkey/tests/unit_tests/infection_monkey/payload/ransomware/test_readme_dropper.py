fe10c1db0b4a6012e05d8f2b65a1c630dbe9ea85
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-02-09 12:49:38 -0500

    Revert "Merge branch '2951-fix-readme-line-ending' into release/2.0.0"
    This reverts commit 6015a642c55374936912dde42ef941a9cc0d0c0e, reversing
    changes made to de00a937c35858e8cc6427a6b4ceda1c8c4621dd.

--- monkey/tests/unit_tests/infection_monkey/payload/ransomware/test_readme_dropper.py
+++ monkey/tests/unit_tests/infection_monkey/payload/ransomware/test_readme_dropper.py
@@ -3,7 +3,6 @@ import filecmp
 import pytest
 from tests.utils import get_file_sha256_hash
 
-from common.utils.environment import is_windows_os
 from infection_monkey.payload.ransomware.readme_dropper import leave_readme
 
 DEST_FILE = "README.TXT"
@@ -20,11 +19,6 @@ def dest_readme(tmp_path):
     return tmp_path / DEST_FILE
 
 
-@pytest.fixture
-def expected_readme_windows(data_for_tests_dir):
-    return data_for_tests_dir / "test_readme_windows.txt"
-
-
 def test_readme_already_exists(src_readme, dest_readme):
     dest_readme.touch()
 
@@ -32,15 +26,6 @@ def test_readme_already_exists(src_readme, dest_readme):
     assert get_file_sha256_hash(dest_readme) == EMPTY_FILE_HASH
 
 
-@pytest.mark.skipif(
-    is_windows_os(), reason="Skip on Windows because the output will have different line endings"
-)
 def test_leave_readme_linux(src_readme, dest_readme):
     leave_readme(src_readme, dest_readme)
     assert filecmp.cmp(src_readme, dest_readme)
-
-
-@pytest.mark.skipif(not is_windows_os(), reason="Readme is already in unix line endings on Linux")
-def test_leave_readme_windows(monkeypatch, src_readme, dest_readme, expected_readme_windows):
-    leave_readme(src_readme, dest_readme)
-    assert filecmp.cmp(dest_readme, expected_readme_windows)
