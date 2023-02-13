855cb3049327018d38f482d1f6c5bd9b0ebdb842
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-02-13 19:28:21 +0000

    Agent: Change common ports to List(NetworkPort)

--- monkey/infection_monkey/network/ports.py
+++ monkey/infection_monkey/network/ports.py
@@ -1,21 +1,23 @@
 from typing import List
 
-COMMON_PORTS: List[int] = [
-    8080,  # http-proxy
-    8008,  # Alternative port for HTTP
-    3389,  # Windows Terminal Server (RDP)
-    1025,  # NFS, IIS
-    3306,  # mysql
-    5985,  # Windows PowerShell Default psSession port
-    5986,  # Windows PowerShell Default psSession port
-    5000,  # A bunch of different stuff (unofficial)
-    5432,  # PostgreSQL
-    1723,  # Microsoft PPTP VPN
-    6600,  # Microsoft Hyper-V Live
-    8888,  # sun-answerbook
-    1433,  # Microsoft SQL Server
-    1434,  # Microsoft SQL Monitor
-    1720,  # h323q931
-    5900,  # vnc
-    6001,  # X11:1
+from common.types import NetworkPort
+
+COMMON_PORTS: List[NetworkPort] = [
+    NetworkPort(8080),  # http-proxy
+    NetworkPort(8008),  # Alternative port for HTTP
+    NetworkPort(3389),  # Windows Terminal Server (RDP)
+    NetworkPort(1025),  # NFS, IIS
+    NetworkPort(3306),  # mysql
+    NetworkPort(5985),  # Windows PowerShell Default psSession port
+    NetworkPort(5986),  # Windows PowerShell Default psSession port
+    NetworkPort(5000),  # A bunch of different stuff (unofficial)
+    NetworkPort(5432),  # PostgreSQL
+    NetworkPort(1723),  # Microsoft PPTP VPN
+    NetworkPort(6600),  # Microsoft Hyper-V Live
+    NetworkPort(8888),  # sun-answerbook
+    NetworkPort(1433),  # Microsoft SQL Server
+    NetworkPort(1434),  # Microsoft SQL Monitor
+    NetworkPort(1720),  # h323q931
+    NetworkPort(5900),  # vnc
+    NetworkPort(6001),  # X11:1
 ]
