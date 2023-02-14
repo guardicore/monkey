d5c32b2ff8ebb6a30a68269da2cd0d3b80ed25b1
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-02-14 12:10:06 -0500

    Island: Move mongo setup functions to mongo_setup.py

--- monkey/monkey_island/cc/database.py
+++ monkey/monkey_island/cc/database.py
@@ -1,6 +1,5 @@
 import gridfs
-from flask_pymongo import MongoClient, PyMongo
-from pymongo.errors import ServerSelectionTimeoutError
+from flask_pymongo import PyMongo
 
 mongo = PyMongo()
 
@@ -14,23 +13,3 @@ class Database:
 
 
 database = Database()
-
-
-def is_db_server_up(mongo_url):
-    client = MongoClient(mongo_url, serverSelectionTimeoutMS=100)
-    try:
-        client.server_info()
-        return True
-    except ServerSelectionTimeoutError:
-        return False
-
-
-def get_db_version(mongo_url):
-    """
-    Return the mongo db version
-    :param mongo_url: Which mongo to check.
-    :return: version as a tuple (e.g. `(u'4', u'0', u'8')`)
-    """
-    client = MongoClient(mongo_url, serverSelectionTimeoutMS=100)
-    server_version = tuple(client.server_info()["version"].split("."))
-    return server_version
