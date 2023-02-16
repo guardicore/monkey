059a878722437a9ce05b54f4097507a4de2d5798
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-16 19:57:52 +0530

    Agent: Add PortScanData.service as a NetworkService

--- monkey/infection_monkey/i_puppet/port_scan_data.py
+++ monkey/infection_monkey/i_puppet/port_scan_data.py
@@ -3,11 +3,12 @@ from typing import Optional
 from pydantic import Field
 
 from common.base_models import InfectionMonkeyBaseModel
-from common.types import NetworkPort, PortStatus
+from common.types import NetworkPort, NetworkService, PortStatus
 
 
 class PortScanData(InfectionMonkeyBaseModel):
     port: NetworkPort
     status: PortStatus
     banner: Optional[str] = Field(default=None)
+    service: Optional[NetworkService] = Field(default=None)
     service_deprecated: Optional[str] = Field(default=None)
