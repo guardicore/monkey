5630459966efaaab5ccb127bba042cf4f1059d69
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-17 11:21:19 +0530

    Common: Add NetworkProtocol to init file

--- monkey/common/types/__init__.py
+++ monkey/common/types/__init__.py
@@ -2,6 +2,6 @@ from .concurrency import Lock, Event
 from .serialization import JSONSerializable
 from .ids import AgentID, HardwareID, MachineID
 from .int_range import IntRange
-from .networking import NetworkService, NetworkPort, PortStatus, SocketAddress
+from .networking import NetworkService, NetworkPort, PortStatus, SocketAddress, NetworkProtocol
 from .plugin_types import PluginName
 from .plugin_types import PluginVersion
