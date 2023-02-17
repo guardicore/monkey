75609f8d976ec8230560cd09296a4cf04b17d731
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-17 11:28:36 +0530

    Agent: Add TargetHost.port_status

--- monkey/infection_monkey/model/host.py
+++ monkey/infection_monkey/model/host.py
@@ -1,10 +1,13 @@
 from ipaddress import IPv4Address
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from pydantic import Field
+from typing_extensions import Literal  # import from `typing` once we switch to Python 3.8
 
 from common import OperatingSystem
 from common.base_models import MutableInfectionMonkeyBaseModel
+from common.types import NetworkPort, NetworkProtocol
+from infection_monkey.i_puppet import PortScanData
 
 
 class TargetHost(MutableInfectionMonkeyBaseModel):
@@ -12,6 +15,10 @@ class TargetHost(MutableInfectionMonkeyBaseModel):
     operating_system: Optional[OperatingSystem] = Field(default=None)
     services: Dict[str, Any] = Field(default={})  # deprecated
     icmp: bool = Field(default=False)
+    port_status: Dict[
+        Union[Literal[NetworkProtocol.TCP], Literal[NetworkProtocol.UDP]],
+        Dict[NetworkPort, PortScanData],
+    ] = Field(default={})
 
     def __hash__(self):
         return hash(self.ip)
