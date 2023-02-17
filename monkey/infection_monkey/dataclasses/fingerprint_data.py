da25169f879e74dc92a24129a1faa952fd3ccdeb
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-17 16:46:47 +0530

    Agent: Make FingerprintData a pydantic model

--- monkey/infection_monkey/dataclasses/fingerprint_data.py
+++ monkey/infection_monkey/dataclasses/fingerprint_data.py
@@ -1,3 +1,12 @@
-from collections import namedtuple
+from typing import Dict, Optional
 
-FingerprintData = namedtuple("FingerprintData", ["os_type", "os_version", "services"])
+from pydantic import Field
+
+from common import OperatingSystem
+from common.base_models import InfectionMonkeyBaseModel
+
+
+class FingerprintData(InfectionMonkeyBaseModel):
+    os_type: Optional[OperatingSystem]
+    os_version: Optional[str]
+    services: Dict[str, Dict[str, str]] = Field(default={})
