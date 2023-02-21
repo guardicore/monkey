ff7af4e13e9561f46e6b90e713fd530a7169692e
vakarisz <vakarisz@yahoo.com>
2023-02-21 16:12:36 +0200

    Common: Add hard coded fingerprinter schemas

---
+++ monkey/monkey_island/cc/repositories/utils/hard_coded_fingerprinter_schemas.py
@@ -0,0 +1,6 @@
+HARD_CODED_EXPLOITER_SCHEMAS = {
+    "smb": {"type": "object", "properties": {}},
+    "ssh": {"type": "object", "properties": {}},
+    "http": {"type": "object", "properties": {}},
+    "mssql": {"type": "object", "properties": {}},
+}
