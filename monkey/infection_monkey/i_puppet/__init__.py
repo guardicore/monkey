d3cd6318769423138ff6dd029935a7fceb63f8b9
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-02-21 15:02:08 +0000

    Hadoop: Implement try_all_discovered_http_ports option

--- monkey/infection_monkey/i_puppet/__init__.py
+++ monkey/infection_monkey/i_puppet/__init__.py
@@ -10,4 +10,4 @@ from .i_puppet import (
 )
 from .i_fingerprinter import IFingerprinter
 from .i_credential_collector import ICredentialCollector
-from .target_host import TargetHost
+from .target_host import TargetHost, TargetHostPorts
