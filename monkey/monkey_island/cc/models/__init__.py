56f35321b2dc38bf055b85c06842a934e4e9c075
Ilija Lazoroski <ilija.la@live.com>
2023-02-23 17:47:21 +0100

    Island: Remove unused UserCredentials model

--- monkey/monkey_island/cc/models/__init__.py
+++ monkey/monkey_island/cc/models/__init__.py
@@ -1,7 +1,6 @@
 # Order of importing matters here, for registering the embedded and referenced documents before
 # using them.
 from .simulation import Simulation, IslandMode
-from .user_credentials import UserCredentials
 from common.types import MachineID
 from .machine import Machine, NetworkServices
 from .communication_type import CommunicationType
