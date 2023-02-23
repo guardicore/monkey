0a53af371969890aa77a1986cfd505a7c391e31d
Ilija Lazoroski <ilija.la@live.com>
2023-02-23 17:09:39 +0100

    Project: Add User and Role params in vulture allowlist

--- vulture_allowlist.py
+++ vulture_allowlist.py
@@ -6,13 +6,6 @@ from common.agent_events import AbstractAgentEvent, FileEncryptionEvent
 from common.agent_plugins import AgentPlugin, AgentPluginManifest
 from common.base_models import InfectionMonkeyModelConfig, MutableInfectionMonkeyModelConfig
 from common.credentials import LMHash, NTHash, SecretEncodingConfig
-from common.hard_coded_manifests import HARD_CODED_PAYLOADS_MANIFESTS
-from common.hard_coded_manifests.hard_coded_credential_collector_manifests import (
-    HARD_CODED_CREDENTIAL_COLLECTOR_MANIFESTS,
-)
-from common.hard_coded_manifests.hard_coded_fingerprinter_manifests import (
-    HARD_CODED_FINGERPRINTER_MANIFESTS,
-)
 from common.types import Lock, NetworkPort, PluginName
 from infection_monkey.exploit.log4shell_utils.ldap_server import LDAPServerFactory
 from infection_monkey.exploit.zerologon import NetrServerPasswordSet, NetrServerPasswordSetResponse
@@ -21,15 +14,6 @@ from infection_monkey.transport.http import FileServHTTPRequestHandler
 from monkey_island.cc.deployment import Deployment
 from monkey_island.cc.models import IslandMode, Machine
 from monkey_island.cc.repositories import IAgentEventRepository, MongoAgentEventRepository
-from monkey_island.cc.repositories.utils.hard_coded_credential_collector_schemas import (
-    HARD_CODED_CREDENTIAL_COLLECTOR_SCHEMAS,
-)
-from monkey_island.cc.repositories.utils.hard_coded_fingerprinter_schemas import (
-    HARD_CODED_FINGERPRINTER_SCHEMAS,
-)
-from monkey_island.cc.repositories.utils.hard_coded_payloads_schemas import (
-    HARD_CODED_PAYLOADS_SCHEMAS,
-)
 from monkey_island.cc.services.reporting.exploitations.monkey_exploitation import MonkeyExploitation
 
 # Pydantic configurations are not picked up
