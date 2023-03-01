263aed91557ee80c2b797da114ee3f8fcd08f745
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-01 21:20:45 +0000

    Island: Implement login using flask-security

--- monkey/tests/unit_tests/monkey_island/cc/resources/auth/test_authenticate.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/auth/test_authenticate.py
@@ -1,10 +1,8 @@
-import re
+from flask import Response
 from unittest.mock import MagicMock
 
 import pytest
 
-from common.utils.exceptions import IncorrectCredentialsError
-from monkey_island.cc.models import User
 from monkey_island.cc.resources.auth import Authenticate
 
 USERNAME = "test_user"
@@ -22,41 +20,52 @@ def make_auth_request(flask_client):
     return inner
 
 
-def test_credential_parsing(make_auth_request, mock_authentication_service):
+def test_credential_parsing(make_auth_request, mock_authentication_service, monkeypatch):
+    monkeypatch.setattr(
+        "monkey_island.cc.resources.auth.authenticate.login",
+        lambda: Response(
+            status=200,
+        ),
+    )
+
     make_auth_request(TEST_REQUEST)
-    mock_authentication_service.authenticate.assert_called_with(USERNAME, PASSWORD)
+    mock_authentication_service.unlock_repository_encryptor.assert_called_with(USERNAME, PASSWORD)
 
 
 def test_empty_credentials(make_auth_request, mock_authentication_service):
     make_auth_request("{}")
-    mock_authentication_service.authenticate.assert_called_with("", "")
+    mock_authentication_service.unlock_repository_encryptor.assert_not_called()
 
 
-def test_authentication_successful(make_auth_request, mock_authentication_service):
-    mock_authentication_service.authenticate = MagicMock(
-        return_value=User(username="test", password="test")
+def test_authentication_successful(make_auth_request, monkeypatch):
+    monkeypatch.setattr(
+        "monkey_island.cc.resources.auth.authenticate.login",
+        lambda: Response(
+            status=200,
+        ),
     )
 
     response = make_auth_request(TEST_REQUEST)
 
     assert response.status_code == 200
-    assert re.match(
-        r"^[a-zA-Z0-9+/=]+\.[a-zA-Z0-9+/=]+\.[a-zA-Z0-9+/=\-_]+$", response.json["csrf_token"]
+
+
+def test_authentication_failure(make_auth_request, mock_authentication_service, monkeypatch):
+    monkeypatch.setattr(
+        "monkey_island.cc.resources.auth.authenticate.login",
+        lambda: Response(
+            status=400,
+        ),
     )
-
-
-def test_authentication_failure(make_auth_request, mock_authentication_service):
-    mock_authentication_service.authenticate = MagicMock(side_effect=IncorrectCredentialsError())
 
     response = make_auth_request(TEST_REQUEST)
 
-    assert "access_token" not in response.json
-    assert response.status_code == 401
-    assert response.json["error"] == "Invalid credentials"
+    assert response.status_code == 400
+    mock_authentication_service.unlock_repository_encryptor.assert_not_called()
 
 
 def test_authentication_error(make_auth_request, mock_authentication_service):
-    mock_authentication_service.authenticate = MagicMock(side_effect=Exception())
+    mock_authentication_service.unlock_repository_encryptor = MagicMock(side_effect=Exception())
 
     response = make_auth_request(TEST_REQUEST)
 
