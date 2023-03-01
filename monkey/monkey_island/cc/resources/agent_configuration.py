24da80dd807165c44d96f642977a61d2fa2e555e
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-01 11:53:35 -0500

    Island: Migrate AgentConfiguration resource to configuration service
    Migrate the AgentConfiguration resource from the
    IAgentConfigurationRepository to the IAgentConfigurationService.

--- monkey/monkey_island/cc/resources/agent_configuration.py
+++ monkey/monkey_island/cc/resources/agent_configuration.py
@@ -5,23 +5,21 @@ from flask import make_response, request
 from common.agent_configuration.agent_configuration import (
     AgentConfiguration as AgentConfigurationObject,
 )
-from monkey_island.cc.repositories import (
-    IAgentConfigurationRepository,
-    PluginConfigurationValidationError,
-)
+from monkey_island.cc.repositories import PluginConfigurationValidationError
 from monkey_island.cc.resources.AbstractResource import AbstractResource
 from monkey_island.cc.resources.request_authentication import jwt_required
+from monkey_island.cc.services import IAgentConfigurationService
 
 
 class AgentConfiguration(AbstractResource):
     urls = ["/api/agent-configuration"]
 
-    def __init__(self, agent_configuration_repository: IAgentConfigurationRepository):
-        self._agent_configuration_repository = agent_configuration_repository
+    def __init__(self, agent_configuration_service: IAgentConfigurationService):
+        self._agent_configuration_service = agent_configuration_service
 
     # Used by the agent. Can't secure
     def get(self):
-        configuration = self._agent_configuration_repository.get_configuration()
+        configuration = self._agent_configuration_service.get_configuration()
         configuration_dict = configuration.dict(simplify=True)
         return make_response(configuration_dict, 200)
 
@@ -29,7 +27,7 @@ class AgentConfiguration(AbstractResource):
     def put(self):
         try:
             configuration_object = AgentConfigurationObject(**request.json)
-            self._agent_configuration_repository.update_configuration(configuration_object)
+            self._agent_configuration_service.update_configuration(configuration_object)
             # API Spec: Should return 204 (NO CONTENT)
             return make_response({}, 200)
         except (
--- monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration.py
@@ -1,20 +1,18 @@
 import json
-from copy import deepcopy
+from http import HTTPStatus
+from typing import Type
+from unittest.mock import MagicMock
 
 import pytest
 from tests.common import StubDIContainer
 from tests.common.example_agent_configuration import AGENT_CONFIGURATION
-from tests.monkey_island import InMemoryAgentConfigurationRepository, InMemoryAgentPluginRepository
-from tests.unit_tests.monkey_island.cc.fake_agent_plugin_data import FAKE_AGENT_PLUGIN_1
+from tests.monkey_island import InMemoryAgentConfigurationService, InMemoryAgentPluginRepository
 from tests.unit_tests.monkey_island.conftest import get_url_for_resource
 
 from common.agent_configuration import AgentConfiguration
-from monkey_island.cc.repositories import (
-    AgentConfigurationValidationDecorator,
-    IAgentConfigurationRepository,
-)
-from monkey_island.cc.repositories.utils import AgentConfigurationSchemaCompiler
+from monkey_island.cc.repositories import PluginConfigurationValidationError
 from monkey_island.cc.resources import AgentConfiguration as AgentConfigurationResource
+from monkey_island.cc.services import IAgentConfigurationService
 
 AGENT_CONFIGURATION_URL = get_url_for_resource(AgentConfigurationResource)
 
@@ -28,14 +26,9 @@ def in_memory_agent_plugin_repository():
 def flask_client(build_flask_client, in_memory_agent_plugin_repository):
     container = StubDIContainer()
 
-    agent_configuration_schema_compiler = AgentConfigurationSchemaCompiler(
-        in_memory_agent_plugin_repository
-    )
-    agent_configuration_repository = AgentConfigurationValidationDecorator(
-        InMemoryAgentConfigurationRepository(), agent_configuration_schema_compiler
-    )
+    agent_configuration_service = InMemoryAgentConfigurationService()
 
-    container.register_instance(IAgentConfigurationRepository, agent_configuration_repository)
+    container.register_instance(IAgentConfigurationService, agent_configuration_service)
 
     with build_flask_client(container) as flask_client:
         yield flask_client
@@ -56,50 +49,29 @@ def test_agent_configuration_endpoint(flask_client):
     assert AgentConfiguration(**json.loads(resp.data)) == AgentConfiguration(**AGENT_CONFIGURATION)
 
 
-def test_agent_configuration_endpoint__bogus_data(flask_client, in_memory_agent_plugin_repository):
-    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_1)
-    agent_configuration = deepcopy(AGENT_CONFIGURATION)
-
-    agent_configuration["propagation"]["exploitation"]["exploiters"] = {
-        "bogus_exploiter": {"bogus_field": "bogus_value"}
-    }
-
-    resp = flask_client.put(
-        AGENT_CONFIGURATION_URL,
-        json=AgentConfiguration(**agent_configuration).dict(simplify=True),
-        follow_redirects=True,
-    )
-    assert resp.status_code == 400
-
-
-def test_agent_configuration_endpoint__invalid_plugin_data(
-    flask_client, in_memory_agent_plugin_repository
+@pytest.mark.parametrize(
+    "error, expected_status",
+    [
+        (Exception, HTTPStatus.INTERNAL_SERVER_ERROR),
+        (json.JSONDecodeError, HTTPStatus.BAD_REQUEST),
+        (PluginConfigurationValidationError, HTTPStatus.BAD_REQUEST),
+        (TypeError, HTTPStatus.BAD_REQUEST),
+        (ValueError, HTTPStatus.BAD_REQUEST),
+    ],
+)
+def test_agent_configuration_service_raises_exception(
+    build_flask_client, error: Type[Exception], expected_status: HTTPStatus
 ):
-    in_memory_agent_plugin_repository.save_plugin(FAKE_AGENT_PLUGIN_1)
-    agent_configuration = deepcopy(AGENT_CONFIGURATION)
+    container = StubDIContainer()
+    mock_agent_configuration_service = MagicMock(spec=IAgentConfigurationService)
+    mock_agent_configuration_service.update_configuration = MagicMock(side_effect=error)
 
-    agent_configuration["propagation"]["exploitation"]["exploiters"] = {
-        FAKE_AGENT_PLUGIN_1.plugin_manifest.name: {
-            "exploitation_success_rate": -123,
-            "propagation_success_rate": 321,
-        }
-    }
+    container.register_instance(IAgentConfigurationService, mock_agent_configuration_service)
 
-    resp = flask_client.put(
-        AGENT_CONFIGURATION_URL,
-        json=AgentConfiguration(**agent_configuration).dict(simplify=True),
-        follow_redirects=True,
-    )
-    assert resp.status_code == 400
-
-
-def test_agent_configuration_invalid_config(flask_client):
-    resp = flask_client.put(AGENT_CONFIGURATION_URL, json={"invalid_config": "invalid_stuff"})
-
-    assert resp.status_code == 400
-
-
-def test_agent_configuration_invalid_json(flask_client):
-    resp = flask_client.put(AGENT_CONFIGURATION_URL, data="InvalidJson!")
-
-    assert resp.status_code == 400
+    with build_flask_client(container) as flask_client:
+        resp = flask_client.put(
+            AGENT_CONFIGURATION_URL,
+            json=AgentConfiguration(**AGENT_CONFIGURATION).dict(simplify=True),
+            follow_redirects=True,
+        )
+    assert resp.status_code == expected_status
