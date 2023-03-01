c2cf8de313b6ec6b02d61356584390510270d15e
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-01 12:46:50 -0500

    Island: Migrate agent agent configuration endpoint to new service

--- monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration_schema.py
+++ monkey/tests/unit_tests/monkey_island/cc/resources/test_agent_configuration_schema.py
@@ -7,28 +7,28 @@ from tests.unit_tests.monkey_island.conftest import get_url_for_resource
 
 from monkey_island.cc.repositories import RetrievalError
 from monkey_island.cc.resources import AgentConfigurationSchema
-from monkey_island.cc.services import AgentConfigurationSchemaService
+from monkey_island.cc.services import IAgentConfigurationService
 
 AGENT_CONFIGURATION_SCHEMA_URL = get_url_for_resource(AgentConfigurationSchema)
 
 
 @pytest.fixture
-def mock_config_schema_service():
-    return MagicMock(spec=AgentConfigurationSchemaService)
+def mock_agent_configuration_service():
+    return MagicMock(spec=IAgentConfigurationService)
 
 
 @pytest.fixture
-def flask_client(build_flask_client, mock_config_schema_service):
+def flask_client(build_flask_client, mock_agent_configuration_service):
     container = StubDIContainer()
-    container.register_instance(AgentConfigurationSchemaService, mock_config_schema_service)
+    container.register_instance(IAgentConfigurationService, mock_agent_configuration_service)
 
     with build_flask_client(container) as flask_client:
         yield flask_client
 
 
-def test_agent_configuration_schema_endpoint(flask_client, mock_config_schema_service):
+def test_agent_configuration_schema_endpoint(flask_client, mock_agent_configuration_service):
     fake_schema = {"schema": True}
-    mock_config_schema_service.get_schema = lambda: fake_schema
+    mock_agent_configuration_service.get_schema = lambda: fake_schema
 
     resp = flask_client.get(AGENT_CONFIGURATION_SCHEMA_URL)
 
@@ -36,8 +36,8 @@ def test_agent_configuration_schema_endpoint(flask_client, mock_config_schema_se
     assert resp.json == fake_schema
 
 
-def test_agent_configuration_schema_endpoint_error(flask_client, mock_config_schema_service):
-    mock_config_schema_service.get_schema = MagicMock(side_effect=RuntimeError)
+def test_agent_configuration_schema_endpoint_error(flask_client, mock_agent_configuration_service):
+    mock_agent_configuration_service.get_schema = MagicMock(side_effect=RuntimeError)
 
     resp = flask_client.get(AGENT_CONFIGURATION_SCHEMA_URL)
 
@@ -45,9 +45,9 @@ def test_agent_configuration_schema_endpoint_error(flask_client, mock_config_sch
 
 
 def test_agent_configuration_schema_endpoint_retrieval_error(
-    flask_client, mock_config_schema_service
+    flask_client, mock_agent_configuration_service
 ):
-    mock_config_schema_service.get_schema = MagicMock(side_effect=RetrievalError)
+    mock_agent_configuration_service.get_schema = MagicMock(side_effect=RetrievalError)
 
     resp = flask_client.get(AGENT_CONFIGURATION_SCHEMA_URL)
 
