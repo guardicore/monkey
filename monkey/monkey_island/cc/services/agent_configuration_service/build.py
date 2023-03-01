438e7033ad5a5ceb5c8f991b4dd27e9d40211cc2
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-01 12:03:30 -0500

    Island: Decorate IAgentConfigurationRepository with schema validator

--- monkey/monkey_island/cc/services/agent_configuration_service/build.py
+++ monkey/monkey_island/cc/services/agent_configuration_service/build.py
@@ -1,5 +1,9 @@
 from common import DIContainer
-from monkey_island.cc.repositories import FileAgentConfigurationRepository
+from monkey_island.cc.repositories import (
+    AgentConfigurationValidationDecorator,
+    FileAgentConfigurationRepository,
+    IAgentConfigurationRepository,
+)
 from monkey_island.cc.repositories.utils import AgentConfigurationSchemaCompiler
 
 from . import IAgentConfigurationService
@@ -7,7 +11,26 @@ from .agent_configuration_service import AgentConfigurationService
 
 
 def build(container: DIContainer) -> IAgentConfigurationService:
-    agent_configuration_repository = container.resolve(FileAgentConfigurationRepository)
     schema_compiler = container.resolve(AgentConfigurationSchemaCompiler)
+    agent_configuration_repository = _build_file_agent_configuration_repository(
+        container, schema_compiler
+    )
 
     return AgentConfigurationService(agent_configuration_repository, schema_compiler)
+
+
+def _build_file_agent_configuration_repository(
+    container: DIContainer, schema_compiler: AgentConfigurationSchemaCompiler
+) -> IAgentConfigurationRepository:
+    file_agent_configuration_repository = container.resolve(FileAgentConfigurationRepository)
+    return _decorate_agent_configuration_repository(
+        file_agent_configuration_repository, schema_compiler
+    )
+
+
+def _decorate_agent_configuration_repository(
+    agent_configuration_repository: IAgentConfigurationRepository,
+    schema_compiler: AgentConfigurationSchemaCompiler,
+) -> IAgentConfigurationRepository:
+
+    return AgentConfigurationValidationDecorator(agent_configuration_repository, schema_compiler)
