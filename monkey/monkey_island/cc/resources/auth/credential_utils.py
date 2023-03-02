3d55b97c8c1c95ed18147e15630bc005d44c662b
Ilija Lazoroski <ilija.la@live.com>
2023-03-02 18:22:39 +0100

    Island: Add docstring for get_username_password_from_request

--- monkey/monkey_island/cc/resources/auth/credential_utils.py
+++ monkey/monkey_island/cc/resources/auth/credential_utils.py
@@ -5,6 +5,13 @@ from flask import Request, request
 
 
 def get_username_password_from_request(_request: Request) -> Tuple[str, str]:
+    """
+    Deserialize the JSON binary data from the request and get the plaintext
+    username and password.
+
+    :param _request: A Flask Request object
+    :raises JSONDecodeError: If invalid JSON data is provided
+    """
     cred_dict = json.loads(request.data)
     username = cred_dict.get("username", "")
     password = cred_dict.get("password", "")
