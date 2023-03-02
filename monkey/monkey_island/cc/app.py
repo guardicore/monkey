bd93bfd0f96eb51f55db8466ca2dc861f2a183af
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-02 22:31:32 +0000

    Island: Register logout endpoint

--- monkey/monkey_island/cc/app.py
+++ monkey/monkey_island/cc/app.py
@@ -30,7 +30,7 @@ from monkey_island.cc.resources import (
     ResetAgentConfiguration,
     TerminateAllAgents,
 )
-from monkey_island.cc.resources.auth import Login, Register, RegistrationStatus
+from monkey_island.cc.resources.auth import Login, Logout, Register, RegistrationStatus
 from monkey_island.cc.resources.exploitations.monkey_exploitation import MonkeyExploitation
 from monkey_island.cc.resources.island_mode import IslandMode
 from monkey_island.cc.resources.local_run import LocalRun
