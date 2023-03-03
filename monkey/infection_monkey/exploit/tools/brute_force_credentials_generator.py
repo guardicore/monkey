3845938fb04199b296cb08adafdcbfe59daebdba
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-03 13:00:51 -0500

    Agent: Add secret_filters param to generate_brute_force_credentials()

--- monkey/infection_monkey/exploit/tools/brute_force_credentials_generator.py
+++ monkey/infection_monkey/exploit/tools/brute_force_credentials_generator.py
@@ -1,11 +1,13 @@
 from itertools import chain
-from typing import Dict, Iterable, List, Sequence, Set, Type
+from typing import Callable, Dict, Iterable, List, Sequence, Set, Type
 
 from common.credentials import Credentials, Identity, Secret
+from common.utils.code_utils import apply_filters
 
 
 def generate_brute_force_credentials(
     input_credentials: Iterable[Credentials],
+    secret_filters: Iterable[Callable[[Secret], bool]] = (),
 ) -> Sequence[Credentials]:
     """
     Generates all possible combinations of identities and secrets from the inputs
@@ -16,16 +18,20 @@ def generate_brute_force_credentials(
     prioritize using known identity/secret pairs before attempting to use random combinations.
 
     :param input_credentials: The credentials used to generate the brute force credentials
+    :param secret_filters: An iterable of filters to apply to the secrets
     :return: A Sequence of credentials
     """
     _input_credentials = set(input_credentials)
-    brute_force_credentials = _generate_all_possible_combinations(_input_credentials)
+    brute_force_credentials = _generate_all_possible_combinations(
+        _input_credentials, secret_filters
+    )
 
     return _sort_known_identity_secret_pairs_first(brute_force_credentials, _input_credentials)
 
 
 def _generate_all_possible_combinations(
     input_credentials: Set[Credentials],
+    secret_filters: Iterable[Callable[[Secret], bool]] = (),
 ) -> Iterable[Credentials]:
     brute_force_credentials: List[Credentials] = []
     identities: Dict[Type[Identity], Set[Identity]] = {}
@@ -41,7 +47,7 @@ def _generate_all_possible_combinations(
 
     # Output will be grouped by secret type. This is not guaranteed by the interface, but if we can
     # arrange these in an orderly sequence, we might as well.
-    for secret in chain.from_iterable(secrets.values()):
+    for secret in apply_filters(chain.from_iterable(secrets.values()), secret_filters):
         for identity in chain.from_iterable(identities.values()):
             brute_force_credentials.append(Credentials(identity=identity, secret=secret))
 
