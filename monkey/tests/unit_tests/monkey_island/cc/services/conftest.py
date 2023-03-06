da1988a44cf5b833f8208f5adfe9889b4a2a4119
Ilija Lazoroski <ilija.la@live.com>
2023-03-06 10:33:01 +0100

    UT: Remove inital user for AuthenticationService tests

--- monkey/tests/unit_tests/monkey_island/cc/services/conftest.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/conftest.py
@@ -6,5 +6,13 @@ from tests.unit_tests.monkey_island.conftest import init_mock_security_app
 def mock_flask_app():
     app, _ = init_mock_security_app()
 
+    ds = app.security.datastore
+
     with app.app_context():
+
+        inital_user = ds.find_user(email="unittest@me.com")
+        if inital_user:
+            ds.delete_user(inital_user)
+            ds.commit()
+
         yield app
