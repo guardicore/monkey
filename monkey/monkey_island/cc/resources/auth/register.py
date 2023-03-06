7401c2d082dd8278baed936b31a9ae7c1281adfc
Shreya Malviya <shreya.malviya@gmail.com>
2023-03-06 20:05:07 +0530

    Island: Apply 'island' role to user on registration

--- monkey/monkey_island/cc/resources/auth/register.py
+++ monkey/monkey_island/cc/resources/auth/register.py
@@ -38,6 +38,12 @@ class Register(AbstractResource):
         if not isinstance(response, Response):
             return response_to_invalid_request()
 
+        # TODO: Create an enum with roles and descriptions
+        # TODO: This will need to be changed when Agent authentication is implemented
+        self._authentication_service.apply_role_to_user(
+            username, {"name": "island", "description": "Monkey Island, C&C server"}
+        )
+
         if response.status_code == HTTPStatus.OK:
             self._authentication_service.reset_island_data()
             self._authentication_service.reset_repository_encryptor(username, password)
