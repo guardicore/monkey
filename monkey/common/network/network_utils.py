fc021202eb348489b2a2cb99097d8649b434dada
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-07 10:54:11 -0500

    Common: Use ifaddr instead of netifaces
    netifaces must be compiled, which leads to more development
    dependencies. ifaddr does not. See issue #3032 for more details

--- monkey/common/network/network_utils.py
+++ monkey/common/network/network_utils.py
@@ -1,8 +1,8 @@
 import ipaddress
 from ipaddress import IPv4Address, IPv4Interface
-from typing import List, Optional, Sequence, Tuple
+from typing import Iterable, List, Optional, Sequence, Tuple
 
-from netifaces import AF_INET, ifaddresses, interfaces
+import ifaddr
 
 
 def get_my_ip_addresses() -> Sequence[IPv4Address]:
@@ -11,16 +11,20 @@ def get_my_ip_addresses() -> Sequence[IPv4Address]:
 
 def get_network_interfaces() -> List[IPv4Interface]:
     local_interfaces = []
-    for interface in interfaces():
-        addresses = ifaddresses(interface).get(AF_INET, [])
-        local_interfaces.extend(
-            [
-                ipaddress.IPv4Interface(link["addr"] + "/" + link["netmask"])
-                for link in addresses
-                if link["addr"] != "127.0.0.1"
-            ]
-        )
+    for adapter in ifaddr.get_adapters():
+        for ip in _select_ipv4_ips(adapter.ips):
+            local_interfaces.append(ipaddress.IPv4Interface(f"{ip.ip}/{ip.network_prefix}"))
+
     return local_interfaces
+
+
+def _select_ipv4_ips(ips: Iterable[ifaddr.IP]) -> Iterable[ifaddr.IP]:
+    return filter(lambda ip: _is_ipv4(ip) and ip.ip != "127.0.0.1", ips)
+
+
+def _is_ipv4(ip: ifaddr.IP) -> bool:
+    # In ifaddr, IPv4 addresses are strings, while IPv6 addresses are tuples
+    return type(ip.ip) is str
 
 
 # TODO: `address_to_port()` should return the port as an integer.
