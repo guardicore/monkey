4c9d88f8ebfa9ce217b6f2cbf50ed38fdbe50c00
Ilija Lazoroski <ilija.la@live.com>
2023-03-07 23:15:23 +0100

    UT: Fix TODO in AuthenticationService tests

--- monkey/tests/unit_tests/monkey_island/cc/services/test_authentication_service.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/test_authentication_service.py
@@ -5,7 +5,7 @@ from flask_security import UserDatastore
 
 from common import UserRoles
 from monkey_island.cc.event_queue import IIslandEventQueue, IslandEventTopic
-from monkey_island.cc.models import IslandMode, User
+from monkey_island.cc.models import IslandMode, Role, User
 from monkey_island.cc.server_utils.encryption import ILockableEncryptor
 from monkey_island.cc.services import AuthenticationService
 
@@ -17,6 +17,9 @@ PASSWORD_HASH = "$2b$12$yQzymz55fRvm8rApg7erluIvIAKSFSDrNIOIrOlxC4sXsDSkeu9z2"
 # Some tests have these fixtures as arguments even though `autouse=True`, because
 # to access the object that a fixture returns, it needs to be specified as an argument.
 # See https://stackoverflow.com/a/37046403.
+
+USER = User(username=USERNAME, password=PASSWORD)
+ROLE = Role(name=UserRoles.ISLAND.name, description=UserRoles.ISLAND.value)
 
 
 @pytest.fixture
@@ -33,9 +36,8 @@ def mock_island_event_queue(autouse=True):
 def mock_user_datastore(autouse=True):
     mock_user_datastore = MagicMock(spec=UserDatastore)
 
-    # TODO: Fix this with actual User and Role models
-    mock_user_datastore.find_user = MagicMock(return_value="some_user_object")
-    mock_user_datastore.find_or_create_role = MagicMock(return_value="some_role_object")
+    mock_user_datastore.find_user = MagicMock(return_value=USER)
+    mock_user_datastore.find_or_create_role = MagicMock(return_value=ROLE)
 
     return mock_user_datastore
 
@@ -91,7 +93,7 @@ def test_role_apply_to_user(
     mock_user_datastore.find_user.called_with(USERNAME)
     mock_user_datastore.find_or_create_role.called_with(UserRoles.ISLAND.name)
 
-    mock_user_datastore.add_role_to_user.called_with("some_user_object", "some_role_object")
+    mock_user_datastore.add_role_to_user.called_with(USER, ROLE)
 
 
 def test_reset_island__unlock_encryptor_on_register(
