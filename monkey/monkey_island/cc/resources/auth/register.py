3976a31e1a2cad99c046d04b9e4c6bdab50a12a0
Ilija Lazoroski <ilija.la@live.com>
2023-03-07 16:33:41 +0100

    Island: Apply role to user in Registe endpoint

--- monkey/monkey_island/cc/resources/auth/register.py
+++ monkey/monkey_island/cc/resources/auth/register.py
@@ -39,6 +39,10 @@ class Register(AbstractResource):
         if not isinstance(response, Response):
             return response_to_invalid_request()
 
+        self._authentication_service.apply_role_to_user(
+            username, {"name": "island", "description": "some_description"}
+        )
+
         if response.status_code == HTTPStatus.OK:
             self._authentication_service.reset_island_data()
             self._authentication_service.reset_repository_encryptor(username, password)
