07c118a8a8a101d389f41e59c91e810fe1d4ba5a
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-10 16:49:08 +0000

    Hadoop: Add path argument to plugin build scripts

--- monkey/agent_plugins/build.sh
+++ monkey/agent_plugins/build.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+# Usage: ./build.sh [plugin_path]
+
 umask 077
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 GID=$(id -g)
 TAG="latest"
 
+if [ -z "$1" ]; then
+    echo "No plugin path specified."
+    exit 1
+else
+    PLUGIN_PATH=$(realpath --relative-to="$SCRIPT_DIR" "$1")
+fi
+
+#shellcheck disable=SC1091
+source "$SCRIPT_DIR/util.sh"
+plugin_filename=$(get_plugin_filename "$SCRIPT_DIR/$PLUGIN_PATH") || fail "Failed to get plugin filename: $plugin_filename"
+
 DOCKER_COMMANDS="
-cd /plugin &&
-bash build_plugin.sh &&
-source util.sh &&
-chown ${UID}:${GID} \"/plugin/$(get_plugin_filename)\"
+cd /plugins &&
+bash build_plugin.sh \"${PLUGIN_PATH}\" &&
+chown ${UID}:${GID} \"/plugins/${PLUGIN_PATH}/$plugin_filename\"
 "
 
 docker pull infectionmonkey/agent-builder:$TAG
 docker run \
     --rm \
-    -v "$SCRIPT_DIR:/plugin" \
+    -v "$SCRIPT_DIR:/plugins" \
     infectionmonkey/agent-builder:$TAG \
     /bin/bash -c "$DOCKER_COMMANDS" | ts  '[%Y-%m-%d %H:%M:%S]'
--- monkey/agent_plugins/build_plugin.sh
+++ monkey/agent_plugins/build_plugin.sh
@@ -1,31 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build plugin package
-# Usage: ./build_plugin.sh
+# Usage: ./build_plugin.sh [plugin_path]
 
 ROOT="$( cd "$( dirname "$0" )" && pwd )"
 
 #shellcheck disable=SC1091
-. "$ROOT/util.sh"
+source "$ROOT/util.sh"
+
+if [ -z "$1" ]; then
+    echo "No plugin path specified."
+    exit 1
+else
+    PLUGIN_PATH=$(realpath "$1")
+fi
 
 set -x
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
+pushd "$PLUGIN_PATH" || fail "$PLUGIN_PATH does not exist"
 pip install pipenv
 pipenv requirements >> requirements.txt
 pip install -r requirements.txt -t src/vendor
 rm requirements.txt
 
 # Package everything up
-cd "$ROOT/src" || exit 1
+pushd "$PLUGIN_PATH/src" || fail "$PLUGIN_PATH/src does not exist"
 
-tar -cf "$ROOT/$SOURCE_FILENAME" ./*.py vendor
+tar -cf "$PLUGIN_PATH/$SOURCE_FILENAME" ./*.py vendor
 
 rm -rf vendor
-cd "$ROOT" || exit 1
+popd || exit 1
 
 plugin_filename=$(get_plugin_filename) || fail "Failed to get plugin filename: $plugin_filename"
-tar -cf "$ROOT/$plugin_filename" "$MANIFEST_FILENAME" "$SCHEMA_FILENAME" "$SOURCE_FILENAME"
-rm "$ROOT/$SOURCE_FILENAME"
+tar -cf "$PLUGIN_PATH/$plugin_filename" "$MANIFEST_FILENAME" "$SCHEMA_FILENAME" "$SOURCE_FILENAME"
+rm "$PLUGIN_PATH/$SOURCE_FILENAME"
+popd || exit 1
