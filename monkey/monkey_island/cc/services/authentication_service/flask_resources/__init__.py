649049c251ce048258b01bf9de3c975fa97ae085
Shreya Malviya <shreya.malviya@gmail.com>
2023-03-10 13:13:41 +0530

    Island: Change '/api/request-otp' to '/api/agent-otp'

--- monkey/monkey_island/cc/services/authentication_service/flask_resources/__init__.py
+++ monkey/monkey_island/cc/services/authentication_service/flask_resources/__init__.py
@@ -3,4 +3,4 @@ from .registration_status import RegistrationStatus
 from .login import Login
 from .logout import Logout
 from .register_resources import register_resources
-from .request_otp import RequestOTP
+from .agent_otp import AgentOTP
--- monkey/monkey_island/cc/services/authentication_service/flask_resources/agent_otp.py
+++ monkey/monkey_island/cc/services/authentication_service/flask_resources/agent_otp.py
@@ -3,19 +3,19 @@ from flask import make_response
 from monkey_island.cc.flask_utils import AbstractResource
 
 
-class RequestOTP(AbstractResource):
+class AgentOTP(AbstractResource):
     """
-    A resource for requesting a one-time password
+    A resource for requesting an Agent's one-time password
 
     One-time passwords may be requested by an Agent that has already authenticated,
-    so that Agents that it propagates can register.
+    so that Agents that it propagates can authenticate with the Island.
     """
 
-    urls = ["/api/request-otp"]
+    urls = ["/api/agent-otp"]
 
     def get(self):
         """
-        Requests a one-time password
+        Requests an Agent's one-time password
 
         :return: One-time password in the response body
         """
