f82dc18ad98b621ad00de0d8ed87699510240cbd
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-16 21:05:33 -0400

    UT: Add a fixture to consolidate duplicate code

--- monkey/tests/unit_tests/infection_monkey/test_monkey.py
+++ monkey/tests/unit_tests/infection_monkey/test_monkey.py
@@ -1,25 +1,25 @@
+import pytest
 from tests.data_for_tests.otp import OTP
 
 from infection_monkey.model import AGENT_OTP_ENVIRONMENT_VARIABLE, OTP_FLAG
 from infection_monkey.monkey import InfectionMonkey
 
 
-def test_get_otp(monkeypatch):
-    monkeypatch.setattr("os.environ", {AGENT_OTP_ENVIRONMENT_VARIABLE: OTP})
+@pytest.fixture(autouse=True)
+def configure_environment_variables(monkeypatch):
+    monkeypatch.setenv(AGENT_OTP_ENVIRONMENT_VARIABLE, OTP)
     monkeypatch.setenv(OTP_FLAG, True)
 
+
+def test_get_otp(monkeypatch):
     assert InfectionMonkey._get_otp() == OTP
 
 
 def test_get_otp__no_opt(monkeypatch):
-    monkeypatch.setattr("os.environ", {AGENT_OTP_ENVIRONMENT_VARIABLE: OTP})
-    monkeypatch.setenv(OTP_FLAG, True)
-
     assert InfectionMonkey._get_otp() == OTP
 
 
 def test_get_otp__feature_flag_disabled(monkeypatch):
-    monkeypatch.setattr("os.environ", {AGENT_OTP_ENVIRONMENT_VARIABLE: OTP})
     try:
         monkeypatch.delenv(OTP_FLAG)
     except KeyError:
