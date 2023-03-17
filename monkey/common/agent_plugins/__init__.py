210ff9da371631cb63512c0e6590c17d2d19c1e1
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-17 14:13:09 +0000

    SMB: Remove RemoteCopyFileError

--- monkey/common/agent_plugins/__init__.py
+++ monkey/common/agent_plugins/__init__.py
@@ -1,4 +1,3 @@
 from .agent_plugin_type import AgentPluginType
 from .agent_plugin_manifest import AgentPluginManifest
 from .agent_plugin import AgentPlugin
-from .exceptions import RemoteCopyFileError
