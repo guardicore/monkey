f118bd2566fd619e646a582fe160a7c3db680c3c
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-21 09:13:45 -0400

    Agent: Pass server to HTTPClient's constructor
    * Removes temporal coupling associated with `HTTPClient.set_url()`
    * Helps in the effort to separate the responsibility of identifying and
      Island API from communicating with an Island API

--- monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_client.py
+++ monkey/tests/unit_tests/infection_monkey/island_api_client/test_http_client.py
@@ -17,7 +17,8 @@ from infection_monkey.island_api_client import (
 from infection_monkey.island_api_client.http_client import RETRIES, HTTPClient
 from infection_monkey.island_api_client.island_api_client_errors import IslandAPIAuthenticationError
 
-SERVER = SocketAddress(ip="1.1.1.1", port=9999)
+SERVER = SocketAddress(ip="127.0.0.1", port=9999)
+SERVER_URL = f"https://{SERVER}/api"
 AGENT_ID = UUID("80988359-a1cd-42a2-9b47-5b94b37cd673")
 
 ISLAND_URI = f"https://{SERVER}/api?action=is-up"
@@ -34,9 +35,8 @@ def request_mock_instance():
 
 
 @pytest.fixture
-def connected_client(request_mock_instance):
-    http_client = HTTPClient()
-    http_client.server_url = f"https://{SERVER}/api"
+def http_client(request_mock_instance):
+    http_client = HTTPClient(SERVER_URL)
     request_mock_instance.get(ISLAND_URI)
     return http_client
 
@@ -49,20 +49,18 @@ def connected_client(request_mock_instance):
     ],
 )
 def test_http_client__error_handling(
-    initial_error, raised_error, connected_client, request_mock_instance
+    initial_error, raised_error, http_client, request_mock_instance
 ):
     request_mock_instance.get(ISLAND_GET_PROPAGATION_CREDENTIALS_URI, exc=initial_error)
 
     with pytest.raises(raised_error):
-        connected_client.get(PROPAGATION_CREDENTIALS_ENDPOINT)
+        http_client.get(PROPAGATION_CREDENTIALS_ENDPOINT)
 
 
-@pytest.mark.parametrize("server", ["http://1.1.1.1:5000", ""])
+@pytest.mark.parametrize("server", [f"http://{SERVER}", ""])
 def test_http_client__unsupported_protocol(server):
-    client = HTTPClient()
-
     with pytest.raises(ValueError):
-        client.server_url = server
+        HTTPClient(server)
 
 
 @pytest.mark.parametrize(
@@ -75,36 +73,24 @@ def test_http_client__unsupported_protocol(server):
     ],
 )
 def test_http_client__status_handling(
-    status_code, expected_error, connected_client, request_mock_instance
+    status_code, expected_error, http_client, request_mock_instance
 ):
     request_mock_instance.put(ISLAND_SEND_LOG_URI, status_code=status_code)
 
     with pytest.raises(expected_error):
-        connected_client.put(LOG_ENDPOINT, "Fake log contents")
+        http_client.put(LOG_ENDPOINT, "Fake log contents")
 
 
-def test_http_client__incorrect_call(connected_client, request_mock_instance):
+def test_http_client__incorrect_call(http_client, request_mock_instance):
     request_mock_instance.get(ISLAND_SEND_LOG_URI)
 
     # get requests have no data/json
     with pytest.raises(IslandAPIError):
-        connected_client.get(LOG_ENDPOINT, hokus_pokus="something")
-
-
-def test_http_client__unconnected():
-    http_client = HTTPClient()
-
-    with requests_mock.Mocker() as m:
-        m.get(ISLAND_SEND_LOG_URI)
-
-        with pytest.raises(IslandAPIError):
-            http_client.get(LOG_ENDPOINT)
+        http_client.get(LOG_ENDPOINT, hokus_pokus="something")
 
 
 def test_http_client__retries(monkeypatch):
-    http_client = HTTPClient()
-    # skip the connect method
-    http_client._server_url = f"https://{SERVER}/api"
+    http_client = HTTPClient(SERVER_URL)
     mock_send = MagicMock(side_effect=ConnectTimeoutError)
     # requests_mock can't be used for this, because it mocks higher level than we are testing
     monkeypatch.setattr("urllib3.connectionpool.HTTPSConnectionPool._validate_conn", mock_send)
@@ -115,30 +101,30 @@ def test_http_client__retries(monkeypatch):
     assert mock_send.call_count == RETRIES + 1
 
 
-def test_http_client__additional_args(monkeypatch, connected_client):
+def test_http_client__additional_args(monkeypatch, http_client):
     get = MagicMock()
     monkeypatch.setattr("requests.Session.get", get)
 
-    connected_client.get(LOG_ENDPOINT, auth="authentication")
+    http_client.get(LOG_ENDPOINT, auth="authentication")
 
     assert get.call_args[1]["auth"] == "authentication"
 
 
-def test_http_client__post(connected_client, monkeypatch):
+def test_http_client__post(http_client, monkeypatch):
     post = MagicMock()
     monkeypatch.setattr("requests.Session.post", post)
 
-    connected_client.post(PROPAGATION_CREDENTIALS_ENDPOINT, data={"foo": "bar"}, timeout=10)
+    http_client.post(PROPAGATION_CREDENTIALS_ENDPOINT, data={"foo": "bar"}, timeout=10)
 
     assert post.call_args[1]["json"] == {"foo": "bar"}
     assert post.call_args[1]["timeout"] == 10
 
 
-def test_http_client__put(connected_client, monkeypatch):
+def test_http_client__put(http_client, monkeypatch):
     put = MagicMock()
     monkeypatch.setattr("requests.Session.put", put)
 
-    connected_client.put(PROPAGATION_CREDENTIALS_ENDPOINT, data={"foo": "bar"}, timeout=10)
+    http_client.put(PROPAGATION_CREDENTIALS_ENDPOINT, data={"foo": "bar"}, timeout=10)
 
     assert put.call_args[1]["json"] == {"foo": "bar"}
     assert put.call_args[1]["timeout"] == 10
