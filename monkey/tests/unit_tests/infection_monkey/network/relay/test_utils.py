6c16973b68eb98488beae4caa84ca755a69afa78
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-03-21 09:57:23 -0400

    Agent: Refactor find_available_island_apis() logic
    find_available_island_apis() will now directly contact the Island
    instead of using the HTTPIslandAPIClient. It's responsibility is
    to identify Island APIs, whereas the HTTPIslandAPIClient's
    responsibility is to communicate with (known) Island APIs.

--- monkey/tests/unit_tests/infection_monkey/network/relay/test_utils.py
+++ monkey/tests/unit_tests/infection_monkey/network/relay/test_utils.py
@@ -1,14 +1,8 @@
 import pytest
+import requests
 import requests_mock
-from tests.data_for_tests.otp import OTP
 
-from common.agent_event_serializers import AgentEventSerializerRegistry
 from common.types import SocketAddress
-from infection_monkey.island_api_client import (
-    HTTPIslandAPIClientFactory,
-    IIslandAPIClient,
-    IslandAPIConnectionError,
-)
 from infection_monkey.network.relay.utils import find_available_island_apis
 
 SERVER_1 = SocketAddress(ip="1.1.1.1", port=12312)
@@ -20,52 +14,44 @@ SERVER_4 = SocketAddress(ip="4.4.4.4", port=5000)
 servers = [SERVER_1, SERVER_2, SERVER_3, SERVER_4]
 
 
-@pytest.fixture
-def island_api_client_factory():
-    return HTTPIslandAPIClientFactory(AgentEventSerializerRegistry(), OTP)
-
-
 @pytest.mark.parametrize(
     "expected_available_servers, server_response_pairs",
     [
-        ([], [(server, {"exc": IslandAPIConnectionError}) for server in servers]),
+        ([], [(server, {"exc": requests.exceptions.RequestException}) for server in servers]),
         (
             servers[1:],
-            [(SERVER_1, {"exc": IslandAPIConnectionError})]
+            [(SERVER_1, {"exc": requests.exceptions.HTTPError})]
             + [(server, {"text": ""}) for server in servers[1:]],  # type: ignore[dict-item]
         ),
     ],
 )
-def test_find_available_island_apis(
-    expected_available_servers, server_response_pairs, island_api_client_factory
-):
+def test_find_available_island_apis(expected_available_servers, server_response_pairs):
     with requests_mock.Mocker() as mock:
         for server, response in server_response_pairs:
             mock.get(f"https://{server}/api?action=is-up", **response)
-            mock.post(f"https://{server}/api/agent-otp-login", json={"token": "fake-token"})
 
-        available_apis = find_available_island_apis(servers, island_api_client_factory)
+        island_api_statuses = find_available_island_apis(servers)
 
-        assert len(available_apis) == len(server_response_pairs)
+        assert len(island_api_statuses) == len(server_response_pairs)
 
-        for server, island_api_client in available_apis.items():
+        for server, reachable in island_api_statuses.items():
             if server in expected_available_servers:
-                assert island_api_client is not None
+                assert reachable
             else:
-                assert island_api_client is None
+                assert not reachable
 
 
-def test_find_available_island_apis__multiple_successes(island_api_client_factory):
+def test_find_available_island_apis__multiple_successes():
     available_servers = [SERVER_2, SERVER_3]
     with requests_mock.Mocker() as mock:
-        mock.get(f"https://{SERVER_1}/api?action=is-up", exc=IslandAPIConnectionError)
+        mock.get(f"https://{SERVER_1}/api?action=is-up", exc=requests.exceptions.ConnectTimeout)
+        mock.get(f"https://{SERVER_4}/api?action=is-up", exc=requests.exceptions.InvalidURL)
         for server in available_servers:
-            mock.post(f"https://{server}/api/agent-otp-login", json={"token": "fake-token"})
             mock.get(f"https://{server}/api?action=is-up", text="")
 
-        available_apis = find_available_island_apis(servers, island_api_client_factory)
+        island_api_statuses = find_available_island_apis(servers)
 
-        assert available_apis[SERVER_1] is None
-        assert available_apis[SERVER_4] is None
+        assert not island_api_statuses[SERVER_1]
+        assert not island_api_statuses[SERVER_4]
         for server in available_servers:
-            assert isinstance(available_apis[server], IIslandAPIClient)
+            assert island_api_statuses[server]
