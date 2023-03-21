3c737db53999667d73291897c82446a8b986a283
vakarisz <vakarisz@yahoo.com>
2023-03-21 15:42:26 +0200

    UT: Remove unnecessary comments in test_secret_variable.py

--- monkey/tests/unit_tests/common/utils/test_secret_variable.py
+++ monkey/tests/unit_tests/common/utils/test_secret_variable.py
@@ -6,12 +6,10 @@ SECRET_TEXT = "my_secret_value"
 
 
 def test_secret_variable__no_logging(capsys):
-    # Arrange
     secret_variable = SecretVariable(SECRET_TEXT)
     logger = logging.getLogger(__name__)
-    # Act
+
     logger.debug(secret_variable)
 
-    # Asser
     captured = capsys.readouterr()
     assert SECRET_TEXT not in captured.out
