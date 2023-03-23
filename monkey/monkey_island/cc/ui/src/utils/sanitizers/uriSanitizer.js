3e55334f4f4215456a9ed74a2928d84ca2f15b78
ordabach <odabach@akamai.com>
2023-03-23 06:38:07 +0000

    UI: Removed the ftps option from the URI sanitizer

--- monkey/monkey_island/cc/ui/src/utils/sanitizers/uriSanitizer.js
+++ monkey/monkey_island/cc/ui/src/utils/sanitizers/uriSanitizer.js
@@ -1,7 +1,7 @@
 const REG_EXP_VALIDATORS = Object.freeze([
     {expression: /[()[\]{};`'"<>]/gmi, expectedTestResult: false},
     {expression: /^([^\w]*)(script|unsafe|javascript|vbscript|app|admin|icloud-sharing|icloud-vetting|help|aim|facetime-audio|applefeedback|ibooks|macappstore|udoc|ts|st|x-apple-helpbasic)/gmi, expectedTestResult: false},
-    {expression: /^(?:(?:(?:f|ht)tps?):|[^a-z]|[a-z+.-]+(?:[^a-z+.\-:]|$))/gmi, expectedTestResult: true}
+    {expression: /^(?:(?:ht)tps?:|[^a-z]|[a-z+.-]+(?:[^a-z+.\-:]|$))/gmi, expectedTestResult: true}
 ]);
 
 const GENERAL_UNSAFE_STRINGS = Object.freeze(['javascript:']);
