aa5632fd8ddc0e8a02703d9965674eac045c5b30
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-23 17:50:06 +0000

    SMB: Check for SMB ports before running exploiter
    Issue #2952
    PR #3143

--- monkey/infection_monkey/i_puppet/target_host.py
+++ monkey/infection_monkey/i_puppet/target_host.py
@@ -1,6 +1,6 @@
 import pprint
 from ipaddress import IPv4Address
-from typing import Dict, Optional
+from typing import Callable, Dict, Optional, Sequence
 
 from pydantic import Field
 
@@ -9,6 +9,8 @@ from common.base_models import MutableInfectionMonkeyBaseModel
 from common.types import NetworkPort
 
 from . import PortScanData
+
+FilterPortFunc = Callable[[PortScanData], bool]
 
 
 class TargetHostPorts(MutableInfectionMonkeyBaseModel):
@@ -21,6 +23,15 @@ class TargetHost(MutableInfectionMonkeyBaseModel):
     operating_system: Optional[OperatingSystem] = Field(default=None)
     icmp: bool = Field(default=False)
     ports_status: TargetHostPorts = Field(default=TargetHostPorts())
+
+    def filter_selected_tcp_ports(
+        self, ports: Sequence[NetworkPort], filter: FilterPortFunc
+    ) -> Sequence[PortScanData]:
+        return [
+            p
+            for p in ports
+            if p in self.ports_status.tcp_ports and filter(self.ports_status.tcp_ports[p])
+        ]
 
     def __hash__(self):
         return hash(self.ip)
