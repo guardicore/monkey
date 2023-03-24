288e87e569708cd60aee366241138af3b62f625d
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-24 16:29:05 +0000

    Agent: Pass Agent ID to Heart
    Issue #3119
    PR #3155

--- monkey/infection_monkey/monkey.py
+++ monkey/infection_monkey/monkey.py
@@ -152,7 +152,7 @@ class InfectionMonkey:
             self._island_api_client, self._manager
         )
 
-        self._heart = Heart(self._island_api_client)
+        self._heart = Heart(self._island_api_client, self._agent_id)
         self._heart.start()
 
         self._current_depth = self._opts.depth
