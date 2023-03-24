a14a01cdcf89012b633e100375b141868f401806
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-24 16:29:05 +0000

    Agent: Pass Agent ID to Heart
    Issue #3119
    PR #3155

--- monkey/infection_monkey/heart.py
+++ monkey/infection_monkey/heart.py
@@ -2,20 +2,20 @@ import logging
 import time
 
 from common.common_consts import HEARTBEAT_INTERVAL
+from common.types import AgentID
 from common.utils.code_utils import PeriodicCaller
 from infection_monkey.island_api_client import IIslandAPIClient
-from infection_monkey.utils.ids import get_agent_id
 
 logger = logging.getLogger(__name__)
 
 
 class Heart:
-    def __init__(self, island_api_client: IIslandAPIClient):
+    def __init__(self, island_api_client: IIslandAPIClient, agent_id: AgentID):
         self._island_api_client = island_api_client
         self._periodic_caller = PeriodicCaller(
             self._send_heartbeats, HEARTBEAT_INTERVAL, "AgentHeart"
         )
-        self._agent_id = get_agent_id()
+        self._agent_id = agent_id
 
     def start(self):
         logger.info("Starting the Agent's heart")
