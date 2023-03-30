73e24f4ce302de559121f35241c879086a0eb971
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-03-30 20:25:20 +0000

    Island: Remove unused imports in token.py

--- monkey/monkey_island/cc/services/authentication_service/flask_resources/token.py
+++ monkey/monkey_island/cc/services/authentication_service/flask_resources/token.py
@@ -1,20 +1,12 @@
 import logging
 from http import HTTPStatus
 
-from flask import Response, make_response, request
-from flask.typing import ResponseValue
-from flask_login import current_user
-from flask_security.views import login
+from flask import request
 
 from monkey_island.cc.flask_utils import AbstractResource, responses
 
 from ..authentication_facade import AuthenticationFacade
-from .utils import (
-    REFRESH_TOKEN_KEY_NAME,
-    add_refresh_token_to_response,
-    get_username_password_from_request,
-    include_auth_token,
-)
+from .utils import REFRESH_TOKEN_KEY_NAME
 
 logger = logging.getLogger(__name__)
 
@@ -46,6 +38,6 @@ class Token(AbstractResource):
                     "user": {"access_token": access_token, REFRESH_TOKEN_KEY_NAME: refresh_token}
                 }
             }
-            return response, 200
+            return response, HTTPStatus.OK
         except Exception:
             return responses.make_response_to_invalid_request()
