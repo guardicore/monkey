7681e3362e9976b43a32c5e059adcff05d1b87b2
Shreya Malviya <shreya.malviya@gmail.com>
2023-03-31 12:50:27 +0530

    UT: Add tests for TokenValidator.get_token_payload()

--- monkey/tests/unit_tests/monkey_island/cc/services/authentication_service/token/test_token_validator.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/authentication_service/token/test_token_validator.py
@@ -41,7 +41,7 @@ def test_validate_refresh_token__expired(freezer):
         token_validator.validate_token(token)
 
 
-def test_validate_refresh_token__invalid(freezer):
+def test_validate_refresh_token__invalid():
     token_expiration = 1 * 60  # 1 minute
     invalid_token = "invalid_token"
 
@@ -50,3 +50,44 @@ def test_validate_refresh_token__invalid(freezer):
 
     with pytest.raises(BadData):
         token_validator.validate_token(invalid_token)
+
+
+def test_get_token_payload():
+    token_expiration_timedelta = 1 * 60  # 1 minute
+    payload = "fake_user_id"
+
+    app, _ = build_app()
+    token_generator = TokenGenerator(app.security)
+    token_validator = TokenValidator(app.security, token_expiration_timedelta)
+
+    token = token_generator.generate_token(payload)
+
+    assert token_validator.get_token_payload(token) == payload
+
+
+def test_get_token_payload__expired_token(freezer):
+    token_expiration = 1 * 60  # 1 minute
+    generation_time = "2020-01-01 00:00:00"
+    validation_time = "2020-01-01 00:03:30"
+    payload = "fake_user_id"
+
+    app, _ = build_app()
+    token_generator = TokenGenerator(app.security)
+    freezer.move_to(generation_time)
+    token = token_generator.generate_token(payload)
+    token_validator = TokenValidator(app.security, token_expiration)
+    freezer.move_to(validation_time)
+
+    with pytest.raises(SignatureExpired):
+        token_validator.get_token_payload(token)
+
+
+def test_get_token_payload__invalid_token():
+    token_expiration = 1 * 60  # 1 minute
+    invalid_token = "invalid_token"
+
+    app, _ = build_app()
+    token_validator = TokenValidator(app.security, token_expiration)
+
+    with pytest.raises(BadData):
+        token_validator.get_token_payload(invalid_token)
