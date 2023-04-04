c731db307160960943d363e0c40eceab206c921d
Ilija Lazoroski <ilija.la@live.com>
2023-04-04 12:38:47 +0200

    Island: Export IOTPGenerator and AuthenticationOTPGenerator from __init__

--- monkey/monkey_island/cc/services/authentication_service/__init__.py
+++ monkey/monkey_island/cc/services/authentication_service/__init__.py
@@ -2,3 +2,6 @@ from .account_role import AccountRole
 from .flask_resources import register_resources
 
 from .setup import setup_authentication
+
+from .i_otp_generator import IOTPGenerator
+from .authentication_otp_generator import AuthenticationOTPGenerator
