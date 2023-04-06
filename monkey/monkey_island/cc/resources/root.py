452e92f0e22d9c93fa7729d4fb719d1502357921
Shreya Malviya <shreya.malviya@gmail.com>
2023-04-06 17:04:23 +0530

    Island: Remove unused code in Root resource

--- monkey/monkey_island/cc/resources/root.py
+++ monkey/monkey_island/cc/resources/root.py
@@ -4,16 +4,12 @@ from http import HTTPStatus
 from flask import request
 
 from monkey_island.cc.flask_utils import AbstractResource
-from monkey_island.cc.repositories import IAgentRepository
 
 logger = logging.getLogger(__name__)
 
 
 class Root(AbstractResource):
     urls = ["/api"]
-
-    def __init__(self, agent_repository: IAgentRepository):
-        self._agent_repository = agent_repository
 
     def get(self, action=None):
         if not action:
