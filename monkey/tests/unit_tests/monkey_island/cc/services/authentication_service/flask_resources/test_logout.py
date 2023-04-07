c24dbff2549f67699f9e9babe0052574e80fa213
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-04-07 16:21:43 +0000

    UT: Fix logout tests

--- monkey/tests/unit_tests/monkey_island/cc/services/authentication_service/flask_resources/test_logout.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/authentication_service/flask_resources/test_logout.py
@@ -52,9 +52,7 @@ def test_logout_failed(
     assert not mock_authentication_facade.remove_user.called
 
 
-def test_logout_successful(
-    monkeypatch, make_logout_request, mock_authentication_facade: AuthenticationFacade
-):
+def test_logout_successful(monkeypatch, make_logout_request):
     monkeypatch.setattr(
         FLASK_LOGOUT_IMPORT,
         lambda: Response(
@@ -65,11 +63,18 @@ def test_logout_successful(
     response = make_logout_request("")
 
     assert response.status_code == HTTPStatus.OK
-    assert not mock_authentication_facade.remove_user.called
 
 
+@pytest.mark.parametrize(
+    "role, expect_remove_user_called",
+    [(AccountRole.AGENT.name, True), (AccountRole.ISLAND_INTERFACE.name, False)],
+)
 def test_logout__removes_agent_user(
-    monkeypatch, make_logout_request, mock_authentication_facade: AuthenticationFacade
+    monkeypatch,
+    make_logout_request,
+    role: AccountRole,
+    expect_remove_user_called: bool,
+    mock_authentication_facade: AuthenticationFacade,
 ):
     monkeypatch.setattr(
         FLASK_LOGOUT_IMPORT,
@@ -78,8 +83,8 @@ def test_logout__removes_agent_user(
         ),
     )
     mock_user = MagicMock(spec=User)
-    mock_user.username = "test_agent_user"
-    mock_user.has_role = lambda role: role == AccountRole.AGENT.name
+    mock_user.username = "test_user"
+    mock_user.has_role = lambda r: r == role
     monkeypatch.setattr(
         "monkey_island.cc.services.authentication_service.flask_resources.logout.current_user",
         mock_user,
@@ -88,4 +93,7 @@ def test_logout__removes_agent_user(
     response = make_logout_request("")
 
     assert response.status_code == HTTPStatus.OK
-    mock_authentication_facade.remove_user.assert_called_once_with(mock_user.username)
+    if expect_remove_user_called:
+        mock_authentication_facade.remove_user.assert_called_once_with(mock_user.username)
+    else:
+        assert not mock_authentication_facade.remove_user.called
