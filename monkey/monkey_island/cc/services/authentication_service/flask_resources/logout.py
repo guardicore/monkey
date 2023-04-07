f7d7b45723cd7319cc236c9fa6019c53077f574c
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-04-07 15:57:27 +0000

    Island: Fix agent user removal on logout

--- monkey/monkey_island/cc/services/authentication_service/flask_resources/logout.py
+++ monkey/monkey_island/cc/services/authentication_service/flask_resources/logout.py
@@ -9,10 +9,9 @@ from flask_security.views import logout
 
 from monkey_island.cc.flask_utils import AbstractResource, responses
 from monkey_island.cc.services.authentication_service import AccountRole
-
-from ..account_role import AccountRole
-from ..authentication_facade import AuthenticationFacade
-from ..user import User
+from monkey_island.cc.services.authentication_service.authentication_facade import (
+    AuthenticationFacade,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +29,11 @@ class Logout(AbstractResource):
     @auth_token_required
     @roles_accepted(AccountRole.AGENT.name, AccountRole.ISLAND_INTERFACE.name)
     def post(self):
+        user_is_agent = False
         try:
             self._authentication_facade.revoke_all_tokens_for_user(current_user)
+            username = current_user.username
+            user_is_agent = current_user.has_role(AccountRole.AGENT.name)
             response: ResponseValue = logout()
         except Exception:
             return responses.make_response_to_invalid_request()
@@ -40,10 +42,7 @@ class Logout(AbstractResource):
             return responses.make_response_to_invalid_request()
 
         if response.status_code == HTTPStatus.OK:
-            self._remove_agent_user(current_user)
+            if user_is_agent:
+                self._authentication_facade.remove_user(username)
 
         return make_response(response)
-
-    def _remove_agent_user(self, user: User):
-        if user.has_role(AccountRole.AGENT.name):
-            self._authentication_facade.remove_user(user.username)
