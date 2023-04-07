104ae2c83d769367f02a33b2e12c5c72af3ef25b
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-04-07 12:55:35 +0000

    Island: Rename AgentRemover -> AgentUserRemover

--- monkey/monkey_island/cc/services/authentication_service/register_event_handlers.py
+++ monkey/monkey_island/cc/services/authentication_service/register_event_handlers.py
@@ -10,13 +10,13 @@ from .authentication_facade import AuthenticationFacade
 def register_event_handlers(container: DIContainer, authentication_facade: AuthenticationFacade):
     agent_event_queue = container.resolve(IAgentEventQueue)
     island_event_queue = container.resolve(IIslandEventQueue)
-    agent_remover = AgentRemover(authentication_facade)
+    agent_remover = AgentUserRemover(authentication_facade)
 
     agent_event_queue.subscribe_type(AgentShutdownEvent, agent_remover.remove_on_shutdown)
     island_event_queue.subscribe(IslandEventTopic.AGENT_TIMED_OUT, agent_remover.remove_on_timeout)
 
 
-class AgentRemover:
+class AgentUserRemover:
     def __init__(self, authentication_facade: AuthenticationFacade):
         self._authentication_facade = authentication_facade
 
