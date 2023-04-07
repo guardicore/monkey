4aef7163ab11f12fff6b795f9aabcf91982c794f
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-04-07 15:37:41 -0400

    UT: Remove OTP feature flag test

--- monkey/tests/unit_tests/infection_monkey/test_monkey.py
+++ monkey/tests/unit_tests/infection_monkey/test_monkey.py
@@ -4,14 +4,12 @@ import pytest
 from tests.data_for_tests.otp import TEST_OTP
 
 from common.common_consts import AGENT_OTP_ENVIRONMENT_VARIABLE
-from infection_monkey.model import OTP_FLAG
 from infection_monkey.monkey import InfectionMonkey
 
 
 @pytest.fixture(autouse=True)
 def configure_environment_variables(monkeypatch):
     monkeypatch.setenv(AGENT_OTP_ENVIRONMENT_VARIABLE, TEST_OTP.get_secret_value())
-    monkeypatch.setenv(OTP_FLAG, True)
 
 
 def test_get_otp(monkeypatch):
@@ -23,13 +21,3 @@ def test_get_otp__no_otp(monkeypatch):
     monkeypatch.delenv(AGENT_OTP_ENVIRONMENT_VARIABLE)
     with pytest.raises(Exception):
         InfectionMonkey._get_otp()
-
-
-def test_get_otp__feature_flag_disabled(monkeypatch):
-    try:
-        monkeypatch.delenv(OTP_FLAG)
-    except KeyError:
-        pass
-
-    # No need for a constant, this code is testing a feature flag that will be removed.
-    assert InfectionMonkey._get_otp().get_secret_value() == "PLACEHOLDER_OTP"
