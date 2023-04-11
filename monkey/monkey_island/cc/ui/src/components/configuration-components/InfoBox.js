b1c74feb8ed84d35cd6f3d94de0b82f0dcf94de9
Shreya Malviya <shreya.malviya@gmail.com>
2023-04-11 18:32:47 +0530

    UI: Modify InfoBox and WarningBox to accept props

--- monkey/monkey_island/cc/ui/src/components/configuration-components/InfoBox.js
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/InfoBox.js
@@ -3,12 +3,15 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faInfoCircle} from '@fortawesome/free-solid-svg-icons/faInfoCircle';
 
 class InfoBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div className='alert alert-info'>
         <FontAwesomeIcon icon={faInfoCircle} style={{'marginRight': '5px'}}/>
-        {this.props.schema.info}
+        {this.props.message}
       </div>
     );
   }
