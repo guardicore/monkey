437a150f4f280bf048a5a42deb737984906b4190
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-04-25 08:33:23 -0400

    UT: Simplify MasqueradeAgentBinaryRepositoryDecorator tests

--- monkey/tests/unit_tests/monkey_island/cc/services/agent_binary_service/test_masquerade_agent_binary_repository_decorator.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/agent_binary_service/test_masquerade_agent_binary_repository_decorator.py
@@ -11,10 +11,19 @@ from monkey_island.cc.services.agent_binary_service.masquerade_agent_binary_repo
     MasqueradeAgentBinaryRepositoryDecorator,
 )
 
-LINUX_AGENT_BINARY = io.BytesIO(b"linux_binary")
-WINDOWS_AGENT_BINARY = io.BytesIO(b"windows_binary")
-
 MASQUE = b"m0nk3y"
+
+LINUX_AGENT_BINARY_BYTES = b"linux_binary"
+LINUX_AGENT_BINARY = io.BytesIO(LINUX_AGENT_BINARY_BYTES)
+MASQUED_LINUX_AGENT_BINARY = io.BytesIO(
+    LINUX_AGENT_BINARY_BYTES + b"\x00" * NULL_BYTES_LENGTH + MASQUE
+)
+
+WINDOWS_AGENT_BINARY_BYTES = b"windows_binary"
+WINDOWS_AGENT_BINARY = io.BytesIO(WINDOWS_AGENT_BINARY_BYTES)
+MASQUED_WINDOWS_AGENT_BINARY = io.BytesIO(
+    WINDOWS_AGENT_BINARY_BYTES + b"\x00" * NULL_BYTES_LENGTH + MASQUE
+)
 
 
 @pytest.fixture
@@ -33,11 +42,10 @@ def test_masquerade_agent_binary_repository_decorator__linux(
     agent_binary_repository = MasqueradeAgentBinaryRepositoryDecorator(
         mock_agent_binary_repository, MASQUE
     )
+
     actual_linux_binary = agent_binary_repository.get_linux_binary()
-    expected_linux_binary = LINUX_AGENT_BINARY
-    expected_linux_binary.seek(0, io.SEEK_END)
-    expected_linux_binary.write(b"\x00" * NULL_BYTES_LENGTH + MASQUE)
-    assert actual_linux_binary == expected_linux_binary
+
+    assert actual_linux_binary.getvalue() == MASQUED_LINUX_AGENT_BINARY.getvalue()  # type: ignore[attr-defined] # noqa: E501
 
 
 def test_masquerade_agent_binary_repository_decorator__windows(
@@ -46,12 +54,10 @@ def test_masquerade_agent_binary_repository_decorator__windows(
     agent_binary_repository = MasqueradeAgentBinaryRepositoryDecorator(
         mock_agent_binary_repository, MASQUE
     )
+
     actual_windows_binary = agent_binary_repository.get_windows_binary()
 
-    expected_windows_binary = WINDOWS_AGENT_BINARY
-    expected_windows_binary.seek(0, io.SEEK_END)
-    expected_windows_binary.write(b"\x00" * NULL_BYTES_LENGTH + MASQUE)
-    assert actual_windows_binary == expected_windows_binary
+    assert actual_windows_binary.getvalue() == MASQUED_WINDOWS_AGENT_BINARY.getvalue()  # type: ignore[attr-defined] # noqa: E501
 
 
 def test_masquerade_agent_binary_repository_decorator__cached_linux(
@@ -60,8 +66,9 @@ def test_masquerade_agent_binary_repository_decorator__cached_linux(
     agent_binary_repository = MasqueradeAgentBinaryRepositoryDecorator(
         mock_agent_binary_repository, MASQUE
     )
+
     actual_linux_binary = agent_binary_repository.get_linux_binary()
-    mock_agent_binary_repository.get_linux_binary = MagicMock(return_value=b"not_linux_binary")
+    mock_agent_binary_repository.get_linux_binary = MagicMock(return_value=b"not_linux_binary")  # type: ignore[assignment,method-assign] # noqa: E501
     cached_linux_binary = agent_binary_repository.get_linux_binary()
 
     assert actual_linux_binary == cached_linux_binary
@@ -73,8 +80,9 @@ def test_masquerade_agent_binary_repository_decorator__cached_windows(
     agent_binary_repository = MasqueradeAgentBinaryRepositoryDecorator(
         mock_agent_binary_repository, MASQUE
     )
+
     actual_windows_binary = agent_binary_repository.get_windows_binary()
-    mock_agent_binary_repository.get_windows_binary = MagicMock(return_value=b"not_windows_binary")
+    mock_agent_binary_repository.get_windows_binary = MagicMock(return_value=b"not_windows_binary")  # type: ignore[assignment,method-assign] # noqa: E501
     cached_windows_binary = agent_binary_repository.get_windows_binary()
 
     assert actual_windows_binary == cached_windows_binary
