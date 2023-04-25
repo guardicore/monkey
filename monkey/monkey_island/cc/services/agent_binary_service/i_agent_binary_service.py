fc895950636ada84428eeb981526f3fd17ef6a09
Ilija Lazoroski <ilija.la@live.com>
2023-04-25 16:25:28 +0200

    Island: Add {get,set}_masque in IAgentBinaryService

--- monkey/monkey_island/cc/services/agent_binary_service/i_agent_binary_service.py
+++ monkey/monkey_island/cc/services/agent_binary_service/i_agent_binary_service.py
@@ -1,5 +1,7 @@
 import abc
-from typing import BinaryIO
+from typing import BinaryIO, Optional
+
+from common import OperatingSystem
 
 
 class IAgentBinaryService(metaclass=abc.ABCMeta):
@@ -23,4 +25,22 @@ class IAgentBinaryService(metaclass=abc.ABCMeta):
 
         :return: A file-like object that represents the windows agent binary
         :raises RetrievalError: If the agent binary could not be retrieved
+        """
+
+    @abc.abstractmethod
+    def get_masque(self, operating_system: OperatingSystem) -> Optional[bytes]:
+        """
+        Retrieve os specific agent binary masque
+
+        :param operating_system: Operating system type
+        :return: A bytes object that represent the masque
+        """
+
+    @abc.abstractmethod
+    def set_masque(self, operating_system: OperatingSystem, masque: Optional[bytes]):
+        """
+        Set os-specific agent binary masque
+
+        :param operating_system: Operating system type
+        :param masque: The value to set the os-specific masque to
         """
