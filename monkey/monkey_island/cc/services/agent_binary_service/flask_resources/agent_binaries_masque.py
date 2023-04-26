86d6ca4908e6a61ebae67237a2fdc62a0895429c
Shreya Malviya <shreya.malviya@gmail.com>
2023-04-26 17:55:54 +0530

    Island: Update AgentBinariesMasque resource to return 404 instead of 400

--- monkey/monkey_island/cc/services/agent_binary_service/flask_resources/agent_binaries_masque.py
+++ monkey/monkey_island/cc/services/agent_binary_service/flask_resources/agent_binaries_masque.py
@@ -24,30 +24,27 @@ class AgentBinariesMasque(AbstractResource):
     def get(self, os):
         try:
             operating_system = OperatingSystem[os.upper()]
-
-            masque = self._agent_binary_service.get_masque(operating_system)
-            if masque is None:
-                masque = b""
-            return make_response(
-                masque, HTTPStatus.OK, {"Content-Type": "application/octet-stream"}
-            )
-
         except KeyError as err:
             message = f'Could not get masque for OS "{os}": {err}'
             logger.error(message)
-            return make_response({"error": message}, HTTPStatus.BAD_REQUEST)
+            return make_response({"error": message}, HTTPStatus.NOT_FOUND)
+
+        masque = self._agent_binary_service.get_masque(operating_system)
+        if masque is None:
+            masque = b""
+        return make_response(masque, HTTPStatus.OK, {"Content-Type": "application/octet-stream"})
 
     @auth_token_required
     @roles_accepted(AccountRole.ISLAND_INTERFACE.name)
     def put(self, os):
+        masque = request.data
+
         try:
             operating_system = OperatingSystem[os.upper()]
-            masque = request.data
-
-            self._agent_binary_service.set_masque(operating_system, masque)
-            return make_response({}, HTTPStatus.NO_CONTENT)
-
         except KeyError as err:
             message = f'Could not set masque for OS "{os}": {err}'
             logger.error(message)
-            return make_response({"error": message}, HTTPStatus.BAD_REQUEST)
+            return make_response({"error": message}, HTTPStatus.NOT_FOUND)
+
+        self._agent_binary_service.set_masque(operating_system, masque)
+        return make_response({}, HTTPStatus.NO_CONTENT)
