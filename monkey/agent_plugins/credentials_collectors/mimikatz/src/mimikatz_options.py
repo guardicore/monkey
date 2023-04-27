6761eab643467f50ffb04589a5236df2a275a99c
Ilija Lazoroski <ilija.la@live.com>
2023-04-27 16:00:37 +0200

    Mimikatz: Improve excluded username prefix description

--- monkey/agent_plugins/credentials_collectors/mimikatz/src/mimikatz_options.py
+++ monkey/agent_plugins/credentials_collectors/mimikatz/src/mimikatz_options.py
@@ -6,5 +6,6 @@ from common.base_models import InfectionMonkeyBaseModel
 class MimikatzOptions(InfectionMonkeyBaseModel):
     excluded_username_prefix: str = Field(
         default="somenewuser",
-        description="A username prefix for which Mimikatz will not collect credentials.",
+        description="Mimikatz will not collect credentials for any user whos username"
+        " starts with this prefix.",
     )
