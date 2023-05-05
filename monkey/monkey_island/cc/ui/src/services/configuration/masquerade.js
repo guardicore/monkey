df3f9508b1fa38de363afbbcdc23092894529ebd
Ilija Lazoroski <ilija.la@live.com>
2023-05-05 08:32:57 +0200

    UI: Add {Linux,Windows} Masque strings to the schema

--- monkey/monkey_island/cc/ui/src/services/configuration/masquerade.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/masquerade.js
@@ -6,19 +6,29 @@ const MASQUERADE = {
   'custom data can be injected into agent binaries, allowing the agents to imitate various types of malware. '+
   'This feature can be particularly helpful when testing custom detection rules.',
   'properties': {
-    'masque_strings': {
-      'title': 'Masque Strings',
+    'linux_masque_strings': {
+      'title': 'Linux Masque Strings',
       'type': 'array',
       'uniqueItems': true,
       'items': {'type': 'string'},
       'default': [],
-      'description': 'List of masque strings that will be insert to the agent binaries '
+      'description': 'List of masque strings that will be insert to the Linux Agent binaries '
+    },
+    'windows_masque_strings': {
+      'title': 'Windows Masque Strings',
+      'type': 'array',
+      'uniqueItems': true,
+      'items': {'type': 'string'},
+      'default': [],
+      'description': 'List of masque strings that will be insert to the Windows Agent binaries '
     }
+
   }
 }
 
 export const defaultMasques = {
- 'masque_strings': []
+ 'linux_masque_strings': [],
+ 'windows_masque_strings': []
 }
 
 export default MASQUERADE;
