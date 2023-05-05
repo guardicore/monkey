04dcddfd093cad531d2e49a9d6387a1e675f2355
Ilija Lazoroski <ilija.la@live.com>
2023-05-05 09:25:26 +0200

    UI: Make Masques a text field

--- monkey/monkey_island/cc/ui/src/services/configuration/masquerade.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/masquerade.js
@@ -1,34 +1,28 @@
 const MASQUERADE = {
   'title': 'Masquerade',
   'type': 'object',
-  'description': 'To identify malware in a file, one common approach is to search for specific ' +
-  'strings or unique byte sequences.\nIn order to enhance Infection Monkey\'s malware emulation,'+
-  'custom data can be injected into agent binaries, allowing the agents to imitate various types of malware. '+
-  'This feature can be particularly helpful when testing custom detection rules.',
+  'description': 'Infection Monkey can emulate malware by injecting custom data into Agent binaries.\n'+
+    'This feature can be particularly helpful when testing custom detection rules.',
   'properties': {
-    'linux_masque_strings': {
-      'title': 'Linux Masque Strings',
-      'type': 'array',
-      'uniqueItems': true,
-      'items': {'type': 'string'},
-      'default': [],
-      'description': 'List of masque strings that will be insert to the Linux Agent binaries '
+    'linux_masque_string': {
+      'title': 'Linux Masque',
+      'type': 'string',
+      'default': '',
+      'description': 'A masque string that will be applied to the Linux Agent binary.'
     },
-    'windows_masque_strings': {
-      'title': 'Windows Masque Strings',
-      'type': 'array',
-      'uniqueItems': true,
-      'items': {'type': 'string'},
-      'default': [],
-      'description': 'List of masque strings that will be insert to the Windows Agent binaries '
+    'windows_masque_string': {
+      'title': 'Windows Masque',
+      'type': 'string',
+      'default': '',
+      'description': 'A masque string that will be applied to the Windows Agent binary.'
     }
 
   }
 }
 
 export const defaultMasques = {
- 'linux_masque_strings': [],
- 'windows_masque_strings': []
+ 'linux_masque_string': [],
+ 'windows_masque_string': []
 }
 
 export default MASQUERADE;
