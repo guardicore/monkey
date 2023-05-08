ae345cdbd0aab5261ac803971c5489eec1ca8459
Shreya Malviya <shreya.malviya@gmail.com>
2023-05-08 16:57:02 +0530

    UI: Set placeholder values in credentials collectors plugins in UISchemaPluginsManipulator.js

--- monkey/monkey_island/cc/ui/src/components/configuration-components/UISchemaManipulators.tsx
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/UISchemaManipulators.tsx
@@ -1,5 +1,10 @@
 import pluginsManipulator from "./UISchemaPluginsManipulator";
 
+const CREDENTIAL_COLLECTORS_TAB = 'credential_collectors';
+const PAYLOADS_TAB = 'payloads';
+const PROPAGATION_TAB = 'propagation';
+
+
 const manipulatorList = [ransomwareDirManipulator, pluginsDirManipulator]
 
 export default function applyUiSchemaManipulators(selectedSection,
@@ -14,14 +19,16 @@ export default function applyUiSchemaManipulators(selectedSection,
 function ransomwareDirManipulator(selectedSection,
                                   formData,
                                   uiSchema) {
-  if (selectedSection === 'payloads'){
+  if (selectedSection === PAYLOADS_TAB){
       uiSchema.ransomware.encryption.directories =
         {'ui:disabled': !formData['ransomware']['encryption']['enabled']};
     }
 }
 
 function pluginsDirManipulator(selectedSection, formData, uiSchema, JSONSchema) {
-  if(selectedSection === 'propagation') {
+  if (selectedSection === PROPAGATION_TAB) {
     pluginsManipulator(uiSchema?.exploitation?.exploiters, JSONSchema?.properties?.exploitation?.properties?.exploiters?.properties)
+  } else if (selectedSection === CREDENTIAL_COLLECTORS_TAB) {
+    pluginsManipulator(uiSchema?.credentials_collectors, JSONSchema?.properties?.credential_collectors?.properties)
   }
 }
