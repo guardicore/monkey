d194c884db5b55deaa6b4f6aab1d7766dfea7876
Ilija Lazoroski <ilija.la@live.com>
2023-05-09 13:37:10 +0200

    Common: Remove hard coded credentials collector manifests

--- monkey/common/__init__.py
+++ monkey/common/__init__.py
@@ -10,5 +10,4 @@ from .agent_signals import AgentSignals
 from .agent_heartbeat import AgentHeartbeat
 from .hard_coded_manifests import (
     HARD_CODED_EXPLOITER_MANIFESTS,
-    HARD_CODED_CREDENTIALS_COLLECTOR_MANIFESTS,
 )
