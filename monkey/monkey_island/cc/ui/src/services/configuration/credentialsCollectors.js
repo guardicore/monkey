1096a46afa7e21682890cfb84c8bf05c0c84a507
Ilija Lazoroski <ilija.la@live.com>
2023-05-09 13:52:24 +0200

    UI: Remove SSHCollector from credentialsCollectors configuration

--- monkey/monkey_island/cc/ui/src/services/configuration/credentialsCollectors.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/credentialsCollectors.js
@@ -2,25 +2,6 @@ const CREDENTIALS_COLLECTORS = {
     'title': 'Credentials Collectors',
     'description': 'Click on a credentials collector for more information.',
     'type': 'object',
-    'pluginDefs': {
-      'SSHCollector':{'name': 'SSHCollector', 'options': {}}
-    },
-    'properties':{
-      'name': {
-        'type': 'string',
-        'anyOf': [
-          {
-            'type': 'string',
-            'enum': ['SSHCollector'],
-            'title': 'SSH Credentials Collector',
-            'safe': true,
-            'info': 'Searches users\' home directories and collects SSH keypairs.'
-          }
-        ]
-      },
-      'options': {
-        'type': 'object'
-      }
-    }
+    'properties':{}
 }
 export default CREDENTIALS_COLLECTORS
