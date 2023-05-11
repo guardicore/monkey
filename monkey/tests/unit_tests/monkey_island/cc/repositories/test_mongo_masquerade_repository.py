db150a0ee4f010a37d2806c50080a90c6e92ac8b
Shreya Malviya <shreya.malviya@gmail.com>
2023-05-11 13:56:39 +0530

    UT: Add tests for MongoMasqueradeRepository

---
+++ monkey/tests/unit_tests/monkey_island/cc/repositories/test_mongo_masquerade_repository.py
@@ -0,0 +1,126 @@
+from unittest.mock import MagicMock
+
+import mongomock
+import pytest
+
+from common import OperatingSystem
+from monkey_island.cc.repositories import (
+    IMasqueradeRepository,
+    MongoMasqueradeRepository,
+    RemovalError,
+    RetrievalError,
+    StorageError,
+)
+
+LINUX_MASQUE = b"linux_m0nk3y"
+LINUX_DOCUMENT = {"operating_system": OperatingSystem.LINUX.value, "masque": LINUX_MASQUE}
+
+WINDOWS_MASQUE = b"windows_m0nk3y"
+WINDOWS_DOCUMENT = {"operating_system": OperatingSystem.WINDOWS.value, "masque": WINDOWS_MASQUE}
+
+
+@pytest.fixture
+def masquerade_repository() -> IMasqueradeRepository:
+    mongo_client = mongomock.MongoClient()
+    mongo_client.monkey_island.masques.insert_many([LINUX_DOCUMENT, WINDOWS_DOCUMENT])
+    return MongoMasqueradeRepository(mongo_client)
+
+
+@pytest.fixture
+def empty_masquerade_repository() -> IMasqueradeRepository:
+    mongo_client = mongomock.MongoClient()
+    return MongoMasqueradeRepository(mongo_client)
+
+
+@pytest.fixture
+def error_raising_mock_mongo_client() -> mongomock.MongoClient:
+    mongo_client = MagicMock(spec=mongomock.MongoClient)
+    mongo_client.monkey_island = MagicMock(spec=mongomock.Database)
+    mongo_client.monkey_island.masques = MagicMock(spec=mongomock.Collection)
+
+    mongo_client.monkey_island.masques.drop = MagicMock(side_effect=Exception("some exception"))
+    mongo_client.monkey_island.masques.find = MagicMock(side_effect=Exception("some exception"))
+    mongo_client.monkey_island.masques.find_one = MagicMock(side_effect=Exception("some exception"))
+    mongo_client.monkey_island.masques.replace_one = MagicMock(
+        side_effect=Exception("some exception")
+    )
+
+    return mongo_client
+
+
+@pytest.fixture
+def error_raising_masquerade_repository(error_raising_mock_mongo_client) -> IMasqueradeRepository:
+    return MongoMasqueradeRepository(error_raising_mock_mongo_client)
+
+
+def test_set_masque__insert(empty_masquerade_repository):
+    empty_masquerade_repository.set_masque(OperatingSystem.LINUX, LINUX_MASQUE)
+
+    assert empty_masquerade_repository.get_masque(OperatingSystem.LINUX) == LINUX_MASQUE
+
+
+def test_set_masque__update(masquerade_repository):
+    new_masque = b"new_windows_m0nk3y"
+    masquerade_repository.set_masque(OperatingSystem.WINDOWS, new_masque)
+
+    assert masquerade_repository.get_masque(OperatingSystem.LINUX) == LINUX_MASQUE
+    assert masquerade_repository.get_masque(OperatingSystem.WINDOWS) == new_masque
+
+
+def test_set_masque__no_changes(masquerade_repository):
+    masquerade_repository.set_masque(OperatingSystem.LINUX, LINUX_MASQUE)
+
+    assert masquerade_repository.get_masque(OperatingSystem.LINUX) == LINUX_MASQUE
+
+
+def test_set_masque__storage_error(error_raising_masquerade_repository):
+    with pytest.raises(StorageError):
+        error_raising_masquerade_repository.set_masque(OperatingSystem.WINDOWS, WINDOWS_MASQUE)
+
+
+def test_get_masque__empty_repo(empty_masquerade_repository):
+    masque = empty_masquerade_repository.get_masque(OperatingSystem.LINUX)
+
+    assert masque == b""
+
+
+def test_get_masque(masquerade_repository):
+    masque = masquerade_repository.get_masque(OperatingSystem.WINDOWS)
+
+    assert masque == WINDOWS_MASQUE
+
+
+def test_get_masque__empty_masque(masquerade_repository):
+    masque = masquerade_repository.get_masque(OperatingSystem.WINDOWS)
+
+    assert masque == WINDOWS_MASQUE
+
+
+def test_get_masque__retrieval_error(error_raising_masquerade_repository):
+    with pytest.raises(RetrievalError):
+        error_raising_masquerade_repository.get_masque(OperatingSystem.LINUX)
+
+
+def test_reset(masquerade_repository):
+    # Ensure the repository is not empty
+    for operating_system in OperatingSystem:
+        masque = masquerade_repository.get_masque(operating_system)
+        assert isinstance(masque, bytes)
+
+    masquerade_repository.reset()
+
+    for operating_system in OperatingSystem:
+        assert masquerade_repository.get_masque(operating_system) == b""
+
+
+def test_usable_after_reset(masquerade_repository):
+    masquerade_repository.reset()
+
+    masquerade_repository.set_masque(OperatingSystem.LINUX, LINUX_MASQUE)
+
+    assert masquerade_repository.get_masque(OperatingSystem.LINUX) == LINUX_MASQUE
+
+
+def test_reset__removal_error(error_raising_masquerade_repository):
+    with pytest.raises(RemovalError):
+        error_raising_masquerade_repository.reset()
