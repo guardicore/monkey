930607a1a5c0457a680bb5296bba11c0b7d1c416
Ilija Lazoroski <ilija.la@live.com>
2023-05-15 12:44:14 +0200

    Agent: Refactor _check_tcp_ports to resolve C901
    Issue: #1076
    PR: #3335

--- monkey/infection_monkey/network_scanning/tcp_scanner.py
+++ monkey/infection_monkey/network_scanning/tcp_scanner.py
@@ -94,89 +94,104 @@ def _check_tcp_ports(
     :param ip:  IP of host to attack
     :param ports_to_scan: An iterable of ports to scan. Must not be empty.
     :param timeout: Amount of time to wait for connection
-    :return: Mapping where the key is an open port and the value is the banner
-    :rtype: Mapping
     """
-    sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(len(ports_to_scan))]
-    for s in sockets:
-        s.setblocking(False)
-
-    possible_ports = set()
-    connected_ports = set()
-    open_ports = {}
-
     event_timestamp = time()
+    open_ports: Dict[NetworkPort, str] = {}
+
     try:
+        ports_sockets = _create_sockets(ports_to_scan)
+        possible_ports, connected_ports_sockets = _connect_sockets(ip, ports_sockets)
         logger.debug(
             "Connecting to the following ports %s" % ",".join((str(x) for x in ports_to_scan))
         )
-        for sock, port in zip(sockets, ports_to_scan):
-            err = sock.connect_ex((ip, port))
-            if err == 0:  # immediate connect
-                connected_ports.add((port, sock))
-                possible_ports.add((port, sock))
-            elif err == 10035:  # WSAEWOULDBLOCK is valid.
-                # https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-connect
-                # says, "Use the select function to determine the completion of the connection
-                # request by checking to see if the socket is writable," which is being done below.
-                possible_ports.add((port, sock))
-            elif err == 115:  # EINPROGRESS     115     /* Operation now in progress */
-                possible_ports.add((port, sock))
-            else:
-                logger.warning("Failed to connect to port %s, error code is %d", port, err)
 
-        if len(possible_ports) != 0:
-            sockets_to_try = possible_ports.copy()
-
+        if possible_ports:
             timer = EggTimer()
             timer.set(timeout)
 
-            while (not timer.is_expired()) and sockets_to_try:
-                # The call to select() may return sockets that are writeable but not actually
-                # connected. Adding this sleep prevents excessive looping.
-                sleep(min(POLL_INTERVAL, timer.time_remaining_sec))
+            _process_connected_sockets(connected_ports_sockets, possible_ports, open_ports, timer)
 
-                sock_objects = [s[1] for s in sockets_to_try]
-
-                _, writeable_sockets, _ = select.select(
-                    [], sock_objects, [], timer.time_remaining_sec
-                )
-                for s in writeable_sockets:
-                    try:  # actual test
-                        connected_ports.add((s.getpeername()[1], s))
-                    except socket.error:  # bad socket, select didn't filter it properly
-                        pass
-
-                sockets_to_try = sockets_to_try - connected_ports
-
-            logger.info(
-                f"Discovered the following ports on {ip}: "
-                f"{pformat([port for port, _ in connected_ports])}"
-            )
-
-            open_ports = {port: "" for port, _ in connected_ports}
-            if len(connected_ports) != 0:
-                readable_sockets, _, _ = select.select(
-                    [s[1] for s in connected_ports], [], [], timer.time_remaining_sec
-                )
-                # read first BANNER_READ bytes. We ignore errors because service might not send a
-                # decodable byte string.
-                for port, sock in connected_ports:
-                    if sock in readable_sockets:
-                        open_ports[port] = sock.recv(BANNER_READ).decode(errors="ignore")
-                    else:
-                        open_ports[port] = ""
+        logger.info(
+            f"Discovered the following ports on {ip}: "
+            f"{pformat([port for port, _ in connected_ports_sockets])}"
+        )
 
     except socket.error as exc:
         logger.warning("Exception when checking ports on host %s, Exception: %s", str(ip), exc)
 
-    _clean_up_sockets(possible_ports, connected_ports)
+    _clean_up_sockets(possible_ports, connected_ports_sockets)
 
     return event_timestamp, open_ports
 
 
+def _create_sockets(ports: Collection[NetworkPort]) -> Dict[NetworkPort, socket.socket]:
+    ports_sockets = {}
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        ports_sockets[port] = sock
+    return ports_sockets
+
+
+def _connect_sockets(ip: str, ports_sockets: Dict[NetworkPort, socket.socket]) -> Tuple[set, set]:
+    possible_ports_sockets = set()
+    connected_ports_sockets = set()
+
+    for port, sock in ports_sockets.items():
+        err = sock.connect_ex((ip, port))
+        if err == 0:  # immediate connect
+            connected_ports_sockets.add((port, sock))
+            possible_ports_sockets.add((port, sock))
+        elif err == 10035:  # WSAEWOULDBLOCK is valid.
+            # https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-connect
+            # says, "Use the select function to determine the completion of the connection
+            # request by checking to see if the socket is writable," which is being done below.
+            possible_ports_sockets.add((port, sock))
+        elif err == 115:  # EINPROGRESS     115     /* Operation now in progress */
+            possible_ports_sockets.add((port, sock))
+        else:
+            logger.warning("Failed to connect to port %s, error code is %d", port, err)
+
+    return possible_ports_sockets, connected_ports_sockets
+
+
+def _process_connected_sockets(
+    connected_ports_sockets: set,
+    possible_ports_sockets: set,
+    open_ports: Dict[NetworkPort, str],
+    timer: EggTimer,
+):
+    ports_sockets_to_try = possible_ports_sockets.copy()
+
+    while not timer.is_expired() and ports_sockets_to_try:
+        sleep(min(POLL_INTERVAL, timer.time_remaining_sec))
+
+        sock_objects = [s[1] for s in ports_sockets_to_try]
+
+        _, writeable_sockets, _ = select.select([], sock_objects, [], timer.time_remaining_sec)
+        for s in writeable_sockets:
+            try:
+                connected_ports_sockets.add((s.getpeername()[1], s))
+            except socket.error:
+                pass
+
+        ports_sockets_to_try = ports_sockets_to_try - connected_ports_sockets
+
+    open_ports.update({port: "" for port, _ in connected_ports_sockets})
+
+    if connected_ports_sockets:
+        readable_sockets, _, _ = select.select(
+            [s[1] for s in connected_ports_sockets], [], [], timer.time_remaining_sec
+        )
+        for port, sock in connected_ports_sockets:
+            if sock in readable_sockets:
+                open_ports[port] = sock.recv(BANNER_READ).decode(errors="ignore")
+            else:
+                open_ports[port] = ""
+
+
 def _clean_up_sockets(
-    possible_ports: Iterable[Tuple[NetworkPort, socket.socket]],
+    possible_ports_sockets: Iterable[Tuple[NetworkPort, socket.socket]],
     connected_ports_sockets: Iterable[Tuple[NetworkPort, socket.socket]],
 ):
     # Only call shutdown() on sockets we know to be connected
@@ -187,7 +202,7 @@ def _clean_up_sockets(
             logger.warning(f"Error occurred while shutting down socket on port {port}: {exc}")
 
     # Call close() for all sockets
-    for port, s in possible_ports:
+    for port, s in possible_ports_sockets:
         try:
             s.close()
         except socket.error as exc:
