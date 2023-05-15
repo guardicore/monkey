a7f9c297af5eb786ec65784bacca7860eee2660a
ordabach <odabach@akamai.com>
2023-05-15 11:50:19 +0000

    UI: Refactor masqueradeUtils

--- monkey/monkey_island/cc/ui/src/components/utils/MasqueradeUtils.js
+++ monkey/monkey_island/cc/ui/src/components/utils/MasqueradeUtils.js
@@ -1,61 +1,51 @@
+import {cloneDeep} from 'lodash';
+
 const MASQUERADE_TEXTS_PREFIX = 'InfectionMonkeyTextsMasquePrefix\0';
-const MASQUERADE_BYTES_PREFIX = 'InfectionMonkeyBase64MasquePrefix\0';
+const MASQUERADE_BASE64_PREFIX = 'InfectionMonkeyBase64MasquePrefix\0';
 const ENCODING = 'utf-8';
 const NULL_BYTE = '\0';
 const OS_TYPES = ['linux', 'windows'];
-const MASQUE_TYPES = ['masque_texts', 'masque_base64']
 
-export const MASQUERADE_TYPE_PREFIX = {
-  TEXTS: MASQUERADE_TEXTS_PREFIX,
-  BASE64: MASQUERADE_BYTES_PREFIX
+export const MASQUE_TYPES = {
+  TEXTS: {key: 'masque_texts', prefix: MASQUERADE_TEXTS_PREFIX},
+  BASE64: {key: 'masque_base64', prefix: MASQUERADE_BASE64_PREFIX}
 }
 
-// TODO: change
-export function transformStringsToBytes(stringsArray, masqueTypePrefix) {
+export function transformStringsToBytes(stringsArray, masquePrefix) {
   if (stringsArray.length > 0) {
-    const encoder = new TextEncoder(ENCODING);
-
     let bytes = stringsArray
-      .map(str => str ? encoder.encode((masqueTypePrefix === MASQUERADE_TYPE_PREFIX.BASE64 ? base64ToText(str) : str) + NULL_BYTE) : [])
+      .map(str => str ? encodeString((masquePrefix === MASQUE_TYPES.BASE64.prefix ? base64ToText(str) : str) + NULL_BYTE) : [])
       .reduce((accumulator, currentString) => [...accumulator, ...currentString], []);
 
-    let prefixBytes = encoder.encode(masqueTypePrefix);
+    let prefixBytes = encodeString(masquePrefix);
     return new Uint8Array([...prefixBytes, ...bytes]);
   }
 
   return [];
 }
 
-// TODO: change -
-export function getStringsFromBytes(bytesArray, masqueTypePrefix) {
-  const encoder = new TextEncoder(ENCODING);
-  const decoder = new TextDecoder(ENCODING);
-  const prefixBytes = encoder.encode(masqueTypePrefix);
-  const prefixIndex = getPrefixIndexFromBytesArray(bytesArray, prefixBytes);
+export function getStringsFromBytes(bytesArray, masquePrefix, masqueDetails) {
+  if (bytesArray && masqueDetails) {
+    const dataViewArray = new DataView(bytesArray, masqueDetails.offsetIndex + masqueDetails.prefixLength, masqueDetails.length);
+    const stringsBytes = decodeBytes(dataViewArray);
+    let stringsArray = stringsBytes.split(NULL_BYTE);
 
-  if (prefixIndex === -1) {
-    return [];
+    if (masquePrefix === MASQUE_TYPES.BASE64.prefix) {
+      stringsArray = stringsArray.map(str => {
+        return textToBase64(str);
+      })
+    }
+    return stringsArray.filter(str => str !== '');
   }
 
-  const dataViewArray = new DataView(bytesArray, prefixIndex + prefixBytes.length);
-  const stringsBytes = decoder.decode(dataViewArray);
-  let stringsArray = stringsBytes.split(NULL_BYTE);
-
-  if (masqueTypePrefix === MASQUERADE_TYPE_PREFIX.BASE64) {
-    stringsArray = stringsArray.map(str => {
-      return textToBase64(str);
-    })
-  }
-  return stringsArray.filter(str => str !== '');
+  return [];
 }
 
 function getPrefixIndexFromBytesArray(bytesArray, prefix) {
-  // const uint8Array = new Uint8Array(bytesArray);
-  console.log(bytesArray);
-  console.log(bytesArray);
-  return bytesArray.findIndex((_value, index) => {
+  const uint8Array = new Uint8Array(bytesArray);
+  return uint8Array.findIndex((_value, index) => {
     for (let i = 0; i < prefix.length; i++) {
-      if (bytesArray[index + i] !== prefix[i]) {
+      if (uint8Array[index + i] !== prefix[i]) {
         return false;
       }
     }
@@ -69,17 +59,11 @@ const base64ToText = (base64) => {
     .split('')
     .map((char) => char.charCodeAt(0));
 
-  // Create a new TextDecoder object to decode the byte array to a string
-  const decoder = new TextDecoder('utf-8');
-  return decoder.decode(new Uint8Array(bytes));
+  return decodeBytes(new Uint8Array(bytes));
 }
 
-
-// TODO: The server should return a base64 string
 const textToBase64 = (text) => {
-  const encoder = new TextEncoder(ENCODING);
-
-  const uint8Array = encoder.encode(text);
+  const uint8Array = encodeString(text);
 
   // Convert the Uint8Array to a base64-encoded string
   const base64String = btoa(String.fromCharCode(...uint8Array));
@@ -88,28 +72,83 @@ const textToBase64 = (text) => {
 }
 
 export const getMasqueradesBytesArrays = (masqueStrings) => {
-  let linuxMasqueBytes = [], windowsMasqueBytes = [];
+  let linuxMasqueBytes = new Uint8Array([]), windowsMasqueBytes = new Uint8Array([]);
 
   OS_TYPES.forEach(osType => {
     let bytesArray = []
 
-    MASQUE_TYPES.forEach(masqueType => {
-      let kuku = MASQUERADE_TYPE_PREFIX.TEXTS;
-      if (masqueType === 'masque_texts') {
-        kuku = MASQUERADE_TYPE_PREFIX.TEXTS;
-      } else {
-        kuku = MASQUERADE_TYPE_PREFIX.BASE64;
-      }
-
-      bytesArray = [...bytesArray, ...transformStringsToBytes(masqueStrings?.[osType]?.[masqueType], kuku)]
-    })
+    Object.values(MASQUE_TYPES).forEach(({key, prefix}) => {
+      bytesArray = [...bytesArray, ...transformStringsToBytes(masqueStrings?.[osType]?.[key], prefix)]
+    });
 
     if (osType === 'linux') {
-      linuxMasqueBytes = [...bytesArray];
+      linuxMasqueBytes = new Uint8Array([...bytesArray]);
     } else if (osType === 'windows') {
-      windowsMasqueBytes = [...bytesArray];
+      windowsMasqueBytes = new Uint8Array([...bytesArray]);
     }
   })
 
   return {linuxMasqueBytes, windowsMasqueBytes};
+}
+
+export const getMasqueradeBytesSubsets = (bytesArray) => {
+  let subsetsDetails = {};
+  const defaultValues = {offsetIndex: null, prefixLength: null, length: null};
+
+  const masqueTypesValues = Object.values(MASQUE_TYPES);
+
+  masqueTypesValues.forEach(({key, _prefix}) => {
+    subsetsDetails[key] = {...defaultValues};
+  });
+
+  masqueTypesValues.forEach(({key, prefix}) => {
+    const prefixBytes = encodeString(prefix);
+    let offsetIndex = getPrefixIndexFromBytesArray(bytesArray, prefixBytes);
+
+    if (offsetIndex !== -1) {
+      subsetsDetails[key].offsetIndex = offsetIndex;
+      subsetsDetails[key].prefixLength = prefix.length;
+    }
+  });
+
+  subsetsDetails = filterOutSubsetWithNullOffsetIndex(subsetsDetails);
+  subsetsDetails = calculateMasquesSubsetsLengths(subsetsDetails, bytesArray.byteLength)
+  return subsetsDetails;
+}
+
+const filterOutSubsetWithNullOffsetIndex = (subsetsDetails) => {
+  const filteredSubsets = {};
+  for (const key in subsetsDetails) {
+    if (subsetsDetails[key].offsetIndex !== null) {
+      filteredSubsets[key] = subsetsDetails[key];
+    }
+  }
+  return filteredSubsets;
+}
+
+const calculateMasquesSubsetsLengths = (subsetsDetails, bytesArrayLength) => {
+  const subsetsDetailsToReturn = cloneDeep(subsetsDetails);
+  const keys = Object.keys(subsetsDetailsToReturn).sort((a, b) => subsetsDetailsToReturn[a].offsetIndex - subsetsDetailsToReturn[b].offsetIndex);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const obj = subsetsDetailsToReturn[key];
+    const subsetOffset = obj.offsetIndex + obj.prefixLength;
+    if (i < keys.length - 1) {
+      obj.length = Math.abs(subsetOffset - subsetsDetailsToReturn[keys[i + 1]].offsetIndex);
+    } else {
+      obj.length = bytesArrayLength - subsetOffset;
+    }
+  }
+  return subsetsDetailsToReturn;
+};
+
+
+const encodeString = str => {
+   const encoder = new TextEncoder(ENCODING);
+   return encoder.encode(str);
+}
+
+const decodeBytes = bytes => {
+   const decoder = new TextDecoder(ENCODING);
+   return decoder.decode(bytes);
 }
