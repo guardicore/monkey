3a153a7222190130691d047ba7ff32aff042973f
Shreya Malviya <shreya.malviya@gmail.com>
2023-05-16 17:51:01 +0530

    UI: Update PolymorphismConfiguration description and title

--- monkey/monkey_island/cc/ui/src/services/configuration/polymorphism.js
+++ monkey/monkey_island/cc/ui/src/services/configuration/polymorphism.js
@@ -2,11 +2,11 @@ const POLYMORPHISM_SCHEMA = {
   'title': 'Polymorphism',
   'properties': {
     'randomized_agent_hash': {
-      'title': 'Emulate polymorphism',
+      'title': 'Randomize Agent hash',
       'type': 'boolean',
       'default': false,
-      'description': 'Emulate polymorphic (or metamorphic) malware by ' +
-                     'modifying the Agent binary before propagation.'
+      'description': 'Emulate the property of polymorphic (or metamorphic) malware that all ' +
+                     'copies have unique hashes by modifying the Agent binary before propagation.'
     }
   }
 }
