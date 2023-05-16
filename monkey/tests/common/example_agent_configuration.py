b7c4a7da1afa37bbc7a4c1643fe6eb2971b3f0b9
Shreya Malviya <shreya.malviya@gmail.com>
2023-05-16 13:42:10 +0530

    UT: Add polymorphism to example Agent configuration

--- monkey/tests/common/example_agent_configuration.py
+++ monkey/tests/common/example_agent_configuration.py
@@ -46,9 +46,12 @@ CREDENTIALS_COLLECTORS: Dict[str, Dict] = {
     "credentials_collectors": {"SSHCollector": {}, "MimikatzCollector": {}}
 }
 
+POLYMORPHISM_CONFIGURATION = {"enabled": False}
+
 AGENT_CONFIGURATION = {
     "keep_tunnel_open_time": 30,
     "credentials_collectors": CREDENTIALS_COLLECTORS,
     "payloads": {},
     "propagation": PROPAGATION_CONFIGURATION,
+    "polymorphism": POLYMORPHISM_CONFIGURATION,
 }
