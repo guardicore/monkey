fc1ffe902058171e02df831adfa7278c6e14a3c0
Shreya Malviya <shreya.malviya@gmail.com>
2023-05-17 18:16:09 +0530

    Common: Change EmailAddress.email_address type from str to EmailStr

--- monkey/common/credentials/email_address.py
+++ monkey/common/credentials/email_address.py
@@ -1,8 +1,10 @@
+from pydantic import EmailStr
+
 from ..base_models import InfectionMonkeyBaseModel
 
 
 class EmailAddress(InfectionMonkeyBaseModel):
-    email_address: str
+    email_address: EmailStr
 
     def __hash__(self) -> int:
         return hash(self.email_address)
