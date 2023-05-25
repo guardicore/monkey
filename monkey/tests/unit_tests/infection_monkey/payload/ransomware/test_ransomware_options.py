4f148e665ece8773d2f75753d7a3ff78518be147
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-05-25 09:39:12 -0400

    Agent: Add algorithm option to RansomwareOptions

--- monkey/tests/unit_tests/infection_monkey/payload/ransomware/test_ransomware_options.py
+++ monkey/tests/unit_tests/infection_monkey/payload/ransomware/test_ransomware_options.py
@@ -5,11 +5,15 @@ from tests.utils import raise_
 
 from common.utils.file_utils import InvalidPath
 from infection_monkey.payload.ransomware import ransomware_options
-from infection_monkey.payload.ransomware.ransomware_options import RansomwareOptions
+from infection_monkey.payload.ransomware.ransomware_options import (
+    EncryptionAlgorithm,
+    RansomwareOptions,
+)
 
 EXTENSION = ".testext"
 LINUX_DIR = "/tmp/test"
 WINDOWS_DIR = "C:\\tmp\\test"
+ALGORITHM = str(EncryptionAlgorithm.BIT_FLIP)
 
 
 @pytest.fixture
@@ -22,6 +26,7 @@ def options_from_island():
                 "linux_target_dir": LINUX_DIR,
                 "windows_target_dir": WINDOWS_DIR,
             },
+            "algorithm": ALGORITHM,
         },
         "other_behaviors": {"readme": None},
     }
@@ -96,3 +101,19 @@ def test_target_dir_is_none(monkeypatch, options_from_island):
 
     options = RansomwareOptions(options_from_island)
     assert options.target_directory is None
+
+
+@pytest.mark.parametrize("algorithm", [a for a in EncryptionAlgorithm])
+def test_valid_encryption_algorithms(options_from_island, algorithm: EncryptionAlgorithm):
+    options_from_island["encryption"]["algorithm"] = algorithm
+
+    options = RansomwareOptions(options_from_island)
+
+    assert options.algorithm == algorithm
+
+
+def test_invalid_encryption_algorithms(options_from_island):
+    options_from_island["encryption"]["algorithm"] = "invalid_algorithm"
+
+    with pytest.raises(ValueError):
+        RansomwareOptions(options_from_island)
