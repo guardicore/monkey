21eade7c9409152093dcdef88d912332caa9606d
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-07-13 14:10:50 +0000

    Agent: Allow a transform function to be applied to agent binary
    Allows users of HTTPAgentBinaryServer to provide a transform function
    upon registration that will alter the agent binary that is downloaded
    for that reservation.

--- monkey/infection_monkey/exploit/i_http_agent_binary_server_registrar.py
+++ monkey/infection_monkey/exploit/i_http_agent_binary_server_registrar.py
@@ -1,11 +1,9 @@
 import abc
 from ipaddress import IPv4Address
-from pathlib import PurePath
-from typing import Optional, Sequence
 
 from common import OperatingSystem
 
-from .http_agent_binary_server import AgentBinaryDownloadTicket, RequestID, RequestType
+from .http_agent_binary_server import AgentBinaryDownloadTicket, AgentBinaryTransform, RequestID
 
 
 class IHTTPAgentBinaryServerRegistrar(metaclass=abc.ABCMeta):
@@ -13,21 +11,15 @@ class IHTTPAgentBinaryServerRegistrar(metaclass=abc.ABCMeta):
     def register_request(
         self,
         operating_system: OperatingSystem,
-        request_type: RequestType,
         requestor_ip: IPv4Address,
-        destination_path: Optional[PurePath],
-        args: Sequence[str],
+        agent_binary_transform: AgentBinaryTransform,
     ) -> AgentBinaryDownloadTicket:
         """
         Registers an Agent to be served with HTTPAgentBinaryServer
 
         :param operating_system: The operating system for the Agent binary to serve
-        :param request_type: The type of request to serve
         :param requestor_ip: The IP address of the client that will download the Agent binary
-        :param destination_path: The destination path into which to drop the Agent binary. This
-            only applies to the dropper script request type
-        :param args: The arguments to pass to the Agent binary. This only applies to the dropper
-            script request type
+        :param agent_binary_transform: A function that transforms the Agent binary before serving
         :raises RuntimeError: If the binary could not be served
         :returns: A ticket to download the Agent binary
         """
