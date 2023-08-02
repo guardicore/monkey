8447b0f8735c0630c0fcce18e8d818e5279397e5
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-02 13:23:31 +0530

    Common: Add AgentPluginMetadata

---
+++ monkey/common/agent_plugins/agent_plugin_metadata.py
@@ -0,0 +1,31 @@
+from pathlib import PurePosixPath
+
+from pydantic import Field
+from semver import VersionInfo
+
+from common.base_models import InfectionMonkeyBaseModel
+
+from . import AgentPluginType
+
+
+class AgentPluginMetadata(InfectionMonkeyBaseModel):
+    """
+    Class for an Agent plugin's metadata
+
+    Attributes:
+        :param name: Plugin name
+        :param type: Plugin type
+        :param resource_path: Path of the plugin file in the AWS S3 bucket
+        :param sha256: Plugin file checksum
+        :param description: Plugin description
+        :param version: Plugin version
+        :param safe: Whether the plugin is safe for use in production environments
+    """
+
+    name: str
+    type_: AgentPluginType
+    resource_path: PurePosixPath
+    sha256: str = Field(regex=r"^[0-9a-fA-F]{64}$")
+    description: str
+    version: VersionInfo
+    safe: bool
