e6497fd6b78d477b12ddeff600ae4ebf997d98d2
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-07 14:21:10 +0530

    Cryptojacker: Add cryptojacker builder

---
+++ monkey/agent_plugins/payloads/cryptojacker/src/cryptojacker_builder.py
@@ -0,0 +1,48 @@
+import logging
+from pprint import pformat
+
+from common.event_queue import IAgentEventQueue
+from common.types import AgentID, PercentLimited, SocketAddress
+
+from .bitcoin_mining_network_traffic_simulator import BitcoinMiningNetworkTrafficSimulator
+from .cpu_utilizer import CPUUtilizer
+from .cryptojacker import Cryptojacker
+from .cryptojacker_options import CryptojackerOptions
+from .memory_utilizer import MemoryUtilizer
+
+logger = logging.getLogger(__name__)
+
+
+def build_cryptojacker(
+    agent_id: AgentID,
+    agent_event_queue: IAgentEventQueue,
+    options: CryptojackerOptions,
+    island_server_address: SocketAddress,
+):
+    logger.debug(f"Cryptojacker configuration:\n{pformat(options)}")
+
+    cpu_utilizer = _build_cpu_utilizer(options.cpu_utilization)
+    memory_utilizer = _build_memory_utilizer(options.memory_utilization)
+    bitcoin_mining_network_traffic_simulator = _build_bitcoin_mining_network_traffic_simulator()
+
+    return Cryptojacker(
+        options=options,
+        utilize_cpu=cpu_utilizer,
+        utilize_memory=memory_utilizer,
+        simulate_bitcoin_mining_network_traffic=bitcoin_mining_network_traffic_simulator,
+        agent_event_queue=agent_event_queue,
+        agent_id=agent_id,
+        island_server_address=island_server_address,
+    )
+
+
+def _build_cpu_utilizer(cpu_utilization: PercentLimited):
+    return CPUUtilizer(cpu_utilization)
+
+
+def _build_memory_utilizer(memory_utilization: PercentLimited):
+    return MemoryUtilizer(memory_utilization)
+
+
+def _build_bitcoin_mining_network_traffic_simulator():
+    return BitcoinMiningNetworkTrafficSimulator()
