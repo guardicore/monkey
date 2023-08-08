2a1be0f4913749510d6dbbccb35937085ddbde54
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-08 12:42:34 +0530

    Cryptojacker: Add CPUUtilizer skeleton

--- monkey/agent_plugins/payloads/cryptojacker/src/cpu_utilizer.py
+++ monkey/agent_plugins/payloads/cryptojacker/src/cpu_utilizer.py
@@ -1,22 +1,34 @@
+import logging
 from typing import Optional
 
-from common.event_queue import IAgentEventPublisher
-from common.types import AgentID, PercentLimited
+from common.types import PercentLimited
+from infection_monkey.utils.threading import create_daemon_thread
+
+from .cryptojacker import THREAD_JOIN_TIMEOUT
+
+logger = logging.getLogger(__name__)
 
 
 class CPUUtilizer:
-    def __init__(
-        self,
-        cpu_utilization: PercentLimited,
-        agent_id: AgentID,
-        agent_event_publisher: IAgentEventPublisher,
-    ):
-        self._cpu_utilization = cpu_utilization
-        self._agent_id = agent_id
-        self._agent_event_publisher = agent_event_publisher
+    def __init__(self, cpu_utilization: PercentLimited):
+        self._cpu_utilizer_thread = create_daemon_thread(
+            target=self._utilize_cpu,
+            name="CPUUtilizationThread",
+            args=(cpu_utilization,),
+        )
 
     def start(self):
+        logger.info("Utilizing CPU")
+        self._cpu_utilizer_thread.start()
+
+    def _utilize_cpu(self, cpu_utilization: PercentLimited):
         pass
 
     def stop(self, timeout: Optional[float] = None):
-        pass
+        logger.info("Stopping CPU utilization")
+        self._cpu_utilizer_thread.join(THREAD_JOIN_TIMEOUT)
+        if self._cpu_utilizer_thread.is_alive():
+            logger.info(
+                "Timed out while waiting for CPU utilization thread to stop, "
+                "it will be stopped forcefully when the parent process terminates"
+            )
