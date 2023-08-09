f5ed246d0a5417955455a2cd7a7b40c2a5f6f6cb
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-09 13:35:24 +0530

    Cryptojacker: Implement Bitcoin mining network traffic simulator

--- monkey/agent_plugins/payloads/cryptojacker/src/bitcoin_mining_network_traffic_simulator.py
+++ monkey/agent_plugins/payloads/cryptojacker/src/bitcoin_mining_network_traffic_simulator.py
@@ -1,7 +1,21 @@
+import base64
+import json
+import logging
+import random
+import urllib.request
 from typing import Optional
+
+import urllib3
 
 from common.event_queue import IAgentEventPublisher
 from common.types import AgentID, SocketAddress
+from common.utils.code_utils import PeriodicCaller
+from infection_monkey.island_api_client.http_client import handle_island_errors
+
+REQUEST_INTERVAL = 10  # seconds
+
+
+logger = logging.getLogger(__name__)
 
 
 class BitcoinMiningNetworkTrafficSimulator:
@@ -15,8 +29,37 @@ class BitcoinMiningNetworkTrafficSimulator:
         self._agent_id = agent_id
         self._agent_event_publisher = agent_event_publisher
 
+        self._send_bitcoin_mining_request_periodically = PeriodicCaller(
+            callback=self._send_bitcoin_mining_request,
+            period=REQUEST_INTERVAL,
+            name="Cryptojacking.BitcoinMiningNetworkTrafficSimulator",
+        )
+
     def start(self):
-        pass
+        logger.info("Starting Bitcoin mining network traffic simulator")
+
+        self._send_bitcoin_mining_request_periodically.start()
+
+    @handle_island_errors
+    def _send_bitcoin_mining_request(self):
+        id_ = random.getrandbits(32)  # noqa: DUO102 (this isn't for cryptographic use)
+        method = "getblocktemplate"
+        params = [{"rules": ["segwit"]}]
+        user = "bitcoin-user"
+        password = "bitcoin-password"
+
+        url = self._island_server_address
+        data = json.dumps({"id": id_, "method": method, "params": params}).encode()
+        auth = base64.encodebytes((user + ":" + password).encode()).decode().strip()
+
+        logger.info(f"Sending Bitcoin mining request to {url}")
+
+        request = urllib.request.Request(
+            url=url, data=data, headers={"Authorization": "Basic {:s}".format(auth)}
+        )
+        urllib3.request.urlopen(request)
 
     def stop(self, timeout: Optional[float] = None):
-        pass
+        logger.info("Stopping Bitcoin mining network traffic simulator")
+
+        self._send_bitcoin_mining_request_periodically.stop(timeout)
