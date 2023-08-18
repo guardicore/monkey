1a8b83a0993d879e8bfa471bbb8917013f2d5a75
Ilija Lazoroski <ilija.la@live.com>
2023-08-18 17:13:05 +0000

    Island: Add `AgentPluginInstallation` endpoint

---
+++ monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/agent_plugin_installation.py
@@ -0,0 +1,32 @@
+import logging
+from http import HTTPStatus
+
+from flask import make_response, request
+from flask_security import auth_token_required, roles_accepted
+
+from monkey_island.cc.flask_utils import AbstractResource
+from monkey_island.cc.services.authentication_service import AccountRole
+
+from .. import IAgentPluginService
+from ..errors import PluginInstallError
+
+logger = logging.getLogger(__name__)
+
+
+class AgentPluginInstallation(AbstractResource):
+    urls = ["/api/agent-plugins"]
+
+    def __init__(self, agent_plugin_service: IAgentPluginService):
+        self._agent_plugin_service = agent_plugin_service
+
+    @auth_token_required
+    @roles_accepted(AccountRole.ISLAND_INTERFACE.name)
+    def put(self):
+        """
+        Install the plugin archive.
+        """
+        try:
+            self._agent_plugin_service.install_agent_plugin_archive(request.data)
+            return make_response({}, HTTPStatus.OK)
+        except PluginInstallError as err:
+            return make_response(str(err), HTTPStatus.UNPROCESSABLE_ENTITY)
--- monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/register.py
+++ monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/register.py
@@ -1,5 +1,6 @@
 from monkey_island.cc.flask_utils import FlaskDIWrapper
 
+from .agent_plugin_installation import AgentPluginInstallation
 from .agent_plugins import AgentPlugins
 from .agent_plugins_manifest import AgentPluginsManifest
 
@@ -7,3 +8,4 @@ from .agent_plugins_manifest import AgentPluginsManifest
 def register_resources(api: FlaskDIWrapper):
     api.add_resource(AgentPlugins)
     api.add_resource(AgentPluginsManifest)
+    api.add_resource(AgentPluginInstallation)
