c32ec58bf4010b1f2de344434acb19e087ae7003
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-21 19:35:22 +0530

    Island: Add force_refresh parameter to AvailableAgentPluginsIndex GET

--- monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/available_agent_plugins_index.py
+++ monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/available_agent_plugins_index.py
@@ -1,7 +1,7 @@
 import logging
 from http import HTTPStatus
 
-from flask import make_response
+from flask import make_response, request
 from flask_security import auth_token_required, roles_accepted
 
 from monkey_island.cc.flask_utils import AbstractResource
@@ -25,6 +25,20 @@ class AvailableAgentPluginsIndex(AbstractResource):
         Get the index of available plugins
         """
 
-        available_plugins = self._agent_plugin_service.get_available_plugins()
+        force_refresh_arg = request.args.get("force_refresh", "false")
+        if force_refresh_arg == "true":
+            force_refresh = True
+        elif force_refresh_arg == "false":
+            force_refresh = False
+        else:
+            err = (
+                f'Invalid value for force_refresh "{force_refresh_arg}", expected "true" or "false"'
+            )
+            logger.error(err)
+            return {"error": str(err)}, HTTPStatus.UNPROCESSABLE_ENTITY
+
+        available_plugins = self._agent_plugin_service.get_available_plugins(
+            force_refresh=force_refresh
+        )
 
         return make_response(available_plugins.dict(simplify=True), HTTPStatus.OK)
