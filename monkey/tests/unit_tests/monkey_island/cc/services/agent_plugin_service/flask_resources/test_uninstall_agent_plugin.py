4b4b5ed27d991d91cc33623431fb508d3cedc170
Ilija Lazoroski <ilija.la@live.com>
2023-08-22 13:09:21 +0000

    UT: Use mocked IAgentPluginService in UninstallAgentPlugin tests

--- monkey/tests/unit_tests/monkey_island/cc/services/agent_plugin_service/flask_resources/test_uninstall_agent_plugin.py
+++ monkey/tests/unit_tests/monkey_island/cc/services/agent_plugin_service/flask_resources/test_uninstall_agent_plugin.py
@@ -1,27 +1,21 @@
 from http import HTTPStatus
+from unittest.mock import MagicMock
 
 import pytest
 from tests.common import StubDIContainer
-from tests.monkey_island import InMemoryAgentPluginRepository
 from tests.unit_tests.common.agent_plugins.test_agent_plugin_manifest import FAKE_TYPE
 from tests.unit_tests.monkey_island.cc.fake_agent_plugin_data import FAKE_AGENT_PLUGIN_1
 from tests.unit_tests.monkey_island.conftest import get_url_for_resource
 
-from common import OperatingSystem
-from monkey_island.cc.repositories import RemovalError, UnknownRecordError
+from common.agent_plugins import AgentPluginType
+from monkey_island.cc.repositories import RemovalError
 from monkey_island.cc.services.agent_plugin_service import IAgentPluginService
-from monkey_island.cc.services.agent_plugin_service.agent_plugin_service import AgentPluginService
 from monkey_island.cc.services.agent_plugin_service.flask_resources import UninstallAgentPlugin
 
 
 @pytest.fixture
-def agent_plugin_repository():
-    return InMemoryAgentPluginRepository()
-
-
-@pytest.fixture
-def agent_plugin_service(agent_plugin_repository):
-    return AgentPluginService(agent_plugin_repository)
+def agent_plugin_service():
+    return MagicMock(spec=IAgentPluginService)
 
 
 @pytest.fixture
@@ -33,9 +27,7 @@ def flask_client(build_flask_client, agent_plugin_service):
         yield flask_client
 
 
-def test_uninstall_agent_plugin(flask_client, agent_plugin_repository):
-    agent_plugin_repository.store_agent_plugin(OperatingSystem.LINUX, FAKE_AGENT_PLUGIN_1)
-
+def test_uninstall_agent_plugin(flask_client, agent_plugin_service):
     resp = flask_client.post(
         get_url_for_resource(
             UninstallAgentPlugin,
@@ -45,10 +37,9 @@ def test_uninstall_agent_plugin(flask_client, agent_plugin_repository):
     )
 
     assert resp.status_code == HTTPStatus.OK
-    with pytest.raises(UnknownRecordError):
-        agent_plugin_repository.get_plugin(
-            OperatingSystem.LINUX, FAKE_TYPE, FAKE_AGENT_PLUGIN_1.plugin_manifest.name
-        )
+    agent_plugin_service.uninstall_agent_plugin.assert_called_with(
+        AgentPluginType(FAKE_TYPE), FAKE_AGENT_PLUGIN_1.plugin_manifest.name
+    )
 
 
 # TODO: Ask do we need some kind of UninstallAgentPluginError which will be handled
@@ -65,19 +56,22 @@ def test_uninstall_agent_plugin(flask_client, agent_plugin_repository):
     "type_",
     ["DummyType", "ExploiteR"],
 )
-def test_uninstall_agent_plugin__not_found_if_type_is_invalid(flask_client, type_):
+def test_uninstall_agent_plugin__not_found_if_type_is_invalid(
+    flask_client, type_, agent_plugin_service
+):
     resp = flask_client.post(
         get_url_for_resource(UninstallAgentPlugin, plugin_type=type_, name="name")
     )
 
     assert resp.status_code == HTTPStatus.NOT_FOUND
+    agent_plugin_service.uninstall_agent_plugin.assert_not_called()
 
 
-def test_uninstall_agent_plugin__server_error(flask_client, agent_plugin_repository):
+def test_uninstall_agent_plugin__server_error(flask_client, agent_plugin_service):
     def raise_removal_error(plugin_type, name):
         raise RemovalError
 
-    agent_plugin_repository.remove_agent_plugin = raise_removal_error
+    agent_plugin_service.uninstall_agent_plugin = raise_removal_error
 
     resp = flask_client.post(
         get_url_for_resource(UninstallAgentPlugin, plugin_type="Payload", name="name")
