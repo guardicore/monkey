039387f811dbc56b45fb71f314bf377a9c9ac767
Shreya Malviya <shreya.malviya@gmail.com>
2023-08-22 12:52:33 +0530

    Island: Build AgentPluginService with MongoAgentPluginRepository

--- monkey/monkey_island/cc/services/agent_plugin_service/build.py
+++ monkey/monkey_island/cc/services/agent_plugin_service/build.py
@@ -1,15 +1,14 @@
 from common import DIContainer
 
-from .agent_plugin_repository_caching_decorator import AgentPluginRepositoryCachingDecorator
 from .agent_plugin_repository_logging_decorator import AgentPluginRepositoryLoggingDecorator
 from .agent_plugin_service import AgentPluginService
-from .file_agent_plugin_repository import FileAgentPluginRepository
 from .i_agent_plugin_repository import IAgentPluginRepository
 from .i_agent_plugin_service import IAgentPluginService
+from .mongo_agent_plugin_repository import MongoAgentPluginRepository
 
 
 def build(container: DIContainer) -> IAgentPluginService:
-    undecorated_agent_plugin_repository = container.resolve(FileAgentPluginRepository)
+    undecorated_agent_plugin_repository = container.resolve(MongoAgentPluginRepository)
     agent_plugin_repository = _decorate_agent_plugin_repository(undecorated_agent_plugin_repository)
     agent_plugin_service = AgentPluginService(agent_plugin_repository)
     container.register_instance(IAgentPluginService, agent_plugin_service)
@@ -20,6 +19,4 @@ def build(container: DIContainer) -> IAgentPluginService:
 def _decorate_agent_plugin_repository(
     plugin_repository: IAgentPluginRepository,
 ) -> IAgentPluginRepository:
-    return AgentPluginRepositoryLoggingDecorator(
-        AgentPluginRepositoryCachingDecorator(plugin_repository)
-    )
+    return AgentPluginRepositoryLoggingDecorator(plugin_repository)
