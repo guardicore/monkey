aebf8097aa614b54580e210f354cf916080a3d9d
Ilija Lazoroski <ilija.la@live.com>
2023-08-23 11:12:09 +0000

    Island: Add ability to install agent plugin from repository

--- monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/install_agent_plugin.py
+++ monkey/monkey_island/cc/services/agent_plugin_service/flask_resources/install_agent_plugin.py
@@ -1,10 +1,12 @@
+import json
 import logging
 from http import HTTPStatus
 
 from flask import make_response, request
 from flask_security import auth_token_required, roles_accepted
 
-from monkey_island.cc.flask_utils import AbstractResource
+from common.agent_plugins import AgentPluginType, PluginName, PluginVersion
+from monkey_island.cc.flask_utils import AbstractResource, responses
 from monkey_island.cc.services.authentication_service import AccountRole
 
 from .. import IAgentPluginService
@@ -26,7 +28,32 @@ class InstallAgentPlugin(AbstractResource):
         Install the plugin archive.
         """
         try:
-            self._agent_plugin_service.install_plugin_archive(request.data)
+            content_type = request.headers.get("Content-Type", "").lower()
+            if content_type == "application/json":
+                try:
+                    self._handle_json_request()
+                except ValueError:
+                    message = "Invalid plugin type."
+                    logger.warning(message)
+                    return responses.make_response_to_invalid_request(message)
+                except Exception:
+                    return responses.make_response_to_invalid_request()
+            else:
+                self._agent_plugin_service.install_plugin_archive(request.data)
             return make_response({}, HTTPStatus.OK)
         except PluginInstallationError as err:
             return make_response(str(err), HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    def _handle_json_request(self):
+        response_json = json.loads(request.data)
+        plugin_type = response_json["plugin_type"]
+        name = response_json["name"]
+        version = response_json["version"]
+
+        plugin_type_ = AgentPluginType(plugin_type)
+        plugin_name = PluginName(name)
+        plugin_version = PluginVersion(version)
+
+        self._agent_plugin_service.install_plugin_from_repository(
+            plugin_type=plugin_type_, plugin_name=plugin_name, plugin_version=plugin_version
+        )
