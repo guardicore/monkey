a8256e2f15861a31285337ccf1375967f0fd0968
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-08-25 16:21:39 +0000

    UI: Display available plugins

--- monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/BasePlugins.js
+++ monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/BasePlugins.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {Box} from '@mui/material';
 import XDataGrid from '../XDataGrid';
-import {getPlugins, getPluginsGridHeaders, getPluginsGridRows} from './mocksHelper';
+import {getPluginsGridHeaders, getPluginsGridRows} from './mocksHelper';
 import styles from '../../../styles/components/plugins-marketplace/BasePlugins.module.scss';
 
 const DEFAULT_LOADING_MESSAGE = 'Loading plugins...';
@@ -12,36 +12,21 @@ const initialState = {
 };
 
 const BasePlugins = (props) => {
-  const {plugins, setPlugins, getRowActions, onRefreshCallback, loadingMessage = DEFAULT_LOADING_MESSAGE} = {...props};
+  const {plugins, getRowActions, onRefreshCallback, loadingMessage = DEFAULT_LOADING_MESSAGE} = {...props};
 
   const [isLoadingPlugins, setIsLoadingPlugins] = useState(false);
-
-  useEffect(() => {
-    setPluginsList();
-  }, []);
 
   const rows = useMemo(() => {
     return getPluginsGridRows(plugins);
   }, [plugins]);
 
-  const setPluginsList = () => {
-    if(plugins.length === 0) {
-      setIsLoadingPlugins(true);
-      getPlugins(false, false, true).then(plugins => {
-        setPlugins(plugins);
-      }).finally(() => {
-        setIsLoadingPlugins(false);
-      });
-    }
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const onRefreshClick = () => {
-    setPluginsList();
-    if(onRefreshCallback) {
-      onRefreshCallback();
-    }
-  }
+  // // eslint-disable-next-line no-unused-vars
+  // const onRefreshClick = () => {
+  //   setPluginsList();
+  //   if(onRefreshCallback) {
+  //     onRefreshCallback();
+  //   }
+  // }
 
   return (
     <Box className={styles['plugins-wrapper']}>
