4478d033670bf065219d0de1d4968ffff88df3dc
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-08-25 16:21:39 +0000

    UI: Display available plugins

--- monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/AvailablePlugins.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/AvailablePlugins.tsx
@@ -11,8 +11,40 @@ import BasePlugins from './BasePlugins';
 import SearchBar from '../SearchBar';
 import {Box} from '@mui/material';
 
+// Provides the plugins, filtering out the installed plugins
+class AvailablePluginsView {
+  availablePlugins = []; // all plugins, grouped by type then name
+  installedPlugins = []; // all installed plugins, grouped by type then name
+
+  constructor(availablePlugins, installedPlugins) {
+    this.availablePlugins = availablePlugins;
+    this.installedPlugins = installedPlugins;
+  }
+
+  pluginInstalled(plugin_type, plugin_name) {
+    return plugin_type in this.installedPlugins && plugin_name in this.installedPlugins[plugin_type];
+  }
+
+  * makeAvailablePluginsIterator() {
+    for (const plugin_type in this.availablePlugins) {
+      for (const plugin_name in this.availablePlugins[plugin_type]) {
+        if (!this.pluginInstalled(plugin_type, plugin_name)) {
+          // There may be multiple versions of the same plugin, so we only want the latest one
+          yield this.availablePlugins[plugin_type][plugin_name].slice(-1)[0];
+        }
+      }
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this.makeAvailablePluginsIterator();
+  }
+};
+
 const AvailablePlugins = () => {
-  const {availablePlugins, setAvailablePlugins} = useContext(PluginsContext);
+  const {availablePlugins} = useContext(PluginsContext);
+  const {installedPlugins} = useContext(PluginsContext);
+  const availablePluginsView = new AvailablePluginsView(availablePlugins, installedPlugins);
 
   const [successfullyInstalledPluginsIds, setSuccessfullyInstalledPluginsIds] = useState([]);
   const [pluginsInInstallationProcess, setPluginsInInstallationProcess] = useState([]);
@@ -79,8 +111,7 @@ const AvailablePlugins = () => {
   return (
     <Box>
       <SearchBar />
-      <BasePlugins plugins={availablePlugins}
-                   setPlugins={setAvailablePlugins}
+      <BasePlugins plugins={availablePluginsView}
                    loadingMessage="Loading all available plugins..."
                    onRefreshCallback={onRefreshCallback}
                    getRowActions={getRowActions}
