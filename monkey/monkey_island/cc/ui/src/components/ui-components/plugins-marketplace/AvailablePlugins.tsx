bbce112ffccdb1109aced89ba4c8546daf44c0ec
vakarisz <vakarisz@yahoo.com>
2023-08-28 17:39:31 +0300

    UI: Simplify AvailablePlugins.tsx

--- monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/AvailablePlugins.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/AvailablePlugins.tsx
@@ -1,6 +1,8 @@
-import React, {useContext, useState} from 'react';
-import {installPlugin} from './mocksHelper';
-import {shallowAdditionOfUniqueValueToArray, shallowRemovalOfUniqueValueFromArray} from '../../../utils/objectUtils';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  shallowAdditionOfUniqueValueToArray,
+  shallowRemovalOfUniqueValueFromArray
+} from '../../../utils/objectUtils';
 import {PluginsContext} from './PluginsContext';
 import {GridActionsCellItem} from '@mui/x-data-grid';
 import {nanoid} from 'nanoid';
@@ -11,46 +13,25 @@ import BasePlugins from './BasePlugins';
 import SearchBar from '../SearchBar';
 import {Box} from '@mui/material';
 import AuthComponent from '../../AuthComponent';
+import {generatePluginId, installedPluginsToArray, pluginIndexToArray} from './utils';
 
-// Provides the plugins, filtering out the installed plugins
-class AvailablePluginsView {
-  availablePlugins = []; // all plugins, grouped by type then name
-  installedPlugins = []; // all installed plugins, grouped by type then name
-
-  constructor(availablePlugins, installedPlugins) {
-    this.availablePlugins = availablePlugins;
-    this.installedPlugins = installedPlugins;
-  }
-
-  pluginInstalled(plugin_type, plugin_name) {
-    return plugin_type in this.installedPlugins && plugin_name in this.installedPlugins[plugin_type];
-  }
-
-  * makeAvailablePluginsIterator() {
-    for (const plugin_type in this.availablePlugins) {
-      for (const plugin_name in this.availablePlugins[plugin_type]) {
-        if (!this.pluginInstalled(plugin_type, plugin_name)) {
-          // There may be multiple versions of the same plugin, so we only want the latest one
-          yield this.availablePlugins[plugin_type][plugin_name].slice(-1)[0];
-        }
-      }
-    }
-  }
-
-  [Symbol.iterator]() {
-    return this.makeAvailablePluginsIterator();
-  }
-};
 
 const AvailablePlugins = () => {
   const {availablePlugins} = useContext(PluginsContext);
   const {installedPlugins} = useContext(PluginsContext);
   const {refreshInstalledPlugins} = useContext(PluginsContext);
-  const availablePluginsView = new AvailablePluginsView(availablePlugins, installedPlugins);
+  const [displayedPlugins, setDisplayedPlugins] = useState([]);
 
   const [successfullyInstalledPluginsIds, setSuccessfullyInstalledPluginsIds] = useState([]);
   const [pluginsInInstallationProcess, setPluginsInInstallationProcess] = useState([]);
   const authComponent = new AuthComponent({});
+
+  useEffect(() => {
+    let installedPluginsIds = installedPluginsToArray(installedPlugins).map(generatePluginId);
+    const installedFilter = (plugin) => !installedPluginsIds.includes(generatePluginId(plugin));
+    let shownPlugins = pluginIndexToArray(availablePlugins).filter(installedFilter)
+    setDisplayedPlugins(shownPlugins);
+  }, [installedPlugins, availablePlugins]);
 
   const onRefreshCallback = () => {
     setSuccessfullyInstalledPluginsIds([]);
@@ -121,7 +102,7 @@ const AvailablePlugins = () => {
   return (
     <Box>
       <SearchBar />
-      <BasePlugins plugins={availablePluginsView}
+      <BasePlugins plugins={displayedPlugins}
                    loadingMessage="Loading all available plugins..."
                    onRefreshCallback={onRefreshCallback}
                    getRowActions={getRowActions}
