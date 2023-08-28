95d5e0d582973d2ec3107d7eacaccae65d694d80
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-08-28 19:06:43 +0000

    UI: Add upgrade action

--- monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/InstalledPlugins.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/plugins-marketplace/InstalledPlugins.tsx
@@ -2,12 +2,14 @@ import React, {useContext, useState} from 'react';
 import {PluginsContext} from '../../contexts/plugins/PluginsContext';
 import {shallowAdditionOfUniqueValueToArray, shallowRemovalOfUniqueValueFromArray} from '../../../utils/objectUtils';
 import {GridActionsCellItem} from '@mui/x-data-grid';
-import {nanoid} from 'nanoid';
+import DeleteIcon from '@mui/icons-material/Delete';
 import DownloadingIcon from '@mui/icons-material/Downloading';
 import DownloadDoneIcon from '@mui/icons-material/DownloadDone';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import UpgradeIcon from '@mui/icons-material/Upgrade';
+import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
 import BasePlugins from './BasePlugins';
 import AuthComponent from '../../AuthComponent';
+import semver from 'semver';
 
 class InstalledPluginsView {
   availablePlugins = []; // all plugins, grouped by type then name
@@ -22,7 +24,10 @@ class InstalledPluginsView {
   * makeInstalledPluginsIterator() {
     for (const plugin_type in this.installedPlugins) {
       for (const plugin_name in this.installedPlugins[plugin_type]) {
-        yield this.installedPlugins[plugin_type][plugin_name];
+        const installed_version = this.installedPlugins[plugin_type][plugin_name]['version'];
+        const latest_version = this.availablePlugins[plugin_type][plugin_name].slice(-1)[0]['version'];
+        const update_available = semver.gt(latest_version, installed_version)
+        yield {...this.installedPlugins[plugin_type][plugin_name], update_available};
       }
     }
   }
@@ -36,6 +41,8 @@ const InstalledPlugins = () => {
   const {availablePlugins, installedPlugins, refreshInstalledPlugins} = useContext(PluginsContext);
   const installedPluginsView = new InstalledPluginsView(availablePlugins, installedPlugins);
 
+  const [successfullyInstalledPluginsIds, setSuccessfullyInstalledPluginsIds] = useState([]);
+  const [pluginsInInstallProcess, setPluginsInInstallProcess] = useState([]);
   const [successfullyUninstalledPluginsIds, setSuccessfullyUninstalledPluginsIds] = useState([]);
   const [pluginsInUninstallProcess, setPluginsInUninstallProcess] = useState([]);
   const authComponent = new AuthComponent({});
@@ -70,16 +77,96 @@ const InstalledPlugins = () => {
         return shallowRemovalOfUniqueValueFromArray(prevState, pluginId);
       }));
     });
-  };
+  }
 
-  const getRowActions = (row) => {
+  const upgradePlugin = (pluginType, name, version) => {
+    const options = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({plugin_type: pluginType, name: name, version: version})
+    }
+    return authComponent.authFetch('/api/install-agent-plugin', options, true);
+  }
+
+  const onUpgradeClick = (pluginId, pluginType, name, version) => {
+    setPluginsInInstallProcess((prevState) => {
+      return shallowAdditionOfUniqueValueToArray(prevState, pluginId);
+    });
+
+    upgradePlugin(pluginType, name, version).then(() => {
+      setSuccessfullyInstalledPluginsIds((prevState) => {
+        return shallowAdditionOfUniqueValueToArray(prevState, pluginId);
+      });
+      refreshInstalledPlugins();
+    }).catch(() => {
+      console.log('error upgrading plugin');
+    }).finally(() => {
+      setPluginsInInstallProcess((prevState => {
+        return shallowRemovalOfUniqueValueFromArray(prevState, pluginId);
+      }));
+    });
+  }
+
+  const getUpgradeAction = (row) => {
+    const pluginId = row.id;
+    if (pluginsInInstallProcess.includes(pluginId)) {
+      return [
+        <GridActionsCellItem
+          key={pluginId + 'upgrade'}
+          icon={<DownloadingIcon/>}
+          label="Upgrading"
+          className="textPrimary"
+          color="inherit"
+        />
+      ]
+    }
+
+    if (successfullyInstalledPluginsIds.includes(pluginId)) {
+      return [
+        <GridActionsCellItem
+          key={pluginId + 'upgrade'}
+          icon={<DownloadDoneIcon/>}
+          label="Upgrade Complete"
+          className="textPrimary"
+          color="inherit"
+        />
+      ]
+    }
+
+    if (row.update_available) {
+      const latest_plugin = availablePlugins[row.type][row.name].slice(-1)[0]
+      return [
+        <GridActionsCellItem
+        key={pluginId + 'upgrade'}
+        icon={<UpgradeIcon/>}
+        label="Upgrade"
+        className="textPrimary"
+        onClick={() => onUpgradeClick(pluginId, row.type, row.name, latest_plugin.version)}
+        color="inherit"
+      />
+      ]
+    }
+
+    return [
+      <GridActionsCellItem
+      key={pluginId + 'upgrade'}
+      icon={<UpgradeIcon/>}
+      label="Upgrade"
+      className="textPrimary"
+      disabled={true}
+      color="inherit"
+    />
+    ];
+  }
+
+  const getUninstallAction = (row) => {
     const pluginId = row.id;
     if (pluginsInUninstallProcess.includes(pluginId)) {
       return [
         <GridActionsCellItem
-          key={nanoid()}
+          key={pluginId + 'uninstall'}
           icon={<DownloadingIcon/>}
-          label="Downloading"
+          label="Uninstalling"
           className="textPrimary"
           color="inherit"
         />
@@ -89,9 +176,9 @@ const InstalledPlugins = () => {
     if (successfullyUninstalledPluginsIds.includes(pluginId)) {
       return [
         <GridActionsCellItem
-          key={nanoid()}
-          icon={<DownloadDoneIcon/>}
-          label="Download Done"
+          key={pluginId + 'uninstall'}
+          icon={<RemoveDoneIcon/>}
+          label="Uninstall Complete"
           className="textPrimary"
           color="inherit"
         />
@@ -102,14 +189,18 @@ const InstalledPlugins = () => {
     const pluginType = row.type;
     return [
       <GridActionsCellItem
-        key={nanoid()}
-        icon={<FileDownloadIcon/>}
-        label="Download"
+        key={pluginId + 'uninstall'}
+        icon={<DeleteIcon/>}
+        label="Uninstall"
         className="textPrimary"
         onClick={() => onUninstallClick(pluginId, pluginType, pluginName)}
         color="inherit"
       />
     ];
+  }
+
+  const getRowActions = (row) => {
+    return [...getUpgradeAction(row), ...getUninstallAction(row)]
   }
 
   return (
