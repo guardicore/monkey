f306b3975a90468fe136f9309c53ad1066efed19
Ilija Lazoroski <ilija.la@live.com>
2023-08-29 11:34:13 +0000

    UI: Add `Upload New Plugin` tab to the MarketPlace

--- monkey/monkey_island/cc/ui/src/components/pages/MarketplacePage.js
+++ monkey/monkey_island/cc/ui/src/components/pages/MarketplacePage.js
@@ -4,6 +4,8 @@ import {Tab, Box, Badge} from '@mui/material';
 import {PluginsContext} from '../contexts/plugins/PluginsContext';
 import AvailablePlugins from '../ui-components/plugins-marketplace/AvailablePlugins';
 import classes from '../../styles/pages/Marketplace.module.scss';
+import UploadNewPlugin from '../ui-components/plugins-marketplace/UploadNewPlugin';
+import AuthComponent from '../AuthComponent';
 
 const TabPanel = (props) => {
   const {children, value, index, ...other} = props;
@@ -48,7 +50,8 @@ const MarketplacePage = () => {
   </div>
 
   return (
-      <Box id={classes['marketplace-page']} className="main col-xl-8 col-lg-8 col-md-9 col-sm-9 offset-xl-2 offset-lg-3 offset-md-3 offset-sm-3">
+    <PluginsContext.Provider value={{availablePlugins, installedPlugins, UploadNewPlugin, refreshAvailablePlugins, refreshInstalledPlugins}}>
+      <Box className="main col-xl-8 col-lg-8 col-md-9 col-sm-9 offset-xl-2 offset-lg-3 offset-md-3 offset-sm-3">
         <h1 className='page-title'>Plugins</h1>
         <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
           <Tabs value={tabValue}
@@ -58,11 +61,13 @@ const MarketplacePage = () => {
                 variant="fullWidth"
                 aria-label="full width tabs">
             <Tab label="Available Plugins" {...a11yProps(0)}/>
-            <Tab label={installedPluginsLabel} {...a11yProps(1)}/>
+            <Tab label="Installed Plugins" {...a11yProps(1)}/>
+            <Tab label="Upload New Plugin" {...a11yProps(2)}/>
           </Tabs>
         </Box>
         <TabPanel value={tabValue} index={0}><AvailablePlugins /></TabPanel>
         <TabPanel value={tabValue} index={1}>Installed Plugins</TabPanel>
+        <TabPanel value={tabValue} index={2}><UploadNewPlugin/></TabPanel>
       </Box>
   )
 };
