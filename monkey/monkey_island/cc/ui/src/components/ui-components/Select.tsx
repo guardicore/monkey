d8a0e82b4b4da77166fbfa50dfcbce726eba7f98
vakarisz <vakarisz@yahoo.com>
2023-08-29 16:27:08 +0300

    UI: Add keys to select component rows

--- monkey/monkey_island/cc/ui/src/components/ui-components/Select.tsx
+++ monkey/monkey_island/cc/ui/src/components/ui-components/Select.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {InputLabel, FormControl, Select, MenuItem, SelectChangeEvent} from '@mui/material';
+import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from '@mui/material';
 
 
 type Option = {
@@ -16,9 +16,16 @@ type SelectProps = {
 
 const SelectComponent = (props: SelectProps) => {
   let selectOptions = [];
-  for(let i = 0; i < props.options.length; i++) {
-    selectOptions.push(<MenuItem value={props.options[i].value}>{props.options[i].label}</MenuItem>)
+  for (let i = 0; i < props.options.length; i++) {
+    let menuItem = (
+      <MenuItem value={props.options[i].value}
+                key={props.options[i].value}>
+        {props.options[i].label}
+      </MenuItem>
+    )
+    selectOptions.push(menuItem)
   }
+
   return (
     <FormControl fullWidth>
       <InputLabel id="demo-simple-select-label">{props.placeholder}</InputLabel>
