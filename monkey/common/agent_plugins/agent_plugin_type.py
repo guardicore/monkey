8ba0190c455709cc39979c835dc0e4d4456e4efb
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-08-30 10:36:11 -0400

    Common: Make AgentPluginType a StrEnum

--- monkey/common/agent_plugins/agent_plugin_type.py
+++ monkey/common/agent_plugins/agent_plugin_type.py
@@ -1,7 +1,7 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class AgentPluginType(Enum):
+class AgentPluginType(StrEnum):
     CREDENTIALS_COLLECTOR = "Credentials_Collector"
     EXPLOITER = "Exploiter"
     FINGERPRINTER = "Fingerprinter"
