222f63ea72b5ef7528df496a51b035948ef7a4de
Shreya Malviya <shreya.malviya@gmail.com>
2023-09-04 11:39:25 +0530

    Build: Update python version in AppImage build script
    AppImage was updated from 3.11.4

--- build_scripts/appimage/appimage.sh
+++ build_scripts/appimage/appimage.sh
@@ -2,7 +2,7 @@
 
 # Changes: python version
 LINUXDEPLOY_URL="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-PYTHON_VERSION="3.11.4"
+PYTHON_VERSION="3.11.5"
 PYTHON_APPIMAGE_URL="https://github.com/niess/python-appimage/releases/download/python3.11/python${PYTHON_VERSION}-cp311-cp311-manylinux2014_x86_64.AppImage"
 APPIMAGE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 APPDIR="$APPIMAGE_DIR/squashfs-root"
