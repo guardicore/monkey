579ea6e3aa893ec270a4f4470414a3b2f6d187b0
Shreya Malviya <shreya.malviya@gmail.com>
2023-09-05 11:03:36 +0530

    UT: Clean up FingerprintingEventHandler tests

--- monkey/tests/unit_tests/monkey_island/cc/agent_event_handlers/test_fingerprinting_event_handler.py
+++ monkey/tests/unit_tests/monkey_island/cc/agent_event_handlers/test_fingerprinting_event_handler.py
@@ -5,7 +5,6 @@ from unittest.mock import MagicMock
 from uuid import UUID
 
 import pytest
-from tests.monkey_island import InMemoryMachineRepository
 
 from common import OperatingSystem
 from common.agent_events import FingerprintingEvent
@@ -18,7 +17,7 @@ from common.types import (
     SocketAddress,
 )
 from monkey_island.cc.agent_event_handlers import FingerprintingEventHandler
-from monkey_island.cc.models import Agent, Machine
+from monkey_island.cc.models import Machine
 from monkey_island.cc.repositories import (
     AgentMachineFacade,
     IAgentRepository,
@@ -29,39 +28,9 @@ from monkey_island.cc.repositories import (
 )
 
 AGENT_ID = UUID("1d8ce743-a0f4-45c5-96af-91106529d3e2")
-SOURCE_MACHINE_ID = 11
-CC_SERVER = SocketAddress(ip="10.10.10.100", port="5000")
-AGENT_SHA256 = "c21dafe326222ba3ba65f5aebb6ea09c50696bf40eebca184caffe54f102746c"
-AGENT = Agent(
-    id=AGENT_ID,
-    machine_id=SOURCE_MACHINE_ID,
-    start_time=0,
-    parent_id=None,
-    cc_server=CC_SERVER,
-    sha256=AGENT_SHA256,
-)
-
-
-@pytest.fixture
-def source_machine() -> Machine:
-    return Machine(
-        id=SOURCE_MACHINE_ID,
-        hardware_id=5,
-        network_interfaces=[IPv4Interface("10.10.10.99/24")],
-    )
-
-
-TARGET_MACHINE_ID = 33
 TARGET_MACHINE_IP = "10.10.10.1"
-
-
-@pytest.fixture
-def target_machine() -> Machine:
-    return Machine(
-        id=TARGET_MACHINE_ID,
-        hardware_id=9,
-        network_interfaces=[IPv4Interface(f"{TARGET_MACHINE_IP}/24")],
-    )
+TARGET_MACHINE_ID = 33
+SEED_ID = 99
 
 
 DISCOVERED_SERVICES = (
@@ -77,6 +46,7 @@ NETWORK_SERVICES = {
     SocketAddress(ip=TARGET_MACHINE_IP, port=NetworkPort(5000)): NetworkService.HTTPS,
     SocketAddress(ip=TARGET_MACHINE_IP, port=NetworkPort(5001)): NetworkService.MSSQL,
 }
+
 
 FINGERPRINTING_EVENT_NO_OS_INFO_NO_SERVICES = FingerprintingEvent(
     source=AGENT_ID,
@@ -102,21 +72,19 @@ FINGERPRINTING_EVENT = FingerprintingEvent(
     discovered_services=DISCOVERED_SERVICES,
 )
 
-EXPECTED_NETWORK_SERVICES = {
-    SocketAddress(ip=TARGET_MACHINE_IP, port=22): NetworkService.UNKNOWN,
-    SocketAddress(ip=TARGET_MACHINE_IP, port=80): NetworkService.UNKNOWN,
-}
+
+@pytest.fixture
+def target_machine() -> Machine:
+    return Machine(
+        id=TARGET_MACHINE_ID,
+        hardware_id=9,
+        network_interfaces=[IPv4Interface(f"{TARGET_MACHINE_IP}/24")],
+    )
 
 
 @pytest.fixture
 def agent_repository() -> IAgentRepository:
-    agent_repository = MagicMock(spec=IAgentRepository)
-    agent_repository.upsert_agent = MagicMock()
-    agent_repository.get_agent_by_id = MagicMock(return_value=AGENT)
-    return agent_repository
-
-
-SEED_ID = 99
+    return MagicMock(spec=IAgentRepository)
 
 
 @pytest.fixture
@@ -128,18 +96,12 @@ def machine_repository(
     machine_repository.get_machine_by_id = MagicMock(side_effect=machine_from_id)
     machine_repository.get_machines_by_ip = MagicMock(side_effect=machines_from_ip)
     machine_repository.get_new_id = MagicMock(side_effect=count(SEED_ID))
-    machine_repository.upsert_machine = MagicMock()
     return machine_repository
 
 
 @pytest.fixture
 def node_repository() -> INodeRepository:
     return MagicMock(ispec=INodeRepository)
-
-
-@pytest.fixture
-def in_memory_machine_repository() -> IMachineRepository:
-    return InMemoryMachineRepository(SEED_ID)
 
 
 @pytest.fixture
@@ -158,23 +120,13 @@ def fingerprinting_event_handler(network_model_update_facade, machine_repository
 
 
 @pytest.fixture
-def machines_by_id(source_machine: Machine, target_machine: Machine) -> Dict[MachineID, Machine]:
-    return {source_machine.id: source_machine, target_machine.id: target_machine}
+def machines_by_id(target_machine: Machine) -> Dict[MachineID, Machine]:
+    return {target_machine.id: target_machine}
 
 
 @pytest.fixture
-def machines_by_ip(
-    source_machine: Machine, target_machine: Machine
-) -> Dict[IPv4Address, Sequence[Machine]]:
-    return {
-        IPv4Address("10.10.10.99"): [source_machine],
-        IPv4Address(TARGET_MACHINE_IP): [target_machine],
-    }
-
-
-@pytest.fixture(params=[SOURCE_MACHINE_ID, TARGET_MACHINE_ID])
-def machine_id(request):
-    return request.param
+def machines_by_ip(target_machine: Machine) -> Dict[IPv4Address, Sequence[Machine]]:
+    return {IPv4Address(TARGET_MACHINE_IP): [target_machine]}
 
 
 @pytest.fixture
