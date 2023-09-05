838eb57cd99929a5b02b834e51c71bce0ee56bda
Ilija Lazoroski <ilija.la@live.com>
2023-09-05 13:57:33 +0000

    Chrome: Add interrupt to the Processors

--- monkey/agent_plugins/credentials_collectors/chrome/src/linux_credentials_database_processor.py
+++ monkey/agent_plugins/credentials_collectors/chrome/src/linux_credentials_database_processor.py
@@ -2,11 +2,14 @@ from pathlib import PurePath
 from typing import Sequence
 
 from common.credentials import Credentials
+from common.types import Event
 
 
 class LinuxCredentialsDatabaseProcessor:
     def __init__(self):
         pass
 
-    def __call__(self, database_paths: Sequence[PurePath]) -> Sequence[Credentials]:
+    def __call__(
+        self, interrupt: Event, database_paths: Sequence[PurePath]
+    ) -> Sequence[Credentials]:
         return []
