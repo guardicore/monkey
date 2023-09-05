a5b3b79ed1345816feeb269ed21e0b22100c61eb
vakarisz <vakarisz@yahoo.com>
2023-09-05 16:37:50 +0300

    UI: Rework plugin filters to use rows instead of back-end data

--- monkey/monkey_island/cc/ui/src/components/utils/SaveJsonToFile.js
+++ monkey/monkey_island/cc/ui/src/components/utils/SaveJsonToFile.js
@@ -2,6 +2,6 @@ import FileSaver from 'file-saver';
 
 export default function saveJsonToFile(dataToSave, filename) {
   const content = JSON.stringify(dataToSave, null, 2);
-  const blob = new Blob([content], {type: 'text/plain;charset=utf-8'});
+  const blob = new Blob([content], {pluginType: 'text/plain;charset=utf-8'});
   FileSaver.saveAs(blob, filename + '.json');
 }
