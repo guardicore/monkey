3abc90ace5b8a1835e61c8300ada491b32ddc3d5
Shreya Malviya <shreya.malviya@gmail.com>
2023-09-08 13:53:45 +0530

    Chrome: Make BrowserCredentialsDatabasePath hashable

--- monkey/agent_plugins/credentials_collectors/chrome/src/utils.py
+++ monkey/agent_plugins/credentials_collectors/chrome/src/utils.py
@@ -3,7 +3,7 @@ from pathlib import PurePath
 from typing import Optional
 
 
-@dataclass
+@dataclass(frozen=True)
 class BrowserCredentialsDatabasePath:
     database_file_path: PurePath
     master_key: Optional[bytes]
