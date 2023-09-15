5c3a869a676e0c216ab95499622ae6db8b5a422d
vakarisz <vakarisz@yahoo.com>
2023-09-15 16:55:34 +0300

    UI: Fix the manual run powershell command
    Issue #3709
    PR #3710

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
@@ -2,11 +2,12 @@ import {AGENT_OTP_ENVIRONMENT_VARIABLE} from './consts';
 
 function getAgentDownloadCommand(ip, otp) {
   return `$execCmd = @"\r\n`
-    + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {\`$true};`
+    + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};\r\n`
     + `(New-Object System.Net.WebClient).DownloadFile('https://${ip}:5000/api/agent-binaries/windows',`
-    + `"""$env:TEMP\\monkey.exe""");\`$env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}' ;`
-    + `Start-Process -FilePath '$env:TEMP\\monkey.exe' -ArgumentList 'm0nk3y -s ${ip}:5000';`
-    + `\r\n"@; \r\n`
+    + `"$env:TEMP\\monkey.exe");\r\n`
+    + `$env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}';\r\n`
+    + `Start-Process -FilePath '$env:TEMP\\monkey.exe' -ArgumentList 'm0nk3y -s ${ip}:5000';\r\n`
+    + `"@; \r\n`
     + `Start-Process -FilePath powershell.exe -ArgumentList $execCmd`;
 }
 
