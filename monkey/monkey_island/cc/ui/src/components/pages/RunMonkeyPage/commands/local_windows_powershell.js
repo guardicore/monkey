8beb8a13f5ea0c921f06fd5e552d2fa40d16c128
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-09-15 20:02:10 +0000

    Revert "UI: Enocde command to avoid dealing with escape sequences"
    This reverts commit 3d733c381ed9ef38eb8fec90ace3ba04e853dd03.
    
    For some reason, the -Credential flag is conflicting with the
    encoded command

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
@@ -1,15 +1,15 @@
 import {AGENT_OTP_ENVIRONMENT_VARIABLE} from './consts';
 
 function getAgentDownloadCommand(ip, otp) {
-  return `$execCmd =  [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes({\r\n`
-  + `$monkey=[System.IO.Path]::GetTempPath() + "monkey.exe"\r\n`
-  + `[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;\r\n`
-  + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};\r\n`
-  + `(New-Object System.Net.WebClient).DownloadFile('https://${ip}:5000/api/agent-binaries/windows',\r\n`
-  + `"$monkey"); $env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}';\r\n`
-  + `Start-Process -FilePath "$monkey" -ArgumentList 'm0nk3y -s ${ip}:5000';\r\n`
-  + `}))\r\n`
-  + `Start-Process powershell -args '-EncodedCommand',$execCmd`;
+  return `$execCmd = @"\r\n`
+    + `\`$monkey=[System.IO.Path]::GetTempPath() + """monkey.exe""";\r\n`
+    + `[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;\r\n`
+    + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {\`$true};\r\n`
+    + `(New-Object System.Net.WebClient).DownloadFile('https://${ip}:5000/api/agent-binaries/windows',\r\n`
+    + `"""\`$monkey""");\`$env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}';\r\n`
+    + `Start-Process -FilePath """\`$monkey""" -ArgumentList 'm0nk3y -s ${ip}:5000';\r\n`
+    + `"@; \r\n`
+    + `Start-Process -FilePath powershell.exe -ArgumentList $execCmd`;
 }
 
 export default function generateLocalWindowsPowershell(ip, username, otp) {
