3011349e6a17da0d6f3c1e6732de86284ce68b9a
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-09-15 19:13:52 +0000

    UI: Escape double quotes in Windows manual run command

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_windows_powershell.js
@@ -2,14 +2,12 @@ import {AGENT_OTP_ENVIRONMENT_VARIABLE} from './consts';
 
 function getAgentDownloadCommand(ip, otp) {
   return `$execCmd = @"\r\n`
-    + `\`$TempDir=[System.IO.Path]::GetTempPath();\r\n`
-    + `read-host "press something"\r\n`
+    + `\`$monkey=[System.IO.Path]::GetTempPath() + """monkey.exe""";\r\n`
     + `[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;\r\n`
     + `[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {\`$true};\r\n`
     + `(New-Object System.Net.WebClient).DownloadFile('https://${ip}:5000/api/agent-binaries/windows',\r\n`
-    + `"\`$TempDir\\monkey.exe");\`$env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}';\r\n`
-    + `Start-Process -FilePath '\`$TempDir\\monkey.exe' -ArgumentList 'm0nk3y -s ${ip}:5000';\r\n`
-    + `read-host "Press any key exit..."\r\n`
+    + `"""\`$monkey""");\`$env:${AGENT_OTP_ENVIRONMENT_VARIABLE}='${otp}';\r\n`
+    + `Start-Process -FilePath """\`$monkey""" -ArgumentList 'm0nk3y -s ${ip}:5000';\r\n`
     + `"@; \r\n`
     + `Start-Process -FilePath powershell.exe -ArgumentList $execCmd`;
 }
