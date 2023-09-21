a40d7d54edd73b7bbf2f40ea0f12ffc83d87a16c
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-09-21 13:10:26 -0400

    Agent: Add return type hint to positive_int()

--- monkey/infection_monkey/utils/argparse_types.py
+++ monkey/infection_monkey/utils/argparse_types.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def positive_int(_input: str):
+def positive_int(_input: str) -> int:
     int_value = int(_input)
     if int_value < 0:
         raise argparse.ArgumentTypeError(f"{_input} is not a positive integer")
