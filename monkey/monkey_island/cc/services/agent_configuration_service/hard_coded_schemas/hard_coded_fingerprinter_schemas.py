a886117eff1441935c55032a8439083682b9b559
Shreya Malviya <shreya.malviya@gmail.com>
2023-10-16 13:09:20 +0530

    Island: Add http_ports option to HTTP fingerprinter schema
    Issue #3769
    PR #3791

--- monkey/monkey_island/cc/services/agent_configuration_service/hard_coded_schemas/hard_coded_fingerprinter_schemas.py
+++ monkey/monkey_island/cc/services/agent_configuration_service/hard_coded_schemas/hard_coded_fingerprinter_schemas.py
@@ -1,6 +1,17 @@
 HARD_CODED_FINGERPRINTER_SCHEMAS = {
     "smb": {"type": "object", "properties": {}},
     "ssh": {"type": "object", "properties": {}},
-    "http": {"type": "object", "properties": {}},
+    "http": {
+        "type": "object",
+        "properties": {
+            "http_ports": {
+                "title": "HTTP Ports",
+                "type": "array",
+                "items": {"type": "integer", "minimum": 0, "maximum": 65535},
+                "default": [80, 8080, 443, 8008, 7001, 8983, 9600],
+                "description": "List of HTTP ports the fingerprinter will scan",
+            }
+        },
+    },
     "mssql": {"type": "object", "properties": {}},
 }
