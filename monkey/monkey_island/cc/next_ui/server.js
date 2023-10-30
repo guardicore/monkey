e98d3b931d938c5c803c09f28fa5cca970f0e80b
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-10-30 15:23:18 +0000

    UI: Log messages when node server fails to start

--- monkey/monkey_island/cc/next_ui/server.js
+++ monkey/monkey_island/cc/next_ui/server.js
@@ -1,26 +1,30 @@
-const next = require('next')
+const next = require('next');
 
 // note the "https" not "http" required module. You will get an error if trying to connect with https
-const https = require('https')
-const {parse} = require('url');
+const https = require('https');
 
 const fs = require('fs');
-const port = 443
-const app = next({dev: false, port: port});
+const port = 443;
+const app = next({ dev: false, port: port });
 
 const sslOptions = {
     key: fs.readFileSync('../server.key'),
-    cert: fs.readFileSync('../server.crt')
-}
+    cert: fs.readFileSync('../server.crt'),
+};
 
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+    console.log('Starting server...');
     const server = https.createServer(sslOptions, (req, res) => {
         return handle(req, res);
-    })
-    server.listen(port, (err) => {
-        if (err) throw err
+    });
+    console.log('Listening on port ' + port);
+    server.on('error', (err) => {
+        console.error('Error listening on port ' + port + ': ' + err);
+        throw err;
+    });
+    server.listen(port, () => {
         console.log('> Ready on https://localhost:' + port);
-    })
-})
+    });
+});
