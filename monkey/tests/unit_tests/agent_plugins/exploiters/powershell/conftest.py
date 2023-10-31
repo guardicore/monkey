b76af86c92c93f2e62fb8e00eabc4fe57ffff3f8
Ilija Lazoroski <ilija.la@live.com>
2023-10-31 08:49:46 +0000

    PowerShell: Import NetworkPort from monkeytypes

--- monkey/tests/unit_tests/agent_plugins/exploiters/powershell/conftest.py
+++ monkey/tests/unit_tests/agent_plugins/exploiters/powershell/conftest.py
@@ -1,9 +1,8 @@
 from unittest.mock import MagicMock
 
 import pytest
-from monkeytypes import NetworkProtocol, OperatingSystem, PortStatus
+from monkeytypes import NetworkPort, NetworkProtocol, OperatingSystem, PortStatus
 
-from common.types import NetworkPort
 from infection_monkey.i_puppet import PortScanData, TargetHostPorts
 
 
