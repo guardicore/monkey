47624ba97aa12753f8fbcaef83167791f736cfbf
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-11-01 16:41:53 +0000

    Island: Convert IslandConfigOptions to a pydantic model

--- monkey/monkey_island/cc/setup/config_setup.py
+++ monkey/monkey_island/cc/setup/config_setup.py
@@ -15,22 +15,26 @@ PACKAGE_CONFIG_PATH = Path(MONKEY_ISLAND_ABS_PATH, "cc", SERVER_CONFIG_FILENAME)
 def get_server_config(island_args: IslandCmdArgs) -> IslandConfigOptions:
     config = IslandConfigOptions()
 
-    _update_config_from_file(config, PACKAGE_CONFIG_PATH)
+    config = _update_config_from_file(config, PACKAGE_CONFIG_PATH)
 
     if island_args.server_config_path:
         path_to_config = expand_path(island_args.server_config_path)
-        _update_config_from_file(config, path_to_config)
+        config = _update_config_from_file(config, path_to_config)
 
     return config
 
 
-def _update_config_from_file(config: IslandConfigOptions, config_path: Path):
+def _update_config_from_file(config: IslandConfigOptions, config_path: Path) -> IslandConfigOptions:
     try:
         config_from_file = _load_server_config_from_file(config_path)
-        config.update(config_from_file)
+        updated_config_dict = config.to_dict()
+        updated_config_dict.update(config_from_file)
+        updated_config = IslandConfigOptions(**updated_config_dict)
         logger.info(f"Server config updated from {config_path}")
+        return updated_config
     except OSError:
         logger.warning(f"Server config not found in path {config_path}")
+    return config
 
 
 def _load_server_config_from_file(server_config_path) -> dict:
--- monkey/monkey_island/cc/setup/island_config_options.py
+++ monkey/monkey_island/cc/setup/island_config_options.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from types import MappingProxyType as ImmutableMapping
-from typing import Mapping, Optional
+from pathlib import Path
+from typing import Annotated
+
+from monkeytypes import InfectionMonkeyBaseModel
+from pydantic import BeforeValidator, Field
 
 from common.utils.file_utils import expand_path
 from monkey_island.cc.server_utils.consts import (
@@ -9,52 +12,27 @@ from monkey_island.cc.server_utils.consts import (
     DEFAULT_DATA_DIR,
     DEFAULT_KEY_PATH,
     DEFAULT_LOG_LEVEL,
+    DEFAULT_NODE_PORT,
     DEFAULT_START_MONGO_DB,
 )
 
-_DATA_DIR = "data_dir"
-_SSL_CERT = "ssl_certificate"
-_SSL_CERT_FILE = "ssl_certificate_file"
-_SSL_CERT_KEY = "ssl_certificate_key_file"
-_MONGODB = "mongodb"
-_START_MONGODB = "start_mongodb"
-_LOG_LEVEL = "log_level"
-NODE_PORT = "node_port"
+
+class MongoDBConfig(InfectionMonkeyBaseModel):
+    start_mongodb: bool = DEFAULT_START_MONGO_DB
 
 
-class IslandConfigOptions:
-    def __init__(self, config_contents: Mapping[str, Mapping] = ImmutableMapping({})):
-        self.data_dir = DEFAULT_DATA_DIR
-        self.log_level = DEFAULT_LOG_LEVEL
-        self.start_mongodb = DEFAULT_START_MONGO_DB
-        self.crt_path = DEFAULT_CRT_PATH
-        self.key_path = DEFAULT_KEY_PATH
-        self.node_port: Optional[int] = None
+class SSLCertificatesConfig(InfectionMonkeyBaseModel):
+    ssl_certificate_file: Annotated[
+        Path, Field(default=Path(DEFAULT_CRT_PATH)), BeforeValidator(expand_path)
+    ]
+    ssl_certificate_key_file: Annotated[
+        Path, Field(default=Path(DEFAULT_KEY_PATH)), BeforeValidator(expand_path)
+    ]
 
-        self._expand_paths()
 
-        self.update(config_contents)
-
-    def update(self, config_contents: Mapping[str, Mapping]):
-        self.data_dir = config_contents.get(_DATA_DIR, self.data_dir)
-
-        self.log_level = config_contents.get(_LOG_LEVEL, self.log_level)
-        self.node_port = config_contents.get(NODE_PORT, self.node_port)
-
-        self.start_mongodb = config_contents.get(
-            _MONGODB, {_START_MONGODB: self.start_mongodb}
-        ).get(_START_MONGODB, self.start_mongodb)
-
-        self.crt_path = config_contents.get(_SSL_CERT, {_SSL_CERT_FILE: self.crt_path}).get(
-            _SSL_CERT_FILE, self.crt_path
-        )
-        self.key_path = config_contents.get(_SSL_CERT, {_SSL_CERT_KEY: self.key_path}).get(
-            _SSL_CERT_KEY, self.key_path
-        )
-
-        self._expand_paths()
-
-    def _expand_paths(self):
-        self.data_dir = expand_path(str(self.data_dir))
-        self.crt_path = expand_path(str(self.crt_path))
-        self.key_path = expand_path(str(self.key_path))
+class IslandConfigOptions(InfectionMonkeyBaseModel):
+    data_dir: Annotated[Path, Field(default=DEFAULT_DATA_DIR), BeforeValidator(expand_path)]
+    log_level: str = DEFAULT_LOG_LEVEL
+    mongodb: MongoDBConfig = MongoDBConfig()
+    ssl_certificate: SSLCertificatesConfig = SSLCertificatesConfig()
+    node_port: int = DEFAULT_NODE_PORT
