c75b1a2de1c416b3954dcb02bd152602882dc42c
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-09 08:48:54 -0500

    Ransomware: Fix WallpaperChangerCallable type hint

--- monkey/agent_plugins/payloads/ransomware/src/typedef.py
+++ monkey/agent_plugins/payloads/ransomware/src/typedef.py
@@ -4,4 +4,4 @@ from typing import Callable, Iterable, TypeAlias
 FileEncryptorCallable: TypeAlias = Callable[[Path], None]
 FileSelectorCallable: TypeAlias = Callable[[Path], Iterable[Path]]
 ReadmeDropperCallable: TypeAlias = Callable[[Path, Path], None]
-WallpaperChangerCallable: TypeAlias = Callable[[Path, Path], None]
+WallpaperChangerCallable: TypeAlias = Callable[[], None]
