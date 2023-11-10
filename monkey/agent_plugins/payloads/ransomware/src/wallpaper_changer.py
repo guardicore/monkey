df3bbf82d235827d78a6669452796ed8e8a32256
vakarisz <vakarisz@yahoo.com>
2023-11-10 13:18:16 +0200

    Ransomware: Small improvements in wallpaper_changer.py

--- monkey/agent_plugins/payloads/ransomware/src/wallpaper_changer.py
+++ monkey/agent_plugins/payloads/ransomware/src/wallpaper_changer.py
@@ -1,9 +1,14 @@
 import ctypes
+import logging
 from pathlib import Path
 
 from monkeytypes import OperatingSystem
 
+from common.utils.environment import is_windows_os
+
 SPI_SETDESKWALLPAPER = 20
+
+logger = logging.getLogger(__name__)
 
 
 class WallpaperChanger:
@@ -13,8 +18,9 @@ class WallpaperChanger:
         self._operating_system = operating_system
 
     def change_wallpaper(self):
-        if self._operating_system == OperatingSystem.WINDOWS:
-            # Directly set the wallpaper for Windows OS using the source image
+        if is_windows_os():
+            logger.info("Attempting to change the wallpaper on Windows OS")
             ctypes.windll.user32.SystemParametersInfoW(  # type: ignore [attr-defined]
                 SPI_SETDESKWALLPAPER, 0, str(self.WALLPAPER_PATH), 3
             )
+            logger.info("Wallpaper changed")
