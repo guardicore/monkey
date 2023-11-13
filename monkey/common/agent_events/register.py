e1a2a5f0dd591dfc4bdc38f6e85bafa7310ab59a
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-13 15:16:19 -0500

    Common: Alphabetize the registration of agent events

--- monkey/common/agent_events/register.py
+++ monkey/common/agent_events/register.py
@@ -18,14 +18,14 @@ from . import AgentEventRegistry
 def register_common_agent_events(
     agent_event_registry: AgentEventRegistry,
 ):
-    agent_event_registry.register(CredentialsStolenEvent)
-    agent_event_registry.register(PingScanEvent)
-    agent_event_registry.register(TCPScanEvent)
-    agent_event_registry.register(FingerprintingEvent)
-    agent_event_registry.register(PropagationEvent)
-    agent_event_registry.register(ExploitationEvent)
-    agent_event_registry.register(PasswordRestorationEvent)
     agent_event_registry.register(AgentShutdownEvent)
+    agent_event_registry.register(CredentialsStolenEvent)
+    agent_event_registry.register(ExploitationEvent)
     agent_event_registry.register(FileEncryptionEvent)
-    agent_event_registry.register(OSDiscoveryEvent)
+    agent_event_registry.register(FingerprintingEvent)
     agent_event_registry.register(HostnameDiscoveryEvent)
+    agent_event_registry.register(OSDiscoveryEvent)
+    agent_event_registry.register(PasswordRestorationEvent)
+    agent_event_registry.register(PingScanEvent)
+    agent_event_registry.register(PropagationEvent)
+    agent_event_registry.register(TCPScanEvent)
