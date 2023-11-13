077edb39fd7c8031ef1d7c4d0b500834d082c002
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-13 15:19:00 -0500

    Common: Register missing events in the AgentEventRegistry

--- monkey/common/agent_events/__init__.py
+++ monkey/common/agent_events/__init__.py
@@ -11,8 +11,8 @@ from .agent_shutdown_event import AgentShutdownEvent
 from .file_encryption_event import FileEncryptionEvent
 from .os_discovery_event import OSDiscoveryEvent
 from .hostname_discovery_event import HostnameDiscoveryEvent
-from .register import register_common_agent_events
 from .cpu_consumption_event import CPUConsumptionEvent
 from .ram_consumption_event import RAMConsumptionEvent
 from .http_request_event import HTTPRequestEvent
 from .defacement_event import DefacementEvent
+from .register import register_common_agent_events
--- monkey/common/agent_events/register.py
+++ monkey/common/agent_events/register.py
@@ -1,14 +1,18 @@
 from common.agent_events import (
     AgentShutdownEvent,
+    CPUConsumptionEvent,
     CredentialsStolenEvent,
+    DefacementEvent,
     ExploitationEvent,
     FileEncryptionEvent,
     FingerprintingEvent,
     HostnameDiscoveryEvent,
+    HTTPRequestEvent,
     OSDiscoveryEvent,
     PasswordRestorationEvent,
     PingScanEvent,
     PropagationEvent,
+    RAMConsumptionEvent,
     TCPScanEvent,
 )
 
@@ -19,13 +23,17 @@ def register_common_agent_events(
     agent_event_registry: AgentEventRegistry,
 ):
     agent_event_registry.register(AgentShutdownEvent)
+    agent_event_registry.register(CPUConsumptionEvent)
     agent_event_registry.register(CredentialsStolenEvent)
+    agent_event_registry.register(DefacementEvent)
     agent_event_registry.register(ExploitationEvent)
     agent_event_registry.register(FileEncryptionEvent)
     agent_event_registry.register(FingerprintingEvent)
     agent_event_registry.register(HostnameDiscoveryEvent)
+    agent_event_registry.register(HTTPRequestEvent)
     agent_event_registry.register(OSDiscoveryEvent)
     agent_event_registry.register(PasswordRestorationEvent)
     agent_event_registry.register(PingScanEvent)
     agent_event_registry.register(PropagationEvent)
+    agent_event_registry.register(RAMConsumptionEvent)
     agent_event_registry.register(TCPScanEvent)
