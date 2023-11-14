6eb01f1695dcafa6f3c5516194d9b36cc0e5bdd2
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-14 15:03:36 -0500

    Common: Automatically register agent events in AgentEventRegistry

--- monkey/common/agent_events/register.py
+++ monkey/common/agent_events/register.py
@@ -1,20 +1,6 @@
-from common.agent_events import (
-    AgentShutdownEvent,
-    CPUConsumptionEvent,
-    CredentialsStolenEvent,
-    DefacementEvent,
-    ExploitationEvent,
-    FileEncryptionEvent,
-    FingerprintingEvent,
-    HostnameDiscoveryEvent,
-    HTTPRequestEvent,
-    OSDiscoveryEvent,
-    PasswordRestorationEvent,
-    PingScanEvent,
-    PropagationEvent,
-    RAMConsumptionEvent,
-    TCPScanEvent,
-)
+import inspect
+
+import common.agent_events
 
 from . import AgentEventRegistry
 
@@ -22,18 +8,11 @@ from . import AgentEventRegistry
 def register_common_agent_events(
     agent_event_registry: AgentEventRegistry,
 ):
-    agent_event_registry.register(AgentShutdownEvent)
-    agent_event_registry.register(CPUConsumptionEvent)
-    agent_event_registry.register(CredentialsStolenEvent)
-    agent_event_registry.register(DefacementEvent)
-    agent_event_registry.register(ExploitationEvent)
-    agent_event_registry.register(FileEncryptionEvent)
-    agent_event_registry.register(FingerprintingEvent)
-    agent_event_registry.register(HostnameDiscoveryEvent)
-    agent_event_registry.register(HTTPRequestEvent)
-    agent_event_registry.register(OSDiscoveryEvent)
-    agent_event_registry.register(PasswordRestorationEvent)
-    agent_event_registry.register(PingScanEvent)
-    agent_event_registry.register(PropagationEvent)
-    agent_event_registry.register(RAMConsumptionEvent)
-    agent_event_registry.register(TCPScanEvent)
+    for name, event_class in inspect.getmembers(common.agent_events, inspect.isclass):
+        if name == "AbstractAgentEvent":
+            continue
+
+        if not issubclass(event_class, common.agent_events.AbstractAgentEvent):
+            continue
+
+        agent_event_registry.register(event_class)
