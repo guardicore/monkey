3b32f340f09bde51a5a0f7c6666b57b30468f55c
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-14 15:32:19 -0500

    Common: Add TODO about code duplication

--- monkey/common/agent_event_serializers/register.py
+++ monkey/common/agent_event_serializers/register.py
@@ -8,6 +8,10 @@ from . import AgentEventSerializerRegistry, PydanticAgentEventSerializer
 def register_common_agent_event_serializers(
     event_serializer_registry: AgentEventSerializerRegistry,
 ):
+    # Note: The code to identify all agent events here duplicates the code from
+    # common.agent_events.register_common_agent_events(). This is difficult to rectify at the
+    # present time without introducing a circular import. This should be fixable once #3799 is
+    # completed and agent events are moved to the monkeyevents package.
     for _, event_class in inspect.getmembers(common.agent_events, inspect.isclass):
         if event_class is common.agent_events.AbstractAgentEvent:
             continue
