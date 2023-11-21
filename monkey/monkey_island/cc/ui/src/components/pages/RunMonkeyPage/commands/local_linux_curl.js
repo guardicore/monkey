1ca5d920563144a8fae52d65b67f89a6c87b65f2
Shreya Malviya <shreya.malviya@gmail.com>
2023-11-21 13:03:54 +0530

    UI: Use custom node port in manual run monkey command generation

--- monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_curl.js
+++ monkey/monkey_island/cc/ui/src/components/pages/RunMonkeyPage/commands/local_linux_curl.js
@@ -1,10 +1,10 @@
 import { AGENT_OTP_ENVIRONMENT_VARIABLE } from './consts';
 
-export default function generateLocalLinuxCurl(ip, username, otp) {
-  let command = `curl https://${ip}:5000/api/agent-binaries/linux -k `
+export default function generateLocalLinuxCurl(ip, port, username, otp) {
+  let command = `curl https://${ip}:${port}/api/agent-binaries/linux -k `
     + `-o monkey-linux-64; `
     + `chmod +x monkey-linux-64; `
-    + `${AGENT_OTP_ENVIRONMENT_VARIABLE}=${otp} ./monkey-linux-64 m0nk3y -s ${ip}:5000;`;
+    + `${AGENT_OTP_ENVIRONMENT_VARIABLE}=${otp} ./monkey-linux-64 m0nk3y -s ${ip}:${port};`;
 
   if (username != '') {
     command = `su - ${username} -c "${command}"`;
