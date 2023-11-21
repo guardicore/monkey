e9668b6f568a90eaa536cf4c010fbf814d07cd1b
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-21 07:15:06 -0500

    Common: Remove duplicate event/serializer registration code

--- monkey/common/agent_events/register_agent_events.py
+++ monkey/common/agent_events/register_agent_events.py
@@ -1,18 +1,9 @@
-import inspect
-
-import monkeyevents
-
 from . import AgentEventRegistry
+from .builtin_agent_events import BUILTIN_AGENT_EVENT_CLASSES
 
 
 def register_common_agent_events(
     agent_event_registry: AgentEventRegistry,
 ):
-    for _, event_class in inspect.getmembers(monkeyevents, inspect.isclass):
-        if event_class is monkeyevents.AbstractAgentEvent:
-            continue
-
-        if not issubclass(event_class, monkeyevents.AbstractAgentEvent):
-            continue
-
+    for event_class in BUILTIN_AGENT_EVENT_CLASSES:
         agent_event_registry.register(event_class)
