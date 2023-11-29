835e7d460514c0d5f0fa701ed03a89bb90de9276
vakarisz <vakarisz@yahoo.com>
2023-11-29 16:25:18 +0200

    UI: Removed commented out code in AppBar.tsx

--- monkey/monkey_island/cc/next_ui/src/_components/app-bar/AppBar.tsx
+++ monkey/monkey_island/cc/next_ui/src/_components/app-bar/AppBar.tsx
@@ -21,23 +21,11 @@ import React, { useState } from 'react';
 const MonkeyAppBar = () => {
     const router = useRouter();
 
-    // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    //     null
-    // );
-
     const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-
-    // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    //     setAnchorElNav(event.currentTarget);
-    // };
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
     };
-
-    // const handleCloseNavMenu = () => {
-    //     setAnchorElNav(null);
-    // };
 
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
