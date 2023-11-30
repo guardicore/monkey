91b472791c1b351ff97e38cdb0ebbbff5b29c9ed
ordabach <odabach@akamai.com>
2023-11-30 13:20:22 +0200

    UI: Remove comments

--- monkey/monkey_island/cc/next_ui/src/app/error.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/error.tsx
@@ -1,4 +1,4 @@
-'use client'; // Error components must be Client Components
+'use client';
 
 import { useEffect } from 'react';
 
@@ -10,7 +10,6 @@ export default function Error({
     reset: () => void;
 }) {
     useEffect(() => {
-        // Log the error to an error reporting service
         console.error(error);
     }, [error]);
 
