c3fdc7ebf9998a6bb27519b16254babafaa06f94
Shreya Malviya <shreya.malviya@gmail.com>
2023-11-30 17:49:11 +0530

    Mimikatz: Add title(s) to options

--- monkey/agent_plugins/credentials_collectors/mimikatz/src/mimikatz_options.py
+++ monkey/agent_plugins/credentials_collectors/mimikatz/src/mimikatz_options.py
@@ -7,6 +7,7 @@ from pydantic import Field
 class MimikatzOptions(InfectionMonkeyBaseModel):
     excluded_username_prefixes: List[str] = Field(
         default=[],
+        title="Excluded Username Prefixes",
         description="Mimikatz will not collect credentials for any user whose username"
         " starts with one of theses prefixes.",
     )
