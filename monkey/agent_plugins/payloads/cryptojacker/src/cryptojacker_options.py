c8e4208ceb760c50c3927926bea13d2b34996dfd
Shreya Malviya <shreya.malviya@gmail.com>
2023-11-30 17:52:23 +0530

    Cryptojacker: Update title(s) in options

--- monkey/agent_plugins/payloads/cryptojacker/src/cryptojacker_options.py
+++ monkey/agent_plugins/payloads/cryptojacker/src/cryptojacker_options.py
@@ -11,17 +11,17 @@ class CryptojackerOptions(InfectionMonkeyBaseModel):
         ge=0,
     )
     cpu_utilization: PercentLimited = Field(  # type: ignore[valid-type]
-        title="CPU utilization",
+        title="CPU Utilization",
         description="The percentage of CPU to use on a machine",
         default=80,
     )
     memory_utilization: PercentLimited = Field(  # type: ignore[valid-type]
-        title="Memory utilization",
+        title="Memory Utilization",
         description="The percentage of memory to use on a machine",
         default=20,
     )
     simulate_bitcoin_mining_network_traffic: bool = Field(
-        title="Simulate Bitcoin mining network traffic",
+        title="Simulate Bitcoin Mining Network Traffic",
         default=False,
         description="If enabled, the Agent will periodically send requests used in Bitcoin mining"
         " over the network.",
