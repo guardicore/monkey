fd812173cd4ecc80bfb4321b45726f88b83fd171
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-05 16:53:08 +0530

    Agent: Update docstring for LocalMachineInfo

--- monkey/infection_monkey/local_machine_info.py
+++ monkey/infection_monkey/local_machine_info.py
@@ -9,6 +9,16 @@ from infection_monkey.utils.monkey_dir import get_monkey_dir_path
 
 
 class LocalMachineInfo(MutableInfectionMonkeyBaseModel):
+    """
+    Contains information about the local machine
+
+    :param operating_system: Operating system of the local machine
+    :param temporary_directory: Path to a temporary directory on the local machine
+                                to store artifacts
+    :param interface_to_target: Interface on the local machine that can be reached
+                                by the target machine
+    """
+
     operating_system: OperatingSystem = Field(default_factory=get_os)
     temporary_directory: Path = Field(default_factory=get_monkey_dir_path)
     interface_to_target: Optional[str] = None
