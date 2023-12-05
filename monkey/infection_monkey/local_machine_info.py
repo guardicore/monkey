6a6b118df5ddb51a3ac89eaf746a8eacd6872fb7
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-05 19:37:27 +0530

    Agent: Add get_interface_to_target(), remove interface_to_target in LocalMachineInfo

--- monkey/infection_monkey/local_machine_info.py
+++ monkey/infection_monkey/local_machine_info.py
@@ -1,26 +1,26 @@
+from ipaddress import IPv4Address
 from pathlib import Path
 from typing import Optional
 
-from monkeytypes import MutableInfectionMonkeyBaseModel, OperatingSystem
+from monkeytypes import InfectionMonkeyBaseModel, OperatingSystem
+
+from infection_monkey.network.tools import get_interface_to_target
 
 
-class LocalMachineInfo(MutableInfectionMonkeyBaseModel):
+class LocalMachineInfo(InfectionMonkeyBaseModel):
     """
     Contains information about the local machine
 
     :param operating_system: Operating system of the local machine
     :param temporary_directory: Path to a temporary directory on the local machine
                                 to store artifacts
-    :param interface_to_target: Interface on the local machine that can be reached
-                                by the target machine
     """
 
     operating_system: OperatingSystem
     temporary_directory: Path
-    interface_to_target: Optional[str] = None
 
-    def set_interface_to_target(self, interface_to_target: str):
-        self.interface_to_target = interface_to_target
-
-    def reset_interface_to_target(self):
-        self.interface_to_target = None
+    def get_interface_to_target(self, target_host_ip: IPv4Address) -> Optional[str]:
+        """
+        Gets an interface on the local machine that can be reached by the target machine
+        """
+        return get_interface_to_target(target_host_ip)
