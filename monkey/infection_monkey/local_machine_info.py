dd85bcf156e36353a2f032e7396981e3d26440e7
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-05 16:52:13 +0530

    Agent: Update LocalMachineInfo model
    'temporary_directory' won't change for a machine so
    we don't need set/reset methods for it, and can have
    a default factory for it

--- monkey/infection_monkey/local_machine_info.py
+++ monkey/infection_monkey/local_machine_info.py
@@ -5,21 +5,16 @@ from monkeytypes import MutableInfectionMonkeyBaseModel, OperatingSystem
 from pydantic import Field
 
 from common.utils.environment import get_os
+from infection_monkey.utils.monkey_dir import get_monkey_dir_path
 
 
 class LocalMachineInfo(MutableInfectionMonkeyBaseModel):
     operating_system: OperatingSystem = Field(default_factory=get_os)
+    temporary_directory: Path = Field(default_factory=get_monkey_dir_path)
     interface_to_target: Optional[str] = None
-    temporary_directory: Optional[Path] = None
 
     def set_interface_to_target(self, interface_to_target: str):
         self.interface_to_target = interface_to_target
 
-    def set_temporary_directory(self, temporary_directory: Path):
-        self.temporary_directory = temporary_directory
-
     def reset_interface_to_target(self):
         self.interface_to_target = None
-
-    def reset_temporary_directory(self):
-        self.temporary_directory = None
