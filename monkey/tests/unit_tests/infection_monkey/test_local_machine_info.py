ca63adbce6ac31deb43b5b98109df191c3c03a83
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-06 13:37:50 +0530

    UT: Update LocalMachineInfo tests

--- monkey/tests/unit_tests/infection_monkey/test_local_machine_info.py
+++ monkey/tests/unit_tests/infection_monkey/test_local_machine_info.py
@@ -1,3 +1,5 @@
+from ipaddress import IPv4Interface
+
 from monkeytypes import OperatingSystem
 
 from infection_monkey.local_machine_info import LocalMachineInfo
@@ -5,11 +7,13 @@ from infection_monkey.local_machine_info import LocalMachineInfo
 LOCAL_MACHINE_INFO_OBJECT = LocalMachineInfo(
     operating_system=OperatingSystem.WINDOWS,
     temporary_directory="temp/o/rary",
+    network_interfaces=frozenset({IPv4Interface("127.0.0.1")}),
 )
 
 LOCAL_MACHINE_INFO_DICT = {
     "operating_system": "windows",
     "temporary_directory": "temp\\o\\rary",
+    "network_interfaces": ["127.0.0.1/32"],
 }
 
 
