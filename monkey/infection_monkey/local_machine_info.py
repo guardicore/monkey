e91002ad77ba1a07330af7afd4844182e7b0bfc1
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-06 13:33:46 +0530

    Agent: Add LocalMachineInfo.network_interfaces

--- monkey/infection_monkey/local_machine_info.py
+++ monkey/infection_monkey/local_machine_info.py
@@ -1,4 +1,4 @@
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, IPv4Interface
 from pathlib import Path
 from typing import Optional
 
@@ -14,13 +14,16 @@ class LocalMachineInfo(InfectionMonkeyBaseModel):
     :param operating_system: Operating system of the local machine
     :param temporary_directory: Path to a temporary directory on the local machine
                                 to store artifacts
+    :param network_interfaces: Network interfaces on the local machine
     """
 
     operating_system: OperatingSystem
     temporary_directory: Path
+    network_interfaces: frozenset[IPv4Interface]
 
     def get_interface_to_target(self, target_host_ip: IPv4Address) -> Optional[str]:
         """
         Gets an interface on the local machine that can be reached by the target machine
         """
+        # TODO: Use `network_interfaces` to get the interface
         return get_interface_to_target(target_host_ip)
