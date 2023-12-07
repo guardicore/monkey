82e559a85934fa2a011eececa180b052db41dc98
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-07 14:25:58 +0530

    Chrome: Use LocalMachineInfo object

--- monkey/agent_plugins/credentials_collectors/chrome/src/plugin.py
+++ monkey/agent_plugins/credentials_collectors/chrome/src/plugin.py
@@ -4,6 +4,7 @@ from typing import Any, Mapping, Sequence
 from monkeytypes import AgentID, Credentials, Event
 
 from common.event_queue import IAgentEventPublisher
+from infection_monkey.local_machine_info import LocalMachineInfo
 
 from .chrome_credentials_collector_builder import build_chrome_credentials_collector
 
@@ -11,9 +12,17 @@ logger = logging.getLogger(__name__)
 
 
 class Plugin:
-    def __init__(self, *, agent_id: AgentID, agent_event_publisher: IAgentEventPublisher, **kwargs):
+    def __init__(
+        self,
+        *,
+        agent_id: AgentID,
+        agent_event_publisher: IAgentEventPublisher,
+        local_machine_info: LocalMachineInfo,
+        **kwargs,
+    ):
         self._agent_id = agent_id
         self._agent_event_publisher = agent_event_publisher
+        self._local_machine_info = local_machine_info
 
     def run(
         self, *, options: Mapping[str, Any], interrupt: Event, **kwargs
@@ -22,7 +31,9 @@ class Plugin:
 
         try:
             chrome_credentials_collector = build_chrome_credentials_collector(
-                self._agent_id, self._agent_event_publisher
+                self._agent_id,
+                self._agent_event_publisher,
+                self._local_machine_info.operating_system,
             )
         except Exception as err:
             msg = (
