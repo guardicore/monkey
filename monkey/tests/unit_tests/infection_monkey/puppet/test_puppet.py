7b890d48aa51649cfd15a6ba8087262cf66f5528
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-07 14:04:22 +0530

    UT: Update Puppet tests

--- monkey/tests/unit_tests/infection_monkey/puppet/test_puppet.py
+++ monkey/tests/unit_tests/infection_monkey/puppet/test_puppet.py
@@ -53,7 +53,6 @@ def puppet(
         plugin_registry=mock_plugin_registry,
         plugin_compatibility_verifier=mock_plugin_compatibility_verifier,
         agent_id=AgentID("4277aa81-660b-4673-b96c-443ed525b4d0"),
-        local_machine_info=MagicMock(),
     )
 
 
