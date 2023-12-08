9068015bf156c6cb5e413db50a6a07b1c5e6bf57
vakarisz <vakarisz@yahoo.com>
2023-12-08 17:14:20 +0200

    Island: Add error in next.js process launch if UI is not built

--- monkey/monkey_island/cc/setup/nextjs/nextjs_process.py
+++ monkey/monkey_island/cc/setup/nextjs/nextjs_process.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import subprocess
+from pathlib import Path
 from typing import Optional
 
 from monkeytypes import NetworkPort
@@ -37,6 +38,12 @@ class NextJsProcess:
             f"Next.js server will be launched with command: {' '.join(self._next_js_run_cmd)}"
         )
         logger.info(f"UI log will be available at {self._log_file}.")
+
+        if not Path(UI_DIR).is_dir():
+            raise FileNotFoundError(
+                f"Next.js UI directory {UI_DIR} does not exist!"
+                f"Run `npm run build` to build the front-end."
+            )
 
         with open(self._log_file, "w") as log:
             node_env = os.environ.copy()
