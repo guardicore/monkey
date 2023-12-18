972c6dbf901f8b94147dfa0cc5d4d0e4cf792440
vakarisz <vakarisz@yahoo.com>
2023-12-18 11:51:50 +0200

    Island: Fix a typo in island interface log

--- monkey/monkey_island/cc/server_setup.py
+++ monkey/monkey_island/cc/server_setup.py
@@ -266,7 +266,7 @@ def _log_web_interface_access_urls(ip_addresses: Sequence[IPv4Address]):
     if NEXT_JS_UI_FEATURE:
         web_interface_urls = ", ".join([f"https://{ip}" for ip in ip_addresses])
     else:
-        web_interface_urls = ", ".join([f"http://{ip}:{FLASK_PORT}" for ip in ip_addresses])
+        web_interface_urls = ", ".join([f"https://{ip}:{FLASK_PORT}" for ip in ip_addresses])
 
     logger.info(
         "To access the web interface, navigate to one of the the following URLs using your "
