9bf983c1fe2d7b51c1e76600023bca8c52ce3adf
vakarisz <vakarisz@yahoo.com>
2024-01-05 13:40:25 +0200

    UI: Remove next-auth package and infrastructure

--- monkey/monkey_island/cc/next_ui/src/providers/RootProvider.tsx
+++ monkey/monkey_island/cc/next_ui/src/providers/RootProvider.tsx
@@ -1,21 +1,15 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { NextAuthProvider } from '@/providers/nextAuthProvider/provider';
-import { AuthProvider } from '@/providers/authProvider/provider';
 import { ReduxProvider } from '@/providers/reduxProvider/provider';
 import ThemeRegistry from '@/providers/theme/ThemeRegistry';
 
 export function RootProvider({ children }: { children: ReactNode }) {
     return (
         <>
-            <NextAuthProvider>
-                <AuthProvider>
-                    <ReduxProvider>
-                        <ThemeRegistry>{children}</ThemeRegistry>
-                    </ReduxProvider>
-                </AuthProvider>
-            </NextAuthProvider>
+            <ReduxProvider>
+                <ThemeRegistry>{children}</ThemeRegistry>
+            </ReduxProvider>
         </>
     );
 }
