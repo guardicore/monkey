e2cda4088d159b4d314c1d9607750d603e82d8c3
vakarisz <vakarisz@yahoo.com>
2024-01-05 12:04:19 +0200

    UI: Show machine list in the home page
    This change is for POC and tests, because it shows that authenticated user is able to retrieve data from a database

--- monkey/monkey_island/cc/next_ui/src/app/(protected)/home/page.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(protected)/home/page.tsx
@@ -1,3 +1,21 @@
-export default function HomePage() {
-    return <div>home page</div>;
-}
+'use client';
+import { useGetAllMachinesQuery } from '@/redux/features/api/authentication/islandApi';
+
+const HomePage = () => {
+    const { data, isLoading } = useGetAllMachinesQuery();
+
+    if (isLoading) return <div>loading...</div>;
+    else return <div>{MachineList(data)}</div>;
+};
+
+const MachineList = (machines) => {
+    return (
+        <ul>
+            {machines.map((machine) => (
+                <li key={machine.id}>{machine.hostname}</li>
+            ))}
+        </ul>
+    );
+};
+
+export default HomePage;
