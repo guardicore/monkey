681a5e87318a8a6eab23b5977d0832cdc4bf9d57
Ilija Lazoroski <ilija.la@live.com>
2024-01-05 12:15:57 +0000

    Island: Import port_is_used from monkeytoolbox

--- monkey/monkey_island/cc/server_setup.py
+++ monkey/monkey_island/cc/server_setup.py
@@ -10,7 +10,6 @@ from typing import Optional, Sequence, Tuple
 import gevent.hub
 import requests
 from gevent.pywsgi import WSGIServer
-from monkeytoolbox import get_my_ip_addresses
 
 from monkey_island.cc import Version
 from monkey_island.cc.deployment import Deployment
@@ -28,9 +27,9 @@ MONKEY_ISLAND_DIR_BASE_PATH = str(Path(__file__).parent.parent)
 if str(MONKEY_ISLAND_DIR_BASE_PATH) not in sys.path:
     sys.path.insert(0, MONKEY_ISLAND_DIR_BASE_PATH)
 
+from monkeytoolbox import get_my_ip_addresses, port_is_used  # noqa: E402
 from ophidian import DIContainer  # noqa: E402
 
-from common.network.network_utils import port_is_used  # noqa: E402
 from common.version import get_version  # noqa: E402
 from monkey_island.cc.app import init_app  # noqa: E402
 from monkey_island.cc.arg_parser import IslandCmdArgs  # noqa: E402
