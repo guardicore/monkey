e42a2d8425bf3df041b9d2c656eb38868bc2d659
Ilija Lazoroski <ilija.la@live.com>
2024-01-05 12:08:16 +0000

    Common: Remove get_network_interfaces
    Migrated to monkeytoolbox

--- monkey/common/network/network_utils.py
+++ monkey/common/network/network_utils.py
@@ -1,29 +1,7 @@
-import ipaddress
-from ipaddress import IPv4Address, IPv4Interface
-from typing import Iterable, List, Optional, Sequence
+from ipaddress import IPv4Address
+from typing import List, Optional, Sequence
 
-import ifaddr
 import psutil
-
-
-def get_network_interfaces() -> List[IPv4Interface]:
-    local_interfaces = []
-    for adapter in ifaddr.get_adapters():
-        for ip in _select_ipv4_ips(adapter.ips):
-            interface = ipaddress.IPv4Interface(f"{ip.ip}/{ip.network_prefix}")
-            if not interface.ip.is_link_local:
-                local_interfaces.append(interface)
-
-    return local_interfaces
-
-
-def _select_ipv4_ips(ips: Iterable[ifaddr.IP]) -> Iterable[ifaddr.IP]:
-    return filter(lambda ip: _is_ipv4(ip) and ip.ip != "127.0.0.1", ips)
-
-
-def _is_ipv4(ip: ifaddr.IP) -> bool:
-    # In ifaddr, IPv4 addresses are strings, while IPv6 addresses are tuples
-    return type(ip.ip) is str
 
 
 def port_is_used(
