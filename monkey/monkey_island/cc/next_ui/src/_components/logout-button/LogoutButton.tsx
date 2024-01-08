3a767b4ba4c5f3d841ff39ad61df443adb2f53dd
vakarisz <vakarisz@yahoo.com>
2024-01-08 14:00:23 +0200

    UI: Reset store on logout

--- monkey/monkey_island/cc/next_ui/src/_components/logout-button/LogoutButton.tsx
+++ monkey/monkey_island/cc/next_ui/src/_components/logout-button/LogoutButton.tsx
@@ -5,11 +5,14 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import { removeToken } from '@/_lib/authentication';
 import { useRouter } from 'next/navigation';
 import { PATHS } from '@/constants/paths.constants';
+import { islandApiSlice } from '@/redux/features/api/islandApiSlice';
+import { store } from '@/redux/store';
 
 const LogoutButton = () => {
     const router = useRouter();
     const handleButtonClick = async () => {
         removeToken();
+        store.dispatch(islandApiSlice.util.resetApiState());
         router.push(PATHS.SIGN_IN);
     };
 
