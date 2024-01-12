e9e3b4515e0fa7fbdc7bd4765771b57761a798d6
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2024-01-12 20:50:53 +0000

    UI: Add logout mutation

--- monkey/monkey_island/cc/next_ui/src/redux/features/api/authentication/authenticationEndpoints.ts
+++ monkey/monkey_island/cc/next_ui/src/redux/features/api/authentication/authenticationEndpoints.ts
@@ -1,17 +1,37 @@
 import { HTTP_METHODS } from '@/constants/http.constants';
+import {
+    EndpointBuilder,
+    QueryFulfilledRejectionReason
+} from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 import { islandApiSlice } from '@/redux/features/api/islandApiSlice';
 import { LoginParams } from '@/redux/features/api/types/islandApi';
 import handleAuthToken from '@/app/(auth)/_lib/handleAuthToken';
+import { PromiseWithKnownReason } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/types';
 
 enum BackendEndpoints {
     LOGIN = '/login',
-    REGISTER = '/register'
+    REGISTER = '/register',
+    LOGOUT = '/logout'
+}
+
+async function handleTokenResponse(
+    queryFulfilled: PromiseWithKnownReason<
+        { data: any; meta: any },
+        QueryFulfilledRejectionReason<any>
+    >
+) {
+    try {
+        const { data } = await queryFulfilled;
+        handleAuthToken(data);
+    } catch (error) {
+        // dispatch();
+    }
 }
 
 export const authenticationEndpoints = islandApiSlice.injectEndpoints({
-    endpoints: (builder: any) => ({
-        login: builder.mutation({
-            query: (loginValues: LoginParams) => ({
+    endpoints: (builder: EndpointBuilder<any, any, any>) => ({
+        login: builder.mutation<void, LoginParams>({
+            query: (loginValues) => ({
                 url: BackendEndpoints.LOGIN,
                 method: HTTP_METHODS.POST,
                 headers: {
@@ -20,11 +40,10 @@ export const authenticationEndpoints = islandApiSlice.injectEndpoints({
                 body: JSON.stringify(loginValues)
             }),
             async onQueryStarted(_, { queryFulfilled }) {
-                const { data } = await queryFulfilled;
-                handleAuthToken(data);
+                await handleTokenResponse(queryFulfilled);
             }
         }),
-        register: builder.mutation({
+        register: builder.mutation<void, LoginParams>({
             query: (loginValues) => ({
                 url: BackendEndpoints.REGISTER,
                 method: HTTP_METHODS.POST,
@@ -34,12 +53,28 @@ export const authenticationEndpoints = islandApiSlice.injectEndpoints({
                 body: JSON.stringify(loginValues)
             }),
             async onQueryStarted(_, { queryFulfilled }) {
-                const response = await queryFulfilled;
-                handleAuthToken(response);
+                await handleTokenResponse(queryFulfilled);
+            }
+        }),
+        logout: builder.mutation<void, void>({
+            query: () => ({
+                url: BackendEndpoints.LOGOUT,
+                method: HTTP_METHODS.POST,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }),
+            async onQueryStarted(_, { dispatch, queryFulfilled }) {
+                try {
+                    await queryFulfilled;
+                    dispatch({ type: 'LOGOUT' });
+                } catch (error) {
+                    // dispatch();
+                }
             }
         })
     })
 });
 
-export const { useLoginMutation, useRegisterMutation } =
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } =
     authenticationEndpoints;
