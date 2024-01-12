821d63fc710f575419a71b0211071248ff788db4
vakarisz <vakarisz@yahoo.com>
2024-01-12 17:34:59 +0200

    UI: Fix redirection on logout

--- monkey/monkey_island/cc/next_ui/src/app/(protected)/layout.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(protected)/layout.tsx
@@ -4,11 +4,24 @@ import MainLayout from '@/layouts/main-layout/MainLayout';
 import { useRouter } from 'next/navigation';
 import { tokenStored } from '@/_lib/authentication';
 import { PATHS } from '@/constants/paths.constants';
+import { useEffect } from 'react';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
-    if (!tokenStored()) {
-        router.push(PATHS.SIGN_IN);
-    }
+
+    useEffect(() => {
+        function checkToken() {
+            if (!tokenStored()) {
+                router.push(PATHS.SIGN_IN);
+            }
+        }
+
+        window.addEventListener('LOGOUT', checkToken);
+
+        return () => {
+            window.removeEventListener('LOGOUT', checkToken);
+        };
+    }, [router]);
+
     return <MainLayout>{children}</MainLayout>;
 }
