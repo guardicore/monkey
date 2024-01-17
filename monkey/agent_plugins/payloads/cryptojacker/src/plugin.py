1ff7164b2e161ed61400bfacfa60c8e675a0cb97
Ilija Lazoroski <ilija.la@live.com>
2024-01-17 18:02:21 +0000

    Plugins: Correctly sort imports across plugins

--- monkey/agent_plugins/payloads/cryptojacker/src/plugin.py
+++ monkey/agent_plugins/payloads/cryptojacker/src/plugin.py
@@ -2,11 +2,11 @@ import logging
 from pprint import pformat
 from typing import Any, Dict
 
+from agentpluginapi import LocalMachineInfo
 from monkeytypes import AgentID, Event, SocketAddress
 
 from common.event_queue import IAgentEventPublisher
 from infection_monkey.i_puppet import PayloadResult
-from agentpluginapi import LocalMachineInfo
 
 from .cryptojacker_builder import build_cryptojacker
 from .cryptojacker_options import CryptojackerOptions
