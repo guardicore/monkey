ae4b62d1ad2ba424a2742786423622273b37887c
vakarisz <vakarisz@yahoo.com>
2024-01-19 14:23:37 +0200

    UI: Add basic authentication provider
    This provider is responsible to handle the timer once the application is loaded

--- monkey/monkey_island/cc/next_ui/src/providers/RootProvider.tsx
+++ monkey/monkey_island/cc/next_ui/src/providers/RootProvider.tsx
@@ -3,12 +3,15 @@
 import { ReactNode } from 'react';
 import { ReduxProvider } from '@/providers/reduxProvider/provider';
 import ThemeRegistry from '@/providers/theme/ThemeRegistry';
+import { AuthProvider } from '@/providers/authProvider/provider';
 
 export function RootProvider({ children }: { children: ReactNode }) {
     return (
         <>
             <ReduxProvider>
-                <ThemeRegistry>{children}</ThemeRegistry>
+                <AuthProvider>
+                    <ThemeRegistry>{children}</ThemeRegistry>
+                </AuthProvider>
             </ReduxProvider>
         </>
     );
--- monkey/monkey_island/cc/next_ui/src/redux/rootReducer.ts
+++ monkey/monkey_island/cc/next_ui/src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { islandApiSlice } from '@/redux/features/api/islandApiSlice';
 import themeSliceReducer from './features/theme/theme.slice';
+import authenticationTimeoutSlice from '@/redux/features/api/authenticationTimeoutSlice';
 
 const rootReducer = combineReducers({
     [islandApiSlice.reducerPath]: islandApiSlice.reducer,
+    authenticationTimeout: authenticationTimeoutSlice,
     theme: themeSliceReducer
 });
 
