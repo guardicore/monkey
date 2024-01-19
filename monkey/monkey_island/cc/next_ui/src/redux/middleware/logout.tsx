03ebaebae918861e16eb72d6f2719a33ce8a7a66
vakarisz <vakarisz@yahoo.com>
2024-01-19 14:21:33 +0200

    UI: Fix an if statement in the logoutMiddleware

--- monkey/monkey_island/cc/next_ui/src/redux/middleware/logout.tsx
+++ monkey/monkey_island/cc/next_ui/src/redux/middleware/logout.tsx
@@ -4,7 +4,7 @@ import { Events } from '@/constants/events.constants';
 import { AuthenticationActions } from '@/redux/features/api/authentication/authenticationActions';
 
 const logoutMiddleware = (store) => (next) => (action) => {
-    if (action.type === AuthenticationActions.logout) {
+    if (action.type === AuthenticationActions.logout.type) {
         removeToken();
         store.dispatch(islandApiSlice.util.resetApiState());
         window.dispatchEvent(new Event(Events.LOGOUT));
