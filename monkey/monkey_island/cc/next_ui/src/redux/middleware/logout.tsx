fc3faf006e4c40f153b32cb054ae833af4d04642
vakarisz <vakarisz@yahoo.com>
2024-01-19 19:20:24 +0200

    UI: Cleanup authentication timer on cleanup

--- monkey/monkey_island/cc/next_ui/src/redux/middleware/logout.tsx
+++ monkey/monkey_island/cc/next_ui/src/redux/middleware/logout.tsx
@@ -2,10 +2,12 @@ import { removeToken } from '@/_lib/authentication';
 import { islandApiSlice } from '@/redux/features/api/islandApiSlice';
 import { Events } from '@/constants/events.constants';
 import { AuthenticationActions } from '@/redux/features/api/authentication/authenticationActions';
+import clearAuthenticationTimer from '@/app/(auth)/_lib/clearAuthenticationTimer';
 
 const logoutMiddleware = (store) => (next) => (action) => {
     if (action.type === AuthenticationActions.logout.type) {
         removeToken();
+        clearAuthenticationTimer();
         store.dispatch(islandApiSlice.util.resetApiState());
         window.dispatchEvent(new Event(Events.LOGOUT));
     }
