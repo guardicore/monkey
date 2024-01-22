ab907b89405f7177aeca9bbd70d0bce86f150eb1
Ilija Lazoroski <ilija.la@live.com>
2024-01-22 12:33:19 +0000

    Agent: Add monkey_events_imports
    This was missed in the fix for #3999 (e00bb6464cc29, PR #4021).

---
+++ monkey/infection_monkey/monkey_events_imports.py
@@ -0,0 +1,13 @@
+from monkeyevents import (  # noqa: F401
+    AbstractAgentEvent,
+    AgentEventTag,
+    CPUConsumptionEvent,
+    CredentialsStolenEvent,
+    DefacementEvent,
+    ExploitationEvent,
+    FileEncryptionEvent,
+    HTTPRequestEvent,
+    PasswordRestorationEvent,
+    PropagationEvent,
+    RAMConsumptionEvent,
+)
