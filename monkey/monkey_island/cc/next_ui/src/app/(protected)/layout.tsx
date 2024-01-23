16aaae570b34116e86cc18595dc33060985411f5
vakarisz <vakarisz@yahoo.com>
2024-01-23 13:59:22 +0200

    UI: Separate auth redirection to a hook

--- monkey/monkey_island/cc/next_ui/src/app/(protected)/layout.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(protected)/layout.tsx
@@ -1,28 +1,10 @@
 'use client';
 import '../../styles/globals.scss';
 import MainLayout from '@/layouts/main-layout/MainLayout';
-import { useRouter } from 'next/navigation';
-import { tokenStored } from '@/_lib/authentication';
-import { PATHS } from '@/constants/paths.constants';
-import { Events } from '@/constants/events.constants';
-import { useEffect } from 'react';
+import useAuthRedirection from '@/app/(protected)/_lib/useAuthRedirection';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-    const router = useRouter();
-
-    useEffect(() => {
-        function checkToken() {
-            if (!tokenStored()) {
-                router.push(PATHS.SIGN_IN);
-            }
-        }
-
-        window.addEventListener(Events.LOGOUT, checkToken);
-
-        return () => {
-            window.removeEventListener(Events.LOGOUT, checkToken);
-        };
-    }, [router]);
+    useAuthRedirection();
 
     return <MainLayout>{children}</MainLayout>;
 }
