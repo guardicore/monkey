4642fb4c7d51b419c4ef47f59cfbd19f3240a66b
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2024-01-23 17:47:19 +0000

    UI: Add note about cypress component test issue

--- monkey/monkey_island/cc/next_ui/cypress/support/component.tsx
+++ monkey/monkey_island/cc/next_ui/cypress/support/component.tsx
@@ -1,6 +1,11 @@
 import { mount } from 'cypress/react18';
 import { ReduxProvider } from '@/providers/reduxProvider/provider';
 
+// Currently, Cypress does not support importing CSS files with Next.js 13+
+// in component tests.
+// See: https://github.com/cypress-io/cypress/issues/27890
+// import '@/styles/globals.scss';
+
 Cypress.Commands.add('mount', (component, options) => {
     // Wrap any parent components needed
     const wrapped = <ReduxProvider>{component}</ReduxProvider>;
