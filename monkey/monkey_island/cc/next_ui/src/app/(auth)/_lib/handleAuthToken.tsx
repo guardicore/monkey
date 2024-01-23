9e77454eda507caca8f565e643c81e7c5e2bf1c5
vakarisz <vakarisz@yahoo.com>
2024-01-23 13:47:49 +0200

    UI: Add a network latency coefficient to token TTL
    To compensate for network latency we shorten the TTL by 10%.

--- monkey/monkey_island/cc/next_ui/src/app/(auth)/_lib/handleAuthToken.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(auth)/_lib/handleAuthToken.tsx
@@ -1,8 +1,19 @@
 import { setToken } from '@/_lib/authentication';
 
+// This coefficient is meant to reduce the TTL to account for network latency.
+// Should be below 1
+const networkLatencyCoefficient = 0.9;
+
+const parseTTLFromResponse = (response): number => {
+    const ttlSeconds = response?.response?.user?.token_ttl_sec;
+    // Default timescale in javascript is milliseconds
+    const ttl = ttlSeconds * 1000;
+    return ttl * networkLatencyCoefficient;
+};
+
 const handleAuthToken = async (response) => {
     const token = response?.response?.user?.authentication_token;
-    const ttl = response?.response?.user?.token_ttl_sec * 1000;
+    const ttl = parseTTLFromResponse(response);
 
     if (!token) {
         return null;
