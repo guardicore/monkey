37d78731d12b077816aacd58eff697b2bc9a8b36
vakarisz <vakarisz@yahoo.com>
2024-02-01 16:45:54 +0200

    UI: light links

--- monkey/monkey_island/cc/next_ui/src/app/(auth)/login/page.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(auth)/login/page.tsx
@@ -89,7 +89,6 @@ const LoginPage = () => {
                         value={loginFormValues.username}
                         onChange={handleLoginFormValueChange}
                         autoFocus
-                        style={{ background: 'black' }}
                     />
                     <TextField
                         margin="normal"
@@ -102,7 +101,6 @@ const LoginPage = () => {
                         autoComplete="current-password"
                         value={loginFormValues.password}
                         onChange={handleLoginFormValueChange}
-                        style={{ background: 'black' }}
                     />
                     <Button
                         type="submit"
