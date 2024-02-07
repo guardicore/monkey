075145227c30259f30d8645a3655474ac4917dd3
vakarisz <vakarisz@yahoo.com>
2024-02-07 16:06:49 +0200

    UI: Remove authenticationTheme.ts
    Testing is done, login page will be able to handle the global theme

--- monkey/monkey_island/cc/next_ui/src/app/(auth)/layout.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(auth)/layout.tsx
@@ -3,16 +3,14 @@ import '../../styles/globals.scss';
 import useAuthenticatedRedirect from '@/app/(auth)/_lib/useAuthenticatedRedirect';
 import ParticleBackground from '@/app/(auth)/_lib/ParticleBackground';
 import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import createAuthenticationTheme from '@/app/(auth)/authenticationTheme';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     useAuthenticatedRedirect();
 
     return (
-        <ThemeProvider theme={(theme) => createAuthenticationTheme(theme)}>
+        <>
             <ParticleBackground />
             {children};
-        </ThemeProvider>
+        </>
     );
 }
