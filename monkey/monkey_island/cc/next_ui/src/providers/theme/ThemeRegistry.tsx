a17a078a1e2051170fb8c4996398bc6a0cec71be
vakarisz <vakarisz@yahoo.com>
2024-02-12 17:09:00 +0200

    UI: Reintroduce CssBaseline
    Baseline cleans up default styles for body and other elements, I'm not sure why MUI doesn't do it by default

--- monkey/monkey_island/cc/next_ui/src/providers/theme/ThemeRegistry.tsx
+++ monkey/monkey_island/cc/next_ui/src/providers/theme/ThemeRegistry.tsx
@@ -2,11 +2,17 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import appTheme from '@/providers/theme/appTheme';
+import { CssBaseline } from '@mui/material';
 
 export default function ThemeRegistry({
     children
 }: {
     children: React.ReactNode;
 }) {
-    return <ThemeProvider theme={appTheme}>{children}</ThemeProvider>;
+    return (
+        <ThemeProvider theme={appTheme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
 }
