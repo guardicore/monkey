b6e37ad0ebfc7df840e4760fa4154bdc5cf96aa3
vakarisz <vakarisz@yahoo.com>
2024-02-12 17:41:52 +0200

    UI: Remove unused global stylesheets

--- monkey/monkey_island/cc/next_ui/src/app/(auth)/layout.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-import '../../styles/globals.scss';
 import useAuthenticatedRedirect from '@/app/(auth)/_lib/useAuthenticatedRedirect';
 import ParticleBackground from '@/app/(auth)/_lib/ParticleBackground';
 import * as React from 'react';
