740a3493d7135563c4e72f4c29c36022a9d818da
vakarisz <vakarisz@yahoo.com>
2024-02-13 15:42:42 +0200

    UI: Change content type header to be json by default

--- monkey/monkey_island/cc/next_ui/src/redux/features/api/islandApiSlice.ts
+++ monkey/monkey_island/cc/next_ui/src/redux/features/api/islandApiSlice.ts
@@ -18,6 +18,7 @@ const baseQuery: BaseQueryFn = fetchBaseQuery({
         if (token) {
             headers.set(AUTHENTICATION_TOKEN_HEADER, token);
         }
+        headers.set('Content-Type', 'application/json');
         return headers;
     },
     timeout: DEFAULT_QUERY_TIMEOUT
