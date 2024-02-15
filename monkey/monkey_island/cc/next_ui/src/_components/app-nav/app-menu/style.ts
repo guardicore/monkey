858189a2b7efd4b8d936d3a2f63378fc27555bfb
vakarisz <vakarisz@yahoo.com>
2024-02-15 10:44:01 +0200

    UI: Remove unnecessary !import statements

--- monkey/monkey_island/cc/next_ui/src/_components/app-nav/app-menu/style.ts
+++ monkey/monkey_island/cc/next_ui/src/_components/app-nav/app-menu/style.ts
@@ -1,7 +1,7 @@
 export const appTabsContainer = {
     width: '100%',
-    height: '100% !important',
+    height: '100%',
     '& .MuiTabs-flexContainer': {
-        height: '100% !important'
+        height: '100%'
     }
 };
