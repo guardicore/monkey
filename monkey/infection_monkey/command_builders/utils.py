2b5597b1a6aec5b487bc607cfa94ed3455cb32c3
Ilija Lazoroski <ilija.la@live.com>
2024-02-16 11:25:02 +0000

    Agent: Raise ValueError if dropper execution mode isn't DROPPER or NONE

--- monkey/infection_monkey/command_builders/utils.py
+++ monkey/infection_monkey/command_builders/utils.py
@@ -8,9 +8,24 @@ from infection_monkey.model import DROPPER_ARG, MONKEY_ARG
 
 
 def get_agent_argument(run_options: Union[WindowsRunOptions, LinuxRunOptions]) -> str:
+    """
+    Get the appropriate agent argument based on the run options.
+
+    :param run_options: The run command options.
+    :raises ValueError: If dropper execution mode is not DROPPER or NONE.
+    :return: The Agent argument to be used.
+    """
     agent_arg = MONKEY_ARG
     if run_options.dropper_execution_mode == DropperExecutionMode.DROPPER:
         agent_arg = DROPPER_ARG
+    if run_options.dropper_execution_mode not in [
+        DropperExecutionMode.DROPPER,
+        DropperExecutionMode.NONE,
+    ]:
+        raise ValueError(
+            "Agent argument can only be used with"
+            "DropperExecutionMode.DROPPER or DropperExecutionMode.NONE."
+        )
 
     return agent_arg
 
@@ -18,6 +33,12 @@ def get_agent_argument(run_options: Union[WindowsRunOptions, LinuxRunOptions]) -
 def get_agent_location(
     run_options: Union[WindowsRunOptions, LinuxRunOptions]
 ) -> Optional[PurePath]:
+    """
+    Get the agent location based on the run options.
+
+    :param run_options: The run command options.
+    :return: The destination path for the agent, or None if not applicable.
+    """
     destination_path = None
     if run_options.dropper_execution_mode == DropperExecutionMode.DROPPER:
         destination_path = (
@@ -34,6 +55,15 @@ def build_monkey_commandline_parameters(
     depth: Optional[int] = None,
     location: Union[str, PurePath, None] = None,
 ) -> Sequence[str]:
+    """
+    Build command-line parameters for the Agent.
+
+    :param parent: The ID of the parent Agent, if applicable.
+    :param servers: A sequence of server addresses.
+    :param depth: The depth of which the Agent will run upon.
+    :param location: The location of the Dropper on the victim machine.
+    :return: A sequence of command-line parameters.
+    """
     cmdline = []
 
     if parent is not None:
