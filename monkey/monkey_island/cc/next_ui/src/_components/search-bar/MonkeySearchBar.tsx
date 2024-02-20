d2d6ca5b4255fc7ffa684bbd366a6c4b40c2944b
vakarisz <vakarisz@yahoo.com>
2024-02-20 12:49:30 +0200

    UI: Add MonkeySearchBar.tsx component

---
+++ monkey/monkey_island/cc/next_ui/src/_components/search-bar/MonkeySearchBar.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { Box, IconButton, InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+import { nanoid } from 'nanoid';
+
+const EMPTY_STRING = '';
+const DEFAULT_VARIANT = 'standard';
+const DEFAULT_PLACEHOLDER = 'Search';
+
+const MonkeySearchBar = (props) => {
+    const {
+        variant = DEFAULT_VARIANT,
+        placeholder = DEFAULT_PLACEHOLDER,
+        label = null,
+        setQuery,
+        ...rest
+    } = { ...props };
+    const [currentValue, setCurrentValue] = useState(EMPTY_STRING);
+
+    useEffect(() => {
+        setQuery && setQuery(currentValue);
+    }, [currentValue]);
+
+    const handleValueChange = (e) => {
+        const currentValue = e?.target?.value?.trim() || EMPTY_STRING;
+        setCurrentValue(currentValue);
+    };
+
+    const clearValue = () => {
+        setCurrentValue(EMPTY_STRING);
+    };
+
+    return (
+        <Box className="search-bar-wrapper">
+            <TextField
+                id={`search-bar-${nanoid()}`}
+                fullWidth
+                variant={variant}
+                label={label}
+                value={currentValue}
+                placeholder={placeholder}
+                onChange={handleValueChange}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                    endAdornment: currentValue !== '' && (
+                        <InputAdornment position="end">
+                            <IconButton onClick={clearValue}>
+                                <ClearIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    )
+                }}
+                {...rest}
+            />
+        </Box>
+    );
+};
+
+export default MonkeySearchBar;
