be17c3a791617e2e3a876b8f39d725d5e952ffdc
Ilija Lazoroski <ilija.la@live.com>
2024-02-21 15:33:42 +0000

    Agent: Remove unneeded plugin parameters
    current_depth
    servers
    otp_provider
    agent_otp_environment variable

--- monkey/infection_monkey/puppet/puppet.py
+++ monkey/infection_monkey/puppet/puppet.py
@@ -93,8 +93,6 @@ class Puppet(IPuppet):
         self,
         name: str,
         host: TargetHost,
-        current_depth: int,
-        servers: Sequence[str],
         options: Mapping,
         interrupt: Event,
     ) -> ExploiterResult:
@@ -121,8 +119,6 @@ class Puppet(IPuppet):
         exploiter = self._plugin_registry.get_plugin(AgentPluginType.EXPLOITER, name)
         exploiter_result = exploiter.run(
             host=host,
-            servers=servers,
-            current_depth=current_depth,
             options=options,
             interrupt=interrupt,
         )
