eaf04e44dc39514479a78ee0b791a21125a7305e
vakarisz <vakarisz@yahoo.com>
2024-02-21 14:25:42 +0200

    UI: Add installed plugin filter

---
+++ monkey/monkey_island/cc/next_ui/src/app/(protected)/plugins/_lib/filters/InstalledPluginFilter.tsx
@@ -0,0 +1,26 @@
+import { PluginRow } from '@/app/(protected)/plugins/_lib/PluginTable';
+import { useGetInstalledPluginsQuery } from '@/redux/features/api/agentPlugins/agentPluginEndpoints';
+import { FilterProps } from '@/app/(protected)/plugins/available/AvailablePluginFilters';
+import { useEffect } from 'react';
+
+type InstalledPluginFilterProps = FilterProps;
+
+const InstalledPluginFilter = (props: InstalledPluginFilterProps) => {
+    const { data: installedPlugins } = useGetInstalledPluginsQuery();
+    const filter = (pluginRow: PluginRow): boolean => {
+        if (!installedPlugins) return true;
+        const installedPlugin = installedPlugins.find(
+            (installedPlugin) => pluginRow.id === installedPlugin.id
+        );
+        return !installedPlugin;
+    };
+    useEffect(() => {
+        props.setFiltersCallback((prevState) => {
+            return { ...prevState, installed: filter };
+        });
+    }, [installedPlugins]);
+
+    return null;
+};
+
+export default InstalledPluginFilter;
