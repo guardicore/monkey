c4001ac132736e01d49819dfdb3f54992f521752
vakarisz <vakarisz@yahoo.com>
2024-03-04 17:57:46 +0200

    UI: Remove useless integration tests

--- monkey/monkey_island/cc/next_ui/src/_components/logout-button/LogoutButton.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LogoutButton from './LogoutButton';
-
-describe('LogoutButton', () => {
-    it('renders', () => {
-        // see: https://on.cypress.io/mounting-react
-        cy.mount(<LogoutButton />);
-    });
-});
