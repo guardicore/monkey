f2fb63364bab8b9d62d3eef3b8f6c2bcc8ca8366
vakarisz <vakarisz@yahoo.com>
2024-03-08 17:37:23 +0200

    UI: Add plugin installation endpoint

--- monkey/monkey_island/cc/next_ui/src/redux/features/api/agentPlugins/types.ts
+++ monkey/monkey_island/cc/next_ui/src/redux/features/api/agentPlugins/types.ts
@@ -50,3 +50,9 @@ export type PluginMetadata = {
 export type PluginMetadataResponse = {
     [key: string]: { [key: string]: PluginMetadata[] };
 };
+
+export type PluginInfo = {
+    pluginType: string;
+    pluginName: string;
+    pluginVersion: string;
+};
