bd630f8a5439237488798e39c34c51e0ddbeb7ff
vakarisz <vakarisz@yahoo.com>
2024-03-13 10:58:22 +0200

    UI: Implement core functionality of InstallAllSafePluginsButton.tsx

--- monkey/monkey_island/cc/next_ui/src/app/(protected)/plugins/_lib/InstallAllSafePluginsButton.tsx
+++ monkey/monkey_island/cc/next_ui/src/app/(protected)/plugins/_lib/InstallAllSafePluginsButton.tsx
@@ -3,19 +3,48 @@ import MonkeyButton, {
     ButtonVariant
 } from '@/_components/buttons/MonkeyButton';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import { useGetAvailablePluginsQuery } from '@/redux/features/api/agentPlugins/agentPluginEndpoints';
+import {
+    useGetAvailablePluginsQuery,
+    useGetInstalledPluginsQuery,
+    useInstallPluginMutation
+} from '@/redux/features/api/agentPlugins/agentPluginEndpoints';
+import { filterOutInstalledPlugins } from '@/app/(protected)/plugins/_lib/filters/InstalledPluginFilter';
+import { filterOutDangerousPlugins } from '@/app/(protected)/plugins/_lib/filters/SafetyFilter';
 
-type InstallAllSafePluginsButtonProps = {};
-
-const InstallAllSafePluginsButton = (
-    props: InstallAllSafePluginsButtonProps
-) => {
+const InstallAllSafePluginsButton = () => {
     const { data: availablePlugins, isLoading: isLoadingAvailablePlugins } =
         useGetAvailablePluginsQuery();
+    const { data: installedPlugins, isLoading: isLoadingInstalledPlugins } =
+        useGetInstalledPluginsQuery();
+    const [installPlugin] = useInstallPluginMutation();
 
-    const isDisabled = isLoadingAvailablePlugins || !availablePlugins;
+    const installAllSafePlugins = () => {
+        if (availablePlugins === undefined || installedPlugins === undefined)
+            throw new Error('Available or installed plugins are undefined');
+        const installablePlugins = filterOutInstalledPlugins(
+            availablePlugins,
+            installedPlugins
+        );
+        const safePlugins = filterOutDangerousPlugins(installablePlugins);
+        safePlugins.forEach((plugin) => {
+            installPlugin({
+                pluginVersion: plugin.version,
+                pluginName: plugin.name,
+                pluginType: plugin.pluginType
+            });
+        });
+    };
+
+    const isDisabled =
+        isLoadingAvailablePlugins ||
+        isLoadingInstalledPlugins ||
+        !availablePlugins;
+
     return (
-        <MonkeyButton variant={ButtonVariant.Contained} disabled={isDisabled}>
+        <MonkeyButton
+            variant={ButtonVariant.Contained}
+            disabled={isDisabled}
+            onClick={installAllSafePlugins}>
             <FileDownloadIcon sx={{ mr: '5px' }} />
             All Safe Plugins
         </MonkeyButton>
